c54884faa92cdf8f3bd6d5e4d5872ef9ee3d9f6a
Abhishek Kona <abhishekk@fb.com>
2013-01-16 11:23:35 -0800

    Fix Regression script. Run it for a shorter time.

--- regression_build_test.sh
+++ regression_build_test.sh
@@ -8,7 +8,7 @@ function send_to_ods {
   curl -s "https://www.intern.facebook.com/intern/agent/ods_set.php?entity=rocksdb_build&key=$key&value=$value"
 }
 
-NUM=100000000
+NUM=10000000
 
 DATA_DIR="$1"
 if [ -z "$DATA_DIR" ]
@@ -24,8 +24,8 @@ STAT_FILE="/tmp/leveldb_test_stats"
 ./db_bench --benchmarks=readrandom --db=$DATA_DIR --use_existing_db=1 --bloom_bits=10 --num=$NUM --reads=$((NUM / 100)) --cache_size=6442450944 --cache_numshardbits=6 --open_files=55000 --statistics=1 --histogram=1 --disable_data_sync=1 --disable_wal=1 --sync=0 --threads=128 > "$STAT_FILE.readrandom"
 
 OVERWRITE_OPS=$(grep overwrite "$STAT_FILE.overwrite" |cut -d"/" -f2 |cut -d" " -f2)
-FILLSEQ_OPS=$(grep fillseq a.out |cut -d"/" -f2 |cut -d" " -f2)
-READRANDOM_OPS=$(grep readrandom a.out |cut -d"/" -f2 |cut -d" " -f2)
+FILLSEQ_OPS=$(grep fillseq "$STAT_FILE.fillseq" |cut -d"/" -f2 |cut -d" " -f2)
+READRANDOM_OPS=$(grep readrandom "$STAT_FILE.readrandom" |cut -d"/" -f2 |cut -d" " -f2)
 send_to_ods rocksdb.build.overwrite.qps $OVERWRITE_OPS
 send_to_ods rocksdb.build.fillseq.qps $FILLSEQ_OPS
 send_to_ods rocksdb.build.readrandom.qps $READRANDOM_OPS
