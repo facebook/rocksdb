e45c7a84448c389822c1a9585976343ca9dbf10b
Dhruba Borthakur <dhruba@fb.com>
2013-02-26 00:13:17 -0800

    Abilty to support upto a million .sst files in the database
    Summary:
    There was an artifical limit of 50K files per database. This is
    insifficient if the database is 1 TB in size and each file is 2 MB.
    
    Test Plan: make check
    
    Reviewers: sheki, emayanke
    
    Reviewed By: emayanke
    
    CC: leveldb
    
    Differential Revision: https://reviews.facebook.net/D8919

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -118,7 +118,7 @@ Options SanitizeOptions(const std::string& dbname,
   Options result = src;
   result.comparator = icmp;
   result.filter_policy = (src.filter_policy != nullptr) ? ipolicy : nullptr;
-  ClipToRange(&result.max_open_files,            20,     50000);
+  ClipToRange(&result.max_open_files,            20,     1000000);
   ClipToRange(&result.write_buffer_size,         64<<10, 1<<30);
   ClipToRange(&result.block_size,                1<<10,  4<<20);
   if (result.info_log == nullptr) {
