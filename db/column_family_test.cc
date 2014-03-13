f071a20f6e0647ea5de9e315c8e7bf9572c5026e
Igor Canadi <icanadi@fb.com>
2014-03-13 13:52:20 -0700

    Need more data in memtable to flush due to 11da8b

--- db/column_family_test.cc
+++ db/column_family_test.cc
@@ -729,7 +729,7 @@ TEST(ColumnFamilyTest, DifferentCompactionStyles) {
   ASSERT_EQ(NumTableFilesAtLevel(0, 0), 0);
   ASSERT_EQ(NumTableFilesAtLevel(1, 0), 0);
   // write some new keys into level 0
-  PutRandomData(0, 100, 4096);
+  PutRandomData(0, 120, 4096);
   ASSERT_OK(Flush(0));
   WaitForCompaction();
   // remember number of files in each level
@@ -742,14 +742,14 @@ TEST(ColumnFamilyTest, DifferentCompactionStyles) {
 
   // SETUP column family "one" -- universal style
   for (int i = 0; i < one.level0_file_num_compaction_trigger - 1; ++i) {
-    PutRandomData(1, 12, 10000);
+    PutRandomData(1, 20, 10000);
     WaitForFlush(1);
     ASSERT_EQ(std::to_string(i + 1), FilesPerLevel(1));
   }
 
   // SETUP column family "two" -- level style with 4 levels
   for (int i = 0; i < two.level0_file_num_compaction_trigger - 1; ++i) {
-    PutRandomData(2, 12, 10000);
+    PutRandomData(2, 20, 10000);
     WaitForFlush(2);
     ASSERT_EQ(std::to_string(i + 1), FilesPerLevel(2));
   }
