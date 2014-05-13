93f26436569a013835646cee66c6fddf7da89501
Yueh-Hsuan Chiang <yhchiang@fb.com>
2014-05-13 13:08:45 -0700

    [Java] Make read benchmarks print out found / not-found information.
    Summary: Make read benchmarks print out found / not-found information.
    
    Test Plan:
    make rocksdbjava
    make jdb_bench
    cd java
    ./jdb_bench.sh
    
    Reviewers: haobo, sdong
    
    CC: leveldb
    
    Differential Revision: https://reviews.facebook.net/D18699

--- java/org/rocksdb/benchmark/DbBenchmark.java
+++ java/org/rocksdb/benchmark/DbBenchmark.java
@@ -800,11 +800,15 @@ public class DbBenchmark {
         }
       }
     }
+    String extra = "";
+    if (benchmark.indexOf("read") >= 0) {
+      extra = String.format(" %s / %s found; ", stats.found_, reads_);
+    }
 
     System.out.printf(
-        "%-16s : %11.5f micros/op; %6.1f MB/s; %d / %d task(s) finished.\n",
+        "%-16s : %11.5f micros/op; %6.1f MB/s;%s %d / %d task(s) finished.\n",
         benchmark, (double) elapsedSeconds / stats.done_ * 1e6,
-        (stats.bytes_ / 1048576.0) / elapsedSeconds,
+        (stats.bytes_ / 1048576.0) / elapsedSeconds, extra,
         taskFinishedCount, concurrentThreads);
   }
 
