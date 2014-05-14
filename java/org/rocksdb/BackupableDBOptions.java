29eef1f87de3a5b7a5297447bc48dd709bdc377e
Ankit Gupta <aigupta@linkedin.com>
2014-05-13 22:40:01 -0700

    Add javadoc for BackupableDBOptions constructor params

--- java/org/rocksdb/BackupableDBOptions.java
+++ java/org/rocksdb/BackupableDBOptions.java
@@ -11,6 +11,27 @@ package org.rocksdb;
  *
  * Note that dispose() must be called before an Options instance
  * become out-of-scope to release the allocated memory in c++.
+ * 
+ * @param path Where to keep the backup files. Has to be different than dbname.
+       Best to set this to dbname_ + "/backups"
+ * @param shareTableFiles If share_table_files == true, backup will assume that 
+ *     table files with same name have the same contents. This enables 
+ *     incremental backups and avoids unnecessary data copies. If 
+ *     share_table_files == false, each backup will be on its own and will not 
+ *     share any data with other backups. default: true
+ * @param sync If sync == true, we can guarantee you'll get consistent backup 
+ *     even on a machine crash/reboot. Backup process is slower with sync 
+ *     enabled. If sync == false, we don't guarantee anything on machine reboot.
+ *     However, chances are some of the backups are consistent. Default: true
+ * @param destroyOldData If true, it will delete whatever backups there are 
+ *     already. Default: false
+ * @param backupLogFiles If false, we won't backup log files. This option can be
+ *     useful for backing up in-memory databases where log file are persisted, 
+ *     but table files are in memory. Default: true
+ * @param backupRateLimit Max bytes that can be transferred in a second during 
+ *     backup. If 0, go as fast as you can. Default: 0
+ * @param restoreRateLimit Max bytes that can be transferred in a second during 
+ *     restore. If 0, go as fast as you can. Default: 0
  */
 public class BackupableDBOptions extends RocksObject {
   public BackupableDBOptions(String path, boolean shareTableFiles, boolean sync,
