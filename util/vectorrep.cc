536e9973e30d70fd510e5ab6e423ef75248ed582
Igor Canadi <icanadi@fb.com>
2014-08-27 11:05:41 -0700

    Remove assert in vector rep
    Summary: This assert makes Insert O(n^2) instead of O(n) in debug mode. Memtable insert is in the critical path. No need to assert uniqunnes of the key here, since we're adding a sequence number to it anyway.
    
    Test Plan: none
    
    Reviewers: sdong, ljin
    
    Reviewed By: ljin
    
    Subscribers: leveldb
    
    Differential Revision: https://reviews.facebook.net/D22443

--- util/vectorrep.cc
+++ util/vectorrep.cc
@@ -106,7 +106,6 @@ class VectorRep : public MemTableRep {
 
 void VectorRep::Insert(KeyHandle handle) {
   auto* key = static_cast<char*>(handle);
-  assert(!Contains(key));
   WriteLock l(&rwlock_);
   assert(!immutable_);
   bucket_->push_back(key);
