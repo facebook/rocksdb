171d4ff4a29fec153d9e53d44bb39657044927a7
Lei Jin <lei@fb.com>
2014-09-08 15:39:53 -0700

    remove TailingIterator reference in db_impl.h
    Summary: as title
    
    Test Plan: make release
    
    Reviewers: igor
    
    Differential Revision: https://reviews.facebook.net/D23073

--- db/db_impl.h
+++ db/db_impl.h
@@ -316,7 +316,6 @@ class DBImpl : public DB {
   friend class DB;
   friend class InternalStats;
 #ifndef ROCKSDB_LITE
-  friend class TailingIterator;
   friend class ForwardIterator;
 #endif
   friend struct SuperVersion;
