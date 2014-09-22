a7574d4fa17c6110b878bd91c674fb012982b7ec
whu_liuchang <nfcj000@qq.com>
2014-09-22 23:37:00 +0800

    Update logging.cc

--- util/logging.cc
+++ util/logging.cc
@@ -45,7 +45,7 @@ int AppendHumanBytes(uint64_t bytes, char* output, int len) {
 
 void AppendNumberTo(std::string* str, uint64_t num) {
   char buf[30];
-  snprintf(buf, sizeof(buf), "%llu", static_cast(unsigned long long) num);
+  snprintf(buf, sizeof(buf), "%llu", static_cast<unsigned long long>(num));
   str->append(buf);
 }
 
