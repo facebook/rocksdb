dec2c9f564d7015f1a0855a1011ac030816dfadf
Yueh-Hsuan Chiang <yhchiang@fb.com>
2015-06-24 01:38:53 -0700

    Make table_properties_collector_test runnable in ROCKSDB_LITE
    Summary: Make table_properties_collector_test runnable in ROCKSDB_LITE
    
    Test Plan: table_properties_collector_test
    
    Reviewers: sdong, rven, anthony, kradhakrishnan, IslamAbdelRahman, igor
    
    Reviewed By: igor
    
    Subscribers: dhruba, leveldb
    
    Differential Revision: https://reviews.facebook.net/D40581

--- db/table_properties_collector_test.cc
+++ db/table_properties_collector_test.cc
@@ -383,6 +383,7 @@ TEST_P(TablePropertiesTest, CustomizedTablePropertiesCollector) {
                                            kBlockBasedTableMagicNumber,
                                            encode_as_internal, options, ikc);
 
+#ifndef ROCKSDB_LITE  // PlainTable is not supported in Lite
     // test plain table
     PlainTableOptions plain_table_options;
     plain_table_options.user_key_len = 8;
@@ -394,6 +395,7 @@ TEST_P(TablePropertiesTest, CustomizedTablePropertiesCollector) {
     TestCustomizedTablePropertiesCollector(backward_mode_,
                                            kPlainTableMagicNumber,
                                            encode_as_internal, options, ikc);
+#endif  // !ROCKSDB_LITE
   }
 }
 
@@ -495,6 +497,7 @@ TEST_P(TablePropertiesTest, InternalKeyPropertiesCollector) {
         std::make_shared<BlockBasedTableFactory>());
   }
 
+#ifndef ROCKSDB_LITE  // PlainTable is not supported in Lite
   PlainTableOptions plain_table_options;
   plain_table_options.user_key_len = 8;
   plain_table_options.bloom_bits_per_key = 8;
@@ -503,6 +506,7 @@ TEST_P(TablePropertiesTest, InternalKeyPropertiesCollector) {
   TestInternalKeyPropertiesCollector(
       backward_mode_, kPlainTableMagicNumber, false /* not sanitize */,
       std::make_shared<PlainTableFactory>(plain_table_options));
+#endif  // !ROCKSDB_LITE
 }
 
 INSTANTIATE_TEST_CASE_P(InternalKeyPropertiesCollector, TablePropertiesTest,
