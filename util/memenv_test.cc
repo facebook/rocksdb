59eca2cc9991ee10fd1af54e5e022a04aef2f819
Islam AbdelRahman <tec@fb.com>
2015-07-20 11:35:10 -0700

    Make memenv_test runnable in ROCKSDB_LITE
    Summary: Make memenv_test runnable in ROCKSDB_LITE
    
    Test Plan: memenv_test
    
    Reviewers: sdong, igor, yhchiang
    
    Reviewed By: yhchiang
    
    Subscribers: dhruba
    
    Differential Revision: https://reviews.facebook.net/D42123

--- util/memenv_test.cc
+++ util/memenv_test.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
+#ifndef ROCKSDB_LITE
+
 #include "db/db_impl.h"
 #include "rocksdb/db.h"
 #include "rocksdb/env.h"
@@ -239,3 +241,13 @@ int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
+#else
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  fprintf(stderr, "SKIPPED as MemEnv is not supported in ROCKSDB_LITE\n");
+  return 0;
+}
+
+#endif  // !ROCKSDB_LITE
