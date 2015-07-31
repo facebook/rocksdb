bd852bf118586f556dee660a6ce7ce80715fe4e8
Andres Noetzli <andres.noetzli@gmail.com>
2015-07-31 14:11:43 -0700

    Fixed typos in db_stress
    Summary: Fixed typos.
    
    Test Plan: None
    
    Reviewers: igor, yhchiang, sdong, anthony, rven
    
    Reviewed By: rven
    
    Subscribers: dhruba, leveldb
    
    Differential Revision: https://reviews.facebook.net/D43365

--- tools/db_stress.cc
+++ tools/db_stress.cc
@@ -86,7 +86,7 @@ DEFINE_int64(max_key, 1 * KB* KB,
 DEFINE_int32(column_families, 10, "Number of column families");
 
 DEFINE_bool(test_batches_snapshots, false,
-            "If set, the test uses MultiGet(), Multiut() and MultiDelete()"
+            "If set, the test uses MultiGet(), MultiPut() and MultiDelete()"
             " which read/write/delete multiple keys in a batch. In this mode,"
             " we do not verify db content by comparing the content with the "
             "pre-allocated array. Instead, we do partial verification inside"
@@ -192,7 +192,7 @@ DEFINE_int32(compaction_thread_pool_adjust_interval, 0,
              "size. Don't change it periodically if the value is 0.");
 
 DEFINE_int32(compaction_thread_pool_variations, 2,
-             "Range of bakground thread pool size variations when adjusted "
+             "Range of background thread pool size variations when adjusted "
              "periodically.");
 
 DEFINE_int32(max_background_flushes, rocksdb::Options().max_background_flushes,
@@ -275,7 +275,7 @@ DEFINE_int32(target_file_size_base, 64 * KB,
              "Target level-1 file size for compaction");
 
 DEFINE_int32(target_file_size_multiplier, 1,
-             "A multiplier to compute targe level-N file size (N >= 2)");
+             "A multiplier to compute target level-N file size (N >= 2)");
 
 DEFINE_uint64(max_bytes_for_level_base, 256 * KB, "Max bytes for level-1");
 
@@ -302,7 +302,7 @@ static const bool FLAGS_prefixpercent_dummy __attribute__((unused)) =
     RegisterFlagValidator(&FLAGS_prefixpercent, &ValidateInt32Percent);
 
 DEFINE_int32(writepercent, 45,
-             " Ratio of deletes to total workload (expressed as a percentage)");
+             "Ratio of writes to total workload (expressed as a percentage)");
 static const bool FLAGS_writepercent_dummy __attribute__((unused)) =
     RegisterFlagValidator(&FLAGS_writepercent, &ValidateInt32Percent);
 
