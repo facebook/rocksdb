d6911111464b3ddc35405200c4f9d77045f9110a
sdong <siying.d@fb.com>
2015-10-23 07:36:22 -0700

    include/posix/io_posix.h should have a once declartion
    Summary: include/posix/io_posix.h doesn't not prevent multiple includes. Need to fix it. It is also breaking unity build.
    
    Test Plan: Run unity build and see error go away.
    
    Reviewers: rven, igor, IslamAbdelRahman, kradhakrishnan, anthony
    
    Subscribers: leveldb, dhruba
    
    Differential Revision: https://reviews.facebook.net/D49281

--- include/posix/io_posix.h
+++ include/posix/io_posix.h
@@ -6,6 +6,7 @@
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
+#pragma once
 #include <deque>
 #include <set>
 #include <dirent.h>
