8be568a9c289c880672c17422ba7299606375f2f
Islam AbdelRahman <tec@fb.com>
2015-11-06 11:19:01 -0800

    Adding new table properties
    Summary:
    This diff introduce new table properties that will be written for block based tables
    These properties are
      - comparator name
      - merge operator name
      - property collectors names
    
    Test Plan:
      - Added a new unit test to verify that these tests are written/read correctly
      - Running all other tests right now (wont land until all tests finish)
    
    Reviewers: rven, kradhakrishnan, igor, sdong, anthony, yhchiang
    
    Reviewed By: yhchiang
    
    Subscribers: dhruba
    
    Differential Revision: https://reviews.facebook.net/D34269

--- table/meta_blocks.cc
+++ table/meta_blocks.cc
@@ -71,8 +71,20 @@ void PropertyBlockBuilder::AddTableProperty(const TableProperties& props) {
   Add(TablePropertiesNames::kFixedKeyLen, props.fixed_key_len);
 
   if (!props.filter_policy_name.empty()) {
-    Add(TablePropertiesNames::kFilterPolicy,
-        props.filter_policy_name);
+    Add(TablePropertiesNames::kFilterPolicy, props.filter_policy_name);
+  }
+
+  if (!props.comparator_name.empty()) {
+    Add(TablePropertiesNames::kComparator, props.comparator_name);
+  }
+
+  if (!props.merge_operator_name.empty()) {
+    Add(TablePropertiesNames::kMergeOperator, props.merge_operator_name);
+  }
+
+  if (!props.property_collectors_names.empty()) {
+    Add(TablePropertiesNames::kPropertyCollectors,
+        props.property_collectors_names);
   }
 }
 
@@ -203,6 +215,12 @@ Status ReadProperties(const Slice& handle_value, RandomAccessFileReader* file,
       *(pos->second) = val;
     } else if (key == TablePropertiesNames::kFilterPolicy) {
       new_table_properties->filter_policy_name = raw_val.ToString();
+    } else if (key == TablePropertiesNames::kComparator) {
+      new_table_properties->comparator_name = raw_val.ToString();
+    } else if (key == TablePropertiesNames::kMergeOperator) {
+      new_table_properties->merge_operator_name = raw_val.ToString();
+    } else if (key == TablePropertiesNames::kPropertyCollectors) {
+      new_table_properties->property_collectors_names = raw_val.ToString();
     } else {
       // handle user-collected properties
       new_table_properties->user_collected_properties.insert(
