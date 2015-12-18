521da3abb33565c3c364a143290d8b4ab38b38e6
Islam AbdelRahman <tec@fb.com>
2015-12-18 11:17:21 -0800

    Fix BlockBasedTableTest.BlockCacheLeak valgrind failure
    Summary:
    I added this line in my previous patch D48999 (which is incorrect)
    We should not release the iterator since releasing it will evict the blocks from cache
    
    Test Plan:
    Run the test under valgrind
    make check
    
    Reviewers: rven, yhchiang, sdong
    
    Reviewed By: sdong
    
    Subscribers: dhruba
    
    Differential Revision: https://reviews.facebook.net/D52161

--- table/table_test.cc
+++ table/table_test.cc
@@ -1807,7 +1807,6 @@ TEST_F(BlockBasedTableTest, BlockCacheLeak) {
     iter->Next();
   }
   ASSERT_OK(iter->status());
-  iter.release();
 
   const ImmutableCFOptions ioptions1(opt);
   ASSERT_OK(c.Reopen(ioptions1));
