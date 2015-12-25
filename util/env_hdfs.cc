b4aa823661dcc701b3e05e464b84f81d6c827531
Shu Zhang <shu_zhang@alumni.brown.edu>
2015-12-24 20:38:35 -0800

    format

--- util/env_hdfs.cc
+++ util/env_hdfs.cc
@@ -596,8 +596,8 @@ Status HdfsEnv::NewLogger(const std::string& fname,
 
 // The factory method for creating an HDFS Env
 Status NewHdfsEnv(Env** hdfs_env, const std::string& fsname) {
-	*hdfs_env = new HdfsEnv(fsname);
-	return Status::OK();
+  *hdfs_env = new HdfsEnv(fsname);
+  return Status::OK();
 }
 }  // namespace rocksdb
 
