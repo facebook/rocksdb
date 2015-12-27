b79ccbd573742f26979dc7efc95b15cf904a5307
Shu Zhang <shu_zhang@alumni.brown.edu>
2015-12-26 19:50:28 -0800

    indent

--- util/env_hdfs.cc
+++ util/env_hdfs.cc
@@ -614,7 +614,7 @@ namespace rocksdb {
  }
 
  Status NewHdfsEnv(Env** hdfs_env, const std::string& fsname) {
-	 return Status::NotSupported("Not compiled with hdfs support");
+   return Status::NotSupported("Not compiled with hdfs support");
  }
 
 }
