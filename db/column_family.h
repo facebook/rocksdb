facebook/rocksdb

commit 7d87f02799bd0a8fd36df24fab5baa4968615c86

support for concurrent adds to memtable
Summary:
This diff adds support for concurrent adds to the skiplist memtable
implementations.  Memory allocation is made thread-safe by the addition of
a spinlock, with small per-core buffers to avoid contention.  Concurrent
memtable writes are made via an additional method and don't impose a
performance overhead on the non-concurrent case, so parallelism can be
selected on a per-batch basis.

Write thread synchronization is an increasing bottleneck for higher levels
of concurrency, so this diff adds --enable_write_thread_adaptive_yield
(default off).  This feature causes threads joining a write batch
group to spin for a short time (default 100 usec) using sched_yield,
rather than going to sleep on a mutex.  If the timing of the yield calls
indicates that another thread has actually run during the yield then
spinning is avoided.  This option improves performance for concurrent
situations even without parallel adds, although it has the potential to
increase CPU usage (and the heuristic adaptation is not yet mature).

Parallel writes are not currently compatible with
inplace updates, update callbacks, or delete filtering.
Enable it with --allow_concurrent_memtable_write (and
--enable_write_thread_adaptive_yield).  Parallel memtable writes
are performance neutral when there is no actual parallelism, and in
my experiments (SSD server-class Linux and varying contention and key
sizes for fillrandom) they are always a performance win when there is
more than one thread.

Statistics are updated earlier in the write path, dropping the number
of DB mutex acquisitions from 2 to 1 for almost all cases.

This diff was motivated and inspired by Yahoo's cLSM work.  It is more
conservative than cLSM: RocksDB's write batch group leader role is
preserved (along with all of the existing flush and write throttling
logic) and concurrent writers are blocked until all memtable insertions

db/column_family.h
@@ -19,12 +19,10 @@
 #include "db/write_controller.h"
 #include "db/table_cache.h"
 #include "db/table_properties_collector.h"
-#include "db/flush_scheduler.h"
 #include "rocksdb/compaction_job_stats.h"
 #include "rocksdb/db.h"
 #include "rocksdb/env.h"
 #include "rocksdb/options.h"
-#include "util/instrumented_mutex.h"
 #include "util/mutable_cf_options.h"
 #include "util/thread_local.h"
 
@@ -134,6 +132,9 @@ struct SuperVersion {
 
 extern Status CheckCompressionSupported(const ColumnFamilyOptions& cf_options);
 
+extern Status CheckConcurrentWritesSupported(
+    const ColumnFamilyOptions& cf_options);
+
 extern ColumnFamilyOptions SanitizeOptions(const DBOptions& db_options,
                                            const InternalKeyComparator* icmp,
                                            const ColumnFamilyOptions& src);
@@ -158,14 +159,16 @@ class ColumnFamilyData {
   // thread-safe
   const std::string& GetName() const { return name_; }
 
-  // Ref() can only be called whily holding a DB mutex or during a
-  // single-threaded write.
+  // Ref() can only be called from a context where the caller can guarantee
+  // that ColumnFamilyData is alive (while holding a non-zero ref already,
+  // holding a DB mutex, or as the leader in a write batch group).
   void Ref() { refs_.fetch_add(1, std::memory_order_relaxed); }
-  // will just decrease reference count to 0, but will not delete it. returns
-  // true if the ref count was decreased to zero. in that case, it can be
-  // deleted by the caller immediately, or later, by calling
-  // FreeDeadColumnFamilies()
-  // Unref() can only be called while holding a DB mutex
+
+  // Unref decreases the reference count, but does not handle deletion
+  // when the count goes to 0.  If this method returns true then the
+  // caller should delete the instance immediately, or later, by calling
+  // FreeDeadColumnFamilies().  Unref() can only be called while holding
+  // a DB mutex, or during single-threaded recovery.
   bool Unref() {
     int old_refs = refs_.fetch_sub(1, std::memory_order_relaxed);
     assert(old_refs > 0);
@@ -497,15 +500,18 @@ class ColumnFamilySet {
 // memtables of different column families (specified by ID in the write batch)
 class ColumnFamilyMemTablesImpl : public ColumnFamilyMemTables {
  public:
-  explicit ColumnFamilyMemTablesImpl(ColumnFamilySet* column_family_set,
-                                     FlushScheduler* flush_scheduler)
-      : column_family_set_(column_family_set),
-        current_(nullptr),
-        flush_scheduler_(flush_scheduler) {}
+  explicit ColumnFamilyMemTablesImpl(ColumnFamilySet* column_family_set)
+      : column_family_set_(column_family_set), current_(nullptr) {}
+
+  // Constructs a ColumnFamilyMemTablesImpl equivalent to one constructed
+  // with the arguments used to construct *orig.
+  explicit ColumnFamilyMemTablesImpl(ColumnFamilyMemTablesImpl* orig)
+      : column_family_set_(orig->column_family_set_), current_(nullptr) {}
 
   // sets current_ to ColumnFamilyData with column_family_id
   // returns false if column family doesn't exist
-  // REQUIRES: under a DB mutex OR from a write thread
+  // REQUIRES: use this function of DBImpl::column_family_memtables_ should be
+  //           under a DB mutex OR from a write thread
   bool Seek(uint32_t column_family_id) override;
 
   // Returns log number of the selected column family
@@ -513,20 +519,23 @@ class ColumnFamilyMemTablesImpl : public ColumnFamilyMemTables {
   uint64_t GetLogNumber() const override;
 
   // REQUIRES: Seek() called first
-  // REQUIRES: under a DB mutex OR from a write thread
+  // REQUIRES: use this function of DBImpl::column_family_memtables_ should be
+  //           under a DB mutex OR from a write thread
   virtual MemTable* GetMemTable() const override;
 
   // Returns column family handle for the selected column family
-  // REQUIRES: under a DB mutex OR from a write thread
+  // REQUIRES: use this function of DBImpl::column_family_memtables_ should be
+  //           under a DB mutex OR from a write thread
   virtual ColumnFamilyHandle* GetColumnFamilyHandle() override;
 
-  // REQUIRES: under a DB mutex OR from a write thread
-  virtual void CheckMemtableFull() override;
+  // Cannot be called while another thread is calling Seek().
+  // REQUIRES: use this function of DBImpl::column_family_memtables_ should be
+  //           under a DB mutex OR from a write thread
+  virtual ColumnFamilyData* current() { return current_; }
 
  private:
   ColumnFamilySet* column_family_set_;
   ColumnFamilyData* current_;
-  FlushScheduler* flush_scheduler_;
   ColumnFamilyHandleInternal handle_;
 };
 

db/write_thread.cc
@@ -4,34 +4,197 @@
 //  of patent rights can be found in the PATENTS file in the same directory.
 
 #include "db/write_thread.h"
+#include <chrono>
+#include <limits>
+#include <thread>
+#include "db/column_family.h"
+#include "port/port.h"
 #include "util/sync_point.h"
 
 namespace rocksdb {
 
-void WriteThread::Await(Writer* w) {
-  std::unique_lock<std::mutex> guard(w->JoinMutex());
-  w->JoinCV().wait(guard, [w] { return w->joined; });
+WriteThread::WriteThread(uint64_t max_yield_usec, uint64_t slow_yield_usec)
+    : max_yield_usec_(max_yield_usec),
+      slow_yield_usec_(slow_yield_usec),
+      newest_writer_(nullptr) {}
+
+uint8_t WriteThread::BlockingAwaitState(Writer* w, uint8_t goal_mask) {
+  // We're going to block.  Lazily create the mutex.  We guarantee
+  // propagation of this construction to the waker via the
+  // STATE_LOCKED_WAITING state.  The waker won't try to touch the mutex
+  // or the condvar unless they CAS away the STATE_LOCKED_WAITING that
+  // we install below.
+  w->CreateMutex();
+
+  auto state = w->state.load(std::memory_order_acquire);
+  assert(state != STATE_LOCKED_WAITING);
+  if ((state & goal_mask) == 0 &&
+      w->state.compare_exchange_strong(state, STATE_LOCKED_WAITING)) {
+    // we have permission (and an obligation) to use StateMutex
+    std::unique_lock<std::mutex> guard(w->StateMutex());
+    w->StateCV().wait(guard, [w] {
+      return w->state.load(std::memory_order_relaxed) != STATE_LOCKED_WAITING;
+    });
+    state = w->state.load(std::memory_order_relaxed);
+  }
+  // else tricky.  Goal is met or CAS failed.  In the latter case the waker
+  // must have changed the state, and compare_exchange_strong has updated
+  // our local variable with the new one.  At the moment WriteThread never
+  // waits for a transition across intermediate states, so we know that
+  // since a state change has occurred the goal must have been met.
+  assert((state & goal_mask) != 0);
+  return state;
 }
 
-void WriteThread::MarkJoined(Writer* w) {
-  std::lock_guard<std::mutex> guard(w->JoinMutex());
-  assert(!w->joined);
-  w->joined = true;
-  w->JoinCV().notify_one();
+uint8_t WriteThread::AwaitState(Writer* w, uint8_t goal_mask,
+                                AdaptationContext* ctx) {
+  uint8_t state;
+
+  // On a modern Xeon each loop takes about 7 nanoseconds (most of which
+  // is the effect of the pause instruction), so 200 iterations is a bit
+  // more than a microsecond.  This is long enough that waits longer than
+  // this can amortize the cost of accessing the clock and yielding.
+  for (uint32_t tries = 0; tries < 200; ++tries) {
+    state = w->state.load(std::memory_order_acquire);
+    if ((state & goal_mask) != 0) {
+      return state;
+    }
+    port::AsmVolatilePause();
+  }
+
+  // If we're only going to end up waiting a short period of time,
+  // it can be a lot more efficient to call std::this_thread::yield()
+  // in a loop than to block in StateMutex().  For reference, on my 4.0
+  // SELinux test server with support for syscall auditing enabled, the
+  // minimum latency between FUTEX_WAKE to returning from FUTEX_WAIT is
+  // 2.7 usec, and the average is more like 10 usec.  That can be a big
+  // drag on RockDB's single-writer design.  Of course, spinning is a
+  // bad idea if other threads are waiting to run or if we're going to
+  // wait for a long time.  How do we decide?
+  //
+  // We break waiting into 3 categories: short-uncontended,
+  // short-contended, and long.  If we had an oracle, then we would always
+  // spin for short-uncontended, always block for long, and our choice for
+  // short-contended might depend on whether we were trying to optimize
+  // RocksDB throughput or avoid being greedy with system resources.
+  //
+  // Bucketing into short or long is easy by measuring elapsed time.
+  // Differentiating short-uncontended from short-contended is a bit
+  // trickier, but not too bad.  We could look for involuntary context
+  // switches using getrusage(RUSAGE_THREAD, ..), but it's less work
+  // (portability code and CPU) to just look for yield calls that take
+  // longer than we expect.  sched_yield() doesn't actually result in any
+  // context switch overhead if there are no other runnable processes
+  // on the current core, in which case it usually takes less than
+  // a microsecond.
+  //
+  // There are two primary tunables here: the threshold between "short"
+  // and "long" waits, and the threshold at which we suspect that a yield
+  // is slow enough to indicate we should probably block.  If these
+  // thresholds are chosen well then CPU-bound workloads that don't
+  // have more threads than cores will experience few context switches
+  // (voluntary or involuntary), and the total number of context switches
+  // (voluntary and involuntary) will not be dramatically larger (maybe
+  // 2x) than the number of voluntary context switches that occur when
+  // --max_yield_wait_micros=0.
+  //
+  // There's another constant, which is the number of slow yields we will
+  // tolerate before reversing our previous decision.  Solitary slow
+  // yields are pretty common (low-priority small jobs ready to run),
+  // so this should be at least 2.  We set this conservatively to 3 so
+  // that we can also immediately schedule a ctx adaptation, rather than
+  // waiting for the next update_ctx.
+
+  const size_t kMaxSlowYieldsWhileSpinning = 3;
+
+  bool update_ctx = false;
+  bool would_spin_again = false;
+
+  if (max_yield_usec_ > 0) {
+    update_ctx = Random::GetTLSInstance()->OneIn(256);
+
+    if (update_ctx || ctx->value.load(std::memory_order_relaxed) >= 0) {
+      // we're updating the adaptation statistics, or spinning has >
+      // 50% chance of being shorter than max_yield_usec_ and causing no
+      // involuntary context switches
+      auto spin_begin = std::chrono::steady_clock::now();
+
+      // this variable doesn't include the final yield (if any) that
+      // causes the goal to be met
+      size_t slow_yield_count = 0;
+
+      auto iter_begin = spin_begin;
+      while ((iter_begin - spin_begin) <=
+             std::chrono::microseconds(max_yield_usec_)) {
+        std::this_thread::yield();
+
+        state = w->state.load(std::memory_order_acquire);
+        if ((state & goal_mask) != 0) {
+          // success
+          would_spin_again = true;
+          break;
+        }
+
+        auto now = std::chrono::steady_clock::now();
+        if (now == iter_begin ||
+            now - iter_begin >= std::chrono::microseconds(slow_yield_usec_)) {
+          // conservatively count it as a slow yield if our clock isn't
+          // accurate enough to measure the yield duration
+          ++slow_yield_count;
+          if (slow_yield_count >= kMaxSlowYieldsWhileSpinning) {
+            // Not just one ivcsw, but several.  Immediately update ctx
+            // and fall back to blocking
+            update_ctx = true;
+            break;
+          }
+        }
+        iter_begin = now;
+      }
+    }
+  }
+
+  if ((state & goal_mask) == 0) {
+    state = BlockingAwaitState(w, goal_mask);
+  }
+
+  if (update_ctx) {
+    auto v = ctx->value.load(std::memory_order_relaxed);
+    // fixed point exponential decay with decay constant 1/1024, with +1
+    // and -1 scaled to avoid overflow for int32_t
+    v = v + (v / 1024) + (would_spin_again ? 1 : -1) * 16384;
+    ctx->value.store(v, std::memory_order_relaxed);
+  }
+
+  assert((state & goal_mask) != 0);
+  return state;
 }
 
-void WriteThread::LinkOne(Writer* w, bool* wait_needed) {
-  assert(!w->joined && !w->done);
+void WriteThread::SetState(Writer* w, uint8_t new_state) {
+  auto state = w->state.load(std::memory_order_acquire);
+  if (state == STATE_LOCKED_WAITING ||
+      !w->state.compare_exchange_strong(state, new_state)) {
+    assert(state == STATE_LOCKED_WAITING);
+
+    std::lock_guard<std::mutex> guard(w->StateMutex());
+    assert(w->state.load(std::memory_order_relaxed) != new_state);
+    w->state.store(new_state, std::memory_order_relaxed);
+    w->StateCV().notify_one();
+  }
+}
+
+void WriteThread::LinkOne(Writer* w, bool* linked_as_leader) {
+  assert(w->state == STATE_INIT);
 
   Writer* writers = newest_writer_.load(std::memory_order_relaxed);
   while (true) {
     w->link_older = writers;
-    if (writers != nullptr) {
-      w->CreateMutex();
-    }
     if (newest_writer_.compare_exchange_strong(writers, w)) {
-      // Success.
-      *wait_needed = (writers != nullptr);
+      if (writers == nullptr) {
+        // this isn't part of the WriteThread machinery, but helps with
+        // debugging and is checked by an assert in WriteImpl
+        w->state.store(STATE_GROUP_LEADER, std::memory_order_relaxed);
+      }
+      *linked_as_leader = (writers == nullptr);
       return;
     }
   }
@@ -50,11 +213,15 @@ void WriteThread::CreateMissingNewerLinks(Writer* head) {
 }
 
 void WriteThread::JoinBatchGroup(Writer* w) {
+  static AdaptationContext ctx{"JoinBatchGroup"};
+
   assert(w->batch != nullptr);
-  bool wait_needed;
-  LinkOne(w, &wait_needed);
-  if (wait_needed) {
-    Await(w);
+  bool linked_as_leader;
+  LinkOne(w, &linked_as_leader);
+  if (!linked_as_leader) {
+    AwaitState(w,
+               STATE_GROUP_LEADER | STATE_PARALLEL_FOLLOWER | STATE_COMPLETED,
+               &ctx);
   }
 }
 
@@ -88,7 +255,7 @@ size_t WriteThread::EnterAsBatchGroupLeader(
   // This is safe regardless of any db mutex status of the caller. Previous
   // calls to ExitAsGroupLeader either didn't call CreateMissingNewerLinks
   // (they emptied the list and then we added ourself as leader) or had to
-  // explicitly wake up us (the list was non-empty when we added ourself,
+  // explicitly wake us up (the list was non-empty when we added ourself,
   // so we have already received our MarkJoined).
   CreateMissingNewerLinks(newest_writer);
 
@@ -135,6 +302,73 @@ size_t WriteThread::EnterAsBatchGroupLeader(
   return size;
 }
 
+void WriteThread::LaunchParallelFollowers(ParallelGroup* pg,
+                                          SequenceNumber sequence) {
+  // EnterAsBatchGroupLeader already created the links from leader to
+  // newer writers in the group
+
+  pg->leader->parallel_group = pg;
+
+  Writer* w = pg->leader;
+  w->sequence = sequence;
+
+  while (w != pg->last_writer) {
+    sequence += WriteBatchInternal::Count(w->batch);
+    w = w->link_newer;
+
+    w->sequence = sequence;
+    w->parallel_group = pg;
+    SetState(w, STATE_PARALLEL_FOLLOWER);
+  }
+}
+
+bool WriteThread::CompleteParallelWorker(Writer* w) {
+  static AdaptationContext ctx{"CompleteParallelWorker"};
+
+  auto* pg = w->parallel_group;
+  if (!w->status.ok()) {
+    std::lock_guard<std::mutex> guard(w->StateMutex());
+    pg->status = w->status;
+  }
+  auto leader = pg->leader;
+  auto early_exit_allowed = pg->early_exit_allowed;
+
+  if (pg->running.load(std::memory_order_acquire) > 1 && pg->running-- > 1) {
+    // we're not the last one
+    AwaitState(w, STATE_COMPLETED, &ctx);
+
+    // Caller only needs to perform exit duties if early exit doesn't
+    // apply and this is the leader.  Can't touch pg here.  Whoever set
+    // our state to STATE_COMPLETED copied pg->status to w.status for us.
+    return w == leader && !(early_exit_allowed && w->status.ok());
+  }
+  // else we're the last parallel worker
+
+  if (w == leader || (early_exit_allowed && pg->status.ok())) {
+    // this thread should perform exit duties
+    w->status = pg->status;
+    return true;
+  } else {
+    // We're the last parallel follower but early commit is not
+    // applicable.  Wake up the leader and then wait for it to exit.
+    assert(w->state == STATE_PARALLEL_FOLLOWER);
+    SetState(leader, STATE_COMPLETED);
+    AwaitState(w, STATE_COMPLETED, &ctx);
+    return false;
+  }
+}
+
+void WriteThread::EarlyExitParallelGroup(Writer* w) {
+  auto* pg = w->parallel_group;
+
+  assert(w->state == STATE_PARALLEL_FOLLOWER);
+  assert(pg->status.ok());
+  ExitAsBatchGroupLeader(pg->leader, pg->last_writer, pg->status);
+  assert(w->state == STATE_COMPLETED);
+  assert(w->status.ok());
+  SetState(pg->leader, STATE_COMPLETED);
+}
+
 void WriteThread::ExitAsBatchGroupLeader(Writer* leader, Writer* last_writer,
                                          Status status) {
   assert(leader->link_older == nullptr);
@@ -166,31 +400,35 @@ void WriteThread::ExitAsBatchGroupLeader(Writer* leader, Writer* last_writer,
     // nullptr when they enqueued (we were definitely enqueued before them
     // and are still in the list).  That means leader handoff occurs when
     // we call MarkJoined
-    MarkJoined(last_writer->link_newer);
+    SetState(last_writer->link_newer, STATE_GROUP_LEADER);
   }
   // else nobody else was waiting, although there might already be a new
   // leader now
 
   while (last_writer != leader) {
     last_writer->status = status;
-    last_writer->done = true;
-    // We must read link_older before calling MarkJoined, because as
-    // soon as it is marked the other thread's AwaitJoined may return
-    // and deallocate the Writer.
+
+    // we need to read link_older before calling SetState, because as soon
+    // as it is marked committed the other thread's Await may return and
+    // deallocate the Writer.
     auto next = last_writer->link_older;
-    MarkJoined(last_writer);
+    SetState(last_writer, STATE_COMPLETED);
+
     last_writer = next;
   }
 }
 
 void WriteThread::EnterUnbatched(Writer* w, InstrumentedMutex* mu) {
+  static AdaptationContext ctx{"EnterUnbatched"};
+  static std::atomic<uint32_t> adaptation_history{};
+
   assert(w->batch == nullptr);
-  bool wait_needed;
-  LinkOne(w, &wait_needed);
-  if (wait_needed) {
+  bool linked_as_leader;
+  LinkOne(w, &linked_as_leader);
+  if (!linked_as_leader) {
     mu->Unlock();
     TEST_SYNC_POINT("WriteThread::EnterUnbatched:Wait");
-    Await(w);
+    AwaitState(w, STATE_GROUP_LEADER, &ctx);
     mu->Lock();
   }
 }

util/concurrent_arena.cc
@@ -1,0 +1,49 @@
+//  Copyright (c) 2013, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
+//
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "util/concurrent_arena.h"
+#include <thread>
+#include "port/likely.h"
+#include "port/port.h"
+#include "util/random.h"
+
+namespace rocksdb {
+
+#if ROCKSDB_SUPPORT_THREAD_LOCAL
+__thread uint32_t ConcurrentArena::tls_cpuid = 0;
+#endif
+
+ConcurrentArena::ConcurrentArena(size_t block_size, size_t huge_page_size)
+    : shard_block_size_(block_size / 8), arena_(block_size, huge_page_size) {
+  // find a power of two >= num_cpus and >= 8
+  auto num_cpus = std::thread::hardware_concurrency();
+  index_mask_ = 7;
+  while (index_mask_ + 1 < num_cpus) {
+    index_mask_ = index_mask_ * 2 + 1;
+  }
+
+  shards_.reset(new Shard[index_mask_ + 1]);
+  Fixup();
+}
+
+ConcurrentArena::Shard* ConcurrentArena::Repick() {
+  int cpuid = port::PhysicalCoreID();
+  if (UNLIKELY(cpuid < 0)) {
+    // cpu id unavailable, just pick randomly
+    cpuid = Random::GetTLSInstance()->Uniform(index_mask_ + 1);
+  }
+#if ROCKSDB_SUPPORT_THREAD_LOCAL
+  // even if we are cpu 0, use a non-zero tls_cpuid so we can tell we
+  // have repicked
+  tls_cpuid = cpuid | (index_mask_ + 1);
+#endif
+  return &shards_[cpuid & index_mask_];
+}
+
+}  // namespace rocksdb

commit 11672df19a4cfc638e8b36843bc154877ff4ca5c

Fix CLANG errors introduced by 7d87f02799bd0a8fd36df24fab5baa4968615c86
Summary: Fix some CLANG errors introduced in 7d87f02799bd0a8fd36df24fab5baa4968615c86

Test Plan: Build with both of CLANG and gcc

Reviewers: rven, yhchiang, kradhakrishnan, anthony, IslamAbdelRahman, ngbronson

Subscribers: leveldb, dhruba

Differential Revision: https://reviews.facebook.net/D52329

db/column_family.h
@@ -229,7 +229,7 @@ class ColumnFamilyData {
   MemTable* mem() { return mem_; }
   Version* current() { return current_; }
   Version* dummy_versions() { return dummy_versions_; }
-  void SetCurrent(Version* current);
+  void SetCurrent(Version* _current);
   uint64_t GetNumLiveVersions() const;  // REQUIRE: DB mutex held
   uint64_t GetTotalSstFilesSize() const;  // REQUIRE: DB mutex held
   void SetMemtable(MemTable* new_mem) { mem_ = new_mem; }
@@ -531,7 +531,7 @@ class ColumnFamilyMemTablesImpl : public ColumnFamilyMemTables {
   // Cannot be called while another thread is calling Seek().
   // REQUIRES: use this function of DBImpl::column_family_memtables_ should be
   //           under a DB mutex OR from a write thread
-  virtual ColumnFamilyData* current() { return current_; }
+  virtual ColumnFamilyData* current() override { return current_; }
 
  private:
   ColumnFamilySet* column_family_set_;

db/db_impl.cc
@@ -4387,7 +4387,8 @@ Status DBImpl::WriteImpl(const WriteOptions& write_options,
         pg.leader = &w;
         pg.last_writer = last_writer;
         pg.early_exit_allowed = !need_log_sync;
-        pg.running.store(write_batch_group.size(), std::memory_order_relaxed);
+        pg.running.store(static_cast<uint32_t>(write_batch_group.size()),
+                         std::memory_order_relaxed);
         write_thread_.LaunchParallelFollowers(&pg, current_sequence);
 
         ColumnFamilyMemTablesImpl column_family_memtables(

db/write_thread.cc
@@ -420,7 +420,6 @@ void WriteThread::ExitAsBatchGroupLeader(Writer* leader, Writer* last_writer,
 
 void WriteThread::EnterUnbatched(Writer* w, InstrumentedMutex* mu) {
   static AdaptationContext ctx{"EnterUnbatched"};
-  static std::atomic<uint32_t> adaptation_history{};
 
   assert(w->batch == nullptr);
   bool linked_as_leader;

util/concurrent_arena.cc
@@ -36,12 +36,13 @@ ConcurrentArena::Shard* ConcurrentArena::Repick() {
   int cpuid = port::PhysicalCoreID();
   if (UNLIKELY(cpuid < 0)) {
     // cpu id unavailable, just pick randomly
-    cpuid = Random::GetTLSInstance()->Uniform(index_mask_ + 1);
+    cpuid =
+        Random::GetTLSInstance()->Uniform(static_cast<int>(index_mask_) + 1);
   }
 #if ROCKSDB_SUPPORT_THREAD_LOCAL
   // even if we are cpu 0, use a non-zero tls_cpuid so we can tell we
   // have repicked
-  tls_cpuid = cpuid | (index_mask_ + 1);
+  tls_cpuid = cpuid | (static_cast<int>(index_mask_) + 1);
 #endif
   return &shards_[cpuid & index_mask_];
 }

util/concurrent_arena.h
@@ -78,7 +78,7 @@ class ConcurrentArena : public Allocator {
 
  private:
   struct Shard {
-    char padding[40];
+    char padding[40] __attribute__((__unused__));
     mutable SpinMutex mutex;
     char* free_begin_;
     std::atomic<size_t> allocated_and_unused_;
@@ -92,7 +92,7 @@ class ConcurrentArena : public Allocator {
   enum ZeroFirstEnum : uint32_t { tls_cpuid = 0 };
 #endif
 
-  char padding0[56];
+  char padding0[56] __attribute__((__unused__));
 
   size_t shard_block_size_;
 
@@ -106,7 +106,7 @@ class ConcurrentArena : public Allocator {
   std::atomic<size_t> memory_allocated_bytes_;
   std::atomic<size_t> irregular_block_num_;
 
-  char padding1[56];
+  char padding1[56] __attribute__((__unused__));
 
   Shard* Repick();
 