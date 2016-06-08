8ff59b2b40434f3a589c46056ac24d77cd06d5a9
Islam AbdelRahman <tec@fb.com>
2016-06-08 12:42:33 -0700

    Disable PersistentCacheTierTest.VolatileCacheInsertWithEviction test under TSAN temporarily
    Summary: Disable the test under TSAN temporary to temporarily the build
    
    Test Plan: run the test under TSAN
    
    Reviewers: kradhakrishnan, andrewkr, sdong
    
    Reviewed By: sdong
    
    Subscribers: andrewkr, dhruba
    
    Differential Revision: https://reviews.facebook.net/D59337

--- utilities/persistent_cache/persistent_cache_test.cc
+++ utilities/persistent_cache/persistent_cache_test.cc
@@ -26,6 +26,7 @@ TEST_F(PersistentCacheTierTest, VolatileCacheInsert) {
   }
 }
 
+#ifndef ROCKSDB_TSAN_RUN
 TEST_F(PersistentCacheTierTest, VolatileCacheInsertWithEviction) {
   for (auto nthreads : {1, 5}) {
     for (auto max_keys : {1 * 1024 * 1024}) {
@@ -35,6 +36,7 @@ TEST_F(PersistentCacheTierTest, VolatileCacheInsertWithEviction) {
     }
   }
 }
+#endif
 
 // test table with volatile page cache
 TEST_F(PersistentCacheDBTest, VolatileCacheTest) {
