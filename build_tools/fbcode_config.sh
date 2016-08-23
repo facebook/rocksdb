99c4af7160a69021e7e5aeb7faf3c4b07fd9c5c5
Yi Wu <yiwu@fb.com>
2016-08-23 13:40:47 -0700

    Make ClockCache available with TSAN build
    Summary: ..to unblock TSAN contbuild.
    
    Test Plan:
      COMPILE_WITH_TSAN=1 make db_stress -j64
      ./db_stress --use_clock_cache=true
    
    Reviewers: sdong, lightmark, IslamAbdelRahman
    
    Reviewed By: IslamAbdelRahman
    
    Subscribers: andrewkr, dhruba, leveldb
    
    Differential Revision: https://reviews.facebook.net/D62397

--- build_tools/fbcode_config.sh
+++ build_tools/fbcode_config.sh
@@ -47,11 +47,6 @@ if test -z $PIC_BUILD; then
   ZSTD_INCLUDE=" -I $ZSTD_BASE/include/"
   ZSTD_LIBS=" $ZSTD_BASE/lib/libzstd.a"
   CFLAGS+=" -DZSTD"
-
-  # location of tbb
-  TBB_INCLUDE=" -isystem $TBB_BASE/include/"
-  TBB_LIBS="$TBB_BASE/lib/libtbb.a"
-  CFLAGS+=" -DTBB"
 fi
 
 # location of gflags headers and libraries
@@ -76,6 +71,15 @@ if test -z $PIC_BUILD; then
   # location of libunwind
   LIBUNWIND="$LIBUNWIND_BASE/lib/libunwind.a"
 fi
+
+# location of TBB
+TBB_INCLUDE=" -isystem $TBB_BASE/include/"
+if test -z $PIC_BUILD; then
+  TBB_LIBS="$TBB_BASE/lib/libtbb.a"
+else
+  TBB_LIBS="$TBB_BASE/lib/libtbb_pic.a"
+fi
+CFLAGS+=" -DTBB"
 
 # use Intel SSE support for checksum calculations
 export USE_SSE=1
