facebook/rocksdb

commit f89caa127baa086cb100976b14da1a531cf0e823

Direct IO capability for RocksDB
Summary:
This patch adds direct IO capability to RocksDB Env.

The direct IO capability is required for persistent cache since NVM is best
accessed as 4K direct IO. SSDs can leverage direct IO for reading.

Direct IO requires the offset and size be sector size aligned, and memory to
be kernel page aligned. Since neither RocksDB/Persistent read cache data
layout is aligned to sector size, the code can accommodate reading unaligned IO size
(or unaligned memory) at the cost of an alloc/copy.

The write code path expects the size and memory to be aligned.

Test Plan: Run RocksDB unit tests

Reviewers: sdong

Subscribers: andrewkr, dhruba, leveldb

Differential Revision: https://reviews.facebook.net/D57393

util/env_posix.cc
@@ -152,6 +152,17 @@ class PosixEnv : public Env {
     if (f == nullptr) {
       *result = nullptr;
       return IOError(fname, errno);
+    } else if (options.use_direct_reads && !options.use_mmap_writes) {
+      int flags = O_RDONLY | O_DIRECT;
+      TEST_SYNC_POINT_CALLBACK("NewSequentialFile:O_DIRECT", &flags);
+      int fd = open(fname.c_str(), flags, 0644);
+      if (fd < 0) {
+        return IOError(fname, errno);
+      }
+      std::unique_ptr<PosixDirectIOSequentialFile> file(
+          new PosixDirectIOSequentialFile(fname, fd));
+      *result = std::move(file);
+      return Status::OK();
     } else {
       int fd = fileno(f);
       SetFD_CLOEXEC(fd, &options);
@@ -189,6 +200,18 @@ class PosixEnv : public Env {
         }
       }
       close(fd);
+    } else if (options.use_direct_reads) {
+      int flags = O_RDONLY | O_DIRECT;
+      TEST_SYNC_POINT_CALLBACK("NewRandomAccessFile:O_DIRECT", &flags);
+      fd = open(fname.c_str(), flags, 0644);
+      if (fd < 0) {
+        s = IOError(fname, errno);
+      } else {
+        std::unique_ptr<PosixDirectIORandomAccessFile> file(
+            new PosixDirectIORandomAccessFile(fname, fd));
+        *result = std::move(file);
+        s = Status::OK();
+      }
     } else {
       result->reset(new PosixRandomAccessFile(fname, fd, options));
     }
@@ -221,6 +244,18 @@ class PosixEnv : public Env {
       }
       if (options.use_mmap_writes && !forceMmapOff) {
         result->reset(new PosixMmapFile(fname, fd, page_size_, options));
+      } else if (options.use_direct_writes) {
+        int flags = O_WRONLY | O_APPEND | O_TRUNC | O_CREAT | O_DIRECT;
+        TEST_SYNC_POINT_CALLBACK("NewWritableFile:O_DIRECT", &flags);
+        fd = open(fname.c_str(), flags, 0644);
+        if (fd < 0) {
+          s = IOError(fname, errno);
+        } else {
+          std::unique_ptr<PosixDirectIOWritableFile> file(
+              new PosixDirectIOWritableFile(fname, fd));
+          *result = std::move(file);
+          s = Status::OK();
+        }
       } else {
         // disable mmap writes
         EnvOptions no_mmap_writes_options = options;
@@ -763,6 +798,9 @@ std::string Env::GenerateUniqueId() {
   return uuid2;
 }
 
+//
+// Default Posix Env
+//
 Env* Env::Default() {
   // The following function call initializes the singletons of ThreadLocalPtr
   // right before the static default_env.  This guarantees default_env will

include/rocksdb/env.h
@@ -68,6 +68,12 @@ struct EnvOptions {
    // If true, then use mmap to write data
   bool use_mmap_writes = true;
 
+  // If true, then use O_DIRECT for reading data
+  bool use_direct_reads = false;
+
+  // If true, then use O_DIRECT for writing data
+  bool use_direct_writes = false;
+
   // If false, fallocate() calls are bypassed
   bool allow_fallocate = true;
 

util/io_posix.cc
@@ -12,6 +12,7 @@
 #include "util/io_posix.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <algorithm>
 #if defined(OS_LINUX)
 #include <linux/fs.h>
 #endif
@@ -45,6 +46,112 @@ int Fadvise(int fd, off_t offset, size_t len, int advice) {
   return 0;  // simply do nothing.
 #endif
 }
+
+/*
+ * DirectIOHelper
+ */
+namespace {
+const size_t kSectorSize = 512;
+#ifdef OS_LINUX
+const size_t kPageSize = sysconf(_SC_PAGESIZE);
+#else
+const size_t kPageSize = 4 * 1024;
+#endif
+
+std::unique_ptr<void, void (&)(void*)> NewAligned(const size_t size) {
+  void* ptr = nullptr;
+  if (posix_memalign(&ptr, 4 * 1024, size) != 0) {
+    return std::unique_ptr<char, void (&)(void*)>(nullptr, free);
+  }
+  std::unique_ptr<void, void (&)(void*)> uptr(ptr, free);
+  return uptr;
+}
+
+size_t Upper(const size_t size, const size_t fac) {
+  if (size % fac == 0) {
+    return size;
+  }
+  return size + (fac - size % fac);
+}
+
+size_t Lower(const size_t size, const size_t fac) {
+  if (size % fac == 0) {
+    return size;
+  }
+  return size - (size % fac);
+}
+
+bool IsSectorAligned(const size_t off) { return off % kSectorSize == 0; }
+
+static bool IsPageAligned(const void* ptr) {
+  return uintptr_t(ptr) % (kPageSize) == 0;
+}
+
+Status ReadAligned(int fd, Slice* data, const uint64_t offset,
+                   const size_t size, char* scratch) {
+  assert(IsSectorAligned(offset));
+  assert(IsSectorAligned(size));
+  assert(IsPageAligned(scratch));
+
+  size_t bytes_read = 0;
+  ssize_t status = -1;
+  while (bytes_read < size) {
+    status =
+        pread(fd, scratch + bytes_read, size - bytes_read, offset + bytes_read);
+    if (status <= 0) {
+      if (errno == EINTR) {
+        continue;
+      }
+      break;
+    }
+    bytes_read += status;
+  }
+
+  *data = Slice(scratch, bytes_read);
+  return status < 0 ? Status::IOError(strerror(errno)) : Status::OK();
+}
+
+Status ReadUnaligned(int fd, Slice* data, const uint64_t offset,
+                     const size_t size, char* scratch) {
+  assert(scratch);
+  assert(!IsSectorAligned(offset) || !IsSectorAligned(size) ||
+         !IsPageAligned(scratch));
+
+  const uint64_t aligned_off = Lower(offset, kSectorSize);
+  const size_t aligned_size = Upper(size + (offset - aligned_off), kSectorSize);
+  auto aligned_scratch = NewAligned(aligned_size);
+  assert(aligned_scratch);
+  if (!aligned_scratch) {
+    return Status::IOError("Unable to allocate");
+  }
+
+  assert(IsSectorAligned(aligned_off));
+  assert(IsSectorAligned(aligned_size));
+  assert(aligned_scratch);
+  assert(IsPageAligned(aligned_scratch.get()));
+  assert(offset + size <= aligned_off + aligned_size);
+
+  Slice scratch_slice;
+  Status s = ReadAligned(fd, &scratch_slice, aligned_off, aligned_size,
+                         reinterpret_cast<char*>(aligned_scratch.get()));
+
+  // copy data upto min(size, what was read)
+  memcpy(scratch, reinterpret_cast<char*>(aligned_scratch.get()) +
+                      (offset % kSectorSize),
+         std::min(size, scratch_slice.size()));
+  *data = Slice(scratch, std::min(size, scratch_slice.size()));
+  return s;
+}
+
+Status DirectIORead(int fd, Slice* result, size_t off, size_t n,
+                    char* scratch) {
+  if (IsSectorAligned(off) && IsSectorAligned(n) &&
+      IsPageAligned(result->data())) {
+    return ReadAligned(fd, result, off, n, scratch);
+  }
+  return ReadUnaligned(fd, result, off, n, scratch);
+}
+}  // namespace
 
 /*
  * PosixSequentialFile
@@ -104,15 +211,37 @@ Status PosixSequentialFile::InvalidateCache(size_t offset, size_t length) {
 #endif
 }
 
+/*
+ * PosixDirectIOSequentialFile
+ */
+Status PosixDirectIOSequentialFile::Read(size_t n, Slice* result,
+                                         char* scratch) {
+  const size_t off = off_.fetch_add(n);
+  return DirectIORead(fd_, result, off, n, scratch);
+}
+
+Status PosixDirectIOSequentialFile::Skip(uint64_t n) {
+  off_ += n;
+  return Status::OK();
+}
+
+Status PosixDirectIOSequentialFile::InvalidateCache(size_t /*offset*/,
+                                                    size_t /*length*/) {
+  return Status::OK();
+}
+
+/*
+ * PosixRandomAccessFile
+ */
 #if defined(OS_LINUX)
-namespace {
-static size_t GetUniqueIdFromFile(int fd, char* id, size_t max_size) {
+size_t PosixHelper::GetUniqueIdFromFile(int fd, char* id, size_t max_size) {
   if (max_size < kMaxVarint64Length * 3) {
     return 0;
   }
 
   struct stat buf;
   int result = fstat(fd, &buf);
+  assert(result != -1);
   if (result == -1) {
     return 0;
   }
@@ -132,12 +261,10 @@ static size_t GetUniqueIdFromFile(int fd, char* id, size_t max_size) {
   assert(rid >= id);
   return static_cast<size_t>(rid - id);
 }
-}  // namespace
 #endif
 
 #if defined(OS_MACOSX)
-namespace {
-static size_t GetUniqueIdFromFile(int fd, char* id, size_t max_size) {
+size_t PosixHelper::GetUniqueIdFromFile(int fd, char* id, size_t max_size) {
   if (max_size < kMaxVarint64Length * 3) {
     return 0;
   }
@@ -155,9 +282,7 @@ static size_t GetUniqueIdFromFile(int fd, char* id, size_t max_size) {
   assert(rid >= id);
   return static_cast<size_t>(rid - id);
 }
-}  // namespace
 #endif
-
 /*
  * PosixRandomAccessFile
  *
@@ -206,7 +331,7 @@ Status PosixRandomAccessFile::Read(uint64_t offset, size_t n, Slice* result,
 
 #if defined(OS_LINUX) || defined(OS_MACOSX)
 size_t PosixRandomAccessFile::GetUniqueId(char* id, size_t max_size) const {
-  return GetUniqueIdFromFile(fd_, id, max_size);
+  return PosixHelper::GetUniqueIdFromFile(fd_, id, max_size);
 }
 #endif
 
@@ -244,6 +369,15 @@ Status PosixRandomAccessFile::InvalidateCache(size_t offset, size_t length) {
   }
   return IOError(filename_, errno);
 #endif
+}
+
+/*
+ * PosixDirectIORandomAccessFile
+ */
+Status PosixDirectIORandomAccessFile::Read(uint64_t offset, size_t n,
+                                           Slice* result, char* scratch) const {
+  Status s = DirectIORead(fd_, result, offset, n, scratch);
+  return s;
 }
 
 /*
@@ -663,9 +797,36 @@ Status PosixWritableFile::RangeSync(uint64_t offset, uint64_t nbytes) {
 }
 
 size_t PosixWritableFile::GetUniqueId(char* id, size_t max_size) const {
-  return GetUniqueIdFromFile(fd_, id, max_size);
+  return PosixHelper::GetUniqueIdFromFile(fd_, id, max_size);
 }
 #endif
+
+/*
+ * PosixDirectIOWritableFile
+ */
+Status PosixDirectIOWritableFile::Append(const Slice& data) {
+  assert(IsSectorAligned(data.size()) && IsPageAligned(data.data()));
+  if (!IsSectorAligned(data.size()) || !IsPageAligned(data.data())) {
+    return Status::IOError("Unaligned buffer for direct IO");
+  }
+  return PosixWritableFile::Append(data);
+}
+
+Status PosixDirectIOWritableFile::PositionedAppend(const Slice& data,
+                                                   uint64_t offset) {
+  assert(IsSectorAligned(offset));
+  assert(IsSectorAligned(data.size()));
+  assert(IsPageAligned(data.data()));
+  if (!IsSectorAligned(offset) || !IsSectorAligned(data.size()) ||
+      !IsPageAligned(data.data())) {
+    return Status::IOError("offset or size is not aligned");
+  }
+  return PosixWritableFile::PositionedAppend(data, offset);
+}
+
+/*
+ * PosixDirectory
+ */
 
 PosixDirectory::~PosixDirectory() { close(fd_); }
 

util/io_posix.h
@@ -8,6 +8,7 @@
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 #pragma once
 #include <unistd.h>
+#include <atomic>
 #include "rocksdb/env.h"
 
 // For non linux platform, the following macros are used only as place
@@ -26,6 +27,11 @@ static Status IOError(const std::string& context, int err_number) {
   return Status::IOError(context, strerror(err_number));
 }
 
+class PosixHelper {
+ public:
+  static size_t GetUniqueIdFromFile(int fd, char* id, size_t max_size);
+};
+
 class PosixSequentialFile : public SequentialFile {
  private:
   std::string filename_;
@@ -43,8 +49,25 @@ class PosixSequentialFile : public SequentialFile {
   virtual Status InvalidateCache(size_t offset, size_t length) override;
 };
 
-class PosixRandomAccessFile : public RandomAccessFile {
+class PosixDirectIOSequentialFile : public SequentialFile {
+ public:
+  explicit PosixDirectIOSequentialFile(const std::string& filename, int fd)
+      : filename_(filename), fd_(fd) {}
+
+  virtual ~PosixDirectIOSequentialFile() {}
+
+  Status Read(size_t n, Slice* result, char* scratch) override;
+  Status Skip(uint64_t n) override;
+  Status InvalidateCache(size_t offset, size_t length) override;
+
  private:
+  const std::string filename_;
+  int fd_ = -1;
+  std::atomic<size_t> off_{0};  // read offset
+};
+
+class PosixRandomAccessFile : public RandomAccessFile {
+ protected:
   std::string filename_;
   int fd_;
   bool use_os_buffer_;
@@ -63,8 +86,23 @@ class PosixRandomAccessFile : public RandomAccessFile {
   virtual Status InvalidateCache(size_t offset, size_t length) override;
 };
 
+// Direct IO random access file direct IO implementation
+class PosixDirectIORandomAccessFile : public PosixRandomAccessFile {
+ public:
+  explicit PosixDirectIORandomAccessFile(const std::string& filename, int fd)
+      : PosixRandomAccessFile(filename, fd, EnvOptions()) {}
+  virtual ~PosixDirectIORandomAccessFile() {}
+
+  Status Read(uint64_t offset, size_t n, Slice* result,
+              char* scratch) const override;
+  virtual void Hint(AccessPattern pattern) override {}
+  Status InvalidateCache(size_t offset, size_t length) override {
+    return Status::OK();
+  }
+};
+
 class PosixWritableFile : public WritableFile {
- private:
+ protected:
   const std::string filename_;
   int fd_;
   uint64_t filesize_;
@@ -74,9 +112,9 @@ class PosixWritableFile : public WritableFile {
 #endif
 
  public:
-  PosixWritableFile(const std::string& fname, int fd,
-                    const EnvOptions& options);
-  ~PosixWritableFile();
+  explicit PosixWritableFile(const std::string& fname, int fd,
+                             const EnvOptions& options);
+  virtual ~PosixWritableFile();
 
   // Means Close() will properly take care of truncate
   // and it does not need any additional information
@@ -94,6 +132,22 @@ class PosixWritableFile : public WritableFile {
   virtual Status RangeSync(uint64_t offset, uint64_t nbytes) override;
   virtual size_t GetUniqueId(char* id, size_t max_size) const override;
 #endif
+};
+
+class PosixDirectIOWritableFile : public PosixWritableFile {
+ public:
+  explicit PosixDirectIOWritableFile(const std::string& filename, int fd)
+      : PosixWritableFile(filename, fd, EnvOptions()) {}
+  virtual ~PosixDirectIOWritableFile() {}
+
+  bool UseOSBuffer() const override { return false; }
+  size_t GetRequiredBufferAlignment() const override { return 4 * 1024; }
+  Status Append(const Slice& data) override;
+  Status PositionedAppend(const Slice& data, uint64_t offset) override;
+  bool UseDirectIO() const override { return true; }
+  Status InvalidateCache(size_t offset, size_t length) override {
+    return Status::OK();
+  }
 };
 
 class PosixMmapReadableFile : public RandomAccessFile {

commit 831e3fe8c3854aacebb86e7d5868a7fb24695d42

Fix fd leak when using direct IOs
We should close the fd, before overriding it. This bug was
introduced by f89caa127baa086cb100976b14da1a531cf0e823

util/env_posix.cc
@@ -157,6 +157,7 @@ class PosixEnv : public Env {
       *result = nullptr;
       return IOError(fname, errno);
     } else if (options.use_direct_reads && !options.use_mmap_writes) {
+      fclose(f);
 #ifdef OS_MACOSX
       int flags = O_RDONLY;
 #else
@@ -215,6 +216,7 @@ class PosixEnv : public Env {
       }
       close(fd);
     } else if (options.use_direct_reads) {
+      close(fd);
 #ifdef OS_MACOSX
       int flags = O_RDONLY;
 #else
@@ -269,6 +271,7 @@ class PosixEnv : public Env {
       if (options.use_mmap_writes && !forceMmapOff) {
         result->reset(new PosixMmapFile(fname, fd, page_size_, options));
       } else if (options.use_direct_writes) {
+        close(fd);
 #ifdef OS_MACOSX
         int flags = O_WRONLY | O_APPEND | O_TRUNC | O_CREAT;
 #else