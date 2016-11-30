96fcefbf1d6e12696c867c94c38d1ed13950286d
Panagiotis Ktistakis <panktist@gmail.com>
2016-11-30 11:05:46 -0800

    c api: expose option for dynamic level size target
    Summary: Closes https://github.com/facebook/rocksdb/pull/1587
    
    Differential Revision: D4245923
    
    Pulled By: yiwu-arbug
    
    fbshipit-source-id: 6ee7291

--- include/rocksdb/c.h
+++ include/rocksdb/c.h
@@ -565,6 +565,9 @@ extern ROCKSDB_LIBRARY_API void rocksdb_options_set_target_file_size_multiplier(
 extern ROCKSDB_LIBRARY_API void rocksdb_options_set_max_bytes_for_level_base(
     rocksdb_options_t*, uint64_t);
 extern ROCKSDB_LIBRARY_API void
+rocksdb_options_set_level_compaction_dynamic_level_bytes(rocksdb_options_t*,
+                                                         unsigned char);
+extern ROCKSDB_LIBRARY_API void
 rocksdb_options_set_max_bytes_for_level_multiplier(rocksdb_options_t*, double);
 extern ROCKSDB_LIBRARY_API void
 rocksdb_options_set_max_bytes_for_level_multiplier_additional(
