e39d080871aca1a5bbca52044e570e5b565a9c5e
Islam AbdelRahman <tec@fb.com>
2016-12-01 10:02:12 -0800

    Fix travis (compile for clang < 3.9)
    Summary:
    Travis fail because it uses clang 3.6 which don't recognize
    `__attribute__((__no_sanitize__("undefined")))`
    Closes https://github.com/facebook/rocksdb/pull/1601
    
    Differential Revision: D4257175
    
    Pulled By: IslamAbdelRahman
    
    fbshipit-source-id: fb4d1ab

--- util/hash.cc
+++ util/hash.cc
@@ -14,7 +14,7 @@
 namespace rocksdb {
 
 // This function may intentionally do a left shift on a -ve number
-#if defined(__clang__)
+#if __clang_major__ > 3 || (__clang_major__ == 3 && __clang_minor__ >= 9)
 __attribute__((__no_sanitize__("undefined")))
 #elif __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
 __attribute__((__no_sanitize_undefined__))
