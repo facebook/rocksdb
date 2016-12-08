20ce081fae1afbd4cbc4e745d29b79f050951555
Islam AbdelRahman <tec@fb.com>
2016-12-08 13:30:09 -0800

    Fix issue where IngestExternalFile insert blocks in block cache with g_seqno=0
    Summary:
    When we Ingest an external file we open it to read some metadata and first/last key
    during doing that we insert blocks into the block cache with global_seqno = 0
    
    If we move the file (did not copy it) into the DB, we will use these blocks with the wrong seqno in the read path
    Closes https://github.com/facebook/rocksdb/pull/1627
    
    Differential Revision: D4293332
    
    Pulled By: yiwu-arbug
    
    fbshipit-source-id: 3ce5523

--- db/external_sst_file_ingestion_job.cc
+++ db/external_sst_file_ingestion_job.cc
@@ -311,8 +311,14 @@ Status ExternalSstFileIngestionJob::GetIngestedFileInfo(
   file_to_ingest->num_entries = props->num_entries;
 
   ParsedInternalKey key;
-  std::unique_ptr<InternalIterator> iter(
-      table_reader->NewIterator(ReadOptions()));
+  ReadOptions ro;
+  // During reading the external file we can cache blocks that we read into
+  // the block cache, if we later change the global seqno of this file, we will
+  // have block in cache that will include keys with wrong seqno.
+  // We need to disable fill_cache so that we read from the file without
+  // updating the block cache.
+  ro.fill_cache = false;
+  std::unique_ptr<InternalIterator> iter(table_reader->NewIterator(ro));
 
   // Get first (smallest) key from file
   iter->SeekToFirst();
