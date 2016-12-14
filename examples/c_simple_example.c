16895b1c97f4276497fa0241367396cf6e6a7459
Jonathan Lee <vjon207@gmail.com>
2016-12-13 22:52:51 -0800

    [Fix #591] Add missing copyright header
    "examples/c_simple_example.c" did not have a proper copyright header.

--- examples/c_simple_example.c
+++ examples/c_simple_example.c
@@ -1,3 +1,8 @@
+// Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
