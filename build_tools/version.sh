715dbf1c5a77c916ca6bddb946188460445f99ea
Pradeep Chhetri <pradeep.chhetri@netradyne.com>
2016-12-21 15:05:48 +0530

    Merged multiple pipes into one awk

--- build_tools/version.sh
+++ build_tools/version.sh
@@ -4,11 +4,11 @@ if [ "$#" = "0" ]; then
   exit 1
 fi
 if [ "$1" = "major" ]; then
-  cat include/rocksdb/version.h  | grep MAJOR | head -n1 | awk '{print $3}'
+  awk '/MAJOR/ {print $3; exit}' include/rocksdb/version.h
 fi
 if [ "$1" = "minor" ]; then
-  cat include/rocksdb/version.h  | grep MINOR | head -n1 | awk '{print $3}'
+  awk '/MINOR/ {print $3; exit}' include/rocksdb/version.h
 fi
 if [ "$1" = "patch" ]; then
-  cat include/rocksdb/version.h  | grep PATCH | head -n1 | awk '{print $3}'
+  awk '/PATCH/ {print $3; exit}' include/rocksdb/version.h
 fi
