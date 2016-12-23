facebook/rocksdb

commit 972f96b3fbae1a4675043bdf4279c9072ad69645

direct io write support
Summary:
rocksdb direct io support

```
[gzh@dev11575.prn2 ~/rocksdb] ./db_bench -benchmarks=fillseq --num=1000000
Initializing RocksDB Options from the specified file
Initializing RocksDB Options from command-line flags
RocksDB:    version 5.0
Date:       Wed Nov 23 13:17:43 2016
CPU:        40 * Intel(R) Xeon(R) CPU E5-2660 v2 @ 2.20GHz
CPUCache:   25600 KB
Keys:       16 bytes each
Values:     100 bytes each (50 bytes after compression)
Entries:    1000000
Prefix:    0 bytes
Keys per prefix:    0
RawSize:    110.6 MB (estimated)
FileSize:   62.9 MB (estimated)
Write rate: 0 bytes/second
Compression: Snappy
Memtablerep: skip_list
Perf Level: 1
WARNING: Assertions are enabled; benchmarks unnecessarily slow
------------------------------------------------
Initializing RocksDB Options from the specified file
Initializing RocksDB Options from command-line flags
DB path: [/tmp/rocksdbtest-112628/dbbench]
fillseq      :       4.393 micros/op 227639 ops/sec;   25.2 MB/s

[gzh@dev11575.prn2 ~/roc
Closes https://github.com/facebook/rocksdb/pull/1564

Differential Revision: D4241093

Pulled By: lightmark

fbshipit-source-id: 98c29e3

include/rocksdb/utilities/env_librados.h
@@ -1,5 +1,7 @@
-// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
-// vim: ts=8 sw=2 smarttab
+// Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
 #ifndef ROCKSDB_UTILITIES_ENV_LIBRADOS_H
 #define ROCKSDB_UTILITIES_ENV_LIBRADOS_H
 
@@ -15,17 +17,16 @@ namespace rocksdb {
 class LibradosWritableFile;
 
 class EnvLibrados : public EnvWrapper {
-public:
+ public:
   // Create a brand new sequentially-readable file with the specified name.
   // On success, stores a pointer to the new file in *result and returns OK.
   // On failure stores nullptr in *result and returns non-OK.  If the file does
   // not exist, returns a non-OK status.
   //
   // The returned file will only be accessed by one thread at a time.
-  Status NewSequentialFile(
-    const std::string& fname,
-    std::unique_ptr<SequentialFile>* result,
-    const EnvOptions& options);
+  Status NewSequentialFile(const std::string& fname,
+                           std::unique_ptr<SequentialFile>* result,
+                           const EnvOptions& options) override;
 
   // Create a brand new random access read-only file with the
   // specified name.  On success, stores a pointer to the new file in
@@ -34,10 +35,9 @@ public:
   // status.
   //
   // The returned file may be concurrently accessed by multiple threads.
-  Status NewRandomAccessFile(
-    const std::string& fname,
-    std::unique_ptr<RandomAccessFile>* result,
-    const EnvOptions& options);
+  Status NewRandomAccessFile(const std::string& fname,
+                             std::unique_ptr<RandomAccessFile>* result,
+                             const EnvOptions& options) override;
 
   // Create an object that writes to a new file with the specified
   // name.  Deletes any existing file with the same name and creates a
@@ -46,17 +46,15 @@ public:
   // returns non-OK.
   //
   // The returned file will only be accessed by one thread at a time.
-  Status NewWritableFile(
-    const std::string& fname,
-    std::unique_ptr<WritableFile>* result,
-    const EnvOptions& options);
+  Status NewWritableFile(const std::string& fname,
+                         std::unique_ptr<WritableFile>* result,
+                         const EnvOptions& options) override;
 
   // Reuse an existing file by renaming it and opening it as writable.
-  Status ReuseWritableFile(
-    const std::string& fname,
-    const std::string& old_fname,
-    std::unique_ptr<WritableFile>* result,
-    const EnvOptions& options);
+  Status ReuseWritableFile(const std::string& fname,
+                           const std::string& old_fname,
+                           std::unique_ptr<WritableFile>* result,
+                           const EnvOptions& options) override;
 
   // Create an object that represents a directory. Will fail if directory
   // doesn't exist. If the directory exists, it will open the directory
@@ -65,47 +63,44 @@ public:
   // On success, stores a pointer to the new Directory in
   // *result and returns OK. On failure stores nullptr in *result and
   // returns non-OK.
-  Status NewDirectory(
-    const std::string& name,
-    std::unique_ptr<Directory>* result);
+  Status NewDirectory(const std::string& name,
+                      std::unique_ptr<Directory>* result) override;
 
   // Returns OK if the named file exists.
   //         NotFound if the named file does not exist,
   //                  the calling process does not have permission to determine
   //                  whether this file exists, or if the path is invalid.
   //         IOError if an IO Error was encountered
-  Status FileExists(const std::string& fname);
+  Status FileExists(const std::string& fname) overrdie;
 
   // Store in *result the names of the children of the specified directory.
   // The names are relative to "dir".
   // Original contents of *results are dropped.
-  Status GetChildren(const std::string& dir,
-                     std::vector<std::string>* result);
+  Status GetChildren(const std::string& dir, std::vector<std::string>* result);
 
   // Delete the named file.
-  Status DeleteFile(const std::string& fname);
+  Status DeleteFile(const std::string& fname) override;
 
   // Create the specified directory. Returns error if directory exists.
-  Status CreateDir(const std::string& dirname);
+  Status CreateDir(const std::string& dirname) override;
 
   // Creates directory if missing. Return Ok if it exists, or successful in
   // Creating.
-  Status CreateDirIfMissing(const std::string& dirname);
+  Status CreateDirIfMissing(const std::string& dirname) override;
 
   // Delete the specified directory.
-  Status DeleteDir(const std::string& dirname);
+  Status DeleteDir(const std::string& dirname) override;
 
   // Store the size of fname in *file_size.
-  Status GetFileSize(const std::string& fname, uint64_t* file_size);
+  Status GetFileSize(const std::string& fname, uint64_t* file_size) override;
 
   // Store the last modification time of fname in *file_mtime.
   Status GetFileModificationTime(const std::string& fname,
-                                 uint64_t* file_mtime);
+                                 uint64_t* file_mtime) override;
   // Rename file src to target.
-  Status RenameFile(const std::string& src,
-                    const std::string& target);
+  Status RenameFile(const std::string& src, const std::string& target) override;
   // Hard Link file src to target.
-  Status LinkFile(const std::string& src, const std::string& target);
+  Status LinkFile(const std::string& src, const std::string& target) override;
 
   // Lock the specified file.  Used to prevent concurrent access to
   // the same db by multiple processes.  On failure, stores nullptr in
@@ -129,8 +124,7 @@ public:
   Status UnlockFile(FileLock* lock);
 
   // Get full directory name for this db.
-  Status GetAbsolutePath(const std::string& db_path,
-                         std::string* output_path);
+  Status GetAbsolutePath(const std::string& db_path, std::string* output_path);
 
   // Generate unique id
   std::string GenerateUniqueId();
@@ -142,31 +136,29 @@ public:
                        const std::string& config_path,
                        const std::string& db_pool);
 
-  explicit EnvLibrados(const std::string& client_name,      // first 3 parameters are for RADOS client init
-                       const std::string& cluster_name,
-                       const uint64_t flags,
-                       const std::string& db_name,
-                       const std::string& config_path,
-                       const std::string& db_pool,
-                       const std::string& wal_dir,
-                       const std::string& wal_pool,
-                       const uint64_t write_buffer_size);
-  ~EnvLibrados() {
-    _rados.shutdown();
-  }
-private:
+  explicit EnvLibrados(
+      const std::string& client_name,  // first 3 parameters are
+                                       // for RADOS client init
+      const std::string& cluster_name, const uint64_t flags,
+      const std::string& db_name, const std::string& config_path,
+      const std::string& db_pool, const std::string& wal_dir,
+      const std::string& wal_pool, const uint64_t write_buffer_size);
+  ~EnvLibrados() { _rados.shutdown(); }
+
+ private:
   std::string _client_name;
   std::string _cluster_name;
   uint64_t _flags;
-  std::string _db_name;                                     // get from user, readable string; Also used as db_id for db metadata
+  std::string _db_name;  // get from user, readable string; Also used as db_id
+                         // for db metadata
   std::string _config_path;
-  librados::Rados _rados;                                   // RADOS client
+  librados::Rados _rados;  // RADOS client
   std::string _db_pool_name;
-  librados::IoCtx _db_pool_ioctx;                           // IoCtx for connecting db_pool
-  std::string _wal_dir;                                     // WAL dir path
+  librados::IoCtx _db_pool_ioctx;  // IoCtx for connecting db_pool
+  std::string _wal_dir;            // WAL dir path
   std::string _wal_pool_name;
-  librados::IoCtx _wal_pool_ioctx;                          // IoCtx for connecting wal_pool
-  uint64_t _write_buffer_size;                              // WritableFile buffer max size
+  librados::IoCtx _wal_pool_ioctx;  // IoCtx for connecting wal_pool
+  uint64_t _write_buffer_size;      // WritableFile buffer max size
 
   /* private function to communicate with rados */
   std::string _CreateFid();
@@ -175,10 +167,8 @@ private:
   Status _RenameFid(const std::string& old_fname, const std::string& new_fname);
   Status _AddFid(const std::string& fname, const std::string& fid);
   Status _DelFid(const std::string& fname);
-  Status _GetSubFnames(
-    const std::string& dirname,
-    std::vector<std::string> * result
-  );
+  Status _GetSubFnames(const std::string& dirname,
+                       std::vector<std::string>* result);
   librados::IoCtx* _GetIoctx(const std::string& prefix);
   friend class LibradosWritableFile;
 };

db/c.cc
@@ -1719,9 +1719,14 @@ void rocksdb_options_set_manifest_preallocation_size(
 void rocksdb_options_set_purge_redundant_kvs_while_flush(rocksdb_options_t* opt,
                                                          unsigned char v) {}
 
-void rocksdb_options_set_allow_os_buffer(rocksdb_options_t* opt,
-                                         unsigned char v) {
-  opt->rep.allow_os_buffer = v;
+void rocksdb_options_set_use_direct_reads(rocksdb_options_t* opt,
+                                          unsigned char v) {
+  opt->rep.use_direct_reads = v;
+}
+
+void rocksdb_options_set_use_direct_writes(rocksdb_options_t* opt,
+                                           unsigned char v) {
+  opt->rep.use_direct_writes = v;
 }
 
 void rocksdb_options_set_allow_mmap_reads(
@@ -2000,7 +2005,7 @@ rocksdb_ratelimiter_t* rocksdb_ratelimiter_create(
 
 void rocksdb_ratelimiter_destroy(rocksdb_ratelimiter_t *limiter) {
   if (limiter->rep) {
-	delete limiter->rep;
+    delete limiter->rep;
   }
   delete limiter;
 }

db/db_impl.cc
@@ -255,11 +255,17 @@ static Status ValidateOptions(
         "More than four DB paths are not supported yet. ");
   }
 
-  if (db_options.allow_mmap_reads && !db_options.allow_os_buffer) {
+  if (db_options.allow_mmap_reads && db_options.use_direct_reads) {
     // Protect against assert in PosixMMapReadableFile constructor
     return Status::NotSupported(
         "If memory mapped reads (allow_mmap_reads) are enabled "
-        "then os caching (allow_os_buffer) must also be enabled. ");
+        "then direct I/O reads (use_direct_reads) must be disabled. ");
+  }
+
+  if (db_options.allow_mmap_writes && db_options.use_direct_writes) {
+    return Status::NotSupported(
+        "If memory mapped writes (allow_mmap_writes) are enabled "
+        "then direct I/O writes (use_direct_writes) must be disabled. ");
   }
 
   return Status::OK();

db/db_test.cc
@@ -3396,19 +3396,21 @@ TEST_F(DBTest, TableOptionsSanitizeTest) {
 TEST_F(DBTest, MmapAndBufferOptions) {
   Options options = CurrentOptions();
 
-  options.allow_os_buffer = false;
+  options.use_direct_reads = true;
   options.allow_mmap_reads = true;
   ASSERT_NOK(TryReopen(options));
 
   // All other combinations are acceptable
-  options.allow_os_buffer = true;
+  options.use_direct_reads = false;
   ASSERT_OK(TryReopen(options));
 
-  options.allow_os_buffer = false;
-  options.allow_mmap_reads = false;
-  ASSERT_OK(TryReopen(options));
+  if (IsDirectIOSupported()) {
+    options.use_direct_reads = true;
+    options.allow_mmap_reads = false;
+    ASSERT_OK(TryReopen(options));
+  }
 
-  options.allow_os_buffer = true;
+  options.use_direct_reads = false;
   ASSERT_OK(TryReopen(options));
 }
 #endif

db/db_test_util.cc
@@ -478,6 +478,22 @@ Status DBTestBase::TryReopen(const Options& options) {
   return DB::Open(options, dbname_, &db_);
 }
 
+bool DBTestBase::IsDirectIOSupported() {
+  EnvOptions env_options;
+  env_options.use_mmap_writes = false;
+  env_options.use_direct_writes = true;
+  std::string tmp = TempFileName(dbname_, 999);
+  Status s;
+  {
+    unique_ptr<WritableFile> file;
+    s = env_->NewWritableFile(tmp, &file, env_options);
+  }
+  if (s.ok()) {
+    s = env_->DeleteFile(tmp);
+  }
+  return s.ok();
+}
+
 Status DBTestBase::Flush(int cf) {
   if (cf == 0) {
     return db_->Flush(FlushOptions());

db/db_test_util.h
@@ -692,6 +692,8 @@ class DBTestBase : public testing::Test {
 
   Status TryReopen(const Options& options);
 
+  bool IsDirectIOSupported();
+
   Status Flush(int cf = 0);
 
   Status Put(const Slice& k, const Slice& v, WriteOptions wo = WriteOptions());

db/forward_iterator_bench.cc
@@ -330,7 +330,7 @@ int main(int argc, char** argv) {
   options.compaction_style = rocksdb::CompactionStyle::kCompactionStyleNone;
   options.level0_slowdown_writes_trigger = 99999;
   options.level0_stop_writes_trigger = 99999;
-  options.allow_os_buffer = false;
+  options.use_direct_writes = true;
   options.write_buffer_size = FLAGS_memtable_size;
   rocksdb::BlockBasedTableOptions table_options;
   table_options.block_cache = rocksdb::NewLRUCache(FLAGS_block_cache_size);

examples/rocksdb_option_file_example.ini
@@ -73,12 +73,13 @@
   error_if_exists=false
   recycle_log_file_num=0
   skip_log_error_on_recovery=false
-  allow_mmap_reads=false
-  allow_os_buffer=true
   db_log_dir=
   new_table_reader_for_compaction_inputs=true
+  allow_mmap_reads=false
   allow_mmap_writes=false
-  
+  use_direct_reads=false
+  use_direct_writes=false
+
 
 [CFOptions "default"]
   compaction_style=kCompactionStyleLevel
@@ -127,7 +128,7 @@
   write_buffer_size=134217728
   disable_auto_compactions=false
   inplace_update_support=false
-  
+
 [TableOptions/BlockBasedTable "default"]
   format_version=2
   whole_key_filtering=true

commit 5e56f2256896a0c9dfe90295805ffa3cb0b06e34

include/rocksdb/utilities/env_librados: fix typo
Broken by 972f96b3fbae1a4675043bdf4279c9072ad69645

Signed-off-by: Sage Weil <sage@redhat.com>

include/rocksdb/utilities/env_librados.h
@@ -71,7 +71,7 @@ class EnvLibrados : public EnvWrapper {
   //                  the calling process does not have permission to determine
   //                  whether this file exists, or if the path is invalid.
   //         IOError if an IO Error was encountered
-  Status FileExists(const std::string& fname) overrdie;
+  Status FileExists(const std::string& fname) override;
 
   // Store in *result the names of the children of the specified directory.
   // The names are relative to "dir".