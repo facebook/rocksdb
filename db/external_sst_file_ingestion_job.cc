8ff352fff5b7cdebb83797478650e93ef782e41a
Shu Zhang <shu_zhang@alumni.brown.edu>
2017-01-19 13:34:18 -0800

    Addressing comments

--- db/external_sst_file_ingestion_job.cc
+++ db/external_sst_file_ingestion_job.cc
@@ -252,7 +252,6 @@ void ExternalSstFileIngestionJob::Cleanup(const Status& status) {
 
 Status ExternalSstFileIngestionJob::GetIngestedFileInfo(
     const std::string& external_file, IngestedFileInfo* file_to_ingest) {
-  static std::unordered_set<int> supported_versions = {1,2};
   file_to_ingest->external_file_path = external_file;
 
   // Get external file size
@@ -307,14 +306,16 @@ Status ExternalSstFileIngestionJob::GetIngestedFileInfo(
     if (file_to_ingest->global_seqno_offset == 0) {
       return Status::Corruption("Was not able to find file global seqno field");
     }
-  } else if (file_to_ingest->version == 1 &&
-        (ingestion_options_.allow_blocking_flush == true ||
-         ingestion_options_.allow_global_seqno == true)) {
-    return Status::InvalidArgument(
-            "external file's version number is not correct");
-  } else if (supported_versions.find(file_to_ingest->version) ==
-          supported_versions.end()){
-    return Status::InvalidArgument("external file version is not supported");
+  } else if (file_to_ingest->version == 1) {
+    // SST file V1 should not have global seqno field
+    assert(seqno_iter == uprops.end());
+    if (ingestion_options_.allow_blocking_flush ||
+            ingestion_options_.allow_global_seqno) {
+      return Status::InvalidArgument(
+            "External SST file V1 does not support global seqno");
+    }
+  } else {
+    return Status::InvalidArgument("External file version is not supported");
   }
   // Get number of entries in table
   file_to_ingest->num_entries = props->num_entries;
