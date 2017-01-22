f090c6c9e03a6f2e8d346c6156d74a9369dac70d
Arun Sharma <asharma@fb.com>
2017-01-22 12:07:41 -0800

    [cmake] Version librocksdb.so
    After make install, I see a directory hierarchy that looks like
    
    ```
    ./usr
    ./usr/include
    ./usr/include/rocksdb
    ./usr/include/rocksdb/filter_policy.h
    [..]
    ./usr/include/rocksdb/iterator.h
    ./usr/include/rocksdb/utilities
    ./usr/include/rocksdb/utilities/ldb_cmd_execute_result.h
    ./usr/include/rocksdb/utilities/lua
    ./usr/include/rocksdb/utilities/lua/rocks_lua_custom_library.h
    ./usr/include/rocksdb/utilities/lua/rocks_lua_util.h
    ./usr/include/rocksdb/utilities/lua/rocks_lua_compaction_filter.h
    ./usr/include/rocksdb/utilities/backupable_db.h
    [..]
    ./usr/include/rocksdb/utilities/env_registry.h
    [..]
    ./usr/include/rocksdb/env.h
    ./usr/lib64
    ./usr/lib64/librocksdb.so.5
    ./usr/lib64/librocksdb.so.5.0.0
    ./usr/lib64/librocksdb.so
    ./usr/lib64/librocksdb.a
    ```

--- build_tools/version.sh
+++ build_tools/version.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 if [ "$#" = "0" ]; then
-  echo "Usage: $0 major|minor|patch"
+  echo "Usage: $0 major|minor|patch|full"
   exit 1
 fi
+
 if [ "$1" = "major" ]; then
   cat include/rocksdb/version.h  | grep MAJOR | head -n1 | awk '{print $3}'
 fi
@@ -11,4 +12,11 @@ if [ "$1" = "minor" ]; then
 fi
 if [ "$1" = "patch" ]; then
   cat include/rocksdb/version.h  | grep PATCH | head -n1 | awk '{print $3}'
+fi
+if [ "$1" = "full" ]; then
+  awk '/#define ROCKSDB/ { env[$2] = $3 }
+       END { printf "%s.%s.%s\n", env["ROCKSDB_MAJOR"],
+                                  env["ROCKSDB_MINOR"],
+                                  env["ROCKSDB_PATCH"] }'  \
+      include/rocksdb/version.h
 fi
