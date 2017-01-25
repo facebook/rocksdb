17c1180603015470ed1f6fabd911adb5c8415215
Andrew Kryczka <andrewkr@fb.com>
2017-01-25 15:54:09 -0800

    Generalize Env registration framework
    Summary:
    The Env registration framework supports registering client Envs and selecting which one to instantiate according to a text field. This enabled things like adding the -env_uri argument to db_bench, so the same binary could be reused with different Envs just by changing CLI config.
    
    Now this problem has come up again in a non-Env context, as I want to instantiate a client Statistics implementation from db_bench, which is configured entirely via text parameters. Also, in the future we may wish to use it for deserializing client objects when loading OPTIONS file.
    
    This diff generalizes the Env registration logic to work with arbitrary types.
    
    - Generalized registration and instantiation code by templating them
    - The entire implementation is in a header file as that's Google style guide's recommendation for template definitions
    - Pattern match with std::regex_match rather than checking prefix, which was the previous behavior
    - Rename functions/files to be non-Env-specific
    Closes https://github.com/facebook/rocksdb/pull/1776
    
    Differential Revision: D4421933
    
    Pulled By: ajkr
    
    fbshipit-source-id: 34647d1

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -47,7 +47,7 @@
 #include "rocksdb/rate_limiter.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/slice_transform.h"
-#include "rocksdb/utilities/env_registry.h"
+#include "rocksdb/utilities/object_registry.h"
 #include "rocksdb/utilities/optimistic_transaction_db.h"
 #include "rocksdb/utilities/options_util.h"
 #include "rocksdb/utilities/sim_cache.h"
@@ -4903,7 +4903,7 @@ int db_bench_tool(int argc, char** argv) {
     fprintf(stderr, "Cannot provide both --hdfs and --env_uri.\n");
     exit(1);
   } else if (!FLAGS_env_uri.empty()) {
-    FLAGS_env = NewEnvFromUri(FLAGS_env_uri, &custom_env_guard);
+    FLAGS_env = NewCustomObject<Env>(FLAGS_env_uri, &custom_env_guard);
     if (FLAGS_env == nullptr) {
       fprintf(stderr, "No Env registered for URI: %s\n", FLAGS_env_uri.c_str());
       exit(1);
