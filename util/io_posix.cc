e894aa3a5bd0d4cfe34bd6c248755be0542dcc02
sdong <siying.d@fb.com>
2017-02-01 12:39:06 -0800

    Fix OSX build break after the fallocate change

--- util/io_posix.cc
+++ util/io_posix.cc
@@ -757,6 +757,7 @@ Status PosixWritableFile::Allocate(uint64_t offset, uint64_t len) {
 }
 #endif
 
+#ifdef OS_LINUX
 Status PosixWritableFile::RangeSync(uint64_t offset, uint64_t nbytes) {
   assert(offset <= std::numeric_limits<off_t>::max());
   assert(nbytes <= std::numeric_limits<off_t>::max());
@@ -771,6 +772,7 @@ Status PosixWritableFile::RangeSync(uint64_t offset, uint64_t nbytes) {
 size_t PosixWritableFile::GetUniqueId(char* id, size_t max_size) const {
   return PosixHelper::GetUniqueIdFromFile(fd_, id, max_size);
 }
+#endif
 
 /*
  * PosixRandomRWFile
