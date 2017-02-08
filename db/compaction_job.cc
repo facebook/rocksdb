e210a9a75053cc3d4b47221049cfd0e58a0c8787
Andrew Kryczka <andrewkr@fb.com>
2017-02-08 09:05:07 -0800

    modify first ShouldStopBefore call

--- db/compaction_job.cc
+++ db/compaction_job.cc
@@ -747,13 +747,13 @@ void CompactionJob::ProcessKeyValueCompaction(SubcompactionState* sub_compact) {
       shutting_down_));
   auto c_iter = sub_compact->c_iter.get();
   c_iter->SeekToFirst();
-  if (c_iter->Valid()) {
+  if (c_iter->Valid() &&
+      sub_compact->compaction->output_level() != 0) {
     // ShouldStopBefore() maintains state based on keys processed so far. The
     // compaction loop always calls it on the "next" key, thus won't tell it the
     // first key. So we do that here.
-    bool should_stop = sub_compact->ShouldStopBefore(
+    sub_compact->ShouldStopBefore(
       c_iter->key(), sub_compact->current_output_file_size);
-    assert(!should_stop);
   }
   const auto& c_iter_stats = c_iter->iter_stats();
   auto sample_begin_offset_iter = sample_begin_offsets.cbegin();
