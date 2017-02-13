a046870a51a56734bad2cf4ca7e4628378302c81
Xiaofei Du <xiaofeidu@fb.com>
2017-02-13 11:10:12 -0800

    Use monotonic time points in write_controller.cc and rate_limiter.cc
    summary: NowMicros() provides non-monotonic time. When wall clock is
    synchronized or changed, the non-monotonicity time points will affect write rate
    controllers. This patch changes write_controller.cc and rate_limiter.cc to use
    monotonic time points.

--- db/write_controller.h
+++ db/write_controller.h
@@ -78,6 +78,9 @@ class WriteController {
   uint64_t max_delayed_write_rate() const { return max_delayed_write_rate_; }
 
  private:
+  uint64_t NowMicrosMonotonic(Env* env);
+
+ private:
   friend class WriteControllerToken;
   friend class StopWriteToken;
   friend class DelayWriteToken;
