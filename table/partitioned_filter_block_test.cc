164fdb43b01d87decffb1c3ccdf0b21002857e48
Maysam Yabandeh <myabandeh@fb.com>
2017-03-28 11:39:29 -0700

    fix lint error

--- table/partitioned_filter_block_test.cc
+++ table/partitioned_filter_block_test.cc
@@ -3,6 +3,7 @@
 //  LICENSE file in the root directory of this source tree. An additional grant
 //  of patent rights can be found in the PATENTS file in the same directory.
 
+#include <algorithm>
 #include <map>
 
 #include "rocksdb/filter_policy.h"
