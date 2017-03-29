49d0f0153fbd3b1fcb88bb638f0d98efc0a8b38a
Islam AbdelRahman <tec@fb.com>
2017-03-29 16:59:00 -0700

    Use a human readable size for level report

--- util/logging.cc
+++ util/logging.cc
@@ -112,6 +112,25 @@ std::string NumberToHumanString(int64_t num) {
   return std::string(buf);
 }
 
+std::string BytesToHumanString(uint64_t bytes) {
+  const char* size_name[] = {"KB", "MB", "GB", "TB"};
+  double final_size = static_cast<double>(bytes);
+  size_t size_idx;
+
+  // always start with KB
+  final_size /= 1024;
+  size_idx = 0;
+
+  while (size_idx < 3 && final_size >= 1024) {
+    final_size /= 1024;
+    size_idx++;
+  }
+
+  char buf[20];
+  snprintf(buf, sizeof(buf), "%.2f %s", final_size, size_name[size_idx]);
+  return std::string(buf);
+}
+
 std::string EscapeString(const Slice& value) {
   std::string r;
   AppendEscapedStringTo(&r, value);
