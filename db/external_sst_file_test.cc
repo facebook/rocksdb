bb510fe202eda7a796dc59b62e29a03b85e40d67
Dhruba Borthakur <dhruba@gmail.com>
2017-04-20 21:36:53 +0000

    The ExternalFileTest does not work with AWS because it creates sst files in arbitrary locations.

--- db/external_sst_file_test.cc
+++ db/external_sst_file_test.cc
@@ -3,7 +3,7 @@
 //  LICENSE file in the root directory of this source tree. An additional grant
 //  of patent rights can be found in the PATENTS file in the same directory.
 
-#ifndef ROCKSDB_LITE
+#if !defined(ROCKSDB_LITE) && !defined(USE_AWS)
 
 #include <functional>
 #include "db/db_test_util.h"
