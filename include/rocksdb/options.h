2150cc1f3e2f8dcd9605451ebe624198808716f3
Aaron Gao <gzh@fb.com>
2017-04-26 15:28:50 -0700

    fix WritableFile buffer size in direct IO
    Summary:
    �fix the buffer size in case of ppl use buffer size as their block_size.
    Closes https://github.com/facebook/rocksdb/pull/2198
    
    Differential Revision: D4956878
    
    Pulled By: lightmark
    
    fbshipit-source-id: 8bb0dc9c133887aadcd625d5261a3d1110b71473

--- include/rocksdb/options.h
+++ include/rocksdb/options.h
@@ -690,7 +690,9 @@ struct DBOptions {
 
   // This is the maximum buffer size that is used by WritableFileWriter.
   // On Windows, we need to maintain an aligned buffer for writes.
-  // We allow the buffer to grow until it's size hits the limit.
+  // We allow the buffer to grow until it's size hits the limit in buffered
+  // IO and fix the buffer size when using direct IO to ensure alignment of
+  // write requests if the logical sector size is unusual
   //
   // Default: 1024 * 1024 (1 MB)
   size_t writable_file_max_buffer_size = 1024 * 1024;
