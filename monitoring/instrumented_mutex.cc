10a12b2a6f8d71193ef733c61273ca805c127dde
Siying Dong <siying.d@fb.com>
2017-04-27 17:50:56 -0700

    Add GPLv2 as an alternative license.
    Summary: Closes https://github.com/facebook/rocksdb/pull/2226
    
    Differential Revision: D4967547
    
    Pulled By: siying
    
    fbshipit-source-id: dd3b58ae1e7a106ab6bb6f37ab5c88575b125ab4

--- monitoring/instrumented_mutex.cc
+++ monitoring/instrumented_mutex.cc
@@ -2,6 +2,8 @@
 //  This source code is licensed under the BSD-style license found in the
 //  LICENSE file in the root directory of this source tree. An additional grant
 //  of patent rights can be found in the PATENTS file in the same directory.
+//  This source code is also licensed under the GPLv2 license found in the
+//  COPYING file in the root directory of this source tree.
 
 #include "monitoring/instrumented_mutex.h"
 #include "monitoring/perf_context_imp.h"
--- utilities/persistent_cache/block_cache_tier_metadata.cc
+++ utilities/persistent_cache/block_cache_tier_metadata.cc
@@ -2,6 +2,8 @@
 //  This source code is licensed under the BSD-style license found in the
 //  LICENSE file in the root directory of this source tree. An additional grant
 //  of patent rights can be found in the PATENTS file in the same directory.
+//  This source code is also licensed under the GPLv2 license found in the
+//  COPYING file in the root directory of this source tree.
 #ifndef ROCKSDB_LITE
 
 #include "utilities/persistent_cache/block_cache_tier_metadata.h"
