d616ebea23fa88cb9c2c8588533526a566d9cfab
Siying Dong <siying.d@fb.com>
2017-04-27 17:50:56 -0700

    Add GPLv2 as an alternative license.
    Summary: Closes https://github.com/facebook/rocksdb/pull/2226
    
    Differential Revision: D4967547
    
    Pulled By: siying
    
    fbshipit-source-id: dd3b58ae1e7a106ab6bb6f37ab5c88575b125ab4

--- db/internal_stats.cc
+++ db/internal_stats.cc
@@ -1,6 +1,8 @@
 //  This source code is licensed under the BSD-style license found in the
 //  LICENSE file in the root directory of this source tree. An additional grant
 //  of patent rights can be found in the PATENTS file in the same directory.
+//  This source code is also licensed under the GPLv2 license found in the
+//  COPYING file in the root directory of this source tree.
 //
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
