c8be647ea4585090341229f858cf49ca2e5935d9
Chen Shen <chenshen@fb.com>
2017-05-01 22:57:45 -0700

    merge ops

---
+++ java/src/main/java/org/rocksdb/CassandraValueMergeOperator.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+package org.rocksdb;
+
+/**
+ * CassandraValueMergeOperator is a merge operator that merges two cassandra wide column
+ * values.
+ */
+public class CassandraValueMergeOperator extends MergeOperator {
+    public CassandraValueMergeOperator() {
+        super(newSharedCassandraValueMergeOperator());
+    }
+
+    private native static long newSharedCassandraValueMergeOperator();
+
+    @Override protected final native void disposeInternal(final long handle);
+}
