761a18d18e8db5441e2a0b288d3dcac79f487757
Yi Wu <yiwu@fb.com>
2017-05-18 21:42:48 -0700

    Suppress clang-analyzer false positive

--- db/db_iter.cc
+++ db/db_iter.cc
@@ -933,7 +933,12 @@ void DBIter::FindPrevUserKey() {
     return;
   }
   size_t num_skipped = 0;
+// Suppress false positive clang analyzer warnings.
+#ifdef __clang_analyzer__
+  ParsedInternalKey ikey(Slice(), 0, 0);
+#else
   ParsedInternalKey ikey;
+#endif  // __clang_analyzer__
   FindParseableKey(&ikey, kReverse);
   int cmp;
   while (iter_->Valid() &&
