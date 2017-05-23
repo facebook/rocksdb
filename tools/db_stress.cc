a1c62c19cc5e0d818ca20a7077b62080e1b7ffda
Sagar Vemuri <svemuri@fb.com>
2017-05-23 14:52:13 -0700

    Fix release build on Linux
    Summary:
    Release builds are failing on Linux with the error:
    ```
    tools/db_stress.cc: In function ‘int main(int, char**)’:
    tools/db_stress.cc:2365:12: error: ‘rocksdb::SyncPoint’ has not been declared
       rocksdb::SyncPoint::GetInstance()->SetCallBack(
                ^
    tools/db_stress.cc:2370:12: error: ‘rocksdb::SyncPoint’ has not been declared
       rocksdb::SyncPoint::GetInstance()->SetCallBack(
                ^
    tools/db_stress.cc:2375:12: error: ‘rocksdb::SyncPoint’ has not been declared
       rocksdb::SyncPoint::GetInstance()->EnableProcessing();
                ^
    make[1]: *** [tools/db_stress.o] Error 1
    make[1]: Leaving directory `/data/sandcastle/boxes/trunk-git-rocksdb-public'
    make: *** [release] Error 2
    ```
    Closes https://github.com/facebook/rocksdb/pull/2355
    
    Differential Revision: D5113552
    
    Pulled By: sagar0
    
    fbshipit-source-id: 351df707277787da5633ba4a40e52edc7c895dc4

--- tools/db_stress.cc
+++ tools/db_stress.cc
@@ -2360,8 +2360,8 @@ int main(int argc, char** argv) {
   SetUsageMessage(std::string("\nUSAGE:\n") + std::string(argv[0]) +
                   " [OPTIONS]...");
   ParseCommandLineFlags(&argc, &argv, true);
-#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) &&  \
-  !defined(OS_AIX)
+#if !defined(NDEBUG) && !defined(OS_MACOSX) && !defined(OS_WIN) && \
+  !defined(OS_SOLARIS) && !defined(OS_AIX)
   rocksdb::SyncPoint::GetInstance()->SetCallBack(
     "NewWritableFile:O_DIRECT", [&](void* arg) {
       int* val = static_cast<int*>(arg);
