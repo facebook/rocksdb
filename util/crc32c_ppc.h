172f4ba3e3ab4aa21161402e144aea6a41a0083a
Kamalalochana Subbaiah <kamasubb@in.ibm.com>
2017-06-05 16:50:10 +0000

    CRC32 Power Optimization Changes Support for PowerPC Architecture Detecting AltiVec Support

---
+++ util/crc32c_ppc.h
@@ -0,0 +1,23 @@
+//  Copyright (c) 2017 International Business Machines Corp.
+//  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
+//  This source code is also licensed under the GPLv2 license found in the
+//  COPYING file in the root directory of this source tree.
+
+#ifndef CRC32C_PPC_H
+#define CRC32C_PPC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern uint32_t crc32c_ppc(uint32_t crc, unsigned char const *buffer,
+                           unsigned len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
