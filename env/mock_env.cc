73ab9f4cddcd214466a406da2aca57ebf63c0b7a
Maysam Yabandeh <myabandeh@fb.com>
2017-06-06 12:43:41 -0700

    fix unused problem

--- env/mock_env.cc
+++ env/mock_env.cc
@@ -379,8 +379,7 @@ class TestMemLogger : public Logger {
       gettimeofday(&now_tv, nullptr);
       const time_t seconds = now_tv.tv_sec;
       struct tm t;
-      auto ret = localtime_r(&seconds, &t);
-      assert(ret != nullptr);
+      assert(localtime_r(&seconds, &t));
       p += snprintf(p, limit - p,
                     "%04d/%02d/%02d-%02d:%02d:%02d.%06d ",
                     t.tm_year + 1900,
