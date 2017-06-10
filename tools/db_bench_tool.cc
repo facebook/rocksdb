e9aebaf158f5d91f5aad7dd4361b6c78c093fe82
Andrew Kryczka <andrewkr@fb.com>
2017-06-09 19:55:39 -0700

    db_bench and db_stress support

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -785,6 +785,9 @@ DEFINE_int32(rate_limit_delay_max_milliseconds, 1000,
 
 DEFINE_uint64(rate_limiter_bytes_per_sec, 0, "Set options.rate_limiter value.");
 
+DEFINE_bool(rate_limit_reads, false,
+            "Use options.rate_limiter on compaction reads");
+
 DEFINE_uint64(
     benchmark_write_rate_limit, 0,
     "If non-zero, db_bench will rate-limit the writes going into RocksDB. This "
@@ -3133,8 +3136,16 @@ void VerifyDBFromDB(std::string& truth_db_name) {
       options.enable_thread_tracking = true;
     }
     if (FLAGS_rate_limiter_bytes_per_sec > 0) {
-      options.rate_limiter.reset(
-          NewGenericRateLimiter(FLAGS_rate_limiter_bytes_per_sec));
+      if (FLAGS_rate_limit_reads && !FLAGS_new_table_reader_for_compaction_inputs) {
+        fprintf(stderr, "rate limit compaction reads must have "
+                        "new_table_reader_for_compaction_inputs set\n");
+        exit(1);
+      }
+      options.rate_limiter.reset(NewGenericRateLimiter(
+          FLAGS_rate_limiter_bytes_per_sec, 100 * 1000 /* refill_period_us */,
+          10 /* fairness */,
+          FLAGS_rate_limit_reads ? RateLimiter::Mode::kReadsOnly
+                                 : RateLimiter::Mode::kWritesOnly));
     }
 
 #ifndef ROCKSDB_LITE
