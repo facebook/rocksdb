a87140a1c50b776f51c05cac8101b69e3a91876a
Sagar Vemuri <svemuri@fb.com>
2017-06-26 16:30:35 -0700

    Support FIFO-Compaction-with-TTL only with unlimited open files.
    Support FIFO-compaction-with-TTL only when max_open_files=-1, as the
    creation_time embedded in every file's table properties need to be
    consulted to figure out the files that need to be deleted. table_reader
    embedded in a FileDescriptor could potentially get deleted if
    max_open_files is not set to -1.
    
    We could initialize the table_reader again to get the table properties
    again, but it would involve a performance penalty due to reading new
    blocks from disk. We could potentially support it in the future, but may
    be not in the first version.

--- db/db_impl_open.cc
+++ db/db_impl_open.cc
@@ -165,11 +165,18 @@ static Status ValidateOptions(
             "universal and level compaction styles. ");
       }
     }
-    if (cfd.options.compaction_options_fifo.ttl > 0 &&
-        cfd.options.table_factory->Name() != BlockBasedTableFactory().Name()) {
-      return Status::NotSupported(
-          "FIFO Compaction with TTL is only supported in "
-          "Block-Based Table format. ");
+    if (cfd.options.compaction_options_fifo.ttl > 0) {
+      if (db_options.max_open_files != -1) {
+        return Status::NotSupported(
+            "FIFO Compaction with TTL is only supported when files are always "
+            "kept open (set max_open_files = -1). ");
+      }
+      if (cfd.options.table_factory->Name() !=
+          BlockBasedTableFactory().Name()) {
+        return Status::NotSupported(
+            "FIFO Compaction with TTL is only supported in "
+            "Block-Based Table format. ");
+      }
     }
   }
 
