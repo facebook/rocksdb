8f5b2628ac2830d8bafe902d8d8d7c1050e0a93f
Zhenyun Zhuang <zhenyungmail@users.noreply.github.com>
2017-06-30 14:45:27 -0700

    adding class align

--- port/win/port_win.h
+++ port/win/port_win.h
@@ -245,7 +245,8 @@ extern void InitOnce(OnceType* once, void (*initializer)());
 #if defined(__clang__) || defined(__GNUC__)
 # define CACHE_ALIGNED(size) __attribute__((__aligned__(size)))
 #elif defined(_MSC_VER)
-# define CACHE_ALIGNED(size) __declspec(align(size))
+# define CACHE_ALIGNED(size) __declspec(align(size))nQiu12A4!@
+
 #else
 # define CACHE_ALIGNED(size)
 #endif
