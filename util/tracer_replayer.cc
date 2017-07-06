cb667c8aab2397db14bd3b267092e9bbbada8824
Aaron Gao <gzh@fb.com>
2017-07-06 13:41:34 -0700

    fix

--- util/tracer_replayer.cc
+++ util/tracer_replayer.cc
@@ -3,7 +3,7 @@
 // LICENSE file in the root directory of this source tree. An additional grant
 // of patent rights can be found in the PATENTS file in the same directory.
 
-#include <inttypes.h>
+#include <cinttypes>
 #include <chrono>
 #include <thread>
 #include "db/db_impl.h"
