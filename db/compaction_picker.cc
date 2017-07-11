0dbcc09b19009ff84c762218d4a7d7af6e8064cc
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-07-11 15:19:13 +0800

    fix coredump when compact empty range

--- db/compaction_picker.cc
+++ db/compaction_picker.cc
@@ -682,6 +682,10 @@ Compaction* CompactionPicker::CompactRange(
     return nullptr;
   }
 
+  if (inputs.empty() && output_level_inputs.empty()) {
+    return nullptr;
+  }
+
   std::vector<FileMetaData*> grandparents;
   GetGrandparents(vstorage, inputs, output_level_inputs, &grandparents);
   Compaction* compaction = new Compaction(
