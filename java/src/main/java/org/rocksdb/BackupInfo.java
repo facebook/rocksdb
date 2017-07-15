6189230202e76241dae4cfcefb5e372b87924de4
Siying Dong <siying.d@fb.com>
2017-07-15 16:03:42 -0700

    Change RocksDB License
    Summary: Closes https://github.com/facebook/rocksdb/pull/2589
    
    Differential Revision: D5431502
    
    Pulled By: siying
    
    fbshipit-source-id: 8ebf8c87883daa9daa54b2303d11ce01ab1f6f75

--- java/src/main/java/org/rocksdb/BackupInfo.java
+++ java/src/main/java/org/rocksdb/BackupInfo.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
-// This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree. An additional grant
-// of patent rights can be found in the PATENTS file in the same directory.
+//  This source code is licensed under both the GPLv2 (found in the
+//  COPYING file in the root directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in the root directory).
 package org.rocksdb;
 
 /**
