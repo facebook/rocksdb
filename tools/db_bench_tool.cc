b441963afdb82a5799bb20e24191eb84759837f5
sdong <siying.d@fb.com>
2017-07-28 16:05:56 -0700

    One more static_cast_with_check<>

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -58,6 +58,7 @@
 #include "rocksdb/utilities/transaction.h"
 #include "rocksdb/utilities/transaction_db.h"
 #include "rocksdb/write_batch.h"
+#include "util/cast_util.h"
 #include "util/compression.h"
 #include "util/crc32c.h"
 #include "util/mutexlock.h"
@@ -2552,7 +2553,9 @@ void VerifyDBFromDB(std::string& truth_db_name) {
     }
     if (FLAGS_simcache_size >= 0) {
       fprintf(stdout, "SIMULATOR CACHE STATISTICS:\n%s\n",
-              static_cast<SimCache*>(cache_.get())->ToString().c_str());
+              static_cast_with_check<SimCache, Cache>(cache_.get())
+                  ->ToString()
+                  .c_str());
     }
   }
 
