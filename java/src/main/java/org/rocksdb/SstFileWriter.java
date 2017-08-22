78cb6b611295ab7e5d1adc9b1707355ef879bc63
Chen Shen <scv119@users.noreply.github.com>
2017-08-22 12:46:29 -0700

    Provide byte[] version of SstFileWriter.merge to reduce GC Stall
    Summary:
    In Java API, `SstFileWriter.put/merge/delete` takes `Slice` type of key and value, which is a Java wrapper object around C++ Slice object.  The Slice object inherited [ `finalize`](https://github.com/facebook/rocksdb/blob/3c327ac2d0fd50bbd82fe1f1af5de909dad769e6/java/src/main/java/org/rocksdb/AbstractNativeReference.java#L69) method, which [added huge overhead](https://softwareengineering.stackexchange.com/questions/288715/is-overriding-object-finalize-really-bad/288753#288753) to JVM while creating new SstFile.
    
    To address this issue, this PR overload the merge method to take Java byte array instead of the Slice object, and added unit test for it.
    
    We also benchmark these two different merge function, where we could see GC Stall reduced from 50%  to 1%, and the throughput increased from 50MB to 200MB.
    Closes https://github.com/facebook/rocksdb/pull/2746
    
    Reviewed By: sagar0
    
    Differential Revision: D5653145
    
    Pulled By: scv119
    
    fbshipit-source-id: b55ea58554b573d0b1c6f6170f8d9223811bc4f5

--- java/src/main/java/org/rocksdb/SstFileWriter.java
+++ java/src/main/java/org/rocksdb/SstFileWriter.java
@@ -117,6 +117,20 @@ public class SstFileWriter extends RocksObject {
     put(nativeHandle_, key.getNativeHandle(), value.getNativeHandle());
   }
 
+ /**
+   * Add a Put key with value to currently opened file.
+   *
+   * @param key the specified key to be inserted.
+   * @param value the value associated with the specified key.
+   *
+   * @throws RocksDBException thrown if error happens in underlying
+   *    native library.
+   */
+public void put(final byte[] key, final byte[] value)
+    throws RocksDBException {
+  put(nativeHandle_, key, value);
+}
+
   /**
    * Add a Merge key with value to currently opened file.
    *
@@ -130,6 +144,21 @@ public class SstFileWriter extends RocksObject {
   public void merge(final Slice key, final Slice value)
       throws RocksDBException {
     merge(nativeHandle_, key.getNativeHandle(), value.getNativeHandle());
+  }
+
+  /**
+   * Add a Merge key with value to currently opened file.
+   *
+   * @param key the specified key to be merged.
+   * @param value the value to be merged with the current value for
+   * the specified key.
+   *
+   * @throws RocksDBException thrown if error happens in underlying
+   *    native library.
+   */
+  public void merge(final byte[] key, final byte[] value)
+      throws RocksDBException {
+    merge(nativeHandle_, key, value);
   }
 
   /**
@@ -172,6 +201,18 @@ public class SstFileWriter extends RocksObject {
   }
 
   /**
+   * Add a deletion key to currently opened file.
+   *
+   * @param key the specified key to be deleted.
+   *
+   * @throws RocksDBException thrown if error happens in underlying
+   *    native library.
+   */
+  public void delete(final byte[] key) throws RocksDBException {
+    delete(nativeHandle_, key);
+  }
+
+  /**
    * Finish the process and close the sst file.
    *
    * @throws RocksDBException thrown if error happens in underlying
@@ -193,11 +234,20 @@ public class SstFileWriter extends RocksObject {
 
   private native void put(final long handle, final long keyHandle,
       final long valueHandle) throws RocksDBException;
+      
+  private native void put(final long handle, final byte[] key,
+      final byte[] value) throws RocksDBException;
 
   private native void merge(final long handle, final long keyHandle,
       final long valueHandle) throws RocksDBException;
 
+  private native void merge(final long handle, final byte[] key,
+      final byte[] value) throws RocksDBException;
+
   private native void delete(final long handle, final long keyHandle)
+      throws RocksDBException;
+
+  private native void delete(final long handle, final byte[] key)
       throws RocksDBException;
 
   private native void finish(final long handle) throws RocksDBException;
