fb0bee8741cc3bd1da093820f7a8c26482905dda
Yi Wu <yiwu@fb.com>
2017-08-24 14:59:14 -0700

    fix lint

--- db/version_builder.cc
+++ db/version_builder.cc
@@ -292,12 +292,12 @@ class VersionBuilder::Rep {
       if (level < num_levels_) {
         FileMetaData* f = new FileMetaData(new_file.second);
         f->refs = 1;
-        
+
         assert(levels_[level].added_files.find(f->fd.GetNumber()) ==
                levels_[level].added_files.end());
         levels_[level].deleted_files.erase(f->fd.GetNumber());
         levels_[level].added_files[f->fd.GetNumber()] = f;
-      } else { 
+      } else {
         uint64_t number = new_file.second.fd.GetNumber();
         if (invalid_levels_[level].count(number) == 0) {
           invalid_levels_[level].insert(number);
