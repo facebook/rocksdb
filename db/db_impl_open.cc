1dc6c6222bab3a8e9e10126765bffa5f76101442
Zhongyi Xie <xiez@fb.com>
2017-09-14 10:23:01 -0700

    Add test kPointInTimeRecoveryCFConsistency

--- db/db_impl_open.cc
+++ db/db_impl_open.cc
@@ -493,6 +493,9 @@ Status DBImpl::RecoverLogFiles(const std::vector<uint64_t>& log_numbers,
   bool stop_replay_by_wal_filter = false;
   bool stop_replay_for_corruption = false;
   bool flushed = false;
+  // record the maximum sequence number seen among all log files
+  // (excluding skipped content)
+  SequenceNumber max_sequence_all_WALs(0);
   for (auto log_number : log_numbers) {
     // The previous incarnation may not have written any MANIFEST
     // records after allocating this log number.  So we manually
@@ -572,6 +575,8 @@ Status DBImpl::RecoverLogFiles(const std::vector<uint64_t>& log_numbers,
       }
       WriteBatchInternal::SetContents(&batch, record);
       SequenceNumber sequence = WriteBatchInternal::Sequence(&batch);
+      if (sequence > max_sequence_all_WALs)
+        max_sequence_all_WALs = sequence;
 
       if (immutable_db_options_.wal_recovery_mode ==
           WALRecoveryMode::kPointInTimeRecovery) {
@@ -739,6 +744,25 @@ Status DBImpl::RecoverLogFiles(const std::vector<uint64_t>& log_numbers,
         (versions_->LastSequence() <= last_sequence)) {
       versions_->SetLastToBeWrittenSequence(last_sequence);
       versions_->SetLastSequence(last_sequence);
+    }
+  }
+  // Compare the largest sequence number in all SST files with
+  // the largest sequence number seen in all WALs. Abort Open() if SN from
+  // SST is larger than SN from WAL. This could happen when WAL is corrupted
+  // and some (but not all) CFs are flushed
+  for (auto cfd : *versions_->GetColumnFamilySet()) {
+    auto* vstorage = cfd->current()->storage_info();
+    if (vstorage->num_levels() <= 0 || vstorage->NumLevelFiles(0) <= 0)
+      continue;
+    SequenceNumber latest_sequence_number_SST =
+        vstorage->LevelFiles(0)[0]->largest_seqno;
+    if (latest_sequence_number_SST > max_sequence_all_WALs) {
+      ROCKS_LOG_FATAL(immutable_db_options_.info_log,
+                    "SST file is ahead of WAL:"
+                    " max sequence of all WALs: #%" PRIu64
+                    " SequenceNumber in SST: #%" PRIu64,
+                    max_sequence_all_WALs, latest_sequence_number_SST);
+      return Status::Corruption("SST file is ahead of WALs");
     }
   }
 
