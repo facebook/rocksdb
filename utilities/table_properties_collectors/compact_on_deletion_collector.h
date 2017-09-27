346426e65686d53b70e2747da0550e8a3402f303
Andrew Kryczka <andrewkr@fb.com>
2017-09-26 17:56:18 -0700

    fix deletion-triggered compaction in table builder

--- utilities/table_properties_collectors/compact_on_deletion_collector.h
+++ utilities/table_properties_collectors/compact_on_deletion_collector.h
@@ -61,7 +61,7 @@ class CompactOnDeletionCollector : public TablePropertiesCollector {
   // @params properties  User will add their collected statistics to
   // `properties`.
   virtual Status Finish(UserCollectedProperties* properties) override {
-    Reset();
+    finished_ = true;
     return Status::OK();
   }
 
@@ -98,6 +98,7 @@ class CompactOnDeletionCollector : public TablePropertiesCollector {
   size_t deletion_trigger_;
   // true if the current SST file needs to be compacted.
   bool need_compaction_;
+  bool finished_;
 };
 }  // namespace rocksdb
 #endif  // !ROCKSDB_LITE
