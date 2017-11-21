e3fe478d9b022274367f130da00074df9c35cfdb
Sagar Vemuri <svemuri@fb.com>
2017-11-20 16:25:35 -0800

    [Blob DB] Update logs.
    Saw some redundant log lines when trying to benchmark blob db. So,
    removed the lines from blob_file.cc, and let the lines in
    blob_db_impl.cc take the lead.

--- utilities/blob_db/blob_file.cc
+++ utilities/blob_db/blob_file.cc
@@ -134,9 +134,6 @@ bool BlobFile::NeedsFsync(bool hard, uint64_t bytes_per_sync) const {
 }
 
 Status BlobFile::WriteFooterAndCloseLocked() {
-  ROCKS_LOG_INFO(parent_->db_options_.info_log,
-                 "File is being closed after footer %s", PathName().c_str());
-
   BlobLogFooter footer;
   footer.blob_count = blob_count_;
   if (HasTTL()) {
@@ -152,7 +149,7 @@ Status BlobFile::WriteFooterAndCloseLocked() {
     file_size_ += BlobLogFooter::kSize;
   } else {
     ROCKS_LOG_ERROR(parent_->db_options_.info_log,
-                    "Failure to read Header for blob-file %s",
+                    "Failure to append footer to blob-file %s",
                     PathName().c_str());
   }
   // delete the sequential writer
