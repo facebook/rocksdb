ded34d44de26b0acd36176e13a82a41275165357
Ben Clay <bclay@twitter.com>
2018-01-08 10:24:16 -0500

    Cosmetic cleanup

--- java/rocksjni/compaction_filter_jnicallback.h
+++ java/rocksjni/compaction_filter_jnicallback.h
@@ -1,4 +1,10 @@
-// Stub compaction filter that hands off to Java
+//  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under both the GPLv2 (found in the
+//  COPYING file in the root directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in the root directory).
+//
+// This file implements the callback "bridge" between Java and C++ for
+// rocksdb::CompactionFilter.
 
 #ifndef JAVA_ROCKSJNI_COMPACTION_FILTER_JNICALLBACK_H_
 #define JAVA_ROCKSJNI_COMPACTION_FILTER_JNICALLBACK_H_
