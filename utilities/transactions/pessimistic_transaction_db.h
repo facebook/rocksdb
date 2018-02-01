e5ba032ef7a5b23888ada2a7c28054e3234f28b1
Maysam Yabandeh <myabandeh@fb.com>
2018-02-01 15:59:10 -0800

    add missing override

--- utilities/transactions/pessimistic_transaction_db.h
+++ utilities/transactions/pessimistic_transaction_db.h
@@ -68,7 +68,7 @@ class PessimisticTransactionDB : public TransactionDB {
     return Write(opts, updates, false);
   }
   virtual Status Write(const WriteOptions& opts, WriteBatch* updates,
-                       bool skip_cc);
+                       bool skip_cc) override;
 
   using StackableDB::CreateColumnFamily;
   virtual Status CreateColumnFamily(const ColumnFamilyOptions& options,
