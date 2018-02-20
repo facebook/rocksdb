18421f39a3965f08e8efc030b9ed4538c928951d
Sagar Vemuri <svemuri@fb.com>
2018-02-20 12:07:55 -0800

    Add "rocksdb.iterator.internal-key" property

--- include/rocksdb/iterator.h
+++ include/rocksdb/iterator.h
@@ -97,6 +97,8 @@ class Iterator : public Cleanable {
   // Property "rocksdb.iterator.super-version-number":
   //   LSM version used by the iterator. The same format as DB Property
   //   kCurrentSuperVersionNumber. See its comment for more information.
+  // Property "rocksdb.iterator.internal-key":
+  //   Get the internal key (e.g. tombstone) at which the iteration stopped.
   virtual Status GetProperty(std::string prop_name, std::string* prop);
 
  private:
