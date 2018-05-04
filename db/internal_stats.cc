171f415b306c65d515d13272e134a7a7647d0e7a
Maysam Yabandeh <myabandeh@fb.com>
2018-05-04 15:14:20 -0700

    Rename vars to satisfy unity built
    Summary:
    Tested by "make unity_test"
    Closes https://github.com/facebook/rocksdb/pull/3807
    
    Differential Revision: D7882657
    
    Pulled By: maysamyabandeh
    
    fbshipit-source-id: 84862c18d7f2fc762bd96ad070eaeb6936e45159

--- db/internal_stats.cc
+++ db/internal_stats.cc
@@ -230,8 +230,8 @@ static const std::string num_live_versions = "num-live-versions";
 static const std::string current_version_number =
     "current-super-version-number";
 static const std::string estimate_live_data_size = "estimate-live-data-size";
-static const std::string min_log_number_to_keep = "min-log-number-to-keep";
-static const std::string base_level = "base-level";
+static const std::string min_log_number_to_keep_str = "min-log-number-to-keep";
+static const std::string base_level_str = "base-level";
 static const std::string total_sst_files_size = "total-sst-files-size";
 static const std::string live_sst_files_size = "live-sst-files-size";
 static const std::string estimate_pending_comp_bytes =
@@ -308,12 +308,12 @@ const std::string DB::Properties::kCurrentSuperVersionNumber =
 const std::string DB::Properties::kEstimateLiveDataSize =
     rocksdb_prefix + estimate_live_data_size;
 const std::string DB::Properties::kMinLogNumberToKeep =
-    rocksdb_prefix + min_log_number_to_keep;
+    rocksdb_prefix + min_log_number_to_keep_str;
 const std::string DB::Properties::kTotalSstFilesSize =
     rocksdb_prefix + total_sst_files_size;
 const std::string DB::Properties::kLiveSstFilesSize =
     rocksdb_prefix + live_sst_files_size;
-const std::string DB::Properties::kBaseLevel = rocksdb_prefix + base_level;
+const std::string DB::Properties::kBaseLevel = rocksdb_prefix + base_level_str;
 const std::string DB::Properties::kEstimatePendingCompactionBytes =
     rocksdb_prefix + estimate_pending_comp_bytes;
 const std::string DB::Properties::kAggregatedTableProperties =
