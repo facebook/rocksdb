816e71d6e278741f3708ce517ffe44ee4e38d5fd
Yanqin Jin <yanqin@fb.com>
2018-06-20 22:26:32 -0700

    Eliminate warnings.

--- util/file_reader_writer.h
+++ util/file_reader_writer.h
@@ -31,9 +31,9 @@ class SequentialFileReader {
 
  public:
   explicit SequentialFileReader(std::unique_ptr<SequentialFile>&& _file,
-                                const std::string _file_name)
+                                const std::string& _file_name)
       : file_(std::move(_file)),
-        file_name_(std::move(_file_name)),
+        file_name_(_file_name),
         offset_(0) {}
 
   SequentialFileReader(SequentialFileReader&& o) ROCKSDB_NOEXCEPT {
