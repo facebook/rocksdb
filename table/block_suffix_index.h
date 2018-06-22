845439bbedcd25a771409e21112eb4b73d0fdb54
Fenggang Wu <fwu@fb.com>
2018-06-22 11:14:20 -0700

    Standalone BlockSuffixIndex implementation with unit test.
    Summary:
    The first step of the BlockSuffixIndex implementation. An string
    based hash table is implemented and unit-tested.
    
    - BlockSuffixIndexBuilder: Add() takes pairs of
      <key, restart_offset>, and formats it into a string when Finish()
      is called.
    - BlockSuffixIndex: initialized by the formatted string, and can
      interpret it as a hash table. Supporting Seek().
    
    Test Plan:
    - Unit test: block_suffix_index_test
    - make check -j 32
    - make asan-check
    
    Reviewers:
    Sagar Vemuri

--- table/block_suffix_index.h
+++ table/block_suffix_index.h
@@ -6,35 +6,53 @@
 #ifndef BLOCK_SUFFIX_INDEX_H
 #define BLOCK_SUFFIX_INDEX_H
 
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 
 namespace rocksdb {
 
+// The format of the suffix hash map is as follows:
+//
+// B B B ... B IDX NUM_BUCK MAP_SIZE
+//
+// NUM_BUCK: Number of buckets, which is the length of the IDX array.
+//
+// IDX:      Array of offsets, each pointing to the starting offset (relative to
+//           MAP_START) of one hash bucket.
+//
+// B:        Bucket, an array consisting of a list of restart interval offsets.
+//           We do not have to store the length of individual buckets, as they
+//           are delimited by the next bucket offset.
+//
+// MAP_SIZE: the size of the hash map.
+//
+// The suffix hash map is construct right in-place of the block without any data
+// been copied.
+
 class BlockSuffixIndexBuilder {
  public:
-
-  BlockSuffixIndexBuilder(uint16_t n): num_buckets_(n), bucket_(n) { }
-
-  void Add(const Slice& suffix, const uint16_t& pos);
-
-  void Finish(std::string&);
+  BlockSuffixIndexBuilder(uint32_t n) : num_buckets_(n), buckets_(n) {}
+  void Add(const Slice &suffix, const uint32_t &pos);
+  std::string Finish();
 
  private:
-  uint16_t num_buckets_;
-  std::vector<std::vector<uint16_t>> bucket_;
+  uint32_t num_buckets_;
+  std::vector<std::vector<uint32_t>> buckets_;
 };
 
 class BlockSuffixIndex {
  public:
-
-  BlockSuffixIndex(Slice* s): suffix_index_(s) {}
-
-  bool Seek(const Slice& target, uint16_t* pos) const;
+  BlockSuffixIndex(std::string &s);
+  bool Seek(const Slice &key, std::vector<uint32_t> &bucket) const;
 
  private:
-  Slice* suffix_index_;
-  uint16_t num_buckets_;
+  std::string suffix_index_;
+  const char *data_;
+  uint32_t size_;
+  uint32_t num_buckets_;
+  uint32_t map_size_;
+  const char *map_start_;
+  const char *bucket_table_;
 };
 
 }  // namespace rocksdb
--- table/block_suffix_index_test.cc
+++ table/block_suffix_index_test.cc
@@ -3,6 +3,10 @@
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
 
+#include <cstdlib>
+#include <string>
+#include <unordered_map>
+
 #include "rocksdb/slice.h"
 #include "table/block_suffix_index.h"
 #include "util/testharness.h"
@@ -10,21 +14,95 @@
 
 namespace rocksdb {
 
+bool SearchForOffset(BlockSuffixIndex& index, Slice& key,
+                     uint32_t& restart_point) {
+  std::vector<uint32_t> bucket;
+  index.Seek(key, bucket);
+  for (auto& e : bucket) {
+    if (e == restart_point) {
+      return true;
+    }
+  }
+  return false;
+}
 
 TEST(BlockTest, BlockSuffixTest) {
-  //TODO(fwu)
-  BlockSuffixIndexBuilder builder(128);
+  // bucket_num = 200, #keys = 100. 50% utilization
+  BlockSuffixIndexBuilder builder(200);
 
-  std::string s = "key";
-  Slice k(s);
-  uint16_t off = 7;
-  builder.Add(k, off);
-  ASSERT_TRUE(true);
+  for (uint32_t i = 0; i < 100; i++) {
+    Slice key("key" + std::to_string(i));
+    uint32_t restart_point = i;
+    builder.Add(key, restart_point);
+  }
+
+  std::string suffix_index_content = builder.Finish();
+
+  BlockSuffixIndex index(suffix_index_content);
+
+  for (uint32_t i = 0; i < 100; i++) {
+    Slice key("key" + std::to_string(i));
+    uint32_t restart_point = i;
+    ASSERT_TRUE(SearchForOffset(index, key, restart_point));
+  }
+}
+
+TEST(BlockTest, BlockSuffixTestCollision) {
+  // bucket_num = 2. There will be intense hash collisions
+  BlockSuffixIndexBuilder builder(2);
+
+  for (uint32_t i = 0; i < 100; i++) {
+    Slice key("key" + std::to_string(i));
+    uint32_t restart_point = i;
+    builder.Add(key, restart_point);
+  }
+
+  std::string suffix_index_content = builder.Finish();
+
+  BlockSuffixIndex index(suffix_index_content);
+
+  for (uint32_t i = 0; i < 100; i++) {
+    Slice key("key" + std::to_string(i));
+    uint32_t restart_point = i;
+    ASSERT_TRUE(SearchForOffset(index, key, restart_point));
+  }
+}
+
+TEST(BlockTest, BlockSuffixTestLarge) {
+  BlockSuffixIndexBuilder builder(1000);
+  std::unordered_map<std::string, uint32_t> m;
+
+  for (uint32_t i = 0; i < 10000000; i++) {
+    if (std::rand() % 2) continue;  // randomly leave half of the keys out
+    std::string key_str = "key" + std::to_string(i);
+    Slice key(key_str);
+    uint32_t restart_point = i;
+    builder.Add(key, restart_point);
+    m[key_str] = restart_point;
+  }
+
+  std::string suffix_index_content = builder.Finish();
+
+  BlockSuffixIndex index(suffix_index_content);
+
+  for (uint32_t i = 0; i < 100; i++) {
+    std::string key_str = "key" + std::to_string(i);
+    Slice key(key_str);
+    uint32_t restart_point = i;
+    if (m.count(key_str)) {
+      ASSERT_TRUE(m[key_str] == restart_point);
+      ASSERT_TRUE(SearchForOffset(index, key, restart_point));
+    } else {
+      // we allow false positve, so don't test the nonexisting keys.
+      // when false positive happens, the search will continue to the
+      // restart intervals to see if the key really exist.
+    }
+  }
 }
 
 }  // namespace rocksdb
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
