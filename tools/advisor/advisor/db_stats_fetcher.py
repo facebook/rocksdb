0fb2bc599547ada2311f916776c9b31fcc313be0
Pooja Malik <poojamalik@fb.com>
2018-06-25 17:36:57 -0700

    updated TimeSeriesData, OdsStatsFetcher, added DBBenchStatsParser

--- tools/advisor/advisor/db_stats_fetcher.py
+++ tools/advisor/advisor/db_stats_fetcher.py
@@ -1,16 +1,81 @@
-from advisor.db_timeseries_parser import TimeSeriesData
+from advisor.db_log_parser import Log
+from advisor.db_timeseries_parser import TimeSeriesData, NO_ENTITY
+import glob
+import re
 import subprocess
+import time
 
 
-class OdsTimeSeriesData(TimeSeriesData):
+class DBBenchStatsParser(TimeSeriesData):
+    STATS = 'STATISTICS:'
+
+    @staticmethod
+    def parse_log_line_for_stats(log_line):
+        token_list = log_line.strip().split()
+        stat_prefix = token_list[0] + '.'
+        stat_values = [
+            token
+            for token in token_list[1:]
+            if token != ':'
+        ]
+        stat_dict = {}
+        for ix, metric in enumerate(stat_values):
+            if ix % 2 == 0:
+                stat_name = stat_prefix + metric
+            else:
+                stat_dict[stat_name] = float(metric)
+        return stat_dict
+
+    def __init__(self, logs_path_prefix):
+        super().__init__()
+        self.logs_file_prefix = logs_path_prefix
+        self.stats_freq_sec = 60
+        self.duration_sec = 60
+
+    def get_keys_from_conditions(self, conditions):
+        reqd_stats = []
+        for cond in conditions:
+            reqd_stats.extend(cond.keys)
+        return reqd_stats
+
+    def add_to_timeseries(self, log):
+        # type: (Log, List[str]) -> Dict[int, float]
+        new_lines = log.get_message().split('\n')
+        log_ts = log.get_timestamp()
+        # the first line in the log does not contain any statistics
+        for line in new_lines[1:]:
+            stats_on_line = self.parse_log_line_for_stats(line)
+            for stat in stats_on_line:
+                if stat in self.keys_ts:
+                    self.keys_ts[stat][log_ts] = stats_on_line[stat]
+
+    def fetch_timeseries(self, statistics):
+        self.keys_ts = {NO_ENTITY: {}}
+        self.keys_ts[NO_ENTITY] = {stat: {} for stat in statistics}
+        for file_name in glob.glob(self.logs_path_prefix + '*'):
+            with open(file_name, 'r') as db_logs:
+                new_log = None
+                for line in db_logs:
+                    if Log.is_new_log(line):
+                        if (
+                            new_log and
+                            re.search(self.STATS, new_log.get_message())
+                        ):
+                            self.add_to_timeseries(new_log)
+                        new_log = Log(line, self.column_families)
+                    else:
+                        # To account for logs split into multiple lines
+                        new_log.append_message(line)
+            # Check for the last log in the file.
+            if new_log and re.search(self.STATS, new_log.get_message()):
+                self.add_to_timeseries(new_log)
+
+
+class OdsStatsFetcher(TimeSeriesData):
     # class constants
     OUTPUT_FILE = 'temp/stats_out.tmp'
     ERROR_FILE = 'temp/stats_err.tmp'
-    COMMAND = (
-        "%s --entity=%s --key=%s --tstart=%s --tend=%s" +
-        " --transform=%s --showtime"
-    )
-    RATE_TRANSFORM_DESC = "rate(%,15m,duration=60)"
+    COMMAND = "%s --entity=%s --key=%s --tstart=%s --tend=%s --showtime"
 
     # static methods
     @staticmethod
@@ -26,28 +91,15 @@ class OdsTimeSeriesData(TimeSeriesData):
         second = float(pair[1].strip())
         return [first, second]
 
-    def __init__(self, client, entities, start_time, end_time, key_prefix):
-        super().__init__(start_time, end_time)
+    def __init__(self, client, entities, key_prefix=None):
+        super().__init__()
         self.client = client
         self.entities = entities
         self.key_prefix = key_prefix
-
-    def attach_prefix_to_keys(self, keys):
-        client_keys = None
-        if isinstance(keys, str):
-            if keys.startswith('[]'):
-                client_keys = self.key_prefix + keys[2:]
-            else:
-                client_keys = keys
-        else:
-            client_keys = []
-            for key in keys:
-                if key.startswith('[]'):
-                    new_key = self.key_prefix + key[2:]
-                    client_keys.append(new_key)
-                else:
-                    client_keys.append(key)
-        return client_keys
+        self.stats_freq_sec = 60
+        self.duration_sec = 60
+        self.end_time = int(time.time())
+        self.start_time = self.end_time - (3 * 60 * 60)
 
     def execute_script(self, command):
         print('executing...')
@@ -58,109 +110,72 @@ class OdsTimeSeriesData(TimeSeriesData):
         out_file.close()
         err_file.close()
 
-    def fetch_rate_url(self, entities, keys, display_type):
-        transform_desc = self.RATE_TRANSFORM_DESC
-        keys = self.attach_prefix_to_keys(keys)
-        if not isinstance(keys, str):
-            keys = ','.join(keys)
-        entities = ','.join(entities)
+    def attach_prefix_to_keys(self, keys):
+        complete_keys = []
+        for key in keys:
+            if key.startswith('[]'):
+                if not self.key_prefix:
+                    raise ValueError('OdsStatsFetcher: need key prefix')
+                complete_keys.append(self.key_prefix + key[2:])
+            else:
+                complete_keys.append(key)
+        return complete_keys
 
-        command = self.COMMAND + " --url=%s"
+    def fetch_timeseries(self, statistics):
+        command = self.COMMAND % (
+            self.client,
+            self._get_string_in_quotes(self.entities),
+            self._get_string_in_quotes(','.join(statistics)),
+            self._get_string_in_quotes(self.start_time),
+            self._get_string_in_quotes(self.end_time)
+        )
+        self.execute_script(command)
+        # Parse ODS output and populate the 'keys_ts' map
+        with open(self.OUTPUT_FILE, 'r') as fp:
+            for line in fp:
+                token_list = line.strip().split('\t')
+                entity = token_list[0]
+                key = token_list[1]
+                if entity not in self.keys_ts:
+                    self.keys_ts[entity] = {}
+                if key not in self.keys_ts[entity]:
+                    self.keys_ts[entity][key] = {}
+                list_of_lists = [
+                    self._get_time_value_pair(pair_string)
+                    for pair_string in token_list[2].split('],')
+                ]
+                value = {pair[0]: pair[1] for pair in list_of_lists}
+                self.keys_ts[entity][key] = value
+
+    def get_keys_from_conditions(self, conditions):
+        reqd_stats = []
+        for cond in conditions:
+            reqd_stats.extend(self.attach_prefix_to_keys(cond.keys))
+        return reqd_stats
+
+    def fetch_rate_url(self, entities, keys, window_len, display, percent):
+        # type: (List[str], List[str], str, str, bool) -> str
+        transform_desc = (
+            "rate(" + window_len + ",duration=" + str(self.duration_sec)
+        )
+        if percent:
+            transform_desc = transform_desc + ",%)"
+        else:
+            transform_desc = transform_desc + ")"
+        keys = self.attach_prefix_to_keys(keys)
+
+        command = self.COMMAND + " --transform=%s --url=%s"
         command = command % (
             self.client,
-            self._get_string_in_quotes(entities),
-            self._get_string_in_quotes(keys),
+            self._get_string_in_quotes(','.join(entities)),
+            self._get_string_in_quotes(','.join(keys)),
             self._get_string_in_quotes(self.start_time),
             self._get_string_in_quotes(self.end_time),
             self._get_string_in_quotes(transform_desc),
-            self._get_string_in_quotes(display_type)
+            self._get_string_in_quotes(display)
         )
         self.execute_script(command)
         url = ""
         with open(self.OUTPUT_FILE, 'r') as fp:
             url = fp.readline()
         return url
-
-    def fetch_burst_epochs(self, key, threshold_lower):
-        transform_desc = (
-            self.RATE_TRANSFORM_DESC + ',filter(' + str(threshold_lower) + ',)'
-        )
-        command = self.COMMAND % (
-            self.client,
-            self._get_string_in_quotes(self.entities),
-            self._get_string_in_quotes(key),
-            self._get_string_in_quotes(self.start_time),
-            self._get_string_in_quotes(self.end_time),
-            self._get_string_in_quotes(transform_desc)
-        )
-        self.execute_script(command)
-        # Parsing ODS output
-        values_dict = {}
-        with open(self.OUTPUT_FILE, 'r') as fp:
-            for line in fp:
-                token_list = line.strip().split('\t')
-                entity = token_list[0]
-                # key = token_list[1]
-                if entity not in values_dict:
-                    values_dict[entity] = {}
-                list_of_lists = [
-                    self._get_time_value_pair(pair_string)
-                    for pair_string in token_list[2].split('],')
-                ]
-                value = {pair[0]: pair[1] for pair in list_of_lists}
-                values_dict[entity] = value
-        return values_dict
-
-    def fetch_aggregated_values(self, keys, aggregation_operator):
-        transform_desc = aggregation_operator.name
-        command = self.COMMAND % (
-            self.client,
-            self._get_string_in_quotes(self.entities),
-            self._get_string_in_quotes(','.join(keys)),
-            self._get_string_in_quotes(self.start_time),
-            self._get_string_in_quotes(self.end_time),
-            self._get_string_in_quotes(transform_desc)
-        )
-        self.execute_script(command)
-        # Parsing ODS output
-        values_dict = {}
-        with open(self.OUTPUT_FILE, 'r') as fp:
-            for line in fp:
-                token_list = line.strip().split('\t')
-                entity = token_list[0]
-                key = token_list[1]
-                if entity not in values_dict:
-                    values_dict[entity] = {}
-                pair = self._get_time_value_pair(token_list[2])
-                value = pair[1]
-                values_dict[entity][key] = value
-        return values_dict
-
-    def check_and_trigger_conditions(self, conditions):
-        for cond in conditions:
-            complete_keys = cond.keys
-            if self.key_prefix:
-                complete_keys = self.attach_prefix_to_keys(cond.keys)
-            if cond.behavior is self.Behavior.bursty:
-                result = (
-                    self.fetch_burst_epochs(complete_keys, cond.rate_threshold)
-                )
-                if result:
-                    print(result)
-                    cond.set_trigger(result)
-            elif cond.behavior is self.Behavior.evaluate_expression:
-                result = (
-                    self.fetch_aggregated_values(
-                        complete_keys, cond.aggregation_op
-                    )
-                )
-                entity_evaluation_dict = {}
-                for entity in result:
-                    keys = [result[entity][key] for key in complete_keys]
-                    try:
-                        if eval(cond.expression):
-                            entity_evaluation_dict[entity] = keys
-                    except Exception as e:
-                        print('OdsTimeSeriesData check_and_trigger: ' + str(e))
-                if entity_evaluation_dict:
-                    cond.set_trigger(entity_evaluation_dict)
--- tools/advisor/advisor/db_timeseries_parser.py
+++ tools/advisor/advisor/db_timeseries_parser.py
@@ -3,9 +3,13 @@
 #  COPYING file in the root directory) and Apache 2.0 License
 #  (found in the LICENSE.Apache file in the root directory).
 
-from abc import abstractmethod
-from enum import Enum
+from abs import abstractmethod
 from advisor.db_log_parser import DataSource
+from enum import Enum
+import math
+
+
+NO_ENTITY = 'ENTITY_PLACEHOLDER'
 
 
 class TimeSeriesData(DataSource):
@@ -20,17 +24,95 @@ class TimeSeriesData(DataSource):
         latest = 4
         oldest = 5
 
-    @abstractmethod
-    def fetch_burst_epochs(self):
-        # returns Dict[instance/entity, Dict[timestamp, value]]
-        pass
-
-    @abstractmethod
-    def fetch_aggregated_values(self):
-        # returns Dict[instance/entity, Dict[key, aggregated_value]]
-        pass
-
-    def __init__(self, start_time, end_time):
+    def __init__(self):
         super().__init__(DataSource.Type.TIME_SERIES)
-        self.start_time = start_time
-        self.end_time = end_time
+        self.keys_ts = None  # Dict[entity, Dict[key, Dict[timestamp, value]]]
+
+    @abstractmethod
+    def get_keys_from_conditions(self):
+        pass
+
+    @abstractmethod
+    def fetch_timeseries(self):
+        pass
+
+    def fetch_burst_epochs(
+        self, statistic, window_sec, threshold, percent=False
+    ):
+        # type: (str, int, float, bool) -> Dict[int, float]
+        if window_sec < self.stats_freq_sec:
+            window_sec = self.stats_freq_sec
+        window_samples = math.ceil(window_sec / self.stats_freq_sec)
+        burst_epochs = {}
+        for entity in self.keys_ts:
+            timestamps = sorted(self.keys_ts[entity][statistic].keys())
+            for ix, timestamp in enumerate(timestamps):
+                if (ix + window_samples) >= len(timestamps):
+                    break
+                first_ts = timestamp
+                last_ts = timestamps[ix + window_samples]
+                first_val = self.keys_ts[entity][statistic][first_ts]
+                last_val = self.keys_ts[entity][statistic][last_ts]
+                diff = last_val - first_val
+                if percent:
+                    diff = diff * 100 / first_val
+                rate = (diff * self.duration_sec) / (last_ts - first_ts)
+                if rate >= threshold:
+                    if entity not in burst_epochs:
+                        burst_epochs[entity] = {}
+                    burst_epochs[entity][first_ts] = rate
+        return burst_epochs
+
+    def fetch_aggregated_values(self, statistics, aggregation_op):
+        # type: (str, AggregationOperator) -> Dict[str, Dict[str, float]]
+        # returned object is Dict[entity, Dict[key, aggregated_value]]
+        result = {}
+        for et in self.keys_ts:
+            result[et] = {}
+            for stat in statistics:
+                agg_val = None
+                if aggregation_op is self.AggregationOperator.latest:
+                    latest_timestamp = max(list(self.keys_ts[et][stat].keys()))
+                    agg_val = self.keys_ts[stat][latest_timestamp]
+                elif aggregation_op is self.AggregationOperator.oldest:
+                    oldest_timestamp = min(list(self.keys_ts[et][stat].keys()))
+                    agg_val = self.keys_ts[stat][oldest_timestamp]
+                elif aggregation_op is self.AggregationOperator.max:
+                    agg_val = max(list(self.keys_ts[et][stat].values()))
+                elif aggregation_op is self.AggregationOperator.min:
+                    agg_val = min(list(self.keys_ts[et][stat].values()))
+                elif aggregation_op is self.AggregationOperator.avg:
+                    values = list(self.keys_ts[et][stat].values())
+                    agg_val = sum(values) / len(values)
+                result[et][stat] = agg_val
+        return result
+
+    def check_and_trigger_conditions(self, conditions):
+        # get the list of statistics that need to be fetched
+        reqd_keys = self.get_keys_from_conditions(conditions)
+        # fetch the required statistics and populate the map 'keys_ts'
+        self.fetch_timeseries(reqd_keys)
+        # Trigger the appropriate conditions
+        for cond in conditions:
+            complete_keys = self.get_keys_from_conditions([cond])
+            if cond.behavior is self.Behavior.bursty:
+                statistic = complete_keys[0]  # there should be only one key
+                result = self.fetch_burst_epochs(
+                        statistic, cond.window_sec, cond.rate_threshold, True
+                )
+                if result:
+                    cond.set_trigger(result)
+            elif cond.behavior is self.Behavior.evaluate_expression:
+                result = self.fetch_aggregated_values(
+                        complete_keys, cond.aggregation_op
+                )
+                entity_evaluation_dict = {}
+                for entity in result:
+                    keys = [result[entity][key] for key in complete_keys]
+                    try:
+                        if eval(cond.expression):
+                            entity_evaluation_dict[entity] = keys
+                    except Exception as e:
+                        print('TimeSeriesData check_and_trigger: ' + str(e))
+                if entity_evaluation_dict:
+                    cond.set_trigger(entity_evaluation_dict)
