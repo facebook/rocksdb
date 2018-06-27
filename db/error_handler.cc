5320e5d04fc5e528d2bdbfc317bfd77cf226c6aa
Anand Ananthabhotla <anand76@devvm1373.frc2.facebook.com>
2018-06-26 18:05:25 -0700

    Fix Infer errors
    Summary:
    1. Formatting
    2. Remove a dead store on db/error_handler.cc, line 121
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- db/error_handler.cc
+++ db/error_handler.cc
@@ -51,37 +51,71 @@ std::map<std::tuple<BackgroundErrorReason, Status::Code, Status::SubCode, bool>,
          Status::Severity::kFatalError},
 };
 
-std::map<std::tuple<BackgroundErrorReason, Status::Code, bool>, Status::Severity> DefaultErrorSeverityMap = {
-  // Errors during BG compaction
-  {std::make_tuple(BackgroundErrorReason::kCompaction, Status::Code::kCorruption, true), Status::Severity::kUnrecoverableError},
-  {std::make_tuple(BackgroundErrorReason::kCompaction, Status::Code::kCorruption, false), Status::Severity::kNoError},
-  {std::make_tuple(BackgroundErrorReason::kCompaction, Status::Code::kIOError, true), Status::Severity::kFatalError},
-  {std::make_tuple(BackgroundErrorReason::kCompaction, Status::Code::kIOError, false), Status::Severity::kNoError},
-  // Errors during BG flush
-  {std::make_tuple(BackgroundErrorReason::kFlush, Status::Code::kCorruption, true), Status::Severity::kUnrecoverableError},
-  {std::make_tuple(BackgroundErrorReason::kFlush, Status::Code::kCorruption, false), Status::Severity::kNoError},
-  {std::make_tuple(BackgroundErrorReason::kFlush, Status::Code::kIOError, true), Status::Severity::kFatalError},
-  {std::make_tuple(BackgroundErrorReason::kFlush, Status::Code::kIOError, false), Status::Severity::kNoError},
-  // Errors during Write
-  {std::make_tuple(BackgroundErrorReason::kWriteCallback, Status::Code::kCorruption, true), Status::Severity::kUnrecoverableError},
-  {std::make_tuple(BackgroundErrorReason::kWriteCallback, Status::Code::kCorruption, false), Status::Severity::kNoError},
-  {std::make_tuple(BackgroundErrorReason::kWriteCallback, Status::Code::kIOError, true), Status::Severity::kFatalError},
-  {std::make_tuple(BackgroundErrorReason::kWriteCallback, Status::Code::kIOError, false), Status::Severity::kNoError},
+std::map<std::tuple<BackgroundErrorReason, Status::Code, bool>, Status::Severity>
+    DefaultErrorSeverityMap = {
+        // Errors during BG compaction
+        {std::make_tuple(BackgroundErrorReason::kCompaction,
+                         Status::Code::kCorruption, true),
+         Status::Severity::kUnrecoverableError},
+        {std::make_tuple(BackgroundErrorReason::kCompaction,
+                         Status::Code::kCorruption, false),
+         Status::Severity::kNoError},
+        {std::make_tuple(BackgroundErrorReason::kCompaction,
+                         Status::Code::kIOError, true),
+         Status::Severity::kFatalError},
+        {std::make_tuple(BackgroundErrorReason::kCompaction,
+                         Status::Code::kIOError, false),
+         Status::Severity::kNoError},
+        // Errors during BG flush
+        {std::make_tuple(BackgroundErrorReason::kFlush,
+                         Status::Code::kCorruption, true),
+         Status::Severity::kUnrecoverableError},
+        {std::make_tuple(BackgroundErrorReason::kFlush,
+                         Status::Code::kCorruption, false),
+         Status::Severity::kNoError},
+        {std::make_tuple(BackgroundErrorReason::kFlush,
+                         Status::Code::kIOError, true),
+         Status::Severity::kFatalError},
+        {std::make_tuple(BackgroundErrorReason::kFlush,
+                         Status::Code::kIOError, false),
+         Status::Severity::kNoError},
+        // Errors during Write
+        {std::make_tuple(BackgroundErrorReason::kWriteCallback,
+                         Status::Code::kCorruption, true),
+         Status::Severity::kUnrecoverableError},
+        {std::make_tuple(BackgroundErrorReason::kWriteCallback,
+                         Status::Code::kCorruption, false),
+         Status::Severity::kNoError},
+        {std::make_tuple(BackgroundErrorReason::kWriteCallback,
+                         Status::Code::kIOError, true),
+         Status::Severity::kFatalError},
+        {std::make_tuple(BackgroundErrorReason::kWriteCallback,
+                         Status::Code::kIOError, false),
+         Status::Severity::kNoError},
 };
 
-std::map<std::tuple<BackgroundErrorReason, bool>, Status::Severity> DefaultReasonMap = {
-  // Errors during BG compaction
-  {std::make_tuple(BackgroundErrorReason::kCompaction, true), Status::Severity::kFatalError},
-  {std::make_tuple(BackgroundErrorReason::kCompaction, false), Status::Severity::kNoError},
-  // Errors during BG flush
-  {std::make_tuple(BackgroundErrorReason::kFlush, true), Status::Severity::kFatalError},
-  {std::make_tuple(BackgroundErrorReason::kFlush, false), Status::Severity::kNoError},
-  // Errors during Write
-  {std::make_tuple(BackgroundErrorReason::kWriteCallback, true), Status::Severity::kFatalError},
-  {std::make_tuple(BackgroundErrorReason::kWriteCallback, false), Status::Severity::kFatalError},
-  // Errors during Memtable update
-  {std::make_tuple(BackgroundErrorReason::kMemTable, true), Status::Severity::kFatalError},
-  {std::make_tuple(BackgroundErrorReason::kMemTable, false), Status::Severity::kFatalError},
+std::map<std::tuple<BackgroundErrorReason, bool>, Status::Severity>
+    DefaultReasonMap = {
+        // Errors during BG compaction
+        {std::make_tuple(BackgroundErrorReason::kCompaction, true),
+          Status::Severity::kFatalError},
+        {std::make_tuple(BackgroundErrorReason::kCompaction, false),
+          Status::Severity::kNoError},
+        // Errors during BG flush
+        {std::make_tuple(BackgroundErrorReason::kFlush, true),
+          Status::Severity::kFatalError},
+        {std::make_tuple(BackgroundErrorReason::kFlush, false),
+          Status::Severity::kNoError},
+        // Errors during Write
+        {std::make_tuple(BackgroundErrorReason::kWriteCallback, true),
+          Status::Severity::kFatalError},
+        {std::make_tuple(BackgroundErrorReason::kWriteCallback, false),
+          Status::Severity::kFatalError},
+        // Errors during Memtable update
+        {std::make_tuple(BackgroundErrorReason::kMemTable, true),
+          Status::Severity::kFatalError},
+        {std::make_tuple(BackgroundErrorReason::kMemTable, false),
+          Status::Severity::kFatalError},
 };
 
 Status ErrorHandler::SetBGError(const Status& bg_err, BackgroundErrorReason reason) {
@@ -118,7 +152,6 @@ Status ErrorHandler::SetBGError(const Status& bg_err, BackgroundErrorReason reas
     auto entry = DefaultReasonMap.find(std::make_tuple(reason, paranoid));
     if (entry != DefaultReasonMap.end()) {
       sev = entry->second;
-      found = true;
     }
   }
 
