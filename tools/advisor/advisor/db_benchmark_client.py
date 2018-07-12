3d6d22ef1dca6cdd8d11d0c582c41fc8456a2c70
Pooja Malik <poojamalik@fb.com>
2018-07-11 17:12:50 -0700

    Adding support in Optimizer for options not supported in options file
    Changes per file:
    config_optimizer_example- command-line args support for options that are
    not supported by options.ini file, but can be given to bench_runner
    db_benchmark_client- fixed the bugs in the location and name of the LOG
    files; support for using the misc_options as db_bench command-line args;
    some testing code
    db_options_parser- method for finding diff between 2 option_configs;
    support for misc_options; some testing code
    rules.ini- support for misc_options
    db_config_optimizer- per-method changes:
      * apply_action_on_value: handle cases when old_value is None
      * improve_db_config: modified to handle the case when a suggestion's
        option was not in the existing config
      * improve_db_config_v2: same as above
      * disambiguate_guidelines: handle the case of disambiguation
        when a guideline's option is not in the existing config
      * run_v2: shifted code for picking a rule and getting
        updated_config to new method: apply_suggestions()
      * apply_suggestions: new method to pick new rule, and get
        updated_config
      * get_backtrack_config: new method to get config to update
        options to so that the latest changes applied, are reversed
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- tools/advisor/advisor/db_benchmark_client.py
+++ tools/advisor/advisor/db_benchmark_client.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from advisor.db_log_parser import DatabaseLogs, NO_FAM
 from advisor.db_options_parser import DatabaseOptions
 from advisor.db_stats_fetcher import LogStatsParser, OdsStatsFetcher
+import os
 import re
 import subprocess
 
@@ -25,15 +26,27 @@ class BenchmarkRunner(ABC):
 class DBBenchRunner(BenchmarkRunner):
     OUTPUT_FILE = "temp/dbbench_out.tmp"
     ERROR_FILE = "temp/dbbench_err.tmp"
+    DB_PATH = "DB path"
+    THROUGHPUT = "ops/sec"
 
     @staticmethod
-    def get_info_log_file_name(log_dir):
-        file_name = log_dir[1:]
+    def get_info_log_file_name(db_path):
+        file_name = db_path[1:]
         to_be_replaced = re.compile('[^0-9a-zA-Z\-_\.]')
-        for character in to_be_replaced.findall(log_dir):
+        for character in to_be_replaced.findall(db_path):
             file_name = file_name.replace(character, '_')
-        file_name = file_name + '_LOG'
+        if not file_name.endswith('_'):
+            file_name += '_'
+        file_name += 'LOG'
         return file_name
+
+    @staticmethod
+    def get_opt_args_str(misc_options_dict):
+        optional_args_str = ""
+        for option_name, option_value in misc_options_dict.items():
+            if option_value:
+                optional_args_str += (" --" + option_name + "=" + option_value)
+        return optional_args_str
 
     def __init__(self, positional_args, ods_args=None):
         # parse positional_args list appropriately
@@ -42,24 +55,33 @@ class DBBenchRunner(BenchmarkRunner):
         self.ods_args = ods_args
         self.supported_benchmarks = None
 
-    def _get_log_options(self, db_options):
+    def _get_log_options(self, db_options, db_path):
         # get the location of the LOG file and the frequency at which stats are
         # dumped in the LOG file
         log_dir_path = None
         stats_freq_sec = None
-        # options to fetch
+        logs_file_prefix = None
+
+        # fetch the options
         dump_period = 'DBOptions.stats_dump_period_sec'
         log_dir = 'DBOptions.db_log_dir'
-        wal_dir = 'DBOptions.wal_dir'
-        log_options = db_options.get_options([dump_period, log_dir, wal_dir])
-
+        log_options = db_options.get_options([dump_period, log_dir])
         if dump_period in log_options:
             stats_freq_sec = int(log_options[dump_period][NO_FAM])
         if log_dir in log_options:
             log_dir_path = log_options[log_dir][NO_FAM]
-        if not log_dir_path and wal_dir in log_options:
-            log_dir_path = log_options[wal_dir][NO_FAM]
-        return (log_dir_path, stats_freq_sec)
+
+        if not log_dir_path:
+            log_dir_path = db_path
+            log_file_name = 'LOG'
+        else:
+            log_file_name = DBBenchRunner.get_info_log_file_name(db_path)
+
+        if not log_dir_path.endswith('/'):
+            log_dir_path += '/'
+        logs_file_prefix = log_dir_path + log_file_name
+
+        return (logs_file_prefix, stats_freq_sec)
 
     def _run_command(self, command):
         # run db_bench and return the
@@ -72,31 +94,37 @@ class DBBenchRunner(BenchmarkRunner):
 
     def run_experiment(self, db_options):
         # type: (List[str], str) -> str
-        # get the log options from the OPTIONS file
-        log_dir_path, stats_freq_sec = self._get_log_options(db_options)
         # generate an options configuration file
         options_file = db_options.generate_options_config(nonce='12345')
+        optional_args_str = DBBenchRunner.get_opt_args_str(
+            db_options.get_misc_options()
+        )
         command = "%s --options_file=%s --benchmarks=%s --statistics"
-        benchmark = 'readwhilewriting'
+        benchmark = 'readwhilewriting'  # 'fillrandom'
         command = command % (self.db_bench_binary, options_file, benchmark)
+        command += optional_args_str
         self._run_command(command)
 
-        # get the throughput of this db_bench experiment
+        # get the db_path and throughput of this db_bench experiment
         throughput = None
+        db_path = None
         with open(self.OUTPUT_FILE, 'r') as fp:
             for line in fp:
                 if line.startswith(benchmark):
                     print(line)  # print output of db_bench run
                     token_list = line.strip().split()
                     for ix, token in enumerate(token_list):
-                        if token.startswith('ops/sec'):
+                        if token.startswith(self.THROUGHPUT):
                             throughput = float(token_list[ix - 1])
                             break
-                    break
+                elif line.startswith(self.DB_PATH):
+                    db_path = line.split('[')[1].split(']')[0]
 
         # Create the LOGS object
-        file_name = DBBenchRunner.get_info_log_file_name(log_dir_path)
-        logs_file_prefix = log_dir_path + '/' + file_name
+        # get the log options from the OPTIONS file
+        logs_file_prefix, stats_freq_sec = self._get_log_options(
+            db_options, db_path
+        )
         db_logs = DatabaseLogs(
             logs_file_prefix, db_options.get_column_families()
         )
@@ -139,7 +167,9 @@ class DBBenchRunner(BenchmarkRunner):
 
 # TODO: remove this method, used only for testing
 def main():
-    pos_args = ['/home/poojamalik/workspace/rocksdb/db_bench']
+    pos_args = [
+        'TEST_TMPDIR=/dev/shm /home/poojamalik/workspace/rocksdb/db_bench'
+    ]
     db_bench_helper = DBBenchRunner(pos_args)
     # populate benchmarks with the available ones in the db_bench tool
     benchmarks = db_bench_helper.get_available_workloads()
@@ -147,16 +177,25 @@ def main():
     print()
     options_file = (
         '/home/poojamalik/workspace/rocksdb/tools/advisor/temp/' +
-        'OPTIONS_default.tmp'
+        'OPTIONS_12345.tmp'
     )
-    db_options = DatabaseOptions(options_file)
+    misc_options = ["rate_limiter_bytes_per_sec=1024000", "bloom_bits=2"]
+    db_options = DatabaseOptions(options_file, misc_options)
     data_sources, _ = db_bench_helper.run_experiment(db_options)
     print(data_sources[0].options_dict)
     print()
     print(data_sources[1].logs_path_prefix)
+    if os.path.isfile(data_sources[1].logs_path_prefix):
+        print('log file exists!')
+    else:
+        print('error: log file does not exist!')
     print(data_sources[1].column_families)
     print()
     print(data_sources[2].logs_file_prefix)
+    if os.path.isfile(data_sources[2].logs_file_prefix):
+        print('log file exists!')
+    else:
+        print('error: log file does not exist!')
     print(data_sources[2].stats_freq_sec)
 
 
--- tools/advisor/advisor/db_options_parser.py
+++ tools/advisor/advisor/db_options_parser.py
@@ -56,14 +56,59 @@ class OptionsSpecParser(IniParser):
 
 
 class DatabaseOptions(DataSource):
-    def __init__(self, rocksdb_options):
+
+    @staticmethod
+    def get_options_diff(opt_old, opt_new):
+        # type: Dict[option, Dict[col_fam, value]] X 2 ->
+        # Dict[option, Dict[col_fam, Tuple(old_value, new_value)]]
+        # note: diff should contain a tuple of values only if they are
+        # different from each other
+        options_union = set(opt_old.keys()).union(set(opt_new.keys()))
+        diff = {}
+        for opt in options_union:
+            diff[opt] = {}
+            # if option in options_union, then it must be in one of the configs
+            if opt not in opt_old:
+                for col_fam in opt_new[opt]:
+                    diff[opt][col_fam] = (None, opt_new[opt][col_fam])
+            elif opt not in opt_new:
+                for col_fam in opt_old[opt]:
+                    diff[opt][col_fam] = (opt_old[opt][col_fam], None)
+            else:
+                for col_fam in opt_old[opt]:
+                    if col_fam in opt_new[opt]:
+                        if opt_old[opt][col_fam] != opt_new[opt][col_fam]:
+                            diff[opt][col_fam] = (
+                                opt_old[opt][col_fam],
+                                opt_new[opt][col_fam]
+                            )
+                    else:
+                        diff[opt][col_fam] = (opt_old[opt][col_fam], None)
+                for col_fam in opt_new[opt]:
+                    if col_fam in opt_old[opt]:
+                        if opt_old[opt][col_fam] != opt_new[opt][col_fam]:
+                            diff[opt][col_fam] = (
+                                opt_old[opt][col_fam],
+                                opt_new[opt][col_fam]
+                            )
+                    else:
+                        diff[opt][col_fam] = (None, opt_new[opt][col_fam])
+            if not diff[opt]:
+                diff.pop(opt)
+        return diff
+
+    def __init__(self, rocksdb_options, misc_options):
         super().__init__(DataSource.Type.DB_OPTIONS)
         # The options are stored in the following data structure:
         # Dict[section_type, Dict[section_name, Dict[option_name, value]]]
         self.options_dict = None
         self.column_families = None
+        self.misc_options = None
         # Load the options from the given file to a dictionary.
         self.load_from_source(rocksdb_options)
+        # Setup the miscellaneous options expected to be List[str], where each
+        # element in the List has the format "<option_name>=<option_value>"
+        self.setup_misc_options(misc_options)
 
     def load_from_source(self, options_path):
         self.options_dict = {}
@@ -95,6 +140,17 @@ class DatabaseOptions(DataSource):
                     error = 'Not able to parse line in Options file.'
                     OptionsSpecParser.exit_with_parse_error(line, error)
 
+    def setup_misc_options(self, misc_options):
+        self.misc_options = {}
+        if misc_options:
+            for option_pair_str in misc_options:
+                option_name = option_pair_str.split('=')[0].strip()
+                option_value = option_pair_str.split('=')[1].strip()
+                self.misc_options[option_name] = option_value
+
+    def get_misc_options(self):
+        return self.misc_options
+
     def get_column_families(self):
         return self.column_families
 
@@ -103,37 +159,49 @@ class DatabaseOptions(DataSource):
         # List[option] -> Dict[option, Dict[col_fam, value]]
         reqd_options_dict = {}
         for option in reqd_options:
-            sec_type = '.'.join(option.split('.')[:-1])
-            opt_name = option.split('.')[-1]
-            if sec_type not in self.options_dict:
-                continue
-            for col_fam in self.options_dict[sec_type]:
-                if opt_name in self.options_dict[sec_type][col_fam]:
-                    if option not in reqd_options_dict:
-                        reqd_options_dict[option] = {}
-                    reqd_options_dict[option][col_fam] = (
-                        self.options_dict[sec_type][col_fam][opt_name]
-                    )
+            if '.' not in option:
+                if option not in self.misc_options:
+                    continue
+                if option not in reqd_options_dict:
+                    reqd_options_dict[option] = {}
+                reqd_options_dict[option][NO_FAM] = self.misc_options[option]
+            else:
+                sec_type = '.'.join(option.split('.')[:-1])
+                opt_name = option.split('.')[-1]
+                if sec_type not in self.options_dict:
+                    continue
+                for col_fam in self.options_dict[sec_type]:
+                    if opt_name in self.options_dict[sec_type][col_fam]:
+                        if option not in reqd_options_dict:
+                            reqd_options_dict[option] = {}
+                        reqd_options_dict[option][col_fam] = (
+                            self.options_dict[sec_type][col_fam][opt_name]
+                        )
         return reqd_options_dict
 
     def update_options(self, options):
-        # type: Dict[str, Dict[str, Any]] -> None
-        # Dict[option, Dict[col_fam, value]] -> None where option is in the
+        # type: Dict[str, Dict[str, Any]] -> None; more details below:
+        # Dict[option, Dict[col_fam, value]] -> None where option is of the
         # form: ('.' delimited section type) + '.' + option
         for option in options:
-            sec_name = '.'.join(option.split('.')[:-1])
-            opt_name = option.split('.')[-1]
-            if sec_name not in self.options_dict:
-                self.options_dict[sec_name] = {}
-            for col_fam in options[option]:
-                # if the option is not already present in the dictionary,
-                # it will be inserted, else it will be updated to the new
-                # value
-                if col_fam not in self.options_dict[sec_name]:
-                    self.options_dict[sec_name][col_fam] = {}
-                self.options_dict[sec_name][col_fam][opt_name] = (
-                    copy.deepcopy(options[option][col_fam])
-                )
+            if '.' not in option:
+                if NO_FAM not in options[option]:
+                    continue
+                self.misc_options[option] = options[option][NO_FAM]
+            else:
+                sec_name = '.'.join(option.split('.')[:-1])
+                opt_name = option.split('.')[-1]
+                if sec_name not in self.options_dict:
+                    self.options_dict[sec_name] = {}
+                for col_fam in options[option]:
+                    # if the option is not already present in the dictionary,
+                    # it will be inserted, else it will be updated to the new
+                    # value
+                    if col_fam not in self.options_dict[sec_name]:
+                        self.options_dict[sec_name][col_fam] = {}
+                    self.options_dict[sec_name][col_fam][opt_name] = (
+                        copy.deepcopy(options[option][col_fam])
+                    )
 
     def generate_options_config(self, nonce):
         # type: str -> str
@@ -212,23 +280,29 @@ class DatabaseOptions(DataSource):
 # TODO: remove these methods once the unit tests for this class are in place
 def main():
     options_file = 'temp/OPTIONS_default.tmp'
-    db_options = DatabaseOptions(options_file)
+    misc_options = ["misc_opt1=10", "misc_opt2=100", "misc_opt3=1000"]
+    db_options = DatabaseOptions(options_file, misc_options)
     print(db_options.get_column_families())
     get_op = db_options.get_options([
         'DBOptions.db_log_dir',
         'DBOptions.is_fd_close_on_exec',
         'CFOptions.memtable_prefix_bloom_size_ratio',
-        'TableOptions.BlockBasedTable.verify_compression'
+        'TableOptions.BlockBasedTable.verify_compression',
+        'misc_opt1',
+        'misc_opt3'
     ])
     print(get_op)
     get_op['DBOptions.db_log_dir'][NO_FAM] = 'some_random_path :)'
     get_op['CFOptions.memtable_prefix_bloom_size_ratio']['default'] = 2.31
     get_op['TableOptions.BlockBasedTable.verify_compression']['default'] = 4.4
+    get_op['misc_opt2'] = {}
+    get_op['misc_opt2'][NO_FAM] = 2
     db_options.update_options(get_op)
-    db_options.generate_options_config(nonce=123)
+    print('options updated in ' + db_options.generate_options_config(123))
+    print('misc options ' + repr(db_options.get_misc_options()))
 
     options_file = 'temp/OPTIONS_123.tmp'
-    db_options = DatabaseOptions(options_file)
+    db_options = DatabaseOptions(options_file, misc_options)
     # only CFOptions
     cond1 = Condition('opt-cond-1')
     cond1 = OptionCondition.create(cond1)
@@ -249,13 +323,16 @@ def main():
     cond2.set_parameter(
         'options', [
             'DBOptions.max_file_opening_threads',
-            'DBOptions.table_cache_numshardbits'
+            'DBOptions.table_cache_numshardbits',
+            'misc_opt2',
+            'misc_opt3'
         ]
     )
-    cond2.set_parameter(
-        'evaluate',
-        'int(options[0])-(4*int(options[1]))>=0'
+    cond2_expr = (
+        '(int(options[0])*int(options[2]))-' +
+        '((4*int(options[1])*int(options[3]))/10)==0'
     )
+    cond2.set_parameter('evaluate', cond2_expr)
     # mix of CFOptions and DBOptions
     cond3 = Condition('opt-cond-3')
     cond3 = OptionCondition.create(cond3)
@@ -263,13 +340,15 @@ def main():
         'options', [
             'DBOptions.max_background_jobs',  # 2
             'DBOptions.write_thread_slow_yield_usec',  # 3
-            'CFOptions.num_levels'  # 7
+            'CFOptions.num_levels',  # 7
+            'misc_opt1'  # 10
         ]
     )
-    cond3.set_parameter(
-        'evaluate',
-        'int(options[2])-(int(options[1])*int(options[0]))>0'
+    cond3_expr = (
+        '(int(options[3])*int(options[2]))-' +
+        '(int(options[1])*int(options[0]))==64'
     )
+    cond3.set_parameter('evaluate', cond3_expr)
 
     db_options.check_and_trigger_conditions([cond1, cond2, cond3])
     print(cond1.get_trigger())  # {'col-fam-B': ['4', '10', '10']}
