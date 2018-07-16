7db1aad71dac5f4e8474a599410b4f95ba657440
Fenggang Wu <fwu@fb.com>
2018-07-16 11:40:11 -0700

    DataBlockHashIndex: reduce index table size by change uint32 to uint16

--- table/data_block_hash_index.cc
+++ table/data_block_hash_index.cc
@@ -2,7 +2,6 @@
 //  This source code is licensed under both the GPLv2 (found in the
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -16,89 +15,89 @@ namespace rocksdb {
 const uint32_t kSeed = 2018;
 const uint32_t kSeed_tag = 214; /* second hash seed */
 
-inline uint32_t HashToBucket(const Slice& s, uint32_t num_buckets) {
-  return rocksdb::Hash(s.data(), s.size(), kSeed) % num_buckets;
+inline uint16_t HashToBucket(const Slice& s, uint16_t num_buckets) {
+  return (uint16_t)rocksdb::Hash(s.data(), s.size(), kSeed) % num_buckets;
 }
 
 void DataBlockHashIndexBuilder::Add(const Slice& key,
-                                  const uint32_t& restart_index) {
-  uint32_t idx = HashToBucket(key, num_buckets_);
+                                    const uint16_t& restart_index) {
+  uint16_t idx = HashToBucket(key, num_buckets_);
   /* push a TAG to avoid false postive */
   /* the TAG is the hash function value of another seed */
-  buckets_[idx].push_back(rocksdb::Hash(key.data(), key.size(), kSeed_tag));
+  uint16_t tag = rocksdb::Hash(key.data(), key.size(), kSeed_tag);
+  buckets_[idx].push_back(tag);
   buckets_[idx].push_back(restart_index);
-  estimate_ += 2 * sizeof(uint32_t);
+  estimate_ += 2 * sizeof(uint16_t);
 }
 
 void DataBlockHashIndexBuilder::Finish(std::string& buffer) {
+  // Because we use uint16_t address, we only support block less than 64KB
+  assert(buffer.size() < (1 << 16));
   // offset is the byte offset within the buffer
-  std::vector<uint32_t> bucket_offsets(num_buckets_, 0);
+  std::vector<uint16_t> bucket_offsets(num_buckets_, 0);
 
-  uint32_t map_start = static_cast<uint32_t>(buffer.size());
+  uint16_t map_start = static_cast<uint16_t>(buffer.size());
 
   // write each bucket to the string
-  for (uint32_t i = 0; i < num_buckets_; i++) {
+  for (uint16_t i = 0; i < num_buckets_; i++) {
     // remember the start offset of the buckets in bucket_offsets
-    bucket_offsets[i] = static_cast<uint32_t>(buffer.size());
-    for (uint32_t elem : buckets_[i]) {
+    bucket_offsets[i] = static_cast<uint16_t>(buffer.size());
+    for (uint16_t elem : buckets_[i]) {
       // the elem is alternative "TAG" and "offset"
-      PutFixed32(&buffer, elem);
+      PutFixed16(&buffer, elem);
     }
   }
 
   // write the bucket_offsets
-  for (uint32_t i = 0; i < num_buckets_; i++) {
-    PutFixed32(&buffer, bucket_offsets[i]);
+  for (uint16_t i = 0; i < num_buckets_; i++) {
+    PutFixed16(&buffer, bucket_offsets[i]);
   }
 
   // write NUM_BUCK
-  PutFixed32(&buffer, num_buckets_);
+  PutFixed16(&buffer, num_buckets_);
 
   // write MAP_START
-  PutFixed32(&buffer, map_start);
+  PutFixed16(&buffer, map_start);
 }
 
 void DataBlockHashIndexBuilder::Reset() {
 //  buckets_.clear();
-  std::fill(buckets_.begin(), buckets_.end(), std::vector<uint32_t>());
-  estimate_ = 0;
+std::fill(buckets_.begin(), buckets_.end(), std::vector<uint16_t>());
+estimate_ = 0;
 }
 
 DataBlockHashIndex::DataBlockHashIndex(Slice block_content) {
   assert(block_content.size() >=
-         2 * sizeof(uint32_t));  // NUM_BUCK and MAP_START
+         2 * sizeof(uint16_t));  // NUM_BUCK and MAP_START
 
   data_ = block_content.data();
-  size_ = static_cast<uint32_t>(block_content.size());
+  size_ = static_cast<uint16_t>(block_content.size());
 
-  map_start_ = data_ + DecodeFixed32(data_ + size_ - sizeof(uint32_t));
+  map_start_ = data_ + DecodeFixed16(data_ + size_ - sizeof(uint16_t));
   assert(map_start_ < data_ + size_);
 
-  num_buckets_ = DecodeFixed32(data_ + size_ - 2 * sizeof(uint32_t));
+  num_buckets_ = DecodeFixed16(data_ + size_ - 2 * sizeof(uint16_t));
   assert(num_buckets_ > 0);
 
-  assert(size_ >= sizeof(uint32_t) * (2 + num_buckets_));
-  bucket_table_ = data_ + size_ - sizeof(uint32_t) * (2 + num_buckets_);
+  assert(size_ >= sizeof(uint16_t) * (2 + num_buckets_));
+  bucket_table_ = data_ + size_ - sizeof(uint16_t) * (2 + num_buckets_);
 
   assert(map_start_ <  bucket_table_);
 }
 
 DataBlockHashIndexIterator* DataBlockHashIndex::NewIterator(
     const Slice& key) const {
-  uint32_t idx = HashToBucket(key, num_buckets_);
-  uint32_t bucket_off = DecodeFixed32(bucket_table_ + idx * sizeof(uint32_t));
+  uint16_t idx = HashToBucket(key, num_buckets_);
+  uint16_t bucket_off = DecodeFixed16(bucket_table_ + idx * sizeof(uint16_t));
   const char* limit;
   if (idx < num_buckets_ - 1) {
     // limited by the start offset of the next bucket
-    limit = data_ +
-            DecodeFixed32(bucket_table_ + (idx + 1) * sizeof(uint32_t));
+    limit = data_ + DecodeFixed16(bucket_table_ + (idx + 1) * sizeof(uint16_t));
   } else {
     // limited by the location of the NUM_BUCK
-    limit = data_ + (size_ - 2 * sizeof(uint32_t));
+    limit = data_ + (size_ - 2 * sizeof(uint16_t));
   }
-
-  uint32_t tag = rocksdb::Hash(key.data(), key.size(), kSeed_tag);
-
+  uint16_t tag = (uint16_t)rocksdb::Hash(key.data(), key.size(), kSeed_tag);
   return new DataBlockHashIndexIterator(data_ + bucket_off, limit, tag);
 }
 
@@ -107,17 +106,18 @@ bool DataBlockHashIndexIterator::Valid() {
 }
 
 void DataBlockHashIndexIterator::Next() {
-  for (current_ += 2 * sizeof(uint32_t); current_ < end_;
-       current_ += 2 * sizeof(uint32_t)) {
+  for (current_ += 2 * sizeof(uint16_t); current_ < end_;
+       current_ += 2 * sizeof(uint16_t)) {
     // stop at a offset that match the tag, i.e. a possible match
-    if (DecodeFixed32(current_) == tag_) {
+    uint16_t tag_found = DecodeFixed16(current_);
+    if (tag_found == tag_) {
       break;
     }
   }
 }
 
-uint32_t DataBlockHashIndexIterator::Value() {
-  return DecodeFixed32(current_+ sizeof(uint32_t));
+uint16_t DataBlockHashIndexIterator::Value() {
+  return DecodeFixed16(current_ + sizeof(uint16_t));
 }
 
 }  // namespace rocksdb
--- table/data_block_hash_index.h
+++ table/data_block_hash_index.h
@@ -34,18 +34,19 @@ namespace rocksdb {
 
 class DataBlockHashIndexBuilder {
  public:
-  DataBlockHashIndexBuilder(uint32_t n) :
-      num_buckets_(n),
-      buckets_(n),
-      estimate_((n + 2) * sizeof(uint32_t) /* n buckets, 2 num at the end */){}
-  void Add(const Slice &key, const uint32_t &restart_index);
+  DataBlockHashIndexBuilder(uint16_t n)
+      : num_buckets_(n),
+        buckets_(n),
+        estimate_((n + 2) *
+                  sizeof(uint16_t) /* n buckets, 2 num at the end */) {}
+  void Add(const Slice& key, const uint16_t& restart_index);
   void Finish(std::string& buffer);
   void Reset();
   inline size_t EstimateSize() { return estimate_; }
 
  private:
-  uint32_t num_buckets_;
-  std::vector<std::vector<uint32_t>> buckets_;
+  uint16_t num_buckets_;
+  std::vector<std::vector<uint16_t>> buckets_;
   size_t estimate_;
 };
 
@@ -55,16 +56,16 @@ class DataBlockHashIndex {
  public:
   DataBlockHashIndex(Slice  block_content);
 
-  inline uint32_t DataBlockHashMapStart() const {
-    return static_cast<uint32_t>(map_start_ - data_);
+  inline uint16_t DataBlockHashMapStart() const {
+    return static_cast<uint16_t>(map_start_ - data_);
   }
 
   DataBlockHashIndexIterator* NewIterator(const Slice& key) const;
 
  private:
   const char *data_;
-  uint32_t size_;
-  uint32_t num_buckets_;
+  uint16_t size_;
+  uint16_t num_buckets_;
   const char *map_start_;    // start of the map
   const char *bucket_table_; // start offset of the bucket index table
 };
@@ -72,18 +73,19 @@ class DataBlockHashIndex {
 class DataBlockHashIndexIterator {
  public:
   DataBlockHashIndexIterator(const char* start, const char* end,
-                           const uint32_t tag):
-      start_(start), end_(end), tag_(tag) {
-    current_ = start - 2 * sizeof(uint32_t);
+                             const uint16_t tag)
+      : start_(start), end_(end), tag_(tag) {
+    current_ = start - 2 * sizeof(uint16_t);
     Next();
   }
   bool Valid();
   void Next();
-  uint32_t Value();
+  uint16_t Value();
+
  private:
   const char* start_; // [start_,  end_) defines the bucket range
   const char* end_;
-  const uint32_t tag_; // the fingerprint (2nd hash value) of the searching key
+  const uint16_t tag_;  // the fingerprint (2nd hash value) of the searching key
   const char* current_;
 };
 
