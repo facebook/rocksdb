aadb77c7b0a2c4652c07dc4a1e322c4a493a14b6
Adam Retter <adam.retter@googlemail.com>
2018-07-23 15:12:17 +0530

    Use some parallelism in the building of crossbuilds

--- java/crossbuild/docker-build-linux-centos.sh
+++ java/crossbuild/docker-build-linux-centos.sh
@@ -9,10 +9,10 @@ cd /rocksdb-local
 # Use scl devtoolset if available (i.e. CentOS <7)
 if hash scl 2>/dev/null; then
 	scl enable devtoolset-2 'make jclean clean'
-	scl enable devtoolset-2 'PORTABLE=1 make rocksdbjavastatic'
+	scl enable devtoolset-2 'PORTABLE=1 make -j8 rocksdbjavastatic'
 else
 	make jclean clean
-        PORTABLE=1 make rocksdbjavastatic
+        PORTABLE=1 make -j8 rocksdbjavastatic
 fi
 
 cp java/target/librocksdbjni-linux*.so java/target/rocksdbjni-*-linux*.jar /rocksdb-host/java/target
