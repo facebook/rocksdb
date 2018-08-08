5d00727a298e65c84069d416035519026c58e0eb
Fenggang Wu <fwu@fb.com>
2018-08-07 23:18:30 -0700

    DataBlockHashIndex: move test from block_test to data_block_hash_index_test

--- table/data_block_hash_index_test.cc
+++ table/data_block_hash_index_test.cc
@@ -8,6 +8,8 @@
 #include <unordered_map>
 
 #include "rocksdb/slice.h"
+#include "table/block.h"
+#include "table/block_builder.h"
 #include "table/data_block_hash_index.h"
 #include "util/testharness.h"
 #include "util/testutil.h"
@@ -27,6 +29,47 @@ bool SearchForOffset(DataBlockHashIndex& index, const char* data,
   }
 
   return entry == restart_point;
+}
+
+// Random KV generator similer to block_test
+static std::string RandomString(Random* rnd, int len) {
+  std::string r;
+  test::RandomString(rnd, len, &r);
+  return r;
+}
+std::string GenerateKey(int primary_key, int secondary_key, int padding_size,
+                        Random *rnd) {
+  char buf[50];
+  char *p = &buf[0];
+  snprintf(buf, sizeof(buf), "%6d%4d", primary_key, secondary_key);
+  std::string k(p);
+  if (padding_size) {
+    k += RandomString(rnd, padding_size);
+  }
+
+  return k;
+}
+
+// Generate random key value pairs.
+// The generated key will be sorted. You can tune the parameters to generated
+// different kinds of test key/value pairs for different scenario.
+void GenerateRandomKVs(std::vector<std::string> *keys,
+                       std::vector<std::string> *values, const int from,
+                       const int len, const int step = 1,
+                       const int padding_size = 0,
+                       const int keys_share_prefix = 1) {
+  Random rnd(302);
+
+  // generate different prefix
+  for (int i = from; i < from + len; i += step) {
+    // generating keys that shares the prefix
+    for (int j = 0; j < keys_share_prefix; ++j) {
+      keys->emplace_back(GenerateKey(i, j, padding_size, &rnd));
+
+      // 100 bytes values
+      values->emplace_back(RandomString(&rnd, 100));
+    }
+  }
 }
 
 TEST(DataBlockHashIndex, DataBlockHashTestSmall) {
@@ -181,6 +224,80 @@ TEST(DataBlockHashIndex, DataBlockHashTestLarge) {
       // when false positive happens, the search will continue to the
       // restart intervals to see if the key really exist.
     }
+  }
+}
+
+TEST(DataBlockHashIndex, BlockTestLarge) {
+  Random rnd(1019);
+  Options options = Options();
+  std::unique_ptr<InternalKeyComparator> ic;
+  ic.reset(new test::PlainInternalKeyComparator(options.comparator));
+
+  std::vector<std::string> keys;
+  std::vector<std::string> values;
+
+  BlockBuilder builder(16 /* block_restart_interval */,
+                       true /* use_delta_encoding */,
+                       BlockBasedTableOptions::kDataBlockHashSearch);
+  int num_records = 500;
+
+  GenerateRandomKVs(&keys, &values, 0, num_records);
+
+  // Generate keys. Adding a trailing "1" to indicate existent keys.
+  // Later will Seeking for keys with a trailing "0" to test seeking
+  // non-existent keys.
+  for (int i = 0; i < num_records; i++) {
+    std::string ukey(keys[i] + "1" /* existing key marker */);
+    InternalKey ikey(ukey, 0, kTypeValue);
+    builder.Add(ikey.Encode().ToString(), values[i]);
+  }
+
+  // read serialized contents of the block
+  Slice rawblock = builder.Finish();
+
+  // create block reader
+  BlockContents contents;
+  contents.data = rawblock;
+  contents.cachable = false;
+  Block reader(std::move(contents), kDisableGlobalSequenceNumber);
+
+  // random seek existent keys
+  for (int i = 0; i < num_records; i++) {
+    auto iter = reader.NewIterator<DataBlockIter>(options.comparator,
+                                                  options.comparator);
+    // find a random key in the lookaside array
+    int index = rnd.Uniform(num_records);
+    std::string ukey(keys[index] + "1" /* existing key marker */);
+    InternalKey ikey(ukey, 0, kTypeValue);
+
+    // search in block for this key
+    bool hash_effective, found;
+    iter->SeekForGet(ikey.Encode().ToString(), &hash_effective, &found);
+    if (hash_effective) {
+      ASSERT_TRUE(found);
+      ASSERT_TRUE(iter->Valid());
+      Slice v = iter->value();
+      ASSERT_EQ(v.ToString().compare(values[index]), 0);
+    }
+    delete iter;
+  }
+
+  // random seek non-existent keys
+  for (int i = 0; i < num_records; i++) {
+    auto iter = reader.NewIterator<DataBlockIter>(options.comparator,
+                                                  options.comparator);
+    // find a random key in the lookaside array
+    int index = rnd.Uniform(num_records);
+    std::string ukey(keys[index] + "0" /* existing key marker */);
+    InternalKey ikey(ukey, 0, kTypeValue);
+
+    // search in block for this key
+    bool hash_effective, found;
+    iter->SeekForGet(ikey.Encode().ToString(), &hash_effective, &found);
+    if (hash_effective) {
+      ASSERT_FALSE(found);
+    }
+    delete iter;
   }
 }
 
