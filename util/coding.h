ce8f236c7345ca63c2819db15ff7eff0c4017328
Maysam Yabandeh <myabandeh@fb.com>
2018-08-09 15:06:26 -0700

    switch to zigzag format

--- util/coding.h
+++ util/coding.h
@@ -64,6 +64,13 @@ extern Slice GetLengthPrefixedSlice(const char* data);
 
 extern Slice GetSliceUntil(Slice* slice, char delimiter);
 
+// Borrowed from https://github.com/facebook/fbthrift/blob/449a5f77f9f9bae72c9eb5e78093247eef185c04/thrift/lib/cpp/util/VarintUtils-inl.h#L202-L208
+constexpr inline uint64_t i64ToZigzag(const int64_t l) {
+  return (static_cast<uint64_t>(l) << 1) ^ static_cast<uint64_t>(l >> 63);
+}
+inline int64_t zigzagToI64(uint64_t n) { return (n >> 1) ^ -(n & 1); }
+
+
 // Pointer-based variants of GetVarint...  These either store a value
 // in *v and return a pointer just past the parsed value, or return
 // nullptr on error.  These routines only look at bytes in the range
@@ -74,8 +81,7 @@ inline const char* GetVarsignedint64Ptr(const char* p, const char* limit,
                                         int64_t* value) {
   uint64_t u = 0;
   const char* ret = GetVarint64Ptr(p, limit, &u);
-  *value = u % 2 == 0 ? static_cast<int64_t>(u / 2)
-                      : static_cast<int64_t>(u / 2) * -1;
+  *value = zigzagToI64(u);
   return ret;
 }
 
@@ -264,10 +270,8 @@ inline void PutVarint64(std::string* dst, uint64_t v) {
 
 inline void PutVarsignedint64(std::string* dst, int64_t v) {
   char buf[kMaxVarint64Length];
-  // Shift the absolute number right for one bit. Then use the least significant
-  // bit to indicate the sign.
-  char* ptr = EncodeVarint64(buf, v >= 0 ? static_cast<uint64_t>(v) * 2 : 
-      static_cast<uint64_t>(-v) * 2 + 1;
+  // Using Zigzag format to convert signed to unsigned
+  char* ptr = EncodeVarint64(buf, i64ToZigzag(v));
   dst->append(buf, static_cast<size_t>(ptr - buf));
 }
 
