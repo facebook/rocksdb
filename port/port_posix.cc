3ec661c8df6d286f3e3c39be4db10617be67aabe
Mahesh Arumugam <maarumug@tetrationanalytics.com>
2018-08-12 08:44:38 -0700

    revert malloc change

--- port/port_posix.cc
+++ port/port_posix.cc
@@ -186,7 +186,15 @@ int GetMaxOpenFiles() {
 }
 
 void *cacheline_aligned_alloc(size_t size) {
+#if __GNUC__ < 5 && defined(__SANITIZE_ADDRESS__)
   return malloc(size);
+#elif ( _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 || defined(__APPLE__))
+  void *m;
+  errno = posix_memalign(&m, CACHE_LINE_SIZE, size);
+  return errno ? nullptr : m;
+#else
+  return malloc(size);
+#endif
 }
 
 void cacheline_aligned_free(void *memblock) {
