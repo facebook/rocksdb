ee234e83e33e05bd58ce45a093f6d67f0b9c9c8a
Andrew Kryczka <andrewkr@fb.com>
2018-08-23 18:22:55 -0700

    Invoke OnTableFileCreated for empty SSTs (#4307)
    Summary:
    The API comment on `OnTableFileCreationStarted` (https://github.com/facebook/rocksdb/blob/b6280d01f9f9c4305c536dfb804775fce3956280/include/rocksdb/listener.h#L331-L333) led users to believe a call to `OnTableFileCreationStarted` will always be matched with a call to `OnTableFileCreated`. However, we were skipping the `OnTableFileCreated` call in one case: no error happens but also no file is generated since there's no data.
    
    This PR adds the call to `OnTableFileCreated` for that case. The filename will be "(nil)" and the size will be zero.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/4307
    
    Differential Revision: D9485201
    
    Pulled By: ajkr
    
    fbshipit-source-id: 2f077ec7913f128487aae2624c69a50762394df6

--- db/compaction_job.cc
+++ db/compaction_job.cc
@@ -1308,9 +1308,7 @@ Status CompactionJob::FinishCompactionOutputFile(
     // VersionEdit.
     assert(!sub_compact->outputs.empty());
     sub_compact->outputs.pop_back();
-    sub_compact->builder.reset();
-    sub_compact->current_output_file_size = 0;
-    return s;
+    meta = nullptr;
   }
 
   if (s.ok() && (current_entries > 0 || tp.num_range_deletions > 0)) {
