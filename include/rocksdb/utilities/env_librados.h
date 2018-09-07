facebook/rocksdb

commit 64324e329eb0a9b4e77241a425a1615ff524c7f1

Support pragma once in all header files and cleanup some warnings (#4339)
Summary:
As you know, almost all compilers support "pragma once" keyword instead of using include guards. To be keep consistency between header files, all header files are edited.

Besides this, try to fix some warnings about loss of data.
Pull Request resolved: https://github.com/facebook/rocksdb/pull/4339

Differential Revision: D9654990

Pulled By: ajkr

fbshipit-source-id: c2cf3d2d03a599847684bed81378c401920ca848

include/rocksdb/utilities/env_librados.h
@@ -2,8 +2,8 @@
 //  This source code is licensed under both the GPLv2 (found in the
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
-#ifndef ROCKSDB_UTILITIES_ENV_LIBRADOS_H
-#define ROCKSDB_UTILITIES_ENV_LIBRADOS_H
+
+#pragma once
 
 #include <memory>
 #include <string>

commit 27b7d4744b186432c98cbe541e6cca969cb7c85c

env_librados.h: drop redundant #endif
without this change, rocksdb_env_librados_test fails to build.

it's a regression introduced by 64324e32

Signed-off-by: Kefu Chai <tchaikov@gmail.com>

include/rocksdb/utilities/env_librados.h
@@ -173,4 +173,3 @@ class EnvLibrados : public EnvWrapper {
   friend class LibradosWritableFile;
 };
 }
-#endif