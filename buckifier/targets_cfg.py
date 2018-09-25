ac1ea0e5dabac8f5245d7a19b510efe56cf64cfb
Yi Wu <yiwu@fb.com>
2018-09-24 22:23:29 -0700

    remove starting empty line in TARGETS

--- buckifier/targets_cfg.py
+++ buckifier/targets_cfg.py
@@ -2,8 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-rocksdb_target_header = """
-load("@fbcode_macros//build_defs:auto_headers.bzl", "AutoHeaders")
+rocksdb_target_header = """load("@fbcode_macros//build_defs:auto_headers.bzl", "AutoHeaders")
 
 REPO_PATH = package_name() + "/"
 
