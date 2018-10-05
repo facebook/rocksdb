897fe6a4a3e45032efdba08f1965379a52a8c2f9
Andrew Gallagher <andrewjcg@fb.com>
2018-10-04 17:05:37 -0700

    rocksdb: put `#pragma once` before `#ifdef`
    Summary: Work around upstream bug with modules: https://bugs.llvm.org/show_bug.cgi?id=39184.
    
    Reviewed By: yiwu-arbug
    
    Differential Revision: D10209569
    
    fbshipit-source-id: 696853a02a3869e9c33d0e61168ad4b0436fa3c0

--- include/rocksdb/sst_file_writer.h
+++ include/rocksdb/sst_file_writer.h
@@ -3,9 +3,9 @@
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
 
-#ifndef ROCKSDB_LITE
-
 #pragma once
+
+#ifndef ROCKSDB_LITE
 
 #include <memory>
 #include <string>
