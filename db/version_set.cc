c9afc7d5fb467923a79f13bd05a30cdcdc95e3ea
Anand Ananthabhotla <anand76@devvm1373.frc2.facebook.com>
2018-10-15 11:54:18 -0700

    Properly determine a truncated CompactRange stop key
    Summary:
    When a CompactRange() call for a level is truncated before the end key
    is reached, because it exceeds max_compaction_bytes, we need to properly
    set the compaction_end parameter to indicate the stop key. The next
    CompactRange will use that as the begin key. We set it to the smallest
    key of the next file in the level after expanding inputs to get a clean
    cut.
    
    Previously, we were setting it before expanding inputs. So we could end
    up recompacting some files. In a pathological case, where a single key
    has many entries spanning all the files in the level (possibly due to
    merge operands without a partial merge operator, thus resulting in
    compaction output identical to the input), this would result in
    an endless loop over the same set of files.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- db/version_set.cc
+++ db/version_set.cc
@@ -2046,7 +2046,7 @@ bool VersionStorageInfo::OverlapInLevel(int level,
 void VersionStorageInfo::GetOverlappingInputs(
     int level, const InternalKey* begin, const InternalKey* end,
     std::vector<FileMetaData*>* inputs, int hint_index, int* file_index,
-    bool expand_range) const {
+    bool expand_range, InternalKey** next_smallest) const {
   if (level >= num_non_empty_levels_) {
     // this level is empty, no overlapping inputs
     return;
@@ -2058,9 +2058,15 @@ void VersionStorageInfo::GetOverlappingInputs(
   }
   const Comparator* user_cmp = user_comparator_;
   if (level > 0) {
-    GetOverlappingInputsRangeBinarySearch(level, begin, end, inputs,
-                                          hint_index, file_index);
+    GetOverlappingInputsRangeBinarySearch(level, begin, end, inputs, hint_index,
+                                          file_index, false, next_smallest);
     return;
+  }
+
+  if (next_smallest) {
+    // next_smallest key only makes sense for non-level 0, where files are
+    // non-overlapping
+    *next_smallest = nullptr;
   }
 
   Slice user_begin, user_end;
@@ -2162,7 +2168,7 @@ void VersionStorageInfo::GetCleanInputsWithinInterval(
 void VersionStorageInfo::GetOverlappingInputsRangeBinarySearch(
     int level, const InternalKey* begin, const InternalKey* end,
     std::vector<FileMetaData*>* inputs, int hint_index, int* file_index,
-    bool within_interval) const {
+    bool within_interval, InternalKey** next_smallest) const {
   assert(level > 0);
   int min = 0;
   int mid = 0;
@@ -2198,6 +2204,9 @@ void VersionStorageInfo::GetOverlappingInputsRangeBinarySearch(
 
   // If there were no overlapping files, return immediately.
   if (!foundOverlap) {
+    if (next_smallest) {
+      next_smallest = nullptr;
+    }
     return;
   }
   // returns the index where an overlap is found
@@ -2217,6 +2226,15 @@ void VersionStorageInfo::GetOverlappingInputsRangeBinarySearch(
   // insert overlapping files into vector
   for (int i = start_index; i <= end_index; i++) {
     inputs->push_back(files_[level][i]);
+  }
+
+  if (next_smallest != nullptr) {
+    // Provide the next key outside the range covered by inputs
+    if (++end_index < static_cast<int>(files_[level].size())) {
+      **next_smallest = files_[level][end_index]->smallest;
+    } else {
+      *next_smallest = nullptr;
+    }
   }
 }
 
