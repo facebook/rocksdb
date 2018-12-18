50d9f0de492721603b1a4df49642ea955edb7089
Andrew Kryczka <andrewkr@fb.com>
2018-12-17 19:03:18 -0800

    Enable DeleteRange in simple crash tests

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -107,6 +107,8 @@ whitebox_default_params = {
 simple_default_params = {
     "allow_concurrent_memtable_write": lambda: random.randint(0, 1),
     "column_families": 1,
+    "delpercent": 4,
+    "delrangepercent": 1,
     "max_background_compactions": 1,
     "max_bytes_for_level_base": 67108864,
     "memtablerep": "skip_list",
