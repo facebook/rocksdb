55e03b67df735d17006d5a17134d98c42f215ed8
Sagar Vemuri <svemuri@fb.com>
2019-01-15 16:25:03 -0800

    Correct the comment about inlined blob option (#4887)
    Summary:
    - Corrected a comment asserting that the values "smaller" than a min_blob_size will be inlined in the base db.
    - Also fixed the type of ttl_range_secs while dumping blobdb options.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/4887
    
    Differential Revision: D13680163
    
    Pulled By: sagar0
    
    fbshipit-source-id: 306c8cf2daa52210ffc334a6924ef44ffdedf887

--- utilities/blob_db/blob_db.h
+++ utilities/blob_db/blob_db.h
@@ -52,7 +52,7 @@ struct BlobDBOptions {
   // and so on
   uint64_t ttl_range_secs = 3600;
 
-  // The smallest value to store in blob log. Value larger than this threshold
+  // The smallest value to store in blob log. Values smaller than this threshold
   // will be inlined in base DB together with the key.
   uint64_t min_blob_size = 0;
 
