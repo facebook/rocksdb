b4a075d3884fdbff57f2f3a37d371d6fc54975b5
Yanqin Jin <yanqin@fb.com>
2019-03-25 18:00:39 -0700

    Reproduce the infinite-wait bug

--- util/repeatable_thread.h
+++ util/repeatable_thread.h
@@ -87,7 +87,8 @@ class RepeatableThread {
         if (dynamic_cast<MockTimeEnv*>(env_) != nullptr) {
           // MockTimeEnv is used. Since it is not easy to mock TimedWait,
           // we wait without timeout to wait for TEST_WaitForRun to wake us up.
-          cond_var_.Wait();
+          //cond_var_.Wait();
+          cond_var_.TimedWait(wait_until);
         } else {
           cond_var_.TimedWait(wait_until);
         }
