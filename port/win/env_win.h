fb508672cf858668cf590d379e7b319a1940afb6
Burton Li <pul@microsoft.com>
2019-03-27 16:31:30 -0700

    Format env_win coding style by google c++ style guide (#5096)
    Summary:
    The existing code for env_win src and header file doesn't fully followed the recommended code style (https://google.github.io/styleguide/cppguide.html#Functions). Fix it for better readability.
    anand1976 siying
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/5096
    
    Differential Revision: D14585358
    
    Pulled By: anand1976
    
    fbshipit-source-id: 7ce35ffe9e922f5c1421b0bbaa5fce7abad57617

--- port/win/env_win.h
+++ port/win/env_win.h
@@ -47,8 +47,7 @@ public:
   WinEnvThreads& operator=(const WinEnvThreads&) = delete;
 
   void Schedule(void(*function)(void*), void* arg, Env::Priority pri,
-    void* tag,
-    void(*unschedFunction)(void* arg));
+                void* tag, void(*unschedFunction)(void* arg));
 
   int UnSchedule(void* arg, Env::Priority pri);
 
@@ -72,8 +71,8 @@ public:
 
 private:
 
-  Env*                     hosted_env_;
-  mutable std::mutex       mu_;
+  Env* hosted_env_;
+  mutable std::mutex mu_;
   std::vector<ThreadPoolImpl> thread_pools_;
   std::vector<WindowsThread> threads_to_join_;
 
@@ -94,18 +93,18 @@ public:
   virtual Status GetCurrentTime(int64_t* unix_time);
 
   virtual Status NewSequentialFile(const std::string& fname,
-    std::unique_ptr<SequentialFile>* result,
-    const EnvOptions& options);
+                                   std::unique_ptr<SequentialFile>* result,
+                                   const EnvOptions& options);
 
   // Helper for NewWritable and ReopenWritableFile
   virtual Status OpenWritableFile(const std::string& fname,
-    std::unique_ptr<WritableFile>* result,
-    const EnvOptions& options,
-    bool reopen);
+                                  std::unique_ptr<WritableFile>* result,
+                                  const EnvOptions& options,
+                                  bool reopen);
 
   virtual Status NewRandomAccessFile(const std::string& fname,
-    std::unique_ptr<RandomAccessFile>* result,
-    const EnvOptions& options);
+                                     std::unique_ptr<RandomAccessFile>* result,
+                                     const EnvOptions& options);
 
   // The returned file will only be accessed by one thread at a time.
   virtual Status NewRandomRWFile(const std::string& fname,
@@ -113,16 +112,16 @@ public:
                                  const EnvOptions& options);
 
   virtual Status NewMemoryMappedFileBuffer(
-    const std::string& fname,
-    std::unique_ptr<MemoryMappedFileBuffer>* result);
+      const std::string& fname,
+      std::unique_ptr<MemoryMappedFileBuffer>* result);
 
   virtual Status NewDirectory(const std::string& name,
-    std::unique_ptr<Directory>* result);
+                              std::unique_ptr<Directory>* result);
 
   virtual Status FileExists(const std::string& fname);
 
   virtual Status GetChildren(const std::string& dir,
-    std::vector<std::string>* result);
+                             std::vector<std::string>* result);
 
   virtual Status CreateDir(const std::string& name);
 
@@ -130,35 +129,31 @@ public:
 
   virtual Status DeleteDir(const std::string& name);
 
-  virtual Status GetFileSize(const std::string& fname,
-    uint64_t* size);
+  virtual Status GetFileSize(const std::string& fname, uint64_t* size);
 
   static uint64_t FileTimeToUnixTime(const FILETIME& ftTime);
 
   virtual Status GetFileModificationTime(const std::string& fname,
-    uint64_t* file_mtime);
+                                         uint64_t* file_mtime);
 
-  virtual Status RenameFile(const std::string& src,
-    const std::string& target);
+  virtual Status RenameFile(const std::string& src, const std::string& target);
 
-  virtual Status LinkFile(const std::string& src,
-    const std::string& target);
+  virtual Status LinkFile(const std::string& src, const std::string& target);
 
   virtual Status NumFileLinks(const std::string& /*fname*/,
                               uint64_t* /*count*/);
 
   virtual Status AreFilesSame(const std::string& first,
-    const std::string& second, bool* res);
+                              const std::string& second, bool* res);
 
-  virtual Status LockFile(const std::string& lockFname,
-    FileLock** lock);
+  virtual Status LockFile(const std::string& lockFname, FileLock** lock);
 
   virtual Status UnlockFile(FileLock* lock);
 
   virtual Status GetTestDirectory(std::string* result);
 
   virtual Status NewLogger(const std::string& fname,
-    std::shared_ptr<Logger>* result);
+                           std::shared_ptr<Logger>* result);
 
   virtual uint64_t NowMicros();
 
@@ -167,18 +162,18 @@ public:
   virtual Status GetHostName(char* name, uint64_t len);
 
   virtual Status GetAbsolutePath(const std::string& db_path,
-    std::string* output_path);
+                                 std::string* output_path);
 
   virtual std::string TimeToString(uint64_t secondsSince1970);
 
   virtual EnvOptions OptimizeForLogWrite(const EnvOptions& env_options,
-    const DBOptions& db_options) const;
+                                         const DBOptions& db_options) const;
 
   virtual EnvOptions OptimizeForManifestWrite(
-    const EnvOptions& env_options) const;
+      const EnvOptions& env_options) const;
 
   virtual EnvOptions OptimizeForManifestRead(
-    const EnvOptions& env_options) const;
+      const EnvOptions& env_options) const;
 
   size_t GetPageSize() const { return page_size_; }
 
@@ -194,11 +189,11 @@ private:
 
   typedef VOID(WINAPI * FnGetSystemTimePreciseAsFileTime)(LPFILETIME);
 
-  Env*            hosted_env_;
-  size_t          page_size_;
-  size_t          allocation_granularity_;
-  uint64_t        perf_counter_frequency_;
-  uint64_t        nano_seconds_per_period_;
+  Env* hosted_env_;
+  size_t page_size_;
+  size_t allocation_granularity_;
+  uint64_t perf_counter_frequency_;
+  uint64_t nano_seconds_per_period_;
   FnGetSystemTimePreciseAsFileTime GetSystemTimePreciseAsFileTime_;
 };
 
@@ -215,12 +210,12 @@ public:
   Status GetCurrentTime(int64_t* unix_time) override;
 
   Status NewSequentialFile(const std::string& fname,
-    std::unique_ptr<SequentialFile>* result,
-    const EnvOptions& options) override;
+                           std::unique_ptr<SequentialFile>* result,
+                           const EnvOptions& options) override;
 
   Status NewRandomAccessFile(const std::string& fname,
-    std::unique_ptr<RandomAccessFile>* result,
-    const EnvOptions& options) override;
+                             std::unique_ptr<RandomAccessFile>* result,
+                             const EnvOptions& options) override;
 
   Status NewWritableFile(const std::string& fname,
                          std::unique_ptr<WritableFile>* result,
@@ -234,25 +229,25 @@ public:
   //
   // The returned file will only be accessed by one thread at a time.
   Status ReopenWritableFile(const std::string& fname,
-    std::unique_ptr<WritableFile>* result,
-    const EnvOptions& options) override;
+                            std::unique_ptr<WritableFile>* result,
+                            const EnvOptions& options) override;
 
   // The returned file will only be accessed by one thread at a time.
   Status NewRandomRWFile(const std::string& fname,
-    std::unique_ptr<RandomRWFile>* result,
-    const EnvOptions& options) override;
+                         std::unique_ptr<RandomRWFile>* result,
+                         const EnvOptions& options) override;
 
   Status NewMemoryMappedFileBuffer(
-    const std::string& fname,
-    std::unique_ptr<MemoryMappedFileBuffer>* result) override;
+      const std::string& fname,
+      std::unique_ptr<MemoryMappedFileBuffer>* result) override;
 
   Status NewDirectory(const std::string& name,
-    std::unique_ptr<Directory>* result) override;
+                      std::unique_ptr<Directory>* result) override;
 
   Status FileExists(const std::string& fname) override;
 
   Status GetChildren(const std::string& dir,
-    std::vector<std::string>* result) override;
+                     std::vector<std::string>* result) override;
 
   Status CreateDir(const std::string& name) override;
 
@@ -261,31 +256,30 @@ public:
   Status DeleteDir(const std::string& name) override;
 
   Status GetFileSize(const std::string& fname,
-    uint64_t* size) override;
+                     uint64_t* size) override;
 
   Status GetFileModificationTime(const std::string& fname,
-    uint64_t* file_mtime) override;
+                                 uint64_t* file_mtime) override;
 
   Status RenameFile(const std::string& src,
-    const std::string& target) override;
+                    const std::string& target) override;
 
   Status LinkFile(const std::string& src,
-    const std::string& target) override;
+                  const std::string& target) override;
 
   Status NumFileLinks(const std::string& fname, uint64_t* count) override;
 
   Status AreFilesSame(const std::string& first,
-    const std::string& second, bool* res) override;
+                      const std::string& second, bool* res) override;
 
-  Status LockFile(const std::string& lockFname,
-    FileLock** lock) override;
+  Status LockFile(const std::string& lockFname, FileLock** lock) override;
 
   Status UnlockFile(FileLock* lock) override;
 
   Status GetTestDirectory(std::string* result) override;
 
   Status NewLogger(const std::string& fname,
-    std::shared_ptr<Logger>* result) override;
+                   std::shared_ptr<Logger>* result) override;
 
   uint64_t NowMicros() override;
 
@@ -294,16 +288,14 @@ public:
   Status GetHostName(char* name, uint64_t len) override;
 
   Status GetAbsolutePath(const std::string& db_path,
-    std::string* output_path) override;
+                         std::string* output_path) override;
 
   std::string TimeToString(uint64_t secondsSince1970) override;
 
-  Status GetThreadList(
-    std::vector<ThreadStatus>* thread_list) override;
+  Status GetThreadList(std::vector<ThreadStatus>* thread_list) override;
 
   void Schedule(void(*function)(void*), void* arg, Env::Priority pri,
-    void* tag,
-    void(*unschedFunction)(void* arg)) override;
+                void* tag, void(*unschedFunction)(void* arg)) override;
 
   int UnSchedule(void* arg, Env::Priority pri) override;
 
@@ -324,18 +316,18 @@ public:
   void IncBackgroundThreadsIfNeeded(int num, Env::Priority pri) override;
 
   EnvOptions OptimizeForManifestRead(
-    const EnvOptions& env_options) const override;
+      const EnvOptions& env_options) const override;
 
   EnvOptions OptimizeForLogWrite(const EnvOptions& env_options,
-    const DBOptions& db_options) const override;
+                                 const DBOptions& db_options) const override;
 
   EnvOptions OptimizeForManifestWrite(
-    const EnvOptions& env_options) const override;
+      const EnvOptions& env_options) const override;
 
 
 private:
 
-  WinEnvIO      winenv_io_;
+  WinEnvIO winenv_io_;
   WinEnvThreads winenv_threads_;
 };
 
