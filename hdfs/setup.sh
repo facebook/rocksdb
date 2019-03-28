b322051ca3f05bdff011fed28820a55881690acf
Yanqin Jin <yanqin@fb.com>
2019-03-28 15:56:42 -0700

    Minor

--- hdfs/setup.sh
+++ hdfs/setup.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server:$JAVA_HOME/jre/lib/amd64:
 
 export CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath --glob`
 for f in `find /usr/lib/hadoop-hdfs | grep jar`; do export CLASSPATH=$CLASSPATH:$f; done
-for f in `find /usr/lib/hadoop | grep '*.jar'`; do export CLASSPATH=$CLASSPATH:$f; done
+for f in `find /usr/lib/hadoop | grep jar`; do export CLASSPATH=$CLASSPATH:$f; done
 for f in `find /usr/lib/hadoop/client | grep jar`; do export CLASSPATH=$CLASSPATH:$f; done
