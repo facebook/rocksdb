c1b44f86ee48e41468b40c33ea82bdd7303c4700
anand76 <anand76@devvm1373.frc2.facebook.com>
2019-05-17 10:26:04 -0700

    Remove memory allocation for DataBlockIter
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- table/block_based/block_based_table_reader.cc
+++ table/block_based/block_based_table_reader.cc
@@ -2933,7 +2933,7 @@ void BlockBasedTable::MultiGet(const ReadOptions& read_options,
       iiter_unique_ptr.reset(iiter);
     }
 
-    std::unique_ptr<DataBlockIter> biter(new DataBlockIter());;
+    DataBlockIter biter;
     BlockHandle bhandle;
     for (auto miter = sst_file_range.begin(); miter != sst_file_range.end();
          ++miter) {
@@ -2945,26 +2945,26 @@ void BlockBasedTable::MultiGet(const ReadOptions& read_options,
       for (iiter->Seek(key); iiter->Valid() && !done; iiter->Next()) {
         if (iiter->value().offset() != bhandle.offset()) {
           bhandle = iiter->value();
-          biter.reset(new DataBlockIter());
+          biter.Invalidate(Status::OK());
           NewDataBlockIterator<DataBlockIter>(
-              rep_, read_options, bhandle, biter.get(), false,
+              rep_, read_options, bhandle, &biter, false,
               true /* key_includes_seq */, get_context);
         }
 
         if (read_options.read_tier == kBlockCacheTier &&
-            biter->status().IsIncomplete()) {
+            biter.status().IsIncomplete()) {
           // couldn't get block from block_cache
           // Update Saver.state to Found because we are only looking for
           // whether we can guarantee the key is not there when "no_io" is set
           get_context->MarkKeyMayExist();
           break;
         }
-        if (!biter->status().ok()) {
-          s = biter->status();
+        if (!biter.status().ok()) {
+          s = biter.status();
           break;
         }
 
-        bool may_exist = biter->SeekForGet(key);
+        bool may_exist = biter.SeekForGet(key);
         if (!may_exist) {
           // HashSeek cannot find the key this block and the the iter is not
           // the end of the block, i.e. cannot be in the following blocks
@@ -2974,20 +2974,20 @@ void BlockBasedTable::MultiGet(const ReadOptions& read_options,
         }
 
         // Call the *saver function on each entry/block until it returns false
-        for (; biter->Valid(); biter->Next()) {
+        for (; biter.Valid(); biter.Next()) {
           ParsedInternalKey parsed_key;
-          if (!ParseInternalKey(biter->key(), &parsed_key)) {
+          if (!ParseInternalKey(biter.key(), &parsed_key)) {
             s = Status::Corruption(Slice());
           }
 
           if (!get_context->SaveValue(
-                  parsed_key, biter->value(), &matched,
-                  biter->IsValuePinned() ? biter.get() : nullptr)) {
+                  parsed_key, biter.value(), &matched,
+                  biter.IsValuePinned() ? &biter : nullptr)) {
             done = true;
             break;
           }
         }
-        s = biter->status();
+        s = biter.status();
         if (done) {
           // Avoid the extra Next which is expensive in two-level indexes
           break;
