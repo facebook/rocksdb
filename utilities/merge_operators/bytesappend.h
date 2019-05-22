50487eb0e800a8ea1384e7f5a692b1ee3d64d210
jesse <madianlong@gmail.com>
2019-05-22 16:27:52 +0800

    develop - add bytes append merge operator

---
+++ utilities/merge_operators/bytesappend.h
@@ -0,0 +1,27 @@
+/**
+*  Created by Jesse Ma on 2019-05-22.
+*  for bytes append operator
+*/
+
+#pragma once
+
+#include <string>
+#include "rocksdb/slice.h"
+#include "rocksdb/merge_operator.h"
+
+namespace rocksdb {
+
+// BytesAppend merge operator appends the bytes to the end of the original value.
+    class BytesAppendOperator : public AssociativeMergeOperator {
+    public:
+        virtual bool Merge(const Slice &key,
+                           const Slice *existing_value,
+                           const Slice &value,
+                           std::string *new_value,
+                           Logger *logger) const override;
+
+        virtual const char *Name() const override;
+    };
+
+}  // namespace rocksdb
+
