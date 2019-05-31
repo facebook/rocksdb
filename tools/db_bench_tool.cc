a9efcbb0cdec03b261b244b936e9f704f9efbb3a
Zhichao Cao <zhichao@fb.com>
2019-05-31 10:14:05 -0700

    Adjusted the GFLAG

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -808,7 +808,8 @@ DEFINE_int64(
     "will not be logged if the trace file size exceeds this threshold. Default "
     "is 64 GB.");
 DEFINE_string(block_cache_trace_file, "", "Block cache trace file path.");
-DEFINE_uint32(trace_replay_threads, 1, "The number of threads to replay. ");
+DEFINE_int32(trace_replay_threads, 1,
+             "The number of threads to replay, must >=1.");
 
 static enum rocksdb::CompressionType StringToCompressionType(const char* ctype) {
   assert(ctype);
@@ -6529,7 +6530,8 @@ class Benchmark {
                       std::move(trace_reader));
     replayer.SetFastForward(
         static_cast<uint32_t>(FLAGS_trace_replay_fast_forward));
-    s = replayer.MultiThreadReplay(FLAGS_trace_replay_threads);
+    s = replayer.MultiThreadReplay(
+        static_cast<uint32_t>(FLAGS_trace_replay_threads));
     if (s.ok()) {
       fprintf(stdout, "Replay started from trace_file: %s\n",
               FLAGS_trace_file.c_str());
