c473cd9bc7b345a496606144c1a98450c1e463e0
haoyuhuang <aronexplorer@gmail.com>
2019-06-06 21:08:05 -0700

    Add STDC_FORMAT_MACROS

--- tools/block_cache_trace_analyzer.cc
+++ tools/block_cache_trace_analyzer.cc
@@ -1,4 +1,13 @@
+//  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under both the GPLv2 (found in the
+//  COPYING file in the root directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in the root directory).
+
 #include "tools/block_cache_trace_analyzer.h"
+
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
 
 #include <inttypes.h>
 #include <set>
