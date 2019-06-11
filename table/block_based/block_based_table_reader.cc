955ce646ec04b23ac791167f7db0d910d1e4d791
haoyuhuang <aronexplorer@gmail.com>
2019-06-11 14:59:12 -0700

    Simplify code path

--- table/block_based/block_based_table_reader.cc
+++ table/block_based/block_based_table_reader.cc
@@ -2272,7 +2272,6 @@ Status BlockBasedTable::MaybeReadBlockAndLoadToCache(
               block_entry->GetValue()->NumRestarts();
       usage = block_entry->GetValue()->ApproximateMemoryUsage();
     }
-
     TraceType trace_block_type = TraceType::kTraceMax;
     switch (block_type) {
       case BlockType::kIndex:
@@ -2286,6 +2285,7 @@ Status BlockBasedTable::MaybeReadBlockAndLoadToCache(
         break;
       default:
         // This cannot happen.
+        assert(false);
         break;
     }
     if (BlockCacheTraceWriter::ShouldTraceReferencedKey(
