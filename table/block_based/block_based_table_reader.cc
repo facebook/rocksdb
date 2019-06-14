7d3057f919b92c8a29cc4697ad484793f709fb56
haoyuhuang <aronexplorer@gmail.com>
2019-06-14 14:24:28 -0700

    Address comments

--- table/block_based/block_based_table_reader.cc
+++ table/block_based/block_based_table_reader.cc
@@ -1949,13 +1949,12 @@ CachableEntry<FilterBlockReader> BlockBasedTable::GetFilter(
     BlockCacheTraceRecord access_record(
         rep_->ioptions.env->NowMicros(),
         /*block_key=*/"", TraceType::kBlockTraceFilterBlock,
-        /*block_size=*/usage, rep_->get_cf_id_for_tracing(),
-        /*cf_name=*/"", rep_->get_level_for_tracing(),
-        rep_->get_sst_number_for_tracing(), lookup_context->caller,
-        is_cache_hit,
+        /*block_size=*/usage, rep_->cf_id_for_tracing(),
+        /*cf_name=*/"", rep_->level_for_tracing(),
+        rep_->sst_number_for_tracing(), lookup_context->caller, is_cache_hit,
         /*no_insert=*/no_io);
     block_cache_tracer_->WriteBlockAccess(access_record, key,
-                                          rep_->get_cf_name_for_tracing(),
+                                          rep_->cf_name_for_tracing(),
                                           /*referenced_key=*/nullptr);
   }
 
@@ -2032,13 +2031,12 @@ CachableEntry<UncompressionDict> BlockBasedTable::GetUncompressionDict(
     BlockCacheTraceRecord access_record(
         rep_->ioptions.env->NowMicros(),
         /*block_key=*/"", TraceType::kBlockTraceUncompressionDictBlock,
-        /*block_size=*/usage, rep_->get_cf_id_for_tracing(),
-        /*cf_name=*/"", rep_->get_level_for_tracing(),
-        rep_->get_sst_number_for_tracing(), lookup_context->caller,
-        is_cache_hit,
+        /*block_size=*/usage, rep_->cf_id_for_tracing(),
+        /*cf_name=*/"", rep_->level_for_tracing(),
+        rep_->sst_number_for_tracing(), lookup_context->caller, is_cache_hit,
         /*no_insert=*/no_io);
     block_cache_tracer_->WriteBlockAccess(access_record, cache_key,
-                                          rep_->get_cf_name_for_tracing(),
+                                          rep_->cf_name_for_tracing(),
                                           /*referenced_key=*/nullptr);
   }
   return {dict, cache_handle ? rep_->table_options.block_cache.get() : nullptr,
@@ -2232,8 +2230,8 @@ Status BlockBasedTable::MaybeReadBlockAndLoadToCache(
     }
   }
 
+  // Fill lookup_context.
   if (block_cache_tracer_ && lookup_context) {
-    // Fill lookup_context.
     size_t usage = 0;
     uint64_t nkeys = 0;
     if (block_entry->GetValue()) {
@@ -2274,13 +2272,12 @@ Status BlockBasedTable::MaybeReadBlockAndLoadToCache(
       BlockCacheTraceRecord access_record(
           rep_->ioptions.env->NowMicros(),
           /*block_key=*/"", trace_block_type,
-          /*block_size=*/usage, rep_->get_cf_id_for_tracing(),
-          /*cf_name=*/"", rep_->get_level_for_tracing(),
-          rep_->get_sst_number_for_tracing(), lookup_context->caller,
-          is_cache_hit,
+          /*block_size=*/usage, rep_->cf_id_for_tracing(),
+          /*cf_name=*/"", rep_->level_for_tracing(),
+          rep_->sst_number_for_tracing(), lookup_context->caller, is_cache_hit,
           /*no_insert=*/no_io);
       block_cache_tracer_->WriteBlockAccess(access_record, key,
-                                            rep_->get_cf_name_for_tracing(),
+                                            rep_->cf_name_for_tracing(),
                                             /*referenced_key=*/nullptr);
     }
   }
@@ -3029,16 +3026,14 @@ Status BlockBasedTable::Get(const ReadOptions& read_options, const Slice& key,
         }
         // Write the block cache access record.
         if (block_cache_tracer_) {
-          // Avoid making copy of block_key, cf_name and referenced_key when
+          // Avoid making copy of block_key, cf_name, and referenced_key when
           // constructing the access record.
           BlockCacheTraceRecord access_record(
               rep_->ioptions.env->NowMicros(),
               /*block_key=*/"", lookup_data_block_context.block_type,
-              lookup_data_block_context.block_size,
-              rep_->get_cf_id_for_tracing(),
-              /*cf_name=*/"", rep_->get_level_for_tracing(),
-              rep_->get_sst_number_for_tracing(),
-              lookup_data_block_context.caller,
+              lookup_data_block_context.block_size, rep_->cf_id_for_tracing(),
+              /*cf_name=*/"", rep_->level_for_tracing(),
+              rep_->sst_number_for_tracing(), lookup_data_block_context.caller,
               lookup_data_block_context.is_cache_hit,
               lookup_data_block_context.no_insert,
               /*referenced_key=*/"", referenced_data_size,
@@ -3046,7 +3041,7 @@ Status BlockBasedTable::Get(const ReadOptions& read_options, const Slice& key,
               does_referenced_key_exist);
           block_cache_tracer_->WriteBlockAccess(
               access_record, lookup_data_block_context.block_key,
-              rep_->get_cf_name_for_tracing(), key);
+              rep_->cf_name_for_tracing(), key);
         }
       }
 
@@ -3197,11 +3192,9 @@ void BlockBasedTable::MultiGet(const ReadOptions& read_options,
           BlockCacheTraceRecord access_record(
               rep_->ioptions.env->NowMicros(),
               /*block_key=*/"", lookup_data_block_context.block_type,
-              lookup_data_block_context.block_size,
-              rep_->get_cf_id_for_tracing(),
-              /*cf_name=*/"", rep_->get_level_for_tracing(),
-              rep_->get_sst_number_for_tracing(),
-              lookup_data_block_context.caller,
+              lookup_data_block_context.block_size, rep_->cf_id_for_tracing(),
+              /*cf_name=*/"", rep_->level_for_tracing(),
+              rep_->sst_number_for_tracing(), lookup_data_block_context.caller,
               lookup_data_block_context.is_cache_hit,
               lookup_data_block_context.no_insert,
               /*referenced_key=*/"", referenced_data_size,
@@ -3209,7 +3202,7 @@ void BlockBasedTable::MultiGet(const ReadOptions& read_options,
               does_referenced_key_exist);
           block_cache_tracer_->WriteBlockAccess(
               access_record, lookup_data_block_context.block_key,
-              rep_->get_cf_name_for_tracing(), key);
+              rep_->cf_name_for_tracing(), key);
         }
 
         if (done) {
