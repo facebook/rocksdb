b5c0bd5f29a9706f84ea8c1dfbd38150622630a9
haoyuhuang <aronexplorer@gmail.com>
2019-06-26 15:47:44 -0700

    use memory_relaxed

--- trace_replay/block_cache_tracer.h
+++ trace_replay/block_cache_tracer.h
@@ -197,7 +197,9 @@ class BlockCacheTracer {
   // Stop writing block cache accesses to the trace_writer.
   void EndTrace();
 
-  bool is_tracing_enabled() const { return writer_.load(); }
+  bool is_tracing_enabled() const {
+    return writer_.load(std::memory_order_relaxed);
+  }
 
   Status WriteBlockAccess(const BlockCacheTraceRecord& record,
                           const Slice& block_key, const Slice& cf_name,
