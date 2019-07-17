5514a32aaffad317a8932941659e00bbafaed077
anand76 <anand76@devvm1373.frc2.facebook.com>
2019-07-17 16:50:34 -0700

    Fix LITE mode build failure
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- db/import_column_family_test.cc
+++ db/import_column_family_test.cc
@@ -555,7 +555,7 @@ int main(int argc, char** argv) {
 #else
 #include <stdio.h>
 
-int main(int argc, char** argv) {
+int main(int /*argc*/, char** /*argv*/) {
   fprintf(stderr,
           "SKIPPED as External SST File Writer and Import are not supported "
           "in ROCKSDB_LITE\n");
