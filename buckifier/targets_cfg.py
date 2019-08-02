25f6abd2deb5ed5c38a060e862c5d734586d7718
Yanqin Jin <yanqin@fb.com>
2019-08-02 10:40:32 -0700

    Change buckifier to support parameterized dependencies (#5648)
    Summary:
    Users may desire to specify extra dependencies via buck. This PR allows users to pass additional dependencies as a JSON object so that the buckifier script can generate TARGETS file with desired extra dependencies.
    
    Test plan (on dev server)
    ```
    $python buckifier/buckify_rocksdb.py '{"fake": {"extra_deps": [":test_dep", "//fakes/module:mock1"], "extra_compiler_flags": ["-DROCKSDB_LITE", "-Os"]}}'
    Generating TARGETS
    Extra dependencies:
    {'': {'extra_compiler_flags': [], 'extra_deps': []}, 'test_dep1': {'extra_compiler_flags': ['-O2', '-DROCKSDB_LITE'], 'extra_deps': [':fake', '//dep1/mock']}}
    Generated TARGETS Summary:
    - 5 libs
    - 0 binarys
    - 296 tests
    ```
    Verify the TARGETS file.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/5648
    
    Differential Revision: D16565043
    
    Pulled By: riversand963
    
    fbshipit-source-id: a6ef02274174fcf159692d7b846e828454d01e89

--- buckifier/targets_cfg.py
+++ buckifier/targets_cfg.py
@@ -140,11 +140,13 @@ test_cfg_template = """    [
         "%s",
         "%s",
         "%s",
+        %s,
+        %s,
     ],
 """
 
 unittests_template = """
-# [test_name, test_src, test_type]
+# [test_name, test_src, test_type, extra_deps, extra_compiler_flags]
 ROCKS_TESTS = [
 %s]
 
@@ -153,6 +155,8 @@ ROCKS_TESTS = [
 # will not be included.
 [
     test_binary(
+        extra_compiler_flags = extra_compiler_flags,
+        extra_deps = extra_deps,
         parallelism = parallelism,
         rocksdb_arch_preprocessor_flags = ROCKSDB_ARCH_PREPROCESSOR_FLAGS,
         rocksdb_compiler_flags = ROCKSDB_COMPILER_FLAGS,
@@ -163,7 +167,7 @@ ROCKS_TESTS = [
         test_cc = test_cc,
         test_name = test_name,
     )
-    for test_name, test_cc, parallelism in ROCKS_TESTS
+    for test_name, test_cc, parallelism, extra_deps, extra_compiler_flags in ROCKS_TESTS
     if not is_opt_mode
 ]
 """
