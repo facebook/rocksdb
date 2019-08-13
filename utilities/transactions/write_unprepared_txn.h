7dacfc273738cece95a77647321178836331ceba
mung <mung@fb.com>
2019-08-13 11:29:24 -0700

    minor comments

--- utilities/transactions/write_unprepared_txn.h
+++ utilities/transactions/write_unprepared_txn.h
@@ -241,9 +241,9 @@ class WriteUnpreparedTxn : public WritePreparedTxn {
 
   // Track the largest sequence number at which we performed snapshot
   // validation. If snapshot validation was skipped because no snapshot was set,
-  // then this is set to kMaxSequenceNumber. This value is useful because it
-  // means that for keys that have unprepared seqnos, we can guarantee that no
-  // committed keys by other transactions can exist between
+  // then this is set to GetLastPublishedSequence. This value is useful because
+  // it means that for keys that have unprepared seqnos, we can guarantee that
+  // no committed keys by other transactions can exist between
   // largest_validated_seq_ and max_unprep_seq. See
   // WriteUnpreparedTxnDB::NewIterator for an explanation for why this is
   // necessary for iterator Prev().
