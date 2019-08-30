b4f4f70bd520ad9e059767f52cf915b0b654ddd8
Ronak Sisodia <ronaksisodia07@gmail.com>
2019-08-30 23:12:49 +0530

    Revert "Update write_thread.h"

--- db/write_thread.cc
+++ db/write_thread.cc
@@ -22,10 +22,6 @@ WriteThread::WriteThread(const ImmutableDBOptions& db_options)
       allow_concurrent_memtable_write_(
           db_options.allow_concurrent_memtable_write),
       enable_pipelined_write_(db_options.enable_pipelined_write),
-      lower_limit_write_batch_group_size_bytes(
-          db_options.lower_limit_write_batch_group_size_bytes),
-      upper_limit_write_batch_group_size_bytes(
-          db_options.upper_limit_write_batch_group_size_bytes),
       newest_writer_(nullptr),
       newest_memtable_writer_(nullptr),
       last_sequence_(0),
@@ -410,9 +406,9 @@ size_t WriteThread::EnterAsBatchGroupLeader(Writer* leader,
   // Allow the group to grow up to a maximum size, but if the
   // original write is small, limit the growth so we do not slow
   // down the small write too much.
-  size_t max_size = upper_limit_write_batch_group_size_bytes;
-  if (size <= lower_limit_write_batch_group_size_bytes) {
-    max_size = size + lower_limit_write_batch_group_size_bytes;
+  size_t max_size = 1 << 20;
+  if (size <= (128 << 10)) {
+    max_size = size + (128 << 10);
   }
 
   leader->write_group = write_group;
@@ -489,9 +485,9 @@ void WriteThread::EnterAsMemTableWriter(Writer* leader,
   // Allow the group to grow up to a maximum size, but if the
   // original write is small, limit the growth so we do not slow
   // down the small write too much.
-  size_t max_size = upper_limit_write_batch_group_size_bytes;
-  if (size <= lower_limit_write_batch_group_size_bytes) {
-    max_size = size + lower_limit_write_batch_group_size_bytes;
+  size_t max_size = 1 << 20;
+  if (size <= (128 << 10)) {
+    max_size = size + (128 << 10);
   }
 
   leader->write_group = write_group;
