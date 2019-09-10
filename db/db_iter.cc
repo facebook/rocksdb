059186e7e542bae494afabecf980b8f0bf1fa4d3
Shylock Hg <tcath2s@gmail.com>
2019-09-10 22:08:05 +0800

    The field not used in lite version.

--- db/db_iter.cc
+++ db/db_iter.cc
@@ -349,11 +349,10 @@ class DBIter final: public Iterator {
   ReadRangeDelAggregator range_del_agg_;
   LocalStatistics local_stats_;
   PinnedIteratorsManager pinned_iters_mgr_;
-#ifdef ROCKSDB_LITE
-  ROCKSDB_FIELD_UNUSED
-#endif
+#ifndef ROCKSDB_LITE
   DBImpl* db_impl_;
   ColumnFamilyData* cfd_;
+#endif
   // for diff snapshots we want the lower bound on the seqnum;
   // if this value > 0 iterator will return internal keys
   SequenceNumber start_seqnum_;
