d3a6726f02e8b9aa17d7fcc76d352f766c070457
Peter Dillinger <peterd@fb.com>
2019-09-16 11:36:58 -0700

    Revert changes from PR#5784 accidentally in PR#5780 (#5810)
    Summary:
    This will allow us to fix history by having the code changes for PR#5784 properly attributed to it.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/5810
    
    Differential Revision: D17400231
    
    Pulled By: pdillinger
    
    fbshipit-source-id: 2da8b1cdf2533cfedb35b5526eadefb38c291f09

--- util/bloom_test.cc
+++ util/bloom_test.cc
@@ -352,8 +352,7 @@ TEST_F(FullBloomTest, FullVaryingLengths) {
     }
     Build();
 
-    ASSERT_LE(FilterSize(),
-              (size_t)((length * 10 / 8) + CACHE_LINE_SIZE * 2 + 5));
+    ASSERT_LE(FilterSize(), (size_t)((length * 10 / 8) + CACHE_LINE_SIZE * 2 + 5)) << length;
 
     // All added keys must match
     for (int i = 0; i < length; i++) {
