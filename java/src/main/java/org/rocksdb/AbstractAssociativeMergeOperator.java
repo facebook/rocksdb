e9196cf7cc53e432f8f1d3929f8e2bd15c47c622
Geoffroy Fouquier <geoffroy.fouquier@exensa.com>
2019-09-18 14:12:44 +0200

    add JavaDoc to AbstractMergeOperator and AbstractAssociativeMergeOperator

--- java/src/main/java/org/rocksdb/AbstractAssociativeMergeOperator.java
+++ java/src/main/java/org/rocksdb/AbstractAssociativeMergeOperator.java
@@ -9,7 +9,25 @@ public abstract class AbstractAssociativeMergeOperator extends MergeOperator
 {
   public AbstractAssociativeMergeOperator() { super(); }
 
+  /**
+   * Merge values of a given key
+   *
+   * @param key the key to merge
+   * @param oldvalues current value of the key
+   * @param newvalue new value of the key
+   * @param rt indicate if returned value is a reference or not
+   *
+   * @return merged value of the key
+   */
   abstract public byte[] merge(byte[] key, byte[] oldvalue, byte[] newvalue, ReturnType rt) throws RocksDBException;
+
+  /**
+    * The name of the MergeOperator. Used to check for MergeOperator
+    * mismatches (i.e., a DB created with one MergeOperator is
+    * accessed using a different MergeOperator)
+    *
+    * @return a string with operator name
+    */
   abstract public String name();
 
   @Override
--- java/src/main/java/org/rocksdb/AbstractMergeOperator.java
+++ java/src/main/java/org/rocksdb/AbstractMergeOperator.java
@@ -15,10 +15,49 @@ public abstract class AbstractMergeOperator extends MergeOperator
     super(allowSingleOperand ? 1L : 0L, allowShouldMerge ? 1L : 0L, partialMultiMerge ? 1L : 0L);
   }
 
+  /**
+   * Full merge of a given key's values
+   *
+   * @param key The key that's associated with this merge operation.
+   * @param oldvalues null indicates that the key does not exist before this op
+   * @param operands the sequence of merge operations to apply, front() first
+   * @param rt indicate if returned value is a reference or not
+   *
+   * @return merged value of the key
+   */
   abstract public byte[] fullMerge(byte[] key, byte[] oldvalue, byte[][] operands, ReturnType rt) throws RocksDBException;
+
+  /**
+   * This function performs merge with multiple operators
+   *
+   * @param key The key that's associated with this merge operation.
+   * @param operands list of merge operators to apply
+   * @param rt indicate if returned value is a reference or not
+   *
+   * @return merged value of the key
+   */
   abstract public byte[] partialMultiMerge(byte[] key, byte[][] operands, ReturnType rt) throws RocksDBException;
+
+  /**
+   * This function performs merge(left_op, right_op)
+   *
+   * @param key The key that's associated with this merge operation.
+   * @param left left merge operator
+   * @param left right merge operator
+   * @param rt indicate if returned value is a reference or not
+   *
+   * @return merged value of the key
+   */
   abstract public byte[] partialMerge(byte[] key, byte[] left, byte[] right, ReturnType rt) throws RocksDBException;
   abstract public boolean shouldMerge(byte[][] operands)throws RocksDBException;
+
+  /**
+    * The name of the MergeOperator. Used to check for MergeOperator
+    * mismatches (i.e., a DB created with one MergeOperator is
+    * accessed using a different MergeOperator)
+    *
+    * @return a string with operator name
+    */
   abstract public String name();
 
   private boolean getBoolean(long longValue) {
