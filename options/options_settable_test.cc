6d4b5e13a032c82d0b36754c513d66c2194fd367
Little-Wallace <bupt2013211450@gmail.com>
2019-09-23 22:33:18 +0800

    fix options test
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- options/options_settable_test.cc
+++ options/options_settable_test.cc
@@ -277,6 +277,8 @@ TEST_F(OptionsSettableTest, DBOptionsAllFieldsSettable) {
                              "advise_random_on_open=true;"
                              "fail_if_options_file_error=false;"
                              "enable_pipelined_write=false;"
+                             "enable_multi_thread_write=false;"
+                             "write_thread_pool_size=0;"
                              "unordered_write=false;"
                              "allow_concurrent_memtable_write=true;"
                              "wal_recovery_mode=kPointInTimeRecovery;"
