ed555d9fce716fd7958b1d27afa5ee648ebf35b0
Daryl Zhang <zhadaryl@amazon.com>
2019-09-26 09:00:02 -0700

    Fix bug 4 in jni options.cc

--- java/rocksjni/options.cc
+++ java/rocksjni/options.cc
@@ -584,7 +584,7 @@ void Java_org_rocksdb_Options_dbPaths(
  */
 void Java_org_rocksdb_Options_setDbPathSupplierFactory(
     JNIEnv* /*env*/, jobject /*jobj*/, jlong jhandle, jlong jfactory_handle) {
-  auto* factory = reinterpret_cast<rocksdb::DbPathSupplierFactory*>(jhandle);
+  auto* factory = reinterpret_cast<rocksdb::DbPathSupplierFactory*>(jfactory_handle);
   reinterpret_cast<rocksdb::Options*>(jhandle)->db_path_supplier_factory.reset(factory);
 }
 
