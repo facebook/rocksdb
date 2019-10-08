2f4e288143f1c0c9f5a9e829d1b20ce62aca4224
Maysam Yabandeh <myabandeh@fb.com>
2019-10-08 16:48:57 -0700

    Enable partitioned index/filter in stress tests (#5895)
    Summary:
    This is the 2nd attempt after the revert of https://github.com/facebook/rocksdb/pull/4020
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/5895
    
    Test Plan:
    ```
    ./tools/db_crashtest.py blackbox --simple --interval=10 --max_key=10000000
    ```
    
    Differential Revision: D17822137
    
    Pulled By: maysamyabandeh
    
    fbshipit-source-id: 3d148c0d8cc129080410ff859c04b544223c8ea3

--- tools/db_stress.cc
+++ tools/db_stress.cc
@@ -354,8 +354,7 @@ DEFINE_bool(use_block_based_filter, false, "use block based filter"
               "instead of full filter for block based table");
 
 DEFINE_bool(partition_filters, false,
-            "use partitioned filters "
-            "for block-based table");
+            "use partitioned filters for block-based table");
 
 DEFINE_int32(
     index_type,
