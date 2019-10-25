b3dc2f3691528c52587452847590d0477cdb017d
Peter Dillinger <peterd@fb.com>
2019-10-25 12:53:11 -0700

    Update xxhash.cc to allow combined compilation (#5969)
    Summary:
    To fix unity_test
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/5969
    
    Test Plan: make unity_test
    
    Differential Revision: D18140426
    
    Pulled By: pdillinger
    
    fbshipit-source-id: d5516e6d665f57e3706b9f9b965b0c458e58ccef

--- util/xxhash.cc
+++ util/xxhash.cc
@@ -119,7 +119,10 @@ static void* XXH_memcpy(void* dest, const void* src, size_t size) { return memcp
 
 #include <limits.h>   /* ULLONG_MAX */
 
+#ifndef XXH_STATIC_LINKING_ONLY
 #define XXH_STATIC_LINKING_ONLY
+#endif
+
 #include "xxhash.h"
 
 /* BEGIN RocksDB customizations */
