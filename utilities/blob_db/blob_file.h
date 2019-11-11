30ae9ae1f02d0281ecc7253a38768bf65014cc26
Levi Tamasi <ltamasi@fb.com>
2019-11-11 10:54:38 -0800

    Remove confusing insert hint

--- utilities/blob_db/blob_file.h
+++ utilities/blob_db/blob_file.h
@@ -131,7 +131,7 @@ class BlobFile {
   void LinkSstFile(uint64_t sst_file_number) {
     auto it = linked_sst_files_.find(sst_file_number);
     assert(it == linked_sst_files_.end());
-    linked_sst_files_.insert(it, sst_file_number);
+    linked_sst_files_.insert(sst_file_number);
   }
 
   // Unlink an SST file whose oldest blob file reference points to this file.
