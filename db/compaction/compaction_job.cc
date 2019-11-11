e3f28753b9197982e9fdc6b70b8e4380ecab8fdf
Sagar Vemuri <svemuri@fb.com>
2019-11-11 14:07:36 -0800

    Cascade TTL Compactions to move expired key ranges to bottom levels faster (#5992)
    Summary:
    When users use Level-Compaction-with-TTL by setting `cf_options.ttl`, the ttl-expired data could take n*ttl time to reach the bottom level (where n is the number of levels) due to how the `creation_time` table property was calculated for the newly created files during compaction. The creation time of new files was set to a max of all compaction-input-files-creation-times which essentially resulted in resetting the ttl as the key range moves across levels. This behavior is now fixed by changing the `creation_time` to be based on minimum of all compaction-input-files-creation-times; this will cause cascading compactions across levels for the ttl-expired data to move to the bottom level, resulting in getting rid of tombstones/deleted-data faster.
    
    This will help start cascading compactions to move the expired key range to the bottom-most level faster.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/5992
    
    Test Plan: `make check`
    
    Differential Revision: D18257883
    
    Pulled By: sagar0
    
    fbshipit-source-id: 00df0bb8d0b7e14d9fc239df2cba8559f3e54cbc

--- db/compaction/compaction_job.cc
+++ db/compaction/compaction_job.cc
@@ -1511,10 +1511,9 @@ Status CompactionJob::OpenCompactionOutputFile(
   }
   uint64_t current_time = static_cast<uint64_t>(temp_current_time);
 
-  uint64_t latest_key_time =
-      sub_compact->compaction->MaxInputFileCreationTime();
-  if (latest_key_time == 0) {
-    latest_key_time = current_time;
+  uint64_t creation_time = sub_compact->compaction->MinInputFileCreationTime();
+  if (creation_time == port::kMaxUint64) {
+    creation_time = current_time;
   }
 
   sub_compact->builder.reset(NewTableBuilder(
@@ -1524,7 +1523,7 @@ Status CompactionJob::OpenCompactionOutputFile(
       sub_compact->compaction->output_compression(),
       0 /*sample_for_compression */,
       sub_compact->compaction->output_compression_opts(),
-      sub_compact->compaction->output_level(), skip_filters, latest_key_time,
+      sub_compact->compaction->output_level(), skip_filters, creation_time,
       0 /* oldest_key_time */, sub_compact->compaction->max_output_file_size(),
       current_time));
   LogFlush(db_options_.info_log);
