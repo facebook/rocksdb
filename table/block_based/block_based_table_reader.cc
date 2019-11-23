760c895bbf6b8df64af1fdea8cb8aac9743d40d0
Zhichao Cao <zhichao@fb.com>
2019-11-22 16:23:44 -0800

    make sure the copied block is raw block

--- table/block_based/block_based_table_reader.cc
+++ table/block_based/block_based_table_reader.cc
@@ -2463,6 +2463,9 @@ void BlockBasedTable::RetrieveMultipleBlocks(
         raw_block_contents = BlockContents(
             CopyBufferToHeap(GetMemoryAllocator(rep_->table_options), raw),
             handle.size());
+#ifndef NDEBUG
+        raw_block_contents.is_raw_block = true;
+#endif
       }
     }
 
