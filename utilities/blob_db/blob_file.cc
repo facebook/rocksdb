57278c37412fb669e9f100678a24d36f2f7f5919
Levi Tamasi <ltamasi@fb.com>
2019-11-26 13:16:39 -0800

    Refactor blob file creation logic (#6066)
    Summary:
    The patch refactors and cleans up the logic around creating new blob files
    by moving the common code of `SelectBlobFile` and `SelectBlobFileTTL`
    to a new helper method `CreateBlobFileAndWriter`, bringing the implementation
    of `SelectBlobFile` and `SelectBlobFileTTL` into sync, and increasing encapsulation
    by adding new constructors for `BlobFile` and `BlobLogHeader`.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/6066
    
    Test Plan:
    Ran `make check` and used the BlobDB mode of `db_bench` to sanity test both
    the TTL and the non-TTL code paths.
    
    Differential Revision: D18646921
    
    Pulled By: ltamasi
    
    fbshipit-source-id: e5705a84807932e31dccab4f49b3e64369cea26d

--- utilities/blob_db/blob_file.cc
+++ utilities/blob_db/blob_file.cc
@@ -10,7 +10,6 @@
 #include <cinttypes>
 
 #include <algorithm>
-#include <limits>
 #include <memory>
 
 #include "db/column_family.h"
@@ -25,45 +24,24 @@ namespace rocksdb {
 
 namespace blob_db {
 
-BlobFile::BlobFile()
-    : parent_(nullptr),
-      file_number_(0),
-      info_log_(nullptr),
-      column_family_id_(std::numeric_limits<uint32_t>::max()),
-      compression_(kNoCompression),
-      has_ttl_(false),
-      blob_count_(0),
-      file_size_(0),
-      closed_(false),
-      immutable_sequence_(0),
-      obsolete_(false),
-      obsolete_sequence_(0),
-      expiration_range_({0, 0}),
-      last_access_(-1),
-      last_fsync_(0),
-      header_valid_(false),
-      footer_valid_(false) {}
-
 BlobFile::BlobFile(const BlobDBImpl* p, const std::string& bdir, uint64_t fn,
                    Logger* info_log)
+    : parent_(p), path_to_dir_(bdir), file_number_(fn), info_log_(info_log) {}
+
+BlobFile::BlobFile(const BlobDBImpl* p, const std::string& bdir, uint64_t fn,
+                   Logger* info_log, uint32_t column_family_id,
+                   CompressionType compression, bool has_ttl,
+                   const ExpirationRange& expiration_range)
     : parent_(p),
       path_to_dir_(bdir),
       file_number_(fn),
       info_log_(info_log),
-      column_family_id_(std::numeric_limits<uint32_t>::max()),
-      compression_(kNoCompression),
-      has_ttl_(false),
-      blob_count_(0),
-      file_size_(0),
-      closed_(false),
-      immutable_sequence_(0),
-      obsolete_(false),
-      obsolete_sequence_(0),
-      expiration_range_({0, 0}),
-      last_access_(-1),
-      last_fsync_(0),
-      header_valid_(false),
-      footer_valid_(false) {}
+      column_family_id_(column_family_id),
+      compression_(compression),
+      has_ttl_(has_ttl),
+      expiration_range_(expiration_range),
+      header_(column_family_id, compression, has_ttl, expiration_range),
+      header_valid_(true) {}
 
 BlobFile::~BlobFile() {
   if (obsolete_) {
