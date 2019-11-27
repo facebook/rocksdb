880e30a8b1e08c854ed2379583307d347bca3323
John Ericson <john.ericson@obsidian.systems>
2019-11-26 21:40:16 -0800

    Work around weird unused errors with Mingw (#6075)
    Summary:
    From the reset of the code, it looks this this maybe can be unconditionally given the attribute? But I couldn't test with MSVC so I defensively put under CPP.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/6075
    
    Differential Revision: D18723749
    
    fbshipit-source-id: 45fc8732c28dd29aab1644225d68f3c6f39bd69b

--- port/win/io_win.cc
+++ port/win/io_win.cc
@@ -1025,7 +1025,12 @@ Status WinRandomRWFile::Close() {
 //////////////////////////////////////////////////////////////////////////
 /// WinMemoryMappedBufer
 WinMemoryMappedBuffer::~WinMemoryMappedBuffer() {
-  BOOL ret = FALSE;
+  BOOL ret
+#if defined(_MSC_VER)
+    = FALSE;
+#else
+    __attribute__((__unused__));
+#endif
   if (base_ != nullptr) {
     ret = ::UnmapViewOfFile(base_);
     assert(ret);
