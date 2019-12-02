bce0f0da9bc694cacf5a58d93ddabef50548763d
Yanqin Jin <yanqin@fb.com>
2019-12-02 14:39:49 -0800

    Fix lint

--- db/db_impl/db_secondary_test.cc
+++ db/db_impl/db_secondary_test.cc
@@ -198,7 +198,7 @@ TEST_F(DBSecondaryTest, OpenAsSecondary) {
 namespace {
 class TraceFileEnv : public EnvWrapper {
  public:
-  TraceFileEnv(Env* target) : EnvWrapper(target) {}
+  explicit TraceFileEnv(Env* target) : EnvWrapper(target) {}
   Status NewRandomAccessFile(const std::string& f,
                              std::unique_ptr<RandomAccessFile>* r,
                              const EnvOptions& env_options) override {
