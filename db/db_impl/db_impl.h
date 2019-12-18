5d199b6130b6de927a0ff6ae200acd9c0e28b992
Mike Kolupaev <kolmike@fb.com>
2019-12-17 20:07:21 -0800

    Fix use-after-free and double-deleting files in BackgroundCallPurge() (#6193)
    Summary:
    The bad code was:
    
    ```
    mutex.Lock(); // `mutex` protects `container`
    for (auto& x : container) {
      mutex.Unlock();
      // do stuff to x
      mutex.Lock();
    }
    ```
    
    It's incorrect because both `x` and the iterator may become invalid if another thread modifies the container while this thread is not holding the mutex.
    
    Broken by https://github.com/facebook/rocksdb/pull/5796 - it replaced a `while (!container.empty())` loop with a `for (auto x : container)`.
    
    (RocksDB code does a lot of such unlocking+re-locking of mutexes, and this type of bugs comes up a lot :/ )
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/6193
    
    Test Plan: Ran some logdevice integration tests that were crashing without this fix.
    
    Differential Revision: D19116874
    
    Pulled By: al13n321
    
    fbshipit-source-id: 9672bc4227c1b68f46f7436db2b96811adb8c703

--- db/db_impl/db_impl.h
+++ db/db_impl/db_impl.h
@@ -1201,7 +1201,7 @@ class DBImpl : public DB {
   };
 
   // PurgeFileInfo is a structure to hold information of files to be deleted in
-  // purge_queue_
+  // purge_files_
   struct PurgeFileInfo {
     std::string fname;
     std::string dir_to_sync;
