e85ec474af251dfe8729c458f31ad10fb609732a
sdong <siying.d@fb.com>
2019-12-18 17:30:45 -0800

    Another minor improvement

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -37,8 +37,8 @@ default_params = {
     "bottommost_compression_type": lambda:
         "disable" if random.randint(0, 1) == 0 else
         random.choice(
-            ["disable", "none", "snappy", "zlib", "bzip2", "lz4",
-             "lz4hc", "xpress", "zstd"]),
+            ["none", "snappy", "zlib", "bzip2", "lz4", "lz4hc", "xpress",
+             "zstd"]),
     "checksum_type" : lambda: random.choice(["kCRC32c", "kxxHash", "kxxHash64"]),
     "compression_max_dict_bytes": lambda: 16384 * random.randint(0, 1),
     "compression_zstd_max_train_bytes": lambda: 65536 * random.randint(0, 1),
