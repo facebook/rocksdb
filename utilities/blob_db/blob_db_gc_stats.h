28df1fbf2d840997d3a9b4593c819fee595f71cb
Levi Tamasi <ltamasi@fb.com>
2020-01-10 13:17:18 -0800

    Add new class BlobDBGarbageCollectionStats

---
+++ utilities/blob_db/blob_db_gc_stats.h
@@ -0,0 +1,40 @@
+//  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under both the GPLv2 (found in the
+//  COPYING file in the root directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in the root directory).
+//
+#pragma once
+
+#ifndef ROCKSDB_LITE
+
+namespace rocksdb {
+
+namespace blob_db {
+
+/**
+ * Statistics related to a single garbage collection pass (i.e. a single compaction).
+ */
+class BlobDBGarbageCollectionStats {
+ public:
+   uint64_t AllBlobs() { return all_blobs_; }
+   uint64_t RelocatedBlobs() { return relocated_blobs_; }
+   uint64_t RelocatedBytes() { return relocated_bytes_; }
+   uint64_t NewFiles() { return new_files_; }
+
+   void AddBlob() { ++all_blobs_; }
+   void AddRelocatedBlob(uint64_t size) {
+     ++relocated_blobs_;
+     relocated_bytes_ += size;
+   }
+   void AddNewFile() { ++new_files_; }
+
+ private:
+   uint64_t all_blobs_ = 0;
+   uint64_t relocated_blobs_ = 0;
+   uint64_t relocated_bytes_ = 0;
+   uint64_t new_files_ = 0;
+};
+
+}  // namespace blob_db
+}  // namespace rocksdb
+#endif  // ROCKSDB_LITE
