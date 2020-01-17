d4b4cb98580b7bbb576d0e2c2faceedf3dc18982
Matt Bell <mappum@gmail.com>
2020-01-17 10:52:13 -0800

    Changed C API declaration order to match implementation

--- include/rocksdb/c.h
+++ include/rocksdb/c.h
@@ -737,8 +737,6 @@ rocksdb_block_based_options_set_pin_top_level_index_and_filter(
     rocksdb_block_based_table_options_t*, unsigned char);
 extern ROCKSDB_LIBRARY_API void rocksdb_options_set_block_based_table_factory(
     rocksdb_options_t* opt, rocksdb_block_based_table_options_t* table_options);
-extern ROCKSDB_LIBRARY_API void rocksdb_options_set_atomic_flush(
-    rocksdb_options_t* opt, unsigned char);
 
 /* Cuckoo table options */
 
@@ -1039,6 +1037,8 @@ extern ROCKSDB_LIBRARY_API void rocksdb_options_set_fifo_compaction_options(
     rocksdb_options_t* opt, rocksdb_fifo_compaction_options_t* fifo);
 extern ROCKSDB_LIBRARY_API void rocksdb_options_set_ratelimiter(
     rocksdb_options_t* opt, rocksdb_ratelimiter_t* limiter);
+extern ROCKSDB_LIBRARY_API void rocksdb_options_set_atomic_flush(
+    rocksdb_options_t* opt, unsigned char);
 
 /* RateLimiter */
 extern ROCKSDB_LIBRARY_API rocksdb_ratelimiter_t* rocksdb_ratelimiter_create(
