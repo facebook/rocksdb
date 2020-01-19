af2be5bfb1e34a91dfe6d2a22c3e380a406341b1
Ziyue Yang <v-ziyyan@microsoft.com>
2020-01-19 23:02:30 +0800

    Add Feature - Add an option for pipelined & parallel compression optimization
    Add an option for number of threads for pipelined & parallel compression
    optimization. The optimization is only enabled when the number of threads
    is larger than 1.
    The option is also added for db_bench.

--- options/options_helper.cc
+++ options/options_helper.cc
@@ -971,6 +971,17 @@ Status ParseCompressionOptions(const std::string& value, const std::string& name
         ParseInt(value.substr(start, value.size() - start));
     end = value.find(':', start);
   }
+  // parallel_threads is optional for backwards compatibility
+  if (end != std::string::npos) {
+    start = end + 1;
+    if (start >= value.size()) {
+      return Status::InvalidArgument(
+          "unable to parse the specified CF option " + name);
+    }
+    compression_opts.parallel_threads =
+        ParseInt(value.substr(start, value.size() - start));
+    end = value.find(':', start);
+  }
   // enabled is optional for backwards compatibility
   if (end != std::string::npos) {
     start = end + 1;
