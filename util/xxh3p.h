8fc20ac468b266a53083175025375985ec04b796
Adam Retter <adam.retter@googlemail.com>
2020-03-11 12:31:06 -0700

    Add ppc64le builds to Travis (#6144)
    Summary:
    Let's see how this goes...
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/6144
    
    Differential Revision: D20387515
    
    Pulled By: pdillinger
    
    fbshipit-source-id: ba2669348c267141dfddff910b4c2224a22cbb38

--- util/xxh3p.h
+++ util/xxh3p.h
@@ -637,7 +637,7 @@ XXH3p_accumulate_512(      void* XXH_RESTRICT acc,
         }
     }
 
-#elif (XXH_VECTOR == XXH_VSX)
+#elif (XXH_VECTOR == XXH_VSX) && /* work around a compiler bug */ (__GNUC__ > 5)
           U64x2* const xacc =        (U64x2*) acc;    /* presumed aligned */
     U64x2 const* const xinput = (U64x2 const*) input;   /* no alignment restriction */
     U64x2 const* const xsecret  = (U64x2 const*) secret;    /* no alignment restriction */
@@ -782,7 +782,7 @@ XXH3p_scrambleAcc(void* XXH_RESTRICT acc, const void* XXH_RESTRICT secret)
             xacc[i] = vmlal_u32(xacc[i], shuffled.val[0], prime);
     }   }
 
-#elif (XXH_VECTOR == XXH_VSX)
+#elif (XXH_VECTOR == XXH_VSX) && /* work around a compiler bug */ (__GNUC__ > 5)
 
           U64x2* const xacc =       (U64x2*) acc;
     const U64x2* const xsecret = (const U64x2*) secret;
