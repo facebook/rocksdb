8787457626afbb4c11ba36668baf9f7cc6800e05
Cheng Chang <xcc@fb.com>
2020-03-12 12:06:46 -0700

    assert ok

--- examples/column_families_example.cc
+++ examples/column_families_example.cc
@@ -28,7 +28,8 @@ int main() {
   assert(s.ok());
 
   // close DB
-  db->DestroyColumnFamilyHandle(cf);
+  s = db->DestroyColumnFamilyHandle(cf);
+  assert(s.ok());
   delete db;
 
   // open DB with two column families
@@ -64,7 +65,8 @@ int main() {
 
   // close db
   for (auto handle : handles) {
-    db->DestroyColumnFamilyHandle(handle);
+    s = db->DestroyColumnFamilyHandle(handle);
+    assert(s.ok());
   }
   delete db;
 
