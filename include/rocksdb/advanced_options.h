2b416afe6404076f226c8099921d2bb75b69c73b
Ziyue Yang <v-ziyyan@microsoft.com>
2020-03-25 17:07:35 +0800

    Add an option for pipelined & parallel compression optimization
    Add an option for number of threads for pipelined & parallel compression
    optimization. The optimization is only enabled when the number of threads
    is larger than 1.
    The option is also added for db_bench.

--- include/rocksdb/advanced_options.h
+++ include/rocksdb/advanced_options.h
@@ -117,6 +117,22 @@ struct CompressionOptions {
   // Default: 0.
   uint32_t zstd_max_train_bytes;
 
+  // Number of threads for parallel compression.
+  // Parallel compression is enabled only if threads > 1.
+  //
+  // This option is valid only when BlockBasedTable is used.
+  //
+  // When parallel compression is enabled, SST size estimation becomes less
+  // accurate, because block building and compression are pipelined, and there
+  // might be inflight blocks being compressed and not finally written, when
+  // current SST size is fetched. This brings inflation of final output file
+  // size.
+  // To be more accurate, this inflation is also estimated by using historical
+  // compression ratio and current bytes inflight.
+  //
+  // Default: 1.
+  uint32_t parallel_threads;
+
   // When the compression options are set by the user, it will be set to "true".
   // For bottommost_compression_opts, to enable it, user must set enabled=true.
   // Otherwise, bottommost compression will use compression_opts as default
@@ -134,14 +150,17 @@ struct CompressionOptions {
         strategy(0),
         max_dict_bytes(0),
         zstd_max_train_bytes(0),
+        parallel_threads(1),
         enabled(false) {}
   CompressionOptions(int wbits, int _lev, int _strategy, int _max_dict_bytes,
-                     int _zstd_max_train_bytes, bool _enabled)
+                     int _zstd_max_train_bytes, int _parallel_threads,
+                     bool _enabled)
       : window_bits(wbits),
         level(_lev),
         strategy(_strategy),
         max_dict_bytes(_max_dict_bytes),
         zstd_max_train_bytes(_zstd_max_train_bytes),
+        parallel_threads(_parallel_threads),
         enabled(_enabled) {}
 };
 
