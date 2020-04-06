21e5bf0d934b67591730945170946833fc9e3642
mrambacher <mrambach@gmail.com>
2020-04-06 15:24:16 -0400

    Change the linking of tests/tools to be against a library rather than a list of objects.  This change substantially reduces the size of the objects produced.
    Added OBJ_DIR option to Makefile to allow objects to be placed in an alternative location.  This value is currently unused (set to the current directory) but will allow objects to be placed in different directories to simplify the build process when building multiple targets (like LITE, release/debug, shared/static, java, etc).

--- src.mk
+++ src.mk
@@ -172,7 +172,6 @@ LIB_SOURCES =                                                   \
   table/two_level_iterator.cc                                   \
   test_util/sync_point.cc                                       \
   test_util/sync_point_impl.cc                                  \
-  test_util/transaction_test_util.cc                            \
   tools/dump/db_dump_tool.cc                                    \
   trace_replay/trace_replay.cc                                  \
   trace_replay/block_cache_tracer.cc                            \
@@ -297,6 +296,7 @@ TEST_LIB_SOURCES =                                              \
   db/db_test_util.cc                                            \
   test_util/testharness.cc                                      \
   test_util/testutil.cc                                         \
+  test_util/transaction_test_util.cc                            \
   utilities/cassandra/test_utils.cc                             \
 
 FOLLY_SOURCES = \
@@ -306,10 +306,32 @@ FOLLY_SOURCES = \
   third-party/folly/folly/synchronization/ParkingLot.cpp                       \
   third-party/folly/folly/synchronization/WaitOptions.cpp                      \
 
-MAIN_SOURCES =                                                          \
-  cache/cache_bench.cc                                                  \
-  cache/cache_test.cc                                                   \
+TOOLS_MAIN_SOURCES =                                                    \
   db_stress_tool/db_stress.cc                                           \
+  tools/blob_dump.cc                                                    \
+  tools/block_cache_analyzer/block_cache_trace_analyzer_tool.cc         \
+  tools/db_repl_stress.cc                                               \
+  tools/db_sanity_test.cc                                               \
+  tools/ldb.cc                                                          \
+  tools/sst_dump.cc                                                     \
+  tools/write_stress.cc                                                 \
+  tools/dump/rocksdb_dump.cc                                            \
+  tools/dump/rocksdb_undump.cc                                          \
+  tools/trace_analyzer.cc                                               \
+
+BENCH_MAIN_SOURCES =                                                    \
+  cache/cache_bench.cc                                                  \
+  db/range_del_aggregator_bench.cc                                      \
+  memtable/memtablerep_bench.cc                                         \
+  table/table_reader_bench.cc                                           \
+  tools/db_bench.cc                                                     \
+  util/filter_bench.cc                                                  \
+  utilities/persistent_cache/persistent_cache_bench.cc                  \
+  #util/log_write_bench.cc                                               \
+
+TEST_MAIN_SOURCES =                                                     \
+  cache/cache_test.cc                                                   \
+  cache/lru_cache_test.cc                                               \
   db/blob/blob_file_addition_test.cc                                    \
   db/blob/blob_file_garbage_test.cc                                     \
   db/blob/db_blob_index_test.cc                                         \
@@ -331,6 +353,7 @@ MAIN_SOURCES =                                                          \
   db/db_dynamic_level_test.cc                                           \
   db/db_encryption_test.cc                                              \
   db/db_flush_test.cc                                                   \
+  db/import_column_family_test.cc                                       \
   db/db_inplace_update_test.cc                                          \
   db/db_io_failure_test.cc                                              \
   db/db_iter_test.cc                                                    \
@@ -357,7 +380,6 @@ MAIN_SOURCES =                                                          \
   db/db_write_test.cc                                                   \
   db/dbformat_test.cc                                                   \
   db/deletefile_test.cc                                                 \
-  db/env_timed_test.cc                                                  \
   db/error_handler_fs_test.cc						\
   db/external_sst_file_basic_test.cc                                    \
   db/external_sst_file_test.cc                                          \
@@ -366,29 +388,21 @@ MAIN_SOURCES =                                                          \
   db/file_reader_writer_test.cc                                         \
   db/filename_test.cc                                                   \
   db/flush_job_test.cc                                                  \
-  db/hash_table_test.cc                                                 \
-  db/hash_test.cc                                                       \
-  db/heap_test.cc                                                       \
   db/listener_test.cc                                                   \
   db/log_test.cc                                                        \
-  db/lru_cache_test.cc                                                  \
   db/manual_compaction_test.cc                                          \
   db/memtable_list_test.cc                                              \
   db/merge_helper_test.cc                                               \
   db/merge_test.cc                                                      \
   db/obsolete_files_test.cc						                                  \
-  db/options_settable_test.cc                                           \
   db/options_file_test.cc                                               \
   db/perf_context_test.cc                                               \
-  db/persistent_cache_test.cc                                           \
   db/plain_table_db_test.cc                                             \
   db/prefix_test.cc                                                     \
   db/repair_test.cc                                                     \
   db/range_del_aggregator_test.cc                                       \
-  db/range_del_aggregator_bench.cc                                      \
   db/range_tombstone_fragmenter_test.cc                                 \
   db/table_properties_collector_test.cc                                 \
-  db/util_merge_operators_test.cc                                       \
   db/version_builder_test.cc                                            \
   db/version_edit_test.cc                                               \
   db/version_set_test.cc                                                \
@@ -400,6 +414,7 @@ MAIN_SOURCES =                                                          \
   env/env_test.cc                                                       \
   env/io_posix_test.cc                                                  \
   env/mock_env_test.cc                                                  \
+  file/delete_scheduler_test.cc                                         \
   file/random_access_file_reader_test.cc                                \
   logging/auto_roll_logger_test.cc                                      \
   logging/env_logger_test.cc                                            \
@@ -407,13 +422,13 @@ MAIN_SOURCES =                                                          \
   memory/arena_test.cc                                                  \
   memory/memkind_kmem_allocator_test.cc                                 \
   memtable/inlineskiplist_test.cc                                       \
-  memtable/memtablerep_bench.cc                                         \
   memtable/skiplist_test.cc                                             \
   memtable/write_buffer_manager_test.cc                                 \
   monitoring/histogram_test.cc                                          \
   monitoring/iostats_context_test.cc                                    \
   monitoring/statistics_test.cc                                         \
   monitoring/stats_history_test.cc                                      \
+  options/options_settable_test.cc                                      \
   options/options_test.cc                                               \
   table/block_based/block_based_filter_block_test.cc                    \
   table/block_based/block_based_table_reader_test.cc                    \
@@ -426,15 +441,9 @@ MAIN_SOURCES =                                                          \
   table/cuckoo/cuckoo_table_reader_test.cc                              \
   table/merger_test.cc                                                  \
   table/sst_file_reader_test.cc                                         \
-  table/table_reader_bench.cc                                           \
   table/table_test.cc                                                   \
   table/block_fetcher_test.cc                                           \
-  third-party/gtest-1.8.1/fused-src/gtest/gtest-all.cc                  \
   tools/block_cache_analyzer/block_cache_trace_analyzer_test.cc         \
-  tools/block_cache_analyzer/block_cache_trace_analyzer_tool.cc         \
-  tools/db_bench.cc                                                     \
-  tools/db_bench_tool_test.cc                                           \
-  tools/db_sanity_test.cc                                               \
   tools/ldb_cmd_test.cc                                                 \
   tools/reduce_levels_test.cc                                           \
   tools/sst_dump_test.cc                                                \
@@ -448,9 +457,11 @@ MAIN_SOURCES =                                                          \
   util/defer_test.cc                                                    \
   util/dynamic_bloom_test.cc                                            \
   util/filelock_test.cc                                                 \
-  util/log_write_bench.cc                                               \
-  util/rate_limiter_test.cc                                             \
+  util/file_reader_writer_test.cc                                       \
+  util/hash_test.cc                                                     \
+  util/heap_test.cc                                                     \
   util/random_test.cc                                                   \
+  util/rate_limiter_test.cc                                             \
   util/repeatable_thread_test.cc                                        \
   util/slice_test.cc                                                    \
   util/slice_transform_test.cc                                          \
@@ -466,11 +477,14 @@ MAIN_SOURCES =                                                          \
   utilities/cassandra/cassandra_row_merge_test.cc                       \
   utilities/cassandra/cassandra_serialize_test.cc                       \
   utilities/checkpoint/checkpoint_test.cc                               \
+  utilities/env_timed_test.cc                                           \
   utilities/memory/memory_test.cc                                       \
   utilities/merge_operators/string_append/stringappend_test.cc          \
   utilities/object_registry_test.cc                                     \
   utilities/option_change_migration/option_change_migration_test.cc     \
   utilities/options/options_util_test.cc                                \
+  utilities/persistent_cache/hash_table_test.cc                         \
+  utilities/persistent_cache/persistent_cache_test.cc                   \
   utilities/simulator_cache/cache_simulator_test.cc                     \
   utilities/simulator_cache/sim_cache_test.cc                           \
   utilities/table_properties_collectors/compact_on_deletion_collector_test.cc  \
@@ -480,7 +494,12 @@ MAIN_SOURCES =                                                          \
   utilities/transactions/write_prepared_transaction_test.cc             \
   utilities/transactions/write_unprepared_transaction_test.cc           \
   utilities/ttl/ttl_test.cc                                             \
+  utilities/util_merge_operators_test.cc                                \
   utilities/write_batch_with_index/write_batch_with_index_test.cc       \
+
+TEST_MAIN_SOURCES_C = \
+  db/c_test.c                                                           \
+
 
 JNI_NATIVE_SOURCES =                                          \
   java/rocksjni/backupenginejni.cc                            \
