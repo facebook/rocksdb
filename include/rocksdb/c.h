3ff603171dfe8fd1281adee168a34ae5af15aab0
Kirill Abrosimov <abrosimov-kirill@yandex.ru>
2020-04-07 14:43:23 -0700

    added new functions to c-api (#5630)
    Summary:
    Few functions from options added to C-api
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/5630
    
    Reviewed By: anand1976
    
    Differential Revision: D20896731
    
    Pulled By: ltamasi
    
    fbshipit-source-id: e4215a58b3c2429ec44e3f0d0381cbf86700fb14

--- include/rocksdb/c.h
+++ include/rocksdb/c.h
@@ -1796,6 +1796,13 @@ extern ROCKSDB_LIBRARY_API uint64_t
 rocksdb_approximate_memory_usage_get_cache_total(
     rocksdb_memory_usage_t* memory_usage);
 
+extern ROCKSDB_LIBRARY_API void rocksdb_options_set_dump_malloc_stats(
+    rocksdb_options_t*, unsigned char);
+
+extern ROCKSDB_LIBRARY_API void
+rocksdb_options_set_memtable_whole_key_filtering(rocksdb_options_t*,
+                                                 unsigned char);
+
 #ifdef __cplusplus
 }  /* end extern "C" */
 #endif
