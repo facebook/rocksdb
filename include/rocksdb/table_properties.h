b54712ed7dae888bae2f4213f11069105d6615ab
anand76 <anand76@devvm1373.frc2.facebook.com>
2020-04-13 19:56:02 -0700

    Log CompactOnDeletionCollectorFactory parameters on DB open (#6686)
    Summary:
    Log it in the info log to help in troubleshooting. It is logged as follows -
    ```
    2020/04/10-10:51:39.886662 7ffff7fef340                   Options.table_properties_collectors: CompactOnDeletionCollector (Sliding window size = 100 Deletion trigger = 90);
    ```
    
    Tests:
    make check
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/6686
    
    Reviewed By: ltamasi
    
    Differential Revision: D21002442
    
    Pulled By: anand1976
    
    fbshipit-source-id: 7adf0dbae7f1febcb00ce61fea5097118ede5c6a

--- include/rocksdb/table_properties.h
+++ include/rocksdb/table_properties.h
@@ -136,6 +136,11 @@ class TablePropertiesCollectorFactory {
 
   // The name of the properties collector can be used for debugging purpose.
   virtual const char* Name() const = 0;
+
+  // Can be overridden by sub-classes to return the Name, followed by
+  // configuration info that will // be logged to the info log when the
+  // DB is opened
+  virtual std::string ToString() const { return Name(); }
 };
 
 // TableProperties contains a bunch of read-only properties of its associated
