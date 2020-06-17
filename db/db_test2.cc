dbe39e5fcecdc9ad1117bed597b60bbb89220925
sdong <siying.d@fb.com>
2020-06-17 11:47:25 -0700

    Address comment.

--- db/db_test2.cc
+++ db/db_test2.cc
@@ -92,8 +92,7 @@ class TestReadOnlyWithCompressedCache
       public testing::WithParamInterface<std::tuple<int, bool>> {
  public:
   TestReadOnlyWithCompressedCache()
-      : DBTestBase("/test_readonly_with_compressed_cache") {}
-  void SetUp() override {
+      : DBTestBase("/test_readonly_with_compressed_cache") {
     max_open_files_ = std::get<0>(GetParam());
     use_mmap_ = std::get<1>(GetParam());
   }
@@ -109,43 +108,42 @@ TEST_P(TestReadOnlyWithCompressedCache, ReadOnlyWithCompressedCache) {
   ASSERT_OK(Put("foo2", "barbarbarbarbarbarbarbar"));
   ASSERT_OK(Flush());
 
-      DB* db_ptr = nullptr;
-      Options options = CurrentOptions();
-      options.allow_mmap_reads = use_mmap_;
-      options.max_open_files = max_open_files_;
-      options.compression = kSnappyCompression;
-      BlockBasedTableOptions table_options;
-      table_options.block_cache_compressed = NewLRUCache(8 * 1024 * 1024);
-      table_options.no_block_cache = true;
-      options.table_factory.reset(NewBlockBasedTableFactory(table_options));
-      options.statistics = CreateDBStatistics();
+  DB* db_ptr = nullptr;
+  Options options = CurrentOptions();
+  options.allow_mmap_reads = use_mmap_;
+  options.max_open_files = max_open_files_;
+  options.compression = kSnappyCompression;
+  BlockBasedTableOptions table_options;
+  table_options.block_cache_compressed = NewLRUCache(8 * 1024 * 1024);
+  table_options.no_block_cache = true;
+  options.table_factory.reset(NewBlockBasedTableFactory(table_options));
+  options.statistics = CreateDBStatistics();
 
-      ASSERT_OK(DB::OpenForReadOnly(options, dbname_, &db_ptr));
+  ASSERT_OK(DB::OpenForReadOnly(options, dbname_, &db_ptr));
 
-      std::string v;
-      ASSERT_OK(db_ptr->Get(ReadOptions(), "foo", &v));
-      ASSERT_EQ("bar", v);
+  std::string v;
+  ASSERT_OK(db_ptr->Get(ReadOptions(), "foo", &v));
+  ASSERT_EQ("bar", v);
+  ASSERT_EQ(0, options.statistics->getTickerCount(BLOCK_CACHE_COMPRESSED_HIT));
+  ASSERT_OK(db_ptr->Get(ReadOptions(), "foo", &v));
+  ASSERT_EQ("bar", v);
+  if (Snappy_Supported()) {
+    if (use_mmap_) {
       ASSERT_EQ(0,
                 options.statistics->getTickerCount(BLOCK_CACHE_COMPRESSED_HIT));
-      ASSERT_OK(db_ptr->Get(ReadOptions(), "foo", &v));
-      ASSERT_EQ("bar", v);
-      if (Snappy_Supported()) {
-        if (use_mmap_) {
-          ASSERT_EQ(0, options.statistics->getTickerCount(
-                           BLOCK_CACHE_COMPRESSED_HIT));
-        } else {
-          ASSERT_EQ(1, options.statistics->getTickerCount(
-                           BLOCK_CACHE_COMPRESSED_HIT));
-        }
-      }
+    } else {
+      ASSERT_EQ(1,
+                options.statistics->getTickerCount(BLOCK_CACHE_COMPRESSED_HIT));
+    }
+  }
 
-      delete db_ptr;
+  delete db_ptr;
 }
 
-INSTANTIATE_TEST_CASE_P(
-    TestReadOnlyWithCompressedCache, TestReadOnlyWithCompressedCache,
-    ::testing::Values(std::make_tuple(-1, false), std::make_tuple(100, false),
-                      std::make_tuple(-1, true), std::make_tuple(100, false)));
+INSTANTIATE_TEST_CASE_P(TestReadOnlyWithCompressedCache,
+                        TestReadOnlyWithCompressedCache,
+                        ::testing::Combine(::testing::Values(-1, 100),
+                                           ::testing::Bool()));
 #endif  // ROCKSDB_LITE
 
 class PrefixFullBloomWithReverseComparator
