facebook/rocksdb

commit d739318ba7b3c78c193877956f3ac153969f8cc1

Fix double define in IO_tracer (#7007)
Summary:
Fix the following error

"./trace_replay/io_tracer.h:20:20: error: redefinition of ‘const unsigned int rocksdb::{anonymous}::kCharSize’
 const unsigned int kCharSize = 1;
                    ^~~~~~~~~
In file included from unity.cc:177:
trace_replay/block_cache_tracer.cc:22:20: note: ‘const unsigned int rocksdb::{anonymous}::kCharSize’ previously defined here
 const unsigned int kCharSize = 1;"
Pull Request resolved: https://github.com/facebook/rocksdb/pull/7007

Reviewed By: akankshamahajan15

Differential Revision: D22142618

Pulled By: zhichao-cao

fbshipit-source-id: e6dcd51ccc21d1f58df52cdc7a1c88e54cf4f6e8

trace_replay/io_tracer.cc
@@ -18,6 +18,10 @@
 
 namespace ROCKSDB_NAMESPACE {
 
+namespace {
+const unsigned int kCharSize = 1;
+}  // namespace
+
 IOTraceWriter::IOTraceWriter(Env* env, const TraceOptions& trace_options,
                              std::unique_ptr<TraceWriter>&& trace_writer)
     : env_(env),

trace_replay/io_tracer.h
@@ -16,10 +16,6 @@
 
 namespace ROCKSDB_NAMESPACE {
 
-namespace {
-const unsigned int kCharSize = 1;
-}  // namespace
-
 struct IOTraceRecord {
   // Required fields for all accesses.
   uint64_t access_timestamp = 0;

commit 56f6c28937ef7357a3a0ac5ad22a50cb825c29cd

Fix unity build broken by #7007
Summary: https://github.com/facebook/rocksdb/pull/7007 broken the unity build. Fix it by moving the const inside the function

Test Plan: make unity and see it to build.

trace_replay/block_cache_tracer.cc
@@ -19,8 +19,6 @@
 namespace ROCKSDB_NAMESPACE {
 
 namespace {
-const unsigned int kCharSize = 1;
-
 bool ShouldTrace(const Slice& block_key, const TraceOptions& trace_options) {
   if (trace_options.sampling_frequency == 0 ||
       trace_options.sampling_frequency == 1) {
@@ -216,6 +214,8 @@ Status BlockCacheTraceReader::ReadAccess(BlockCacheTraceRecord* record) {
   record->access_timestamp = trace.ts;
   record->block_type = trace.type;
   Slice enc_slice = Slice(trace.payload);
+
+  const unsigned int kCharSize = 1;
 
   Slice block_key;
   if (!GetLengthPrefixedSlice(&enc_slice, &block_key)) {

trace_replay/io_tracer.cc
@@ -17,11 +17,6 @@
 #include "util/string_util.h"
 
 namespace ROCKSDB_NAMESPACE {
-
-namespace {
-const unsigned int kCharSize = 1;
-}  // namespace
-
 IOTraceWriter::IOTraceWriter(Env* env, const TraceOptions& trace_options,
                              std::unique_ptr<TraceWriter>&& trace_writer)
     : env_(env),
@@ -143,6 +138,7 @@ Status IOTraceReader::ReadIOOp(IOTraceRecord* record) {
   record->file_name = file_name.ToString();
   // TODO: Read below options based on file_operation.
   record->len = static_cast<size_t>(enc_slice[0]);
+  const unsigned int kCharSize = 1;
   enc_slice.remove_prefix(kCharSize);
   if (enc_slice.empty()) {
     return Status::Incomplete(