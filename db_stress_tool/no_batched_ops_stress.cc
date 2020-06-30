5984c12fd265b25aad2af1b45f91cbdcefd5d1a6
Cheng Chang <xcc@fb.com>
2020-06-29 17:27:06 -0700

    add txn id as key's prefix to prevent deadlock

--- db_stress_tool/no_batched_ops_stress.cc
+++ db_stress_tool/no_batched_ops_stress.cc
@@ -268,7 +268,8 @@ class NonBatchedOpsStressTest : public StressTest {
     }
 #endif
     for (size_t i = 0; i < num_keys; ++i) {
-      key_str.emplace_back(Key(rand_keys[i]));
+      std::string key = txn->GetName() + "-" + Key(rand_keys[i]);
+      key_str.emplace_back(key);
       keys.emplace_back(key_str.back());
 #ifndef ROCKSDB_LITE
       if (use_txn) {
