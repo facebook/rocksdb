21e5495ba7b67438aa7cf72ffed66597b302beba
wenh <wenh@devvm1252.frc0.facebook.com>
2020-07-02 17:58:09 -0700

    reverted changes to version_set; fixed a typo in previous commit

--- db/version_set.h
+++ db/version_set.h
@@ -963,9 +963,7 @@ class VersionSet {
 
   Status TryRecover(const std::vector<ColumnFamilyDescriptor>& column_families,
                     bool read_only, std::string* db_id,
-                    bool* has_missing_table_file,
-                    const std::vector<std::string>& dbname_children,
-                    const Status& dbname_children_s);
+                    bool* has_missing_table_file);
 
   // Try to recover the version set to the most recent consistent state
   // recorded in the specified manifest.
