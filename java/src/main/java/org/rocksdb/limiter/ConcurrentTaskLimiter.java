45663aa4e7d5920c1d64ed9c322d52dd814d458a
Tomasz Posłuszny <tefposluszny@gmail.com>
2020-09-03 13:29:49 +0200

    Implement ConcurrentTaskLimiter Java API

--- java/src/main/java/org/rocksdb/limiter/ConcurrentTaskLimiter.java
+++ java/src/main/java/org/rocksdb/limiter/ConcurrentTaskLimiter.java
@@ -3,7 +3,12 @@ package org.rocksdb.limiter;
 import org.rocksdb.RocksObject;
 
 public abstract class ConcurrentTaskLimiter extends RocksObject {
-    protected ConcurrentTaskLimiter(final long nativeHandle) {
-        super(nativeHandle);
-    }
+  protected ConcurrentTaskLimiter(final long nativeHandle) {
+    super(nativeHandle);
+  }
+
+  public abstract String name();
+  public abstract ConcurrentTaskLimiter setMaxOutstandingTask(final int maxOutstandinsTask);
+  public abstract ConcurrentTaskLimiter resetMaxOutstandingTask();
+  public abstract int outstandingTask();
 }
--- java/src/main/java/org/rocksdb/limiter/ConcurrentTaskLimiterImpl.java
+++ java/src/main/java/org/rocksdb/limiter/ConcurrentTaskLimiterImpl.java
@@ -1,14 +1,44 @@
 package org.rocksdb.limiter;
 
 public class ConcurrentTaskLimiterImpl extends ConcurrentTaskLimiter {
-    public ConcurrentTaskLimiterImpl(final String name,
-                                     final int maxOutstandingTask) {
-        super(newConcurrentTaskLimiterImpl0(name, maxOutstandingTask));
-    }
+  public ConcurrentTaskLimiterImpl(final String name,
+                                   final int maxOutstandingTask) {
+    super(newConcurrentTaskLimiterImpl0(name, maxOutstandingTask));
+  }
 
-    private native static long newConcurrentTaskLimiterImpl0(final String name,
-                                                             final int maxOutstandingTask);
+  @Override
+  public String name() {
+    assert (isOwningHandle());
+    return name(nativeHandle_);
+  }
 
-    @Override
-    protected final native void disposeInternal(final long handle);
+  @Override
+  public ConcurrentTaskLimiter setMaxOutstandingTask(final int maxOutstandinsTask) {
+    assert (isOwningHandle());
+    setMaxOutstandingTask(nativeHandle_, maxOutstandinsTask);
+    return this;
+  }
+
+  @Override
+  public ConcurrentTaskLimiter resetMaxOutstandingTask() {
+    assert (isOwningHandle());
+    resetMaxOutstandingTask(nativeHandle_);
+    return this;
+  }
+
+  @Override
+  public int outstandingTask() {
+    assert (isOwningHandle());
+    return outstandingTask(nativeHandle_);
+  }
+
+  private static native long newConcurrentTaskLimiterImpl0(
+          final String name, final int maxOutstandingTask);
+  private static native String name(final long handle);
+  private static native void setMaxOutstandingTask(final long handle, final int limit);
+  private static native void resetMaxOutstandingTask(final long handle);
+  private static native int outstandingTask(final long handle);
+
+  @Override
+  protected final native void disposeInternal(final long handle);
 }
