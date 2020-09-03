eeb2a46771e9c7eb3701bf92cf77df534dc90e95
Tomasz Posłuszny <tefposluszny@gmail.com>
2020-09-03 16:00:21 +0200

    Implement JNI for ConcurrentTaskLimiter

--- java/rocksjni/concurrent_task_limiter.cc
+++ java/rocksjni/concurrent_task_limiter.cc
@@ -28,6 +28,54 @@ jlong Java_org_rocksdb_limiter_ConcurrentTaskLimiterImpl_newConcurrentTaskLimite
 
 /*
  * Class:     org_rocksdb_limiter_ConcurrentTaskLimiterImpl
+ * Method:    name
+ * Signature: (J)Ljava/lang/String;
+ */
+jstring Java_org_rocksdb_limiter_ConcurrentTaskLimiterImpl_name
+  (JNIEnv *env, jclass, jlong handle) {
+  const auto& limiter =
+      *reinterpret_cast<std::shared_ptr<ROCKSDB_NAMESPACE::ConcurrentTaskLimiter>*>(handle);
+  return ROCKSDB_NAMESPACE::JniUtil::toJavaString(env, &limiter->GetName());
+}
+
+/*
+ * Class:     org_rocksdb_limiter_ConcurrentTaskLimiterImpl
+ * Method:    setMaxOutstandingTask
+ * Signature: (JI)V
+ */
+void Java_org_rocksdb_limiter_ConcurrentTaskLimiterImpl_setMaxOutstandingTask
+  (JNIEnv *, jclass, jlong handle, jint max_outstanding_task) {
+  const auto& limiter =
+      *reinterpret_cast<std::shared_ptr<ROCKSDB_NAMESPACE::ConcurrentTaskLimiter>*>(handle);
+  limiter->SetMaxOutstandingTask(static_cast<int32_t>(max_outstanding_task));
+}
+
+/*
+ * Class:     org_rocksdb_limiter_ConcurrentTaskLimiterImpl
+ * Method:    resetMaxOutstandingTask
+ * Signature: (J)V
+ */
+void Java_org_rocksdb_limiter_ConcurrentTaskLimiterImpl_resetMaxOutstandingTask
+  (JNIEnv *, jclass, jlong handle) {
+  const auto& limiter =
+      *reinterpret_cast<std::shared_ptr<ROCKSDB_NAMESPACE::ConcurrentTaskLimiter>*>(handle);
+  limiter->ResetMaxOutstandingTask();
+}
+
+/*
+ * Class:     org_rocksdb_limiter_ConcurrentTaskLimiterImpl
+ * Method:    outstandingTask
+ * Signature: (J)I
+ */
+jint Java_org_rocksdb_limiter_ConcurrentTaskLimiterImpl_outstandingTask
+  (JNIEnv *, jclass, jlong handle) {
+  const auto& limiter =
+      *reinterpret_cast<std::shared_ptr<ROCKSDB_NAMESPACE::ConcurrentTaskLimiter>*>(handle);
+  return static_cast<jint>(limiter->GetOutstandingTask());
+}
+
+/*
+ * Class:     org_rocksdb_limiter_ConcurrentTaskLimiterImpl
  * Method:    disposeInternal
  * Signature: (J)V
  */
