8a6c925ca7b31cff42f1e053b13a79e4713964a1
Peter Dillinger <peterd@fb.com>
2020-09-15 22:10:18 -0700

    Restore file size in backup table file names (and other cleanup)
    Summary: Prior to 6.12, backup files using share_files_with_checksum had
    the file size encoded in the file name, after the last '_' and before
    the last '.'. We considered this an implementation detail subject to
    change, and indeed removed this information from the file name (with an
    option to use old behavior) because it was considered
    ineffective/inefficient for file name uniqueness. However, some
    downstream RocksDB users were relying on this information since the file
    size is not explicitly in the backup manifest file.
    
    This primary purpose of this change is "retrofitting" the 6.12 release
    (not yet a public release) to simultaneously support the benefits of the
    new naming scheme (I/O performance and data correctness at scale) and
    preserve the file size information, both as default behaviors. With this
    change, we are essentially making the file size information encoded in
    the file name an official, though obscure, extension of the backup meta
    file format.
    
    We preserve an option (kLegacyCrc32cAndFileSize) to use the original
    "legacy" naming scheme, with its caveats, and make it easy to omit the
    file size information (no kFlagIncludeFileSize), for more compact file
    names. But note that changing the naming scheme used on an existing db
    and backup directory can lead to transient space amplification, as some
    files will be stored under two names in the shared_checksum directory.
    Because some backups were saved using the original 6.12 naming scheme,
    we offer two ways of dealing with those files: SST files generated by
    older 6.12 versions can either use the default naming scheme in effect
    when the SST files were generated (kFlagMatchInterimNaming, default, no
    transient space amplification) or can use a new naming scheme (no
    kFlagMatchInterimNaming, potential space amplification because some
    already stored files getting a new name).
    
    We don't have a natural way to detect which files were generated by
    previous 6.12 versions, but this change hacks one in by changing DB
    session ids to now use a more concise encoding, reducing file name
    length, saving ~dozen bytes from SST files, and making them visually
    distinct from DB ids so that they are less likely to be mixed up.
    
    Finally, recognizing that the backup file names have become a de facto
    part of the backup meta schema, this change makes them easier to parse
    and extend by putting a distinct marker, 's', before DB session ids
    embedded in the name. When we extend this to allow custom checksums in
    the name, they can get their own marker to ensure safe parsing. For
    backward compatibility, file size does not get a marker but is assumed
    for _[0-9]+[.]
    
    Test Plan: unit tests included. Sync point callbacks are used to mimic
    previous version SST files.

--- db/db_basic_test.cc
+++ db/db_basic_test.cc
@@ -8,6 +8,7 @@
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
 #include <cstring>
+#include <regex>
 
 #include "db/db_test_util.h"
 #include "port/stack_trace.h"
@@ -61,6 +62,13 @@ TEST_F(DBBasicTest, UniqueSession) {
   ASSERT_NE(sid2, sid3);
 
   ASSERT_EQ(sid2, sid4);
+
+  // Expected compact format for session ids (see notes in implementation)
+  std::regex expected("[0-9A-Z]{20}");
+  const std::string match("match");
+  EXPECT_EQ(match, std::regex_replace(sid1, expected, match));
+  EXPECT_EQ(match, std::regex_replace(sid2, expected, match));
+  EXPECT_EQ(match, std::regex_replace(sid3, expected, match));
 
 #ifndef ROCKSDB_LITE
   Close();
