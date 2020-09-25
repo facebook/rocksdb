e276c6ef198a62a74cbfe7f43b2c706ed7f2f1ef
Yanqin Jin <yanqin@fb.com>
2020-09-25 11:18:44 -0700

    Fix format

--- db/version_edit_handler.cc
+++ db/version_edit_handler.cc
@@ -7,9 +7,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#include <cinttypes>
-
 #include "db/version_edit_handler.h"
+
+#include <cinttypes>
 
 #include "monitoring/persistent_stats_history.h"
 
