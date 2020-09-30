9f1ab41b4f2b599b1fdb02d679bc9d4f70e6cac5
Ramkumar Vadivelu <ramvadiv@fb.com>
2020-09-30 15:02:42 -0700

    reverting an extra check in db_iter_test.cc to see whether it contributed to circleci: build-linux-shared_lib-alt_namespace-status_checked test failure

--- db/db_iter_test.cc
+++ db/db_iter_test.cc
@@ -100,8 +100,9 @@ class TestIterator : public InternalIterator {
     for (auto it = data_.begin(); it != data_.end(); ++it) {
       ParsedInternalKey ikey;
       Status pikStatus = ParseInternalKey(it->first, &ikey);
+      pikStatus.PermitUncheckedError();
       assert(pikStatus.ok());
-      if (!pikStatus.ok() || ikey.user_key != _key) {
+      if (ikey.user_key != _key) {
         continue;
       }
       if (valid_ && data_.begin() + iter_ > it) {
