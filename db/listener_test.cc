6d91c1ffe389dbeab390b56a75f85cf4872de18d
mrambacher <mrambach@gmail.com>
2020-10-19 05:57:32 -0400

    Fix Circile CI Build issues

--- db/listener_test.cc
+++ db/listener_test.cc
@@ -676,7 +676,7 @@ class TableFileCreationListener : public EventListener {
  public:
   class TestEnv : public EnvWrapper {
    public:
-    TestEnv(Env* target) : EnvWrapper(target) {}
+    TestEnv(Env* t) : EnvWrapper(t) {}
 
     void SetStatus(Status s) { status_ = s; }
 
@@ -1105,25 +1105,6 @@ TEST_F(EventListenerTest, OnFileOperationTest) {
 }  // namespace ROCKSDB_NAMESPACE
 
 #endif  // ROCKSDB_LITE
-
-#include <execinfo.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-void handler(int sig) {
-  void* array[10];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 10);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, (int)size, STDERR_FILENO);
-  exit(1);
-}
 
 int main(int argc, char** argv) {
   signal(SIGSEGV, handler);
