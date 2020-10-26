3fad133f83c06349f36f014f81c13f29af6607d9
Jan <jsteemann@users.noreply.github.com>
2020-10-26 20:21:55 +0100

    Update include/rocksdb/memory_allocator.h

--- include/rocksdb/memory_allocator.h
+++ include/rocksdb/memory_allocator.h
@@ -69,7 +69,7 @@ struct JemallocAllocatorOptions {
 // To mitigate mutex contention for using one single arena, jemalloc tcache
 // (thread-local cache) is enabled to cache unused allocations for future use.
 // The tcache normally incurs 0.5M extra memory usage per-thread. The usage
-// can be reduced by limitting allocation sizes to cache.
+// can be reduced by limiting allocation sizes to cache.
 extern Status NewJemallocNodumpAllocator(
     JemallocAllocatorOptions& options,
     std::shared_ptr<MemoryAllocator>* memory_allocator);
