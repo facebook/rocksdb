fde0cd7ced0035f7c3ca276c372a28c55775d7c3
Yanqin Jin <yanqin@fb.com>
2020-11-03 20:33:45 -0800

    Add API to verify whole sst file checksum (#7578)
    Summary:
    Existing API `VerifyChecksum()` allows application to verify sst files' block checksums.
    Since whole file, user-specified checksum is tracked in MANIFEST, we can expose a new
    API to verify sst files' file checksums.
    
    ```
    // Compute table file checksums if applicable and compare with MANIFEST.
    // Returns OK if no file has mismatching whole-file checksum.
    Status DB::VerifyFileChecksums(const ReadOptions& /*read_options*/);
    ```
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/7578
    
    Test Plan: make check
    
    Reviewed By: pdillinger
    
    Differential Revision: D24436783
    
    Pulled By: riversand963
    
    fbshipit-source-id: 52b51519b842f2b3c4e3351998a97c86cbec85b3

--- db/db_basic_test.cc
+++ db/db_basic_test.cc
@@ -3304,6 +3304,28 @@ TEST_F(DBBasicTest, ManifestWriteFailure) {
   Reopen(options);
 }
 
+#ifndef ROCKSDB_LITE
+TEST_F(DBBasicTest, VerifyFileChecksums) {
+  Options options = GetDefaultOptions();
+  options.create_if_missing = true;
+  options.env = env_;
+  DestroyAndReopen(options);
+  ASSERT_OK(Put("a", "value"));
+  ASSERT_OK(Flush());
+  ASSERT_TRUE(db_->VerifyFileChecksums(ReadOptions()).IsInvalidArgument());
+
+  options.file_checksum_gen_factory = GetFileChecksumGenCrc32cFactory();
+  Reopen(options);
+  ASSERT_OK(db_->VerifyFileChecksums(ReadOptions()));
+
+  // Write an L0 with checksum computed.
+  ASSERT_OK(Put("b", "value"));
+  ASSERT_OK(Flush());
+
+  ASSERT_OK(db_->VerifyFileChecksums(ReadOptions()));
+}
+#endif  // !ROCKSDB_LITE
+
 // A test class for intercepting random reads and injecting artificial
 // delays. Used for testing the deadline/timeout feature
 class DBBasicTestDeadline
