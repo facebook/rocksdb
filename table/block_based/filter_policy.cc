54818c4dab70cf63921a86c5bca2e195cd9fad8a
Peter Dillinger <peterd@fb.com>
2020-11-11 12:03:32 -0800

    Satisfy server formatter

--- table/block_based/filter_policy.cc
+++ table/block_based/filter_policy.cc
@@ -7,10 +7,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
+#include "rocksdb/filter_policy.h"
+
 #include <array>
 #include <deque>
-
-#include "rocksdb/filter_policy.h"
 
 #include "rocksdb/slice.h"
 #include "table/block_based/block_based_filter_block.h"
