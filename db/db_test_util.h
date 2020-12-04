8830c3ab479af4b5a0f3cf1f9d8e4ea41620c404
Andrew Kryczka <andrewkr@fb.com>
2020-12-04 15:26:40 -0800

    remove CalculateKvProtectionInfo

--- db/db_test_util.h
+++ db/db_test_util.h
@@ -110,11 +110,6 @@ struct OptionsOverride {
 
 }  // namespace anon
 
-KvProtectionInfo CalculateKvProtectionInfo(const std::string& key,
-                                           const std::string& value,
-                                           SequenceNumber seqno,
-                                           ValueType value_type);
-
 enum SkipPolicy { kSkipNone = 0, kSkipNoSnapshot = 1, kSkipNoPrefix = 2 };
 
 // A hacky skip list mem table that triggers flush after number of entries.
