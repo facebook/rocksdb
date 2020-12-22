459ff1c5ca56ac0c9de95c3516a5c68bbb2ebf5c
anand76 <anand76@devvm1373.frc2.facebook.com>
2020-12-22 11:47:54 -0800

    Remove some changes made for testing purposes

--- tools/regression_test.sh
+++ tools/regression_test.sh
@@ -137,11 +137,11 @@ function main {
   fi
   if [ $TEST_MODE -ge 1 ]; then
       build_checkpoint
-      #run_db_bench "readrandom"
-      #run_db_bench "readwhilewriting"
-      #run_db_bench "deleterandom" $((NUM_KEYS / 10 / $NUM_THREADS))
-      #run_db_bench "seekrandom"
-      #run_db_bench "seekrandomwhilewriting"
+      run_db_bench "readrandom"
+      run_db_bench "readwhilewriting"
+      run_db_bench "deleterandom" $((NUM_KEYS / 10 / $NUM_THREADS))
+      run_db_bench "seekrandom"
+      run_db_bench "seekrandomwhilewriting"
       run_db_bench "multireadrandom" 
   fi
 
