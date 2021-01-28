a53af0ac682b21312492e342784f8f2483268bb8
sdong <siying.d@fb.com>
2021-01-28 14:46:45 -0800

    Make PartitionedIndexUserToInternalKey more robust

--- db/column_family.cc
+++ db/column_family.cc
@@ -215,7 +215,7 @@ ColumnFamilyOptions SanitizeOptions(const ImmutableDBOptions& db_options,
   // calculate a proper value from writer_buffer_size;
   if (result.arena_block_size <= 0) {
     result.arena_block_size =
-      std::min(size_t{1024 * 1024}, result.write_buffer_size / 8);
+        std::min(size_t{1024 * 1024}, result.write_buffer_size / 8);
 
     // Align up to 4k
     const size_t align = 4 * 1024;
