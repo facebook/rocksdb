43031a8b764e7573057f2b11ffbf0faa34c88bc3
Jay Zhuang <zjay@fb.com>
2021-02-02 10:25:19 -0800

    Revert "timestamp can only be increased"
    This reverts commit 62db8f245ed7659f25426f68009b63039804e936.

--- db/db_impl/db_impl_compaction_flush.cc
+++ db/db_impl/db_impl_compaction_flush.cc
@@ -816,9 +816,9 @@ Status DBImpl::IncreaseFullHistoryTsLow(ColumnFamilyData* cfd,
   std::string current_ts_low = cfd->GetFullHistoryTsLow();
   const Comparator* ucmp = cfd->user_comparator();
   if (!current_ts_low.empty() &&
-      ucmp->CompareTimestamp(ts_low, current_ts_low) <= 0) {
+      ucmp->CompareTimestamp(ts_low, current_ts_low) < 0) {
     return Status::InvalidArgument(
-        "Invalid full_history_timestamp_low, it must be grater than the current value");
+        "Cannot decrease full_history_timestamp_low");
   }
 
   return versions_->LogAndApply(cfd, *cfd->GetLatestMutableCFOptions(), &edit,
