1f11d07f242e4c135cd0da1125ee3a1ec16aeecb
Yanqin Jin <yanqin@fb.com>
2021-03-01 17:23:11 -0800

    Enable compact filter for blob in dbstress and dbbench (#8011)
    Summary:
    As title.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/8011
    
    Test Plan:
    ```
    ./db_bench -enable_blob_files=1 -use_keep_filter=1 -disable_auto_compactions=1
    /db_stress -enable_blob_files=1 -enable_compaction_filter=1 -acquire_snapshot_one_in=0 -compact_range_one_in=0 -iterpercent=0 -test_batches_snapshots=0 -readpercent=10 -prefixpercent=20 -writepercent=55 -delpercent=15 -continuous_verification_interval=0
    ```
    
    Reviewed By: ltamasi
    
    Differential Revision: D26736061
    
    Pulled By: riversand963
    
    fbshipit-source-id: 1c7834903c28431ce23324c4f259ed71255614e2

--- db_stress_tool/db_stress_test_base.cc
+++ db_stress_tool/db_stress_test_base.cc
@@ -242,9 +242,13 @@ void StressTest::FinishInitDb(SharedState* shared) {
     PreloadDbAndReopenAsReadOnly(FLAGS_max_key, shared);
   }
   if (FLAGS_enable_compaction_filter) {
-    reinterpret_cast<DbStressCompactionFilterFactory*>(
-        options_.compaction_filter_factory.get())
-        ->SetSharedState(shared);
+    auto* compaction_filter_factory =
+        reinterpret_cast<DbStressCompactionFilterFactory*>(
+            options_.compaction_filter_factory.get());
+    assert(compaction_filter_factory);
+    compaction_filter_factory->SetSharedState(shared);
+    fprintf(stdout, "Compaction filter factory: %s\n",
+            compaction_filter_factory->Name());
   }
 }
 
@@ -2218,12 +2222,11 @@ void StressTest::Open() {
 
   if ((options_.enable_blob_files || options_.enable_blob_garbage_collection ||
        FLAGS_allow_setting_blob_options_dynamically) &&
-      (FLAGS_use_merge || FLAGS_enable_compaction_filter ||
-       FLAGS_backup_one_in > 0 || FLAGS_best_efforts_recovery)) {
-    fprintf(
-        stderr,
-        "Integrated BlobDB is currently incompatible with Merge, compaction "
-        "filters, backup/restore, and best-effort recovery\n");
+      (FLAGS_use_merge || FLAGS_backup_one_in > 0 ||
+       FLAGS_best_efforts_recovery)) {
+    fprintf(stderr,
+            "Integrated BlobDB is currently incompatible with Merge, "
+            "backup/restore, and best-effort recovery\n");
     exit(1);
   }
 
