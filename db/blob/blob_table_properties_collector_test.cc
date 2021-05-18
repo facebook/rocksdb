e56bf7b0ebdfe9876724dc93c7dc1295058461fb
Levi Tamasi <ltamasi@fb.com>
2021-05-18 11:19:03 -0700

    Add BlobTablePropertiesCollectorTest.InternalAddMultipleAndFinish

--- db/blob/blob_table_properties_collector_test.cc
+++ db/blob/blob_table_properties_collector_test.cc
@@ -5,6 +5,7 @@
 
 #include "db/blob/blob_table_properties_collector.h"
 
+#include <algorithm>
 #include <string>
 #include <utility>
 #include <vector>
@@ -20,7 +21,8 @@ namespace ROCKSDB_NAMESPACE {
 
 class BlobTablePropertiesCollectorTest : public testing::Test {
  public:
-  using BlobStatsVec = std::vector<std::pair<uint64_t, BlobStats>>;
+  using BlobStatsPair = std::pair<uint64_t, BlobStats>;
+  using BlobStatsVec = std::vector<BlobStatsPair>;
 
   static Status Decode(const std::string& encoded, BlobStatsVec* decoded) {
     assert(decoded);
@@ -75,6 +77,59 @@ TEST_F(BlobTablePropertiesCollectorTest, InternalAddOneAndFinish) {
   ASSERT_EQ(stats[0].second.GetBytes(),
             size + BlobLogRecord::CalculateAdjustmentForRecordHeader(
                        sizeof(user_key) - 1));
+}
+
+TEST_F(BlobTablePropertiesCollectorTest, InternalAddMultipleAndFinish) {
+  BlobTablePropertiesCollector collector;
+
+  constexpr char user_key[] = "user_key";
+  constexpr uint64_t format_overhead =
+      BlobLogRecord::CalculateAdjustmentForRecordHeader(sizeof(user_key) - 1);
+  constexpr uint64_t file_size = 1000000;
+
+  // Add a total of four blob references, with two pointing to file #10 and two
+  // to file #11
+  for (uint64_t i = 0; i < 4; ++i) {
+    const SequenceNumber seq = 100 + i;
+
+    InternalKey key(user_key, seq, kTypeBlobIndex);
+    const Slice key_slice = key.Encode();
+
+    const uint64_t blob_file_number = 10 + i / 2;
+    const uint64_t offset = (i + 1) * 1000;
+    const uint64_t size = (i + 1) * 100;
+
+    std::string value;
+    BlobIndex::EncodeBlob(&value, blob_file_number, offset, size,
+                          kNoCompression);
+
+    const Slice value_slice(value);
+
+    ASSERT_OK(collector.InternalAdd(key_slice, value_slice, file_size));
+  }
+
+  UserCollectedProperties user_props;
+
+  ASSERT_OK(collector.Finish(&user_props));
+
+  auto it = user_props.find(TablePropertiesNames::kBlobFileMapping);
+  ASSERT_NE(it, user_props.end());
+
+  BlobStatsVec stats;
+  ASSERT_OK(Decode(it->second, &stats));
+
+  std::sort(stats.begin(), stats.end(),
+            [](const BlobStatsPair& lhs, const BlobStatsPair& rhs) {
+              return lhs.first < rhs.first;
+            });
+
+  ASSERT_EQ(stats.size(), 2);
+  ASSERT_EQ(stats[0].first, 10);
+  ASSERT_EQ(stats[0].second.GetCount(), 2);
+  ASSERT_EQ(stats[0].second.GetBytes(), 300 + 2 * format_overhead);
+  ASSERT_EQ(stats[1].first, 11);
+  ASSERT_EQ(stats[1].second.GetCount(), 2);
+  ASSERT_EQ(stats[1].second.GetBytes(), 700 + 2 * format_overhead);
 }
 
 }  // namespace ROCKSDB_NAMESPACE
