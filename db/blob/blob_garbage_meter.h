0d6df42d380d76e89f675b0e6475e6f50e95d7ef
Levi Tamasi <ltamasi@fb.com>
2021-05-21 14:02:04 -0700

    Turn Add{In,Out}Flow into Process{In,Out}Flow

--- db/blob/blob_garbage_meter.h
+++ db/blob/blob_garbage_meter.h
@@ -10,6 +10,10 @@
 #include <unordered_map>
 #include <vector>
 
+#include "db/blob/blob_constants.h"
+#include "db/blob/blob_index.h"
+#include "db/blob/blob_log_format.h"
+#include "db/dbformat.h"
 #include "rocksdb/rocksdb_namespace.h"
 
 namespace ROCKSDB_NAMESPACE {
@@ -34,8 +38,14 @@ class BlobGarbageMeter {
 
   class BlobInOutFlow {
    public:
-    void AddInFlow(uint64_t bytes) { in_flow_.Add(bytes); }
-    void AddOutFlow(uint64_t bytes) { out_flow_.Add(bytes); }
+    void AddInFlow(uint64_t bytes) {
+      in_flow_.Add(bytes);
+      assert(IsValid());
+    }
+    void AddOutFlow(uint64_t bytes) {
+      out_flow_.Add(bytes);
+      assert(IsValid());
+    }
 
     bool IsValid() const {
       return in_flow_.GetCount() >= out_flow_.GetCount() &&
@@ -62,15 +72,86 @@ class BlobGarbageMeter {
   };
 
  public:
-  void AddInFlow(uint64_t blob_file_number, uint64_t bytes) {
+  Status ProcessInFlow(const Slice& key, const Slice& value) {
+    uint64_t blob_file_number = kInvalidBlobFileNumber;
+    uint64_t bytes = 0;
+
+    const Status s = Parse(key, value, &blob_file_number, &bytes);
+    if (!s.ok()) {
+      return s;
+    }
+    if (blob_file_number == kInvalidBlobFileNumber) {
+      return Status::OK();
+    }
+
     flows_[blob_file_number].AddInFlow(bytes);
+
+    return Status::OK();
   }
 
-  void AddOutFlow(uint64_t blob_file_number, uint64_t bytes) {
-    flows_[blob_file_number].AddOutFlow(bytes);
+  Status ProcessOutFlow(const Slice& key, const Slice& value) {
+    uint64_t blob_file_number = kInvalidBlobFileNumber;
+    uint64_t bytes = 0;
+
+    const Status s = Parse(key, value, &blob_file_number, &bytes);
+    if (!s.ok()) {
+      return s;
+    }
+    if (blob_file_number == kInvalidBlobFileNumber) {
+      return Status::OK();
+    }
+
+    auto it = flows_.find(blob_file_number);
+    if (it == flows_.end()) {
+      return Status::OK();
+    }
+
+    it->second.AddOutFlow(bytes);
+
+    return Status::OK();
   }
 
  private:
+  static Status Parse(const Slice& key, const Slice& value,
+                      uint64_t* blob_file_number, uint64_t* bytes) {
+    assert(blob_file_number);
+    assert(bytes);
+
+    ParsedInternalKey ikey;
+
+    {
+      constexpr bool log_err_key = false;
+      const Status s = ParseInternalKey(key, &ikey, log_err_key);
+      if (!s.ok()) {
+        return s;
+      }
+    }
+
+    if (ikey.type != kTypeBlobIndex) {
+      return Status::OK();
+    }
+
+    BlobIndex blob_index;
+
+    {
+      const Status s = blob_index.DecodeFrom(value);
+      if (!s.ok()) {
+        return s;
+      }
+    }
+
+    if (blob_index.IsInlined() || blob_index.HasTTL()) {
+      return Status::Corruption("Unexpected TTL/inlined blob index");
+    }
+
+    *blob_file_number = blob_index.file_number();
+    *bytes =
+        blob_index.size() +
+        BlobLogRecord::CalculateAdjustmentForRecordHeader(ikey.user_key.size());
+
+    return Status::OK();
+  }
+
   std::unordered_map<uint64_t, BlobInOutFlow> flows_;
 };
 
