3469d60fccdba4e4f30b374cf28fd6fe42cfb092
Peter Dillinger <peterd@fb.com>
2021-05-21 17:10:29 -0700

    Add table properties for number of entries added to filters (#8323)
    Summary:
    With Ribbon filter work and possible variance in actual bits
    per key (or prefix; general term "entry") to achieve certain FP rates,
    I've received a request to be able to track actual bits per key in
    generated filters. This change adds a num_filter_entries table
    property, which can be combined with filter_size to get bits per key
    (entry).
    
    This can vary from num_entries in at least these ways:
    * Different versions of same key are only counted once in filters.
    * With prefix filters, several user keys map to the same filter entry.
    * A single filter can include both prefixes and user keys.
    
    Note that FilterBlockBuilder::NumAdded() didn't do anything useful
    except distinguish empty from non-empty.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/8323
    
    Test Plan: basic unit test included, others updated
    
    Reviewed By: jay-zhuang
    
    Differential Revision: D28596210
    
    Pulled By: pdillinger
    
    fbshipit-source-id: 529a111f3c84501e5a470bc84705e436ee68c376

--- table/block_based/block_based_filter_block.cc
+++ table/block_based/block_based_filter_block.cc
@@ -68,7 +68,7 @@ BlockBasedFilterBlockBuilder::BlockBasedFilterBlockBuilder(
       whole_key_filtering_(table_opt.whole_key_filtering),
       prev_prefix_start_(0),
       prev_prefix_size_(0),
-      num_added_(0) {
+      total_added_in_built_(0) {
   assert(policy_);
 }
 
@@ -78,6 +78,10 @@ void BlockBasedFilterBlockBuilder::StartBlock(uint64_t block_offset) {
   while (filter_index > filter_offsets_.size()) {
     GenerateFilter();
   }
+}
+
+size_t BlockBasedFilterBlockBuilder::EstimateEntriesAdded() {
+  return total_added_in_built_ + start_.size();
 }
 
 void BlockBasedFilterBlockBuilder::Add(const Slice& key_without_ts) {
@@ -92,7 +96,6 @@ void BlockBasedFilterBlockBuilder::Add(const Slice& key_without_ts) {
 
 // Add key to filter if needed
 inline void BlockBasedFilterBlockBuilder::AddKey(const Slice& key) {
-  num_added_++;
   start_.push_back(entries_.size());
   entries_.append(key.data(), key.size());
 }
@@ -118,6 +121,7 @@ Slice BlockBasedFilterBlockBuilder::Finish(const BlockHandle& /*tmp*/,
                                            Status* status) {
   // In this impl we ignore BlockHandle
   *status = Status::OK();
+
   if (!start_.empty()) {
     GenerateFilter();
   }
@@ -140,6 +144,7 @@ void BlockBasedFilterBlockBuilder::GenerateFilter() {
     filter_offsets_.push_back(static_cast<uint32_t>(result_.size()));
     return;
   }
+  total_added_in_built_ += num_entries;
 
   // Make list of keys from flattened key structure
   start_.push_back(entries_.size());  // Simplify length computation
