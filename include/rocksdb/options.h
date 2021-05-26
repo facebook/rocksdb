5b0fb4ccafc8beec2e5e561af8bc5115b3fda33b
sdong <siying.d@fb.com>
2021-05-25 17:08:35 -0700

    Improve comments of iterate_upper_bound
    Summary: ReadOptions.iterate_upper_bound's comment is confusing. Improve it.

--- include/rocksdb/options.h
+++ include/rocksdb/options.h
@@ -1332,9 +1332,15 @@ struct ReadOptions {
   // "iterate_upper_bound" defines the extent up to which the forward iterator
   // can returns entries. Once the bound is reached, Valid() will be false.
   // "iterate_upper_bound" is exclusive ie the bound value is
-  // not a valid entry. If prefix_extractor is not null, the Seek target
-  // and iterate_upper_bound need to have the same prefix.
-  // This is because ordering is not guaranteed outside of prefix domain.
+  // not a valid entry. If prefix_extractor is not null:
+  // 1. If options.auto_prefix_mode = true, iterate_upper_bound will be used
+  //    to infer prefix to use with the seek key.
+  // 2. If options.auto_prefix_mode = false, iterate_upper_bound only takes
+  //    effective if it shares the same prefix as the seek key. If
+  //    iterate_upper_bound is outside the same prefix, keys returned outside
+  //    the prefix range will be undefined, just as iterate_upper_bound = null.
+  // If iterate_upper_bound is not null, SeekToLast() will place to the
+  // position at the same key smaller than iterate_upper_bound.
   //
   // Default: nullptr
   const Slice* iterate_upper_bound;
