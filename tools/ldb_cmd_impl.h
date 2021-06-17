6e554d6e4726dce5f76829c8f113fe620d230ce2
Baptiste Lemaire <blemaire@fb.com>
2021-06-17 12:58:45 -0700

    Working version of list_live_files_metadata. Work with multiple column families.

--- tools/ldb_cmd_impl.h
+++ tools/ldb_cmd_impl.h
@@ -46,6 +46,20 @@ class DBFileDumperCommand : public LDBCommand {
   virtual void DoCommand() override;
 };
 
+class DBLiveFileMetadataDumperCommand : public LDBCommand {
+ public:
+  static std::string Name() { return "list_live_files_metadata"; }
+
+  DBLiveFileMetadataDumperCommand(
+      const std::vector<std::string>& params,
+      const std::map<std::string, std::string>& options,
+      const std::vector<std::string>& flags);
+
+  static void Help(std::string& ret);
+
+  virtual void DoCommand() override;
+};
+
 class DBDumperCommand : public LDBCommand {
  public:
   static std::string Name() { return "dump"; }
