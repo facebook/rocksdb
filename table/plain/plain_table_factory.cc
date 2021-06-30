9db1dd2b18e7708eeaa31946b4c094353171011c
mrambacher <mrambach@gmail.com>
2021-06-29 20:44:43 -0400

    Fix for new customizable_util helpers

--- table/plain/plain_table_factory.cc
+++ table/plain/plain_table_factory.cc
@@ -10,7 +10,6 @@
 #include <memory>
 
 #include "db/dbformat.h"
-#include "options/configurable_helper.h"
 #include "port/port.h"
 #include "rocksdb/convenience.h"
 #include "rocksdb/utilities/object_registry.h"
@@ -248,21 +247,11 @@ Status MemTableRepFactory::CreateFromString(
 #endif  // ROCKSDB_LITE
   std::string id;
   std::unordered_map<std::string, std::string> opt_map;
-  Status status =
-      ConfigurableHelper::GetOptionsMap(value, result->get(), &id, &opt_map);
+  Status status = Customizable::GetOptionsMap(config_options, result->get(),
+                                              value, &id, &opt_map);
   if (!status.ok()) {  // GetOptionsMap failed
     return status;
-  }
-  std::string curr_opts;
-#ifndef ROCKSDB_LITE
-  if (*result != nullptr && (*result)->GetId() == id) {
-    // Try to get the existing options, ignoring any errors
-    ConfigOptions embedded = config_options;
-    embedded.delimiter = ";";
-    (*result)->GetOptionString(embedded, &curr_opts).PermitUncheckedError();
-  }
-#endif
-  if (value.empty()) {
+  } else if (value.empty()) {
     // No Id and no options.  Clear the object
     result->reset();
     return Status::OK();
@@ -300,9 +289,16 @@ Status MemTableRepFactory::CreateFromString(
     } else {
       return status;
     }
-  } else if (!curr_opts.empty() || !opt_map.empty()) {
-    status = ConfigurableHelper::ConfigureNewObject(
-        config_options, result->get(), id, curr_opts, opt_map);
+  } else if (result->get() != nullptr) {
+    //**TODO: Use Customizable::NewObject
+    if (!opt_map.empty()) {
+      status = result->get()->ConfigureFromMap(config_options, opt_map);
+    }
+    if (status.ok() && config_options.invoke_prepare_options) {
+      status = result->get()->PrepareOptions(config_options);
+    }
+  } else if (!opt_map.empty()) {
+    status = Status::InvalidArgument("Cannot configure null object");
   }
   return status;
 }
