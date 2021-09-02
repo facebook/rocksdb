fcf3e35283e45ea3dd7c07a1d5a812b147119ded
tianyugou <>
2021-09-02 17:28:46 +0800

    fix v3

--- db/db_impl/db_impl.h
+++ db/db_impl/db_impl.h
@@ -149,6 +149,10 @@ class DBImpl : public DB {
   virtual Status Put(const WriteOptions& options,
                      ColumnFamilyHandle* column_family, const Slice& key,
                      const Slice& value) override;
+  virtual async_wal_result AsyncPut(const WriteOptions& options,
+                                    ColumnFamilyHandle* column_family,
+                                    const Slice& key,
+                                    const Slice& value) override;
   using DB::Merge;
   virtual Status Merge(const WriteOptions& options,
                        ColumnFamilyHandle* column_family, const Slice& key,
