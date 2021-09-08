91b95cadeea3cf551b9e74155e58c9a8de7f07eb
Hui Xiao <huixiao@fb.com>
2021-09-08 12:34:35 -0700

    Account for dictionary-building buffer in global memory limit (#8428)
    Summary:
    Context:
    Some data blocks are temporarily buffered in memory in BlockBasedTableBuilder for building compression dictionary used in data block compression. Currently this memory usage is not counted toward our global memory usage utilizing block cache capacity. To improve that, this PR charges that memory usage into the block cache to achieve better memory tracking and limiting.
    
    - Reserve memory in block cache for buffered data blocks that are used to build a compression dictionary
    - Release all the memory associated with buffering the data blocks mentioned above in EnterUnbuffered(), which is called when (a) buffer limit is exceeded after buffering OR (b) the block cache becomes full after reservation OR (c) BlockBasedTableBuilder calls Finish()
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/8428
    
    Test Plan:
    - Passing existing unit tests
    - Passing new unit tests
    
    Reviewed By: ajkr
    
    Differential Revision: D30755305
    
    Pulled By: hx235
    
    fbshipit-source-id: 6e66665020b775154a94c4c5e0f2adaeaff13981

--- cache/cache_entry_roles.cc
+++ cache/cache_entry_roles.cc
@@ -19,6 +19,7 @@ std::array<const char*, kNumCacheEntryRoles> kCacheEntryRoleToCamelString{{
     "IndexBlock",
     "OtherBlock",
     "WriteBuffer",
+    "CompressionDictionaryBuildingBuffer",
     "Misc",
 }};
 
@@ -30,6 +31,7 @@ std::array<const char*, kNumCacheEntryRoles> kCacheEntryRoleToHyphenString{{
     "index-block",
     "other-block",
     "write-buffer",
+    "compression-dictionary-building-buffer",
     "misc",
 }};
 
