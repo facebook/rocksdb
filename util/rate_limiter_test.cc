a91b76a0553119e03275e1c63c99b38cc7c42027
Hui Xiao <huixiao@fb.com>
2021-09-09 09:24:05 -0700

    Add assertion to verify callback is called

--- util/rate_limiter_test.cc
+++ util/rate_limiter_test.cc
@@ -97,7 +97,9 @@ TEST_F(RateLimiterTest, GetTotalPendingRequests) {
     ASSERT_EQ(limiter->GetTotalPendingRequests(static_cast<Env::IOPriority>(i)),
               0);
   }
-
+  // This is a variable for making sure the following callback is called 
+  // and the assertions in it are indeed excuted
+  bool nonzero_pending_requests_verified_ = false;
   SyncPoint::GetInstance()->SetCallBack(
       "GenericRateLimiter::Request:PostEnqueueRequest", [&](void* arg) {
         port::Mutex* request_mutex = (port::Mutex*)arg;
@@ -112,11 +114,13 @@ TEST_F(RateLimiterTest, GetTotalPendingRequests) {
         // We lock the mutex again so that the request thread can resume running
         // with the mutex locked
         request_mutex->Lock();
+        nonzero_pending_requests_verified_ = true;
       });
 
   SyncPoint::GetInstance()->EnableProcessing();
   limiter->Request(20, Env::IO_USER, nullptr /* stats */,
                    RateLimiter::OpType::kWrite);
+  ASSERT_EQ(nonzero_pending_requests_verified_, true);
   EXPECT_EQ(limiter->GetTotalPendingRequests(Env::IO_USER), 0);
   EXPECT_EQ(limiter->GetTotalPendingRequests(Env::IO_HIGH), 0);
   EXPECT_EQ(limiter->GetTotalPendingRequests(Env::IO_MID), 0);
