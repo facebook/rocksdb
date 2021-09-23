b1b6700ef9fae7f3e146b2300bfdea1569a0b1c1
Bijan Oviedo <boviedo@google.com>
2021-09-22 17:00:52 -0700

    crc32c

--- util/crc32c.h
+++ util/crc32c.h
@@ -12,17 +12,22 @@
 #include <stdint.h>
 #include <string>
 
+#include "crc32c/crc32c.h"
 #include "rocksdb/rocksdb_namespace.h"
 
 namespace ROCKSDB_NAMESPACE {
 namespace crc32c {
 
-extern std::string IsFastCrc32Supported();
+inline std::string IsFastCrc32Supported() {
+  return "Supported on x86";
+}
 
 // Return the crc32c of concat(A, data[0,n-1]) where init_crc is the
 // crc32c of some string A.  Extend() is often used to maintain the
 // crc32c of a stream of data.
-extern uint32_t Extend(uint32_t init_crc, const char* data, size_t n);
+inline uint32_t Extend(uint32_t init_crc, const char* data, size_t n) {
+  return ::crc32c::Extend(init_crc, reinterpret_cast<const uint8_t*>(data), n);
+}
 
 // Takes two unmasked crc32c values, and the length of the string from
 // which `crc2` was computed, and computes a crc32c value for the
