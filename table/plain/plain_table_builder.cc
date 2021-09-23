ef2e69599a5036caa0956be7fccc94907a302f90
Hui Xiao <huixiao@fb.com>
2021-09-23 11:08:08 -0700

    Make format

--- table/plain/plain_table_builder.cc
+++ table/plain/plain_table_builder.cc
@@ -59,8 +59,8 @@ extern const uint64_t kLegacyPlainTableMagicNumber = 0x4f3418eb7a8f13b8ull;
 PlainTableBuilder::PlainTableBuilder(
     const ImmutableOptions& ioptions, const MutableCFOptions& moptions,
     const IntTblPropCollectorFactories* int_tbl_prop_collector_factories,
-    uint32_t column_family_id, int level_at_creation, WritableFileWriter* file, uint32_t user_key_len,
-    EncodingType encoding_type, size_t index_sparseness,
+    uint32_t column_family_id, int level_at_creation, WritableFileWriter* file,
+    uint32_t user_key_len, EncodingType encoding_type, size_t index_sparseness,
     uint32_t bloom_bits_per_key, const std::string& column_family_name,
     uint32_t num_probes, size_t huge_page_tlb_size, double hash_table_ratio,
     bool store_index_in_file, const std::string& db_id,
