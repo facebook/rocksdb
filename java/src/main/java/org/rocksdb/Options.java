73e6b89fad5ff2d9a0fdb06bdf2dad5b7d9b2a8b
Alan Paxton <alan@isomaly.com>
2021-11-01 11:58:03 -0700

    Java wrapper for blob_gc_force_threshold as blobGarbageCollectionForceThreshold (#9109)
    Summary:
    Extra option added as a supplement to https://github.com/facebook/rocksdb/pull/8999
    
    Closes https://github.com/facebook/rocksdb/issues/8221
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/9109
    
    Reviewed By: mrambacher
    
    Differential Revision: D32065039
    
    Pulled By: ltamasi
    
    fbshipit-source-id: 6c484050a30fe0523850a8a3c95dc85b0a501362

--- java/src/main/java/org/rocksdb/Options.java
+++ java/src/main/java/org/rocksdb/Options.java
@@ -2077,7 +2077,7 @@ public class Options extends RocksObject
   }
 
   @Override
-  public Options setBlobGarbageCollectionAgeCutoff(double blobGarbageCollectionAgeCutoff) {
+  public Options setBlobGarbageCollectionAgeCutoff(final double blobGarbageCollectionAgeCutoff) {
     setBlobGarbageCollectionAgeCutoff(nativeHandle_, blobGarbageCollectionAgeCutoff);
     return this;
   }
@@ -2085,6 +2085,18 @@ public class Options extends RocksObject
   @Override
   public double blobGarbageCollectionAgeCutoff() {
     return blobGarbageCollectionAgeCutoff(nativeHandle_);
+  }
+
+  @Override
+  public Options setBlobGarbageCollectionForceThreshold(
+      final double blobGarbageCollectionForceThreshold) {
+    setBlobGarbageCollectionForceThreshold(nativeHandle_, blobGarbageCollectionForceThreshold);
+    return this;
+  }
+
+  @Override
+  public double blobGarbageCollectionForceThreshold() {
+    return blobGarbageCollectionForceThreshold(nativeHandle_);
   }
 
   //
@@ -2517,8 +2529,11 @@ public class Options extends RocksObject
       final long nativeHandle_, final boolean enableBlobGarbageCollection);
   private native boolean enableBlobGarbageCollection(final long nativeHandle_);
   private native void setBlobGarbageCollectionAgeCutoff(
-      final long nativeHandle_, double blobGarbageCollectionAgeCutoff);
+      final long nativeHandle_, final double blobGarbageCollectionAgeCutoff);
   private native double blobGarbageCollectionAgeCutoff(final long nativeHandle_);
+  private native void setBlobGarbageCollectionForceThreshold(
+      final long nativeHandle_, final double blobGarbageCollectionForceThreshold);
+  private native double blobGarbageCollectionForceThreshold(final long nativeHandle_);
 
   // instance variables
   // NOTE: If you add new member variables, please update the copy constructor above!
