a06bedcfc9284ac7508511b54b0405e74f8ea3bf
Adam Retter <adam.retter@googlemail.com>
2021-12-05 18:45:01 +0100

    Fix fstatfs call for compilation on 32 bit systems

--- env/io_posix.cc
+++ env/io_posix.cc
@@ -1543,7 +1543,11 @@ PosixDirectory::PosixDirectory(int fd) : fd_(fd) {
 #ifdef OS_LINUX
   struct statfs buf;
   int ret = fstatfs(fd, &buf);
+#if UINTPTR_MAX == 0xffffffff
+  is_btrfs_ = (ret == 0 && static_cast<uint32_t>(buf.f_type) == BTRFS_SUPER_MAGIC);
+#else
   is_btrfs_ = (ret == 0 && buf.f_type == BTRFS_SUPER_MAGIC);
+#endif
 #endif
 }
 
