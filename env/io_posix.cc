a6326e401de2f2eb47c178cb93289d56e206ecbf
Adam Retter <adam.retter@googlemail.com>
2021-12-07 10:47:34 +0100

    Make format

--- env/io_posix.cc
+++ env/io_posix.cc
@@ -1544,7 +1544,8 @@ PosixDirectory::PosixDirectory(int fd) : fd_(fd) {
   struct statfs buf;
   int ret = fstatfs(fd, &buf);
 #if UINTPTR_MAX == 0xffffffff
-  is_btrfs_ = (ret == 0 && static_cast<uint32_t>(buf.f_type) == BTRFS_SUPER_MAGIC);
+  is_btrfs_ =
+      (ret == 0 && static_cast<uint32_t>(buf.f_type) == BTRFS_SUPER_MAGIC);
 #else
   is_btrfs_ = (ret == 0 && buf.f_type == BTRFS_SUPER_MAGIC);
 #endif
