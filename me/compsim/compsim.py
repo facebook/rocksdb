fa4da76bd10452e3a23d8950e69abc7495c02b4a
Mark Callaghan <mdcallag@gmail.com>
2021-12-22 10:46:58 -0800

    updates

--- me/compsim/compsim.py
+++ me/compsim/compsim.py
@@ -28,18 +28,12 @@ def parse_options(argv):
   parser.add_argument('--run_time', type=int, default=5000,
                       help='Run for this many memtable flushes')
   parser.add_argument('--debug', default=False, action='store_true')
-  parser.add_argument('--keep_pct', type=int, default=90, choices=range(1,100),
+  parser.add_argument('--keep_pct', type=int, default=100, choices=range(1,100),
                       help='Percentage of the compaction input that is kept because it is redundant. '\
                            'Must be 0 for workload=insert and in [1,100] for workload=overwrite.')
-  parser.add_argument('--space_amp_goal', type=int, default=50,
+  parser.add_argument('--space_amp_goal', type=int, default=51,
                       help='Triggers compaction when sizeof(non-max runs) is >= (space_amp_goal/100 * sizeof(max run)). '\
                            'Ignored when 0, must be >= 0.')
-
-  # Tuning options common to all algorithms
-  parser.add_argument('--min_merge_width', type=int, default=4,
-                      help='See min_threshold for STCS in ScyllaDB, min_merge_width for RocksDB')
-  parser.add_argument('--max_merge_width', type=int, default=32,
-                      help='See max_threshold for STCS in ScyllaDB, max_merge_width for RocksDB')
 
   # Options for STCS (ScyllaDB & Cassandra)
   parser.add_argument('--stcs_bucket_high', type=float, default=1.5,
@@ -48,6 +42,8 @@ def parse_options(argv):
                       help='See bucket_low for STCS in ScyllaDB')
   parser.add_argument('--stcs_min_sstable_size_mb', type=int, default=80,
                       help='See min_sstable_size for STCS in ScyllaDB')
+  parser.add_argument('--stcs_min_threshold', type=int, default=4)
+  parser.add_argument('--stcs_max_threshold', type=int, default=16)
 
   # Options for prefix (HBase and RocksDB)
   parser.add_argument('--prefix_read_amp_trigger', type=int, default=9,
@@ -56,9 +52,10 @@ def parse_options(argv):
                       help='If max_merge_width has not been reached, stop adding sorted runs '\
                            'to the next minor compaction when the next run is more than this '\
                            'much larger than the sum of the sizes of the sorted runs already selected.')
-  # TODO: support this
-  parser.add_argument('--prefix_benefit', default=False, action='store_true',
-                      help='When true ignore --prefix_size_ratio and use cost/benefit analysis to determine number of runs to merge')
+  parser.add_argument('--prefix_v2', default=False, action='store_true',
+                      help='Prefix with a few tweaks')
+  parser.add_argument('--prefix_min_merge_width', type=int, default=4)
+  parser.add_argument('--prefix_max_merge_width', type=int, default=16)
 
   # Options for tower
   # TODO ???
@@ -66,6 +63,8 @@ def parse_options(argv):
                       help='Trigger minor compaction when the L0 has this many runs')
   parser.add_argument('--tower_size_ratio', type=float, default=1.5,
                       help='Similar to --prefix_size_ratio')
+  parser.add_argument('--tower_min_merge_width', type=int, default=4)
+  parser.add_argument('--tower_max_merge_width', type=int, default=16)
 
   args = parser.parse_args()
   return args
@@ -119,40 +118,64 @@ def final_stats(runs, interval_stats, global_stats):
     sum_runs += s['runs']
     if s['runs'] > max_runs: max_runs = s['runs']
 
-  print('final: %s %.1f runs(max,avg), %.0f %.1f size(max,avg), %s ingest, %.1f write-amp, %d major, %d minor' % (
+  print('final: %s %.1f runs(max,avg), %.0f %.1f size(max,avg), %s ingest, %.1f write-amp, %d sa_major, %d ra_major, %d minor' % (
       max_runs, sum_runs/len(interval_stats), 
       max_size, sum_size/len(interval_stats), 
-      ingest, write_amp, global_stats['major'], global_stats['minor']))
+      ingest, write_amp, global_stats['sa_major'], global_stats['ra_major'], global_stats['minor']))
 
-def  merge_runs(runs_to_merge, args, now, all_runs, global_stats, append, sort_by_size):
+def  merge_runs(runs_to_merge, args, now, all_runs, global_stats, sort_by_size):
+  assert args.workload  == 'overwrite'
+
   min_ts = next_id
   max_ts = -1
   size = 0
   avg_wr = 0
+
   for r in runs_to_merge:
     if r['min_ts'] < min_ts: min_ts = r['min_ts']
     if r['max_ts'] > max_ts: max_ts = r['max_ts']
+
+    # Sizeof output run should be between (max, sum) where max is sizeof largest input run
+    # and sum is the sum of the sizes of the input run.
     if args.keep_pct != 100:
-      size = size + (r['size'] * (args.keep_pct/100.0))
+      print('keep_pct != 100 not supported yet')
+      sys.exit(-1)
+      # TODO: figure this out. This code is buggy, if the input has sizes (64, 576) and keep_pct=90
+      #       then the output also has size 576.
+      # size = size + (r['size'] * (args.keep_pct/100.0))
     else:
       size = size + r['size']
+
     # Compute weighted average of avg_wr from input runs
     avg_wr = avg_wr + r['size'] * r['avg_wr']
+    if args.debug: print('merge_runs: size = %.1f' % size)
 
-  if args.workload == 'overwrite' and size > args.max_size_gb * 1024:
-    size = args.max_size_gb * 1024
+  if args.workload == 'overwrite':
+    if len(runs_to_merge) == len(all_runs):
+      # This is a major compaction
+      size = args.max_size_gb * 1024
+    elif size > args.max_size_gb * 1024:
+      size = args.max_size_gb * 1024
 
   # avg_wr is a weighted average of the number of times bytes in this run have been written
   avg_wr = avg_wr / size
   # increment because compaction just copied these runs to a new run
   avg_wr += 1
   global_stats['compact_wr'] += size
-  if args.debug: print('merge_runs: %s input, %s size' % (runs_to_str(runs_to_merge, short=True), size))
+
+  # Find place in all_runs at which merge_runs begins
+  insert_at = -1
+  if len(runs_to_merge) < len(all_runs):
+    for x, r in enumerate(all_runs):
+      insert_at = x
+      if r['id'] == runs_to_merge[0]['id']:
+        break
+    assert insert_at != -1
 
   new_run = make_run(size, min_ts, max_ts, avg_wr)
 
+  # Remove runs in merge_runs from all_runs
   to_remove = [r['id'] for r in runs_to_merge]
-
   for id in to_remove:
     found = False
     for x, r2 in enumerate(all_runs):
@@ -164,11 +187,15 @@ def  merge_runs(runs_to_merge, args, now, all_runs, global_stats, append, sort_b
     if not found: print('merge_runs: not found %d' % id)
     assert found
 
-  # Insert new_run into runs and then order based on size
-  if append:
-    all_runs.append(new_run)
+  if args.debug:
+    print('merge_runs: %s input, %.1f size, %d ins_at' % (
+          runs_to_str(runs_to_merge, short=True), size, insert_at))
+
+  # Insert new_run into all_runs
+  if insert_at != -1:
+    all_runs.insert(insert_at, new_run)
   else:
-    all_runs.insert(0, new_run)
+    all_runs.append(new_run)
 
   if sort_by_size:
     all_runs.sort(key=lambda r: r['size'])
@@ -229,7 +256,7 @@ def stcs_minor_compact(args, now, runs, global_stats):
 
   # Then choose a bucket to compact
 
-  possible_buckets = [b for b in buckets if len(b['runs']) >= args.min_merge_width]
+  possible_buckets = [b for b in buckets if len(b['runs']) >= args.stcs_min_threshold]
 
   if not possible_buckets:
     if args.debug: print('stcs_compact: no candidate buckets')
@@ -243,14 +270,14 @@ def stcs_minor_compact(args, now, runs, global_stats):
 
   # Then trim the bucket if it has too many runs, ignore the coldest sorted runs from it
   # TODO: for now trim is random
-  if len(compact_bucket['runs']) > args.max_merge_width:
+  if len(compact_bucket['runs']) > args.stcs_max_threshold:
     random.shuffle(compact_bucket['runs'])
-    compact_bucket['runs'] = compact_bucket['runs'][0:args.max_merge_width]
+    compact_bucket['runs'] = compact_bucket['runs'][0:args.stcs_max_threshold]
     if args.debug:
       print('Compact bucket after max trim: %.1f avg_size, %d runs %s' % (
           compact_bucket['avg_sz'], len(compact_bucket['runs']), runs_to_str(compact_bucket['runs'])))
 
-  merge_runs(compact_bucket['runs'], args, now, runs, global_stats, True, True)
+  merge_runs(compact_bucket['runs'], args, now, runs, global_stats, True)
   global_stats['minor'] += 1
 
   return True
@@ -266,15 +293,15 @@ def stcs_major_compact(args, now, runs, global_stats):
   # else do nothing
 
   if len(buckets[-1]['runs']) > 1:
-    merge_runs(buckets[-1]['runs'], args, now, runs, global_stats, True, True)
-    global_stats['major'] += 1
+    merge_runs(buckets[-1]['runs'], args, now, runs, global_stats, True)
+    global_stats['sa_major'] += 1
     if args.debug: print('major: 2+ runs in max bucket')
     return True
   elif len(buckets) >=2 and (
       sum_run_sizes(buckets[-2]['runs']) >=
       ((args.space_amp_goal / 100.0) * sum_run_sizes(buckets[-1]['runs']))):
-    merge_runs(buckets[-1]['runs'] + buckets[-2]['runs'], args, now, runs, global_stats, True, True)
-    global_stats['major'] += 1
+    merge_runs(buckets[-1]['runs'] + buckets[-2]['runs'], args, now, runs, global_stats, True)
+    global_stats['sa_major'] += 1
     if args.debug: print('major: next-to-max bucket too large')
     return True
   else:
@@ -284,34 +311,106 @@ def stcs_compact(args, now, runs, global_stats):
   stcs_major_compact(args, now, runs, global_stats)
   stcs_minor_compact(args, now, runs, global_stats)
 
-def prefix_minor_compact(args, now, runs, global_stats):
+def prefix_compact_too_many_runs(args, now, runs, use_size_ratio, global_stats):
   if len(runs) < args.prefix_read_amp_trigger: return False
-  assert len(runs) >= 2
+  assert len(runs) >= args.prefix_min_merge_width
 
-  runs_to_merge = []
-  size_sum = 0
+  max_merge_width = args.prefix_max_merge_width
+  orig_max_merge_width = max_merge_width
+  min_merge_width = args.prefix_min_merge_width
+ 
+  if not use_size_ratio:
+    # This is the 2nd pass that "forces" a minor compaction using a few sorted runs
 
-  for x, r in enumerate(runs[:]):
-    runs_to_merge = runs[:x]
+    if not args.prefix_v2:
+      max_merge_width = len(runs) - args.prefix_read_amp_trigger + 1
 
-    if x >= args.max_merge_width:
-      # Stop when max_merge_width runs were found
-      break;
+      if len(runs) == args.prefix_read_amp_trigger:
+        # See ">" here, maybe should be ">="
+        # https://github.com/facebook/rocksdb/blob/806d8916da26dfc82ed361a56dde790132f0d6a6/db/compaction/compaction_picker_universal.cc#L438
+        # Also see https://github.com/facebook/rocksdb/issues/9275
+        if args.debug: print('prefix_ctmr: false with not use_size_ratio and len equal')
+        return False
 
-    if x >= args.min_merge_width and (size_sum * args.prefix_size_ratio) < r['size']:
-      # Stop adding runs min_merge_width were found and the current run is too big
-      # relative to the sum of sizes of the previous runs.
-      break;
+      if max_merge_width < min_merge_width:
+        # See https://github.com/facebook/rocksdb/issues/9275
+        if args.debug: print('prefix_ctmr: false with max_mw < min_mw')
+        return False
 
-    size_sum += r['size']
+    else:
+      max_merge_width = len(runs) - args.prefix_read_amp_trigger + 1
+      assert max_merge_width >= 1
+      if max_merge_width == 1: max_merge_width = 2
+      if max_merge_width < min_merge_width: min_merge_width = max_merge_width
 
-  assert runs_to_merge
-  if args.debug: print('prefix_minor')
-  merge_runs(runs_to_merge, args, now, runs, global_stats, False, False)
-  global_stats['minor'] += 1
-  return True
+  if max_merge_width < 2:
+    print('uh oh: %d maxmw, %d nruns, %d ra_trigger\n' % (max_merge_width, len(runs), args.prefix_read_amp_trigger))
+    sys.exit(-1)
 
-def prefix_major_compact(args, now, runs, global_stats):
+  if args.debug:
+    print('prefix_compact_too_many_runs: %s use_size_ratio, (%d, %d) merge_width, %d nruns' %
+           (use_size_ratio, min_merge_width, max_merge_width, len(runs)))
+
+  for startx in range(len(runs) - min_merge_width + 1):
+    # Try to form the compaction input starting at runs[startx]
+
+    lastx = -1
+    size_sum = runs[startx]['size']
+
+    extended = False
+
+    # x starts at 0 and when x == 0 the number of sorted runs in the candidate sequence of runs is 2
+    # because iteration started at startx+1 and startx is also in the candidate sequence, thus the
+    # use of "x+2" below
+
+    for x, r in enumerate(runs[(startx+1):]):
+
+      next_run_too_big = (size_sum * args.prefix_size_ratio) < r['size']
+
+      if (extended or use_size_ratio) and next_run_too_big:
+        # Stop adding runs when the next one is too big relative to the sum of sizes of the previous runs
+        if args.debug: print('too_many: stop 1 before %d' % x)
+        break;
+
+      size_sum += r['size']
+      lastx = x
+
+      if (x+2) >= max_merge_width:
+        if not use_size_ratio and args.prefix_v2 and (x+2) < orig_max_merge_width and not next_run_too_big:
+          # There might be a good run that just doesn't satisfy min_merge_width
+          if args.debug: print('prefix_v2: continue with %d lastx, %.1f size' % (lastx, r['size']))
+          extended = True
+        else:
+          # Stop when max_merge_width runs were found
+          # Above, x starts at 0, which is relative to (startx+1)
+          if args.debug: print('too_many: stop 2 at %d' % x)
+          break;
+
+    if args.debug: print('too_many: lastx = %d' % lastx)
+
+    # Again, x starts at 0
+    if lastx != -1: lastx += startx + 1
+
+    if args.debug: print('prefix_ctmr: %d startx, %d lastx' % (startx, lastx))
+
+    if lastx != -1 and (lastx - startx + 1) >= min_merge_width:
+      runs_to_merge = runs[startx:(lastx+1)]
+      if args.debug: print('prefix_minor')
+      if len(runs_to_merge) < len(runs):
+        global_stats['minor'] += 1
+      else:
+        global_stats['ra_major'] += 1
+      assert len(runs_to_merge) <= orig_max_merge_width
+      merge_runs(runs_to_merge, args, now, runs, global_stats, False)
+      return True
+
+  if args.debug: print('prefix_ctmr: false at end')
+  return False
+
+def prefix_compact_space(args, now, runs, global_stats):
+  if not args.prefix_v2 and len(runs) < args.prefix_read_amp_trigger:
+    return False
+
   if args.space_amp_goal == 0 or len(runs) < 2:
     return False
 
@@ -319,23 +418,30 @@ def prefix_major_compact(args, now, runs, global_stats):
   if size_without_max < ((args.space_amp_goal/100.0) * runs[-1]['size']):
     return False
 
-  # Passing previous check means there might be too much space-amp, do a major compaction
+  # There is too much space-amp, do a major compaction
   runs_to_merge = runs
-  if len(runs_to_merge) > args.max_merge_width:
-    runs_to_merge = runs[(len(runs) - args.max_merge_width):]
-    assert len(runs_to_merge) == args.max_merge_width
+  if len(runs_to_merge) > args.prefix_max_merge_width:
+    runs_to_merge = runs[(len(runs) - args.prefix_max_merge_width):]
+    assert len(runs_to_merge) == args.prefix_max_merge_width
  
   new_len = len(runs) - len(runs_to_merge) + 1 
   if args.debug: print('prefix_major')
-  merge_runs(runs_to_merge, args, now, runs, global_stats, True, False)
-  global_stats['major'] += 1
+  merge_runs(runs_to_merge, args, now, runs, global_stats, False)
+  global_stats['sa_major'] += 1
   assert new_len == len(runs)
 
   return True
 
 def prefix_compact(args, now, runs, global_stats):
-  if not prefix_major_compact(args, now, runs, global_stats):
-    prefix_minor_compact(args, now, runs, global_stats)
+  if prefix_compact_space(args, now, runs, global_stats):
+    return
+  elif prefix_compact_too_many_runs(args, now, runs, True, global_stats):
+    return
+  elif prefix_compact_too_many_runs(args, now, runs, False, global_stats):
+    return
+
+  # if not prefix_major_compact(args, now, runs, global_stats):
+  #   prefix_minor_compact(args, now, runs, global_stats)
 
 def tower_minor_compact(args, now, runs, global_stats):
   assert len(runs) >= args.tower_l0_trigger
@@ -345,23 +451,29 @@ def tower_minor_compact(args, now, runs, global_stats):
 
   if args.debug: print('tower_minor: %s' % runs_to_str(runs))
 
+  lastx = -1
   for x, r in enumerate(runs[:]):
-    runs_to_merge = runs[:x]
 
-    # Always accept the first N, they are from memtable flushes
     if x < args.tower_l0_trigger:
+      # Always accept the first N, they are from memtable flushes
       assert r['size'] <= args.memtable_mb
 
     elif (size_sum * args.tower_size_ratio) < r['size']:
       # Next run is too much larger than sum of sizes of previous runs
       break
 
+    lastx = x
     size_sum += r['size']
 
-  assert runs_to_merge
   if args.debug: print('tower_minor')
-  merge_runs(runs_to_merge, args, now, runs, global_stats, False, False)
-  global_stats['minor'] += 1
+  assert lastx != -1
+  runs_to_merge = runs[:(lastx+1)]
+
+  if len(runs_to_merge) < len(runs):
+    global_stats['minor'] += 1
+  else:
+    global_stats['ra_major'] += 1
+  merge_runs(runs_to_merge, args, now, runs, global_stats, False)
   return True
 
 def tower_major_compact(args, now, runs, global_stats):
@@ -376,14 +488,14 @@ def tower_major_compact(args, now, runs, global_stats):
   runs_to_merge = runs
 
   # TODO: does max_merge_width apply here?
-  #if len(runs_to_merge) > args.max_merge_width:
-  #  runs_to_merge = runs[(len(runs) - args.max_merge_width):]
-  #  assert len(runs_to_merge) == args.max_merge_width
+  #if len(runs_to_merge) > args.tower_max_merge_width:
+  #  runs_to_merge = runs[(len(runs) - args.tower_max_merge_width):]
+  #  assert len(runs_to_merge) == args.tower_max_merge_width
 
   new_len = len(runs) - len(runs_to_merge) + 1 
   if args.debug: print('tower_major')
-  merge_runs(runs_to_merge, args, now, runs, global_stats, True, False)
-  global_stats['major'] += 1
+  merge_runs(runs_to_merge, args, now, runs, global_stats, False)
+  global_stats['sa_major'] += 1
   assert new_len == len(runs)
 
   return True
@@ -403,7 +515,7 @@ def tower_compact(args, now, runs, global_stats):
 def run_sim(args, compact_func):
   runs = []
   interval_stats = []
-  global_stats = { 'ingest':0, 'compact_wr':0, 'minor':0, 'major':0, 'tower_l0_size':0 }
+  global_stats = { 'ingest':0, 'compact_wr':0, 'minor':0, 'sa_major':0, 'ra_major':0, 'tower_l0_size':0 }
 
   # TODO: better estimate for avg_wr, maybe l2(size/fanout)??
   runs.insert(0, make_run(args.init_size_gb * 1024, 0, 0, 10))
@@ -414,8 +526,8 @@ def run_sim(args, compact_func):
     update_stats(x, runs, interval_stats, global_stats)
     iw = global_stats['ingest']
     cw = global_stats['compact_wr']
-    print('full: %d at, %d i, %d c, %.1f wa : %s' % (
-        x, iw, cw, (cw + iw) / (iw * 1.0), runs_to_str(runs, full=True)))
+    #print('full: %d at, %d i, %d c, %.1f wa : %s' % (
+    #    x, iw, cw, (cw + iw) / (iw * 1.0), runs_to_str(runs, full=True)))
     print('short: %d at, %d i, %d c, %.1f wa : %s' % (
         x, iw, cw, (cw + iw) / (iw * 1.0), runs_to_str(runs, short=True)))
     compact_func(args, x, runs, global_stats)
@@ -435,11 +547,9 @@ def main(argv):
   args = parse_options(argv)
   random.seed(args.seed)
 
-  if args.prefix_benefit:
-    print('--tower_benefit not supported yet')
-    sys.exit(-1)
-
   if args.workload == 'insert':
+    print('--workload=insert not supported yet')
+    sys.exit(-1)
     if args.keep_pct != 100:
       print('keep_pct was %d and must be 100 for --workload=insert' % args.keep_pct)
       sys.exit(-1)
--- me/compsim/r.sh
+++ me/compsim/r.sh
@@ -1,10 +1,38 @@
+rt=$1
+
+gb=64
+wl=overwrite
+minmw=4
+
 for ra in 4 8 16; do
-# for sag in 20 30 40 50 60 70 80 90 100; do
-for sag in 10 15 ; do
-echo Test for ra $ra and sag $sag at $( date )
-for a in stcs prefix tower; do
-  pypy3 compsim.py --algorithm=$a --run_time=1000000 --init_size_gb=128 --space_amp_goal=$sag --min_merge_width=$ra --tower_l0_trigger=$ra --prefix_read_amp_trigger=$(( $ra + 1 )) --workload=overwrite  > o.sag${sag}.r${ra}.$a ;
-done
+for sag in 10 15 20 30 40 50 60 70 80 90 100; do
+  echo; echo Test for ra $ra and sag $sag at $( date )
+
+  a=stcs
+  pypy3 compsim.py --algorithm=$a --run_time=$rt --init_size_gb=$gb --space_amp_goal=$sag --stcs_min_threshold=$ra --workload=$wl > o.sag${sag}.r${ra}.$a 
+  tail -1 o.sag${sag}.r${ra}.$a
+  gzip o.sag${sag}.r${ra}.$a
+
+  if [ $ra -eq 4 ]; then
+    continue
+  fi
+
+  a=tower
+  pypy3 compsim.py --algorithm=$a --run_time=$rt --init_size_gb=$gb --space_amp_goal=$sag --tower_min_merge_width=$minmw --tower_l0_trigger=$ra --workload=$wl > o.sag${sag}.r${ra}.$a 
+  tail -1 o.sag${sag}.r${ra}.$a
+  gzip o.sag${sag}.r${ra}.$a
+
+  a=prefix
+  pypy3 compsim.py --algorithm=$a --run_time=$rt --init_size_gb=$gb --space_amp_goal=$sag --prefix_min_merge_width=$minmw --prefix_read_amp_trigger=$ra --workload=$wl > o.sag${sag}.r${ra}.$a 
+  tail -1 o.sag${sag}.r${ra}.$a
+  gzip o.sag${sag}.r${ra}.$a
+
+  a=prefix
+  pypy3 compsim.py --algorithm=$a --run_time=$rt --init_size_gb=$gb --space_amp_goal=$sag --prefix_min_merge_width=$minmw --prefix_read_amp_trigger=$ra --workload=$wl --prefix_v2 > o.sag${sag}.r${ra}.${a}_v2
+  tail -1 o.sag${sag}.r${ra}.${a}_v2
+  gzip o.sag${sag}.r${ra}.${a}_v2
+
+
 done
 done
 
