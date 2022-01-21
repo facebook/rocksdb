a01e4d2584ee11503a0d7bb11bf7d2f613f8215e
Jay Zhuang <zjay@fb.com>
2022-01-21 13:45:31 -0800

    Add tests

--- options/options_settable_test.cc
+++ options/options_settable_test.cc
@@ -519,6 +519,7 @@ TEST_F(OptionsSettableTest, ColumnFamilyOptionsAllFieldsSettable) {
       "blob_garbage_collection_age_cutoff=0.5;"
       "blob_garbage_collection_force_threshold=0.75;"
       "blob_compaction_readahead_size=262144;"
+      "bottommost_temperature=kWarm;"
       "compaction_options_fifo={max_table_files_size=3;allow_"
       "compaction=false;age_for_warm=1;};",
       new_options));
