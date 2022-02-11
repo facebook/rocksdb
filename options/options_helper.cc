e9e61cbf35cad7dcf47b2968f8804b299f201b6e
matthewvon <matthewv@matthewv.com>
2022-02-11 13:29:28 -0500

    bad merge

--- options/options_helper.cc
+++ options/options_helper.cc
@@ -163,7 +163,6 @@ DBOptions BuildDBOptions(const ImmutableDBOptions& immutable_db_options,
   options.avoid_flush_during_shutdown =
       mutable_db_options.avoid_flush_during_shutdown;
   options.allow_ingest_behind = immutable_db_options.allow_ingest_behind;
-  options.preserve_deletes = immutable_db_options.preserve_deletes;
   options.two_write_queues = immutable_db_options.two_write_queues;
   options.manual_wal_flush = immutable_db_options.manual_wal_flush;
   options.wal_compression = immutable_db_options.wal_compression;
