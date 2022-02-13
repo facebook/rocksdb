dc23b9e6c5b9fefa70e55d59958861e11af66cfb
sg20180546 <sg20180546@gmail.com>
2022-02-13 19:57:23 +0900

    options_util_test_failed_fix1

--- test_util/testutil.cc
+++ test_util/testutil.cc
@@ -413,7 +413,8 @@ void RandomInitCFOptions(ColumnFamilyOptions* cf_opt, DBOptions& db_options,
   cf_opt->max_write_buffer_number_to_maintain = rnd->Uniform(100);
   cf_opt->max_write_buffer_size_to_maintain = rnd->Uniform(10000);
   cf_opt->min_write_buffer_number_to_merge = rnd->Uniform(100);
-  cf_opt->num_levels = rnd->Uniform(100);
+  int notzero=rnd->Uniform(100);
+  cf_opt->num_levels = notzero ? notzero : 1;
   cf_opt->target_file_size_multiplier = rnd->Uniform(100);
 
   // vector int options
