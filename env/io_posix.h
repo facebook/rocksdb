c730bce98a7061c1f4d4424fe2852c4c15621623
Jay Zhuang <zjay@fb.com>
2022-03-12 13:23:40 -0800

    make format

--- env/io_posix.h
+++ env/io_posix.h
@@ -65,8 +65,9 @@ struct Posix_IOHandle {
 };
 
 inline void UpdateResult(struct io_uring_cqe* cqe, const std::string& file_name,
-                         ROCKSDB_MAYBE_UNUSED size_t len, size_t iov_len, bool async_read,
-                         size_t& finished_len, FSReadRequest* req) {
+                         ROCKSDB_MAYBE_UNUSED size_t len, size_t iov_len,
+                         bool async_read, size_t& finished_len,
+                         FSReadRequest* req) {
   if (cqe->res < 0) {
     req->result = Slice(req->scratch, 0);
     req->status = IOError("Req failed", file_name, cqe->res);
