f74de1fa960b14a614e83144aca43663e3880256
Yanqin Jin <yanqin@fb.com>
2022-03-14 16:50:13 -0700

    Fix a TSAN-reported bug caused by concurrent accesss to std::deque (#9686)
    Summary:
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/9686
    
    According to https://www.cplusplus.com/reference/deque/deque/back/,
    "
    The container is accessed (neither the const nor the non-const versions modify the container).
    The last element is potentially accessed or modified by the caller. Concurrently accessing or modifying other elements is safe.
    "
    
    Also according to https://www.cplusplus.com/reference/deque/deque/pop_front/,
    "
    The container is modified.
    The first element is modified. Concurrently accessing or modifying other elements is safe (although see iterator validity above).
    "
    In RocksDB, we never pop the last element of `DBImpl::alive_log_files_`. We have been
    exploiting this fact and the above two properties when ensuring correctness when
    `DBImpl::alive_log_files_` may be accessed concurrently. Specifically, it can be accessed
    in the write path when db mutex is released. Sometimes, the log_mute_ is held. It can also be accessed in `FindObsoleteFiles()`
    when db mutex is always held. It can also be accessed
    during recovery when db mutex is also held.
    Given the fact that we never pop the last element of alive_log_files_, we currently do not
    acquire additional locks when accessing it in `WriteToWAL()` as follows
    ```
    alive_log_files_.back().AddSize(log_entry.size());
    ```
    
    This is problematic.
    
    Check source code of deque.h
    ```
      back() _GLIBCXX_NOEXCEPT
      {
    __glibcxx_requires_nonempty();
    ...
      }
    
      pop_front() _GLIBCXX_NOEXCEPT
      {
    ...
      if (this->_M_impl._M_start._M_cur
          != this->_M_impl._M_start._M_last - 1)
        {
          ...
          ++this->_M_impl._M_start._M_cur;
        }
      ...
      }
    ```
    
    `back()` will actually call `__glibcxx_requires_nonempty()` first.
    If `__glibcxx_requires_nonempty()` is enabled and not an empty macro,
    it will call `empty()`
    ```
    bool empty() {
    return this->_M_impl._M_finish == this->_M_impl._M_start;
    }
    ```
    You can see that it will access `this->_M_impl._M_start`, racing with `pop_front()`.
    Therefore, TSAN will actually catch the bug in this case.
    
    To be able to use TSAN on our library and unit tests, we should always coordinate
    concurrent accesses to STL containers properly.
    
    We need to pass information about db mutex and log mutex into `WriteToWAL()`, otherwise
    it's impossible to know which mutex to acquire inside the function.
    
    To fix this, we can catch the tail of `alive_log_files_` by reference, so that we do not have to call `back()` in `WriteToWAL()`.
    
    Reviewed By: pdillinger
    
    Differential Revision: D34780309
    
    fbshipit-source-id: 47db7b044a19d3f1f40ece1a974e30e52a777c0b

--- db/db_impl/db_impl_open.cc
+++ db/db_impl/db_impl_open.cc
@@ -1364,6 +1364,7 @@ Status DBImpl::RestoreAliveLogFiles(const std::vector<uint64_t>& wal_numbers) {
     total_log_size_ += log.size;
     alive_log_files_.push_back(log);
   }
+  alive_log_files_tail_ = alive_log_files_.rbegin();
   if (two_write_queues_) {
     log_write_mutex_.Unlock();
   }
@@ -1775,6 +1776,7 @@ Status DBImpl::Open(const DBOptions& db_options, const std::string& dbname,
       }
       impl->alive_log_files_.push_back(
           DBImpl::LogFileNumberSize(impl->logfile_number_));
+      impl->alive_log_files_tail_ = impl->alive_log_files_.rbegin();
       if (impl->two_write_queues_) {
         impl->log_write_mutex_.Unlock();
       }
@@ -1796,7 +1798,7 @@ Status DBImpl::Open(const DBOptions& db_options, const std::string& dbname,
         uint64_t log_used, log_size;
         log::Writer* log_writer = impl->logs_.back().writer;
         s = impl->WriteToWAL(empty_batch, log_writer, &log_used, &log_size,
-                             Env::IO_TOTAL);
+                             Env::IO_TOTAL, /*with_db_mutex==*/true);
         if (s.ok()) {
           // Need to fsync, otherwise it might get lost after a power reset.
           s = impl->FlushWAL(false);
