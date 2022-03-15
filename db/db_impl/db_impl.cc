eed1009693ebf8c1102ed431db700eadb63a121c
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2022-03-15 15:23:10 +0300

    Remove redundant move

--- db/db_impl/db_impl.cc
+++ db/db_impl/db_impl.cc
@@ -1330,11 +1330,11 @@ Status DBImpl::FlushWAL(bool sync) {
       // future writes
       IOStatusCheck(io_s);
       // whether sync or not, we should abort the rest of function upon error
-      return std::move(io_s);
+      return io_s;
     }
     if (!sync) {
       ROCKS_LOG_DEBUG(immutable_db_options_.info_log, "FlushWAL sync=false");
-      return std::move(io_s);
+      return io_s;
     }
   }
   if (!sync) {
@@ -1433,7 +1433,7 @@ Status DBImpl::LockWAL() {
     // future writes
     WriteStatusCheck(status);
   }
-  return std::move(status);
+  return status;
 }
 
 Status DBImpl::UnlockWAL() {
