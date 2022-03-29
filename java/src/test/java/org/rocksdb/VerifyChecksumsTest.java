fd1a88c35a32bfa8f475a700ac0802907f28e522
Alan Paxton <alan.paxton@gmail.com>
2022-03-29 08:11:44 +0100

    pass verify_checksums flag in 1 place it wasn’t
    There appear to be others.

--- java/src/test/java/org/rocksdb/VerifyChecksumsTest.java
+++ java/src/test/java/org/rocksdb/VerifyChecksumsTest.java
@@ -1,30 +1,26 @@
 package org.rocksdb;
 
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class VerifyChecksumsTest {
-
   @ClassRule
   public static final RocksNativeLibraryResource ROCKS_NATIVE_LIBRARY_RESOURCE =
       new RocksNativeLibraryResource();
 
-  @Rule
-  public TemporaryFolder dbFolder = new TemporaryFolder();
+  @Rule public TemporaryFolder dbFolder = new TemporaryFolder();
 
   @SuppressWarnings("ObjectAllocationInLoop")
   @Test
   public void verifyChecksums() throws RocksDBException {
-
     final String dbPath = dbFolder.getRoot().getAbsolutePath();
 
     final int KV_COUNT = 10000;
@@ -33,14 +29,12 @@ public class VerifyChecksumsTest {
     final List<String> sortedElements = new ArrayList<>(elements);
     Collections.sort(sortedElements);
 
-    //noinspection SingleStatementInBlock
-    try (final Statistics statistics = new Statistics(); final Options options =
-        new Options()
-            .setCreateIfMissing(true)
-            .setStatistics(statistics)) {
+    // noinspection SingleStatementInBlock
+    try (final Statistics statistics = new Statistics();
+         final Options options = new Options().setCreateIfMissing(true).setStatistics(statistics)) {
       try (final RocksDB db = RocksDB.open(options, dbPath)) {
         for (int i = 0; i < KV_COUNT; i++) {
-          //noinspection ObjectAllocationInLoop
+          // noinspection ObjectAllocationInLoop
           final String key = MessageFormat.format("key{0}", elements.get(i));
           final String value = MessageFormat.format("value{0}", elements.get(i));
           System.out.println(key + "-->" + value);
@@ -49,8 +43,10 @@ public class VerifyChecksumsTest {
         db.flush(new FlushOptions());
       }
 
-      for (final boolean verifyFlag : new boolean[]{true, false}) {
+      final long beforeChecksumComputeCount =
+          statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT);
 
+      for (final boolean verifyFlag : new boolean[] {false, true}) {
         try (final RocksDB db = RocksDB.open(options, dbPath)) {
           final ReadOptions readOptions = new ReadOptions();
           readOptions.setReadaheadSize(32 * 1024);
@@ -64,8 +60,10 @@ public class VerifyChecksumsTest {
               final byte[] key = rocksIterator.key();
               final byte[] value = rocksIterator.value();
               System.out.println(new String(key) + "-->" + new String(value));
-              assertThat(key).isEqualTo((MessageFormat.format("key{0}", sortedElements.get(i))).getBytes());
-              assertThat(value).isEqualTo((MessageFormat.format("value{0}", sortedElements.get(i))).getBytes());
+              assertThat(key).isEqualTo(
+                  (MessageFormat.format("key{0}", sortedElements.get(i))).getBytes());
+              assertThat(value).isEqualTo(
+                  (MessageFormat.format("value{0}", sortedElements.get(i))).getBytes());
               rocksIterator.next();
               rocksIterator.status();
               i++;
@@ -75,9 +73,11 @@ public class VerifyChecksumsTest {
           if (verifyFlag) {
             // We don't need to be exact - we are checking that the checksums happen
             // exactly how many depends on block size etc etc, so may not be entirely stable
-            assertThat(statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)).isGreaterThan(10);
+            assertThat(statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT))
+                .isGreaterThan(beforeChecksumComputeCount + 20);
           } else {
-            assertThat(statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)).isEqualTo(0);
+            assertThat(statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT))
+                .isEqualTo(beforeChecksumComputeCount);
           }
         }
       }
