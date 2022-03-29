d8080a0d93e406fae3f2d6061842907c0bb80647
Alan Paxton <alan.paxton@gmail.com>
2022-03-29 12:15:05 +0100

    Test iterate, get, mulltiGet all working

--- java/src/test/java/org/rocksdb/VerifyChecksumsTest.java
+++ java/src/test/java/org/rocksdb/VerifyChecksumsTest.java
@@ -18,75 +18,179 @@ public class VerifyChecksumsTest {
 
   @Rule public TemporaryFolder dbFolder = new TemporaryFolder();
 
-  @SuppressWarnings("ObjectAllocationInLoop")
-  @Test
-  public void verifyChecksums() throws RocksDBException {
-    final String dbPath = dbFolder.getRoot().getAbsolutePath();
-
-    final int KV_COUNT = 10000;
+  /**
+   * Class to factor out DB manipulations within the test
+   */
+  abstract static class Operations {
+    final int kv_count;
     final List<String> elements = new ArrayList<>();
-    for (int i = 0; i < KV_COUNT; i++) elements.add(MessageFormat.format("{0,number,#}", i));
-    final List<String> sortedElements = new ArrayList<>(elements);
-    Collections.sort(sortedElements);
+    final List<String> sortedElements = new ArrayList<>();
+
+    Operations(final int kv_count) {
+      this.kv_count = kv_count;
+      for (int i = 0; i < kv_count; i++) elements.add(MessageFormat.format("{0,number,#}", i));
+      sortedElements.addAll(elements);
+      Collections.sort(sortedElements);
+    }
+
+    void fill(final RocksDB db) throws RocksDBException {
+      for (int i = 0; i < kv_count; i++) {
+        final String key = MessageFormat.format("key{0}", elements.get(i));
+        final String value = MessageFormat.format("value{0}", elements.get(i));
+        // noinspection ObjectAllocationInLoop
+        db.put(key.getBytes(), value.getBytes());
+      }
+      db.flush(new FlushOptions());
+    }
+
+    @SuppressWarnings("ObjectAllocationInLoop")
+    void get(final RocksDB db, final boolean verifyFlag) throws RocksDBException {
+      try (final ReadOptions readOptions = new ReadOptions()) {
+        readOptions.setReadaheadSize(32 * 1024);
+        readOptions.setFillCache(false);
+        readOptions.setVerifyChecksums(verifyFlag);
+
+        for (int i = 0; i < kv_count / 10; i++) {
+          @SuppressWarnings("UnsecureRandomNumberGeneration")
+          final int index = Double.valueOf(Math.random() * kv_count).intValue();
+          final String key = MessageFormat.format("key{0}", sortedElements.get(index));
+          final String expectedValue = MessageFormat.format("value{0}", sortedElements.get(index));
+
+          final byte[] value = db.get(readOptions, key.getBytes());
+          assertThat(value).isEqualTo(expectedValue.getBytes());
+        }
+      }
+    }
+
+    @SuppressWarnings("ObjectAllocationInLoop")
+    void multiGet(final RocksDB db, final boolean verifyFlag) throws RocksDBException {
+      try (final ReadOptions readOptions = new ReadOptions()) {
+        readOptions.setReadaheadSize(32 * 1024);
+        readOptions.setFillCache(false);
+        readOptions.setVerifyChecksums(verifyFlag);
+
+        final List<byte[]> keys = new ArrayList<>();
+        final List<String> expectedValues = new ArrayList<>();
+
+        for (int i = 0; i < kv_count / 10; i++) {
+          @SuppressWarnings("UnsecureRandomNumberGeneration")
+          final int index = Double.valueOf(Math.random() * kv_count).intValue();
+          keys.add(MessageFormat.format("key{0}", sortedElements.get(index)).getBytes());
+
+          expectedValues.add(MessageFormat.format("value{0}", sortedElements.get(index)));
+        }
+
+        final List<byte[]> values = db.multiGetAsList(readOptions, keys);
+        for (int i = 0; i < keys.size(); i++) {
+          assertThat(values.get(i)).isEqualTo(expectedValues.get(i).getBytes());
+        }
+      }
+    }
+
+    void iterate(final RocksDB db, final boolean verifyFlag) throws RocksDBException {
+      final ReadOptions readOptions = new ReadOptions();
+      readOptions.setReadaheadSize(32 * 1024);
+      readOptions.setFillCache(false);
+      readOptions.setVerifyChecksums(verifyFlag);
+      int i = 0;
+      try (final RocksIterator rocksIterator = db.newIterator(readOptions)) {
+        rocksIterator.seekToFirst();
+        rocksIterator.status();
+        while (rocksIterator.isValid()) {
+          final byte[] key = rocksIterator.key();
+          final byte[] value = rocksIterator.value();
+          // noinspection ObjectAllocationInLoop
+          assertThat(key).isEqualTo(
+              (MessageFormat.format("key{0}", sortedElements.get(i))).getBytes());
+          // noinspection ObjectAllocationInLoop
+          assertThat(value).isEqualTo(
+              (MessageFormat.format("value{0}", sortedElements.get(i))).getBytes());
+          rocksIterator.next();
+          rocksIterator.status();
+          i++;
+        }
+      }
+      assertThat(i).isEqualTo(kv_count);
+    }
+
+    abstract void performOperations(final RocksDB db, final boolean verifyFlag)
+        throws RocksDBException;
+  }
+
+  private static final int KV_COUNT = 10000;
+
+  private void verifyChecksums(final Operations operations) throws RocksDBException {
+    final String dbPath = dbFolder.getRoot().getAbsolutePath();
 
     // noinspection SingleStatementInBlock
     try (final Statistics statistics = new Statistics();
          final Options options = new Options().setCreateIfMissing(true).setStatistics(statistics)) {
       try (final RocksDB db = RocksDB.open(options, dbPath)) {
-        //0
-        System.out.println(MessageFormat.format("newly open {0}", statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)));
-        for (int i = 0; i < KV_COUNT; i++) {
-          // noinspection ObjectAllocationInLoop
-          final String key = MessageFormat.format("key{0}", elements.get(i));
-          final String value = MessageFormat.format("value{0}", elements.get(i));
-          db.put(key.getBytes(), value.getBytes());
-        }
-        db.flush(new FlushOptions());
+        // 0
+        System.out.println(MessageFormat.format(
+            "newly open {0}", statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)));
+        operations.fill(db);
         //
-        System.out.println(MessageFormat.format("flushed {0}", statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)));
+        System.out.println(MessageFormat.format(
+            "flushed {0}", statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)));
       }
 
-      //2
-      System.out.println(MessageFormat.format("closed-after-write {0}", statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)));
+      // 2
+      System.out.println(MessageFormat.format("closed-after-write {0}",
+          statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)));
 
-      for (final boolean verifyFlag : new boolean[] {false, true}) {
+      for (final boolean verifyFlag : new boolean[] {false, true, false, true}) {
         try (final RocksDB db = RocksDB.open(options, dbPath)) {
-          // ?
-          System.out.println(MessageFormat.format("re-opened {0}", statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT)));
-          final ReadOptions readOptions = new ReadOptions();
-          readOptions.setReadaheadSize(32 * 1024);
-          readOptions.setFillCache(false);
-          readOptions.setVerifyChecksums(verifyFlag);
-          int i = 0;
-          try (final RocksIterator rocksIterator = db.newIterator(readOptions)) {
-            rocksIterator.seekToFirst();
-            rocksIterator.status();
-            while (rocksIterator.isValid()) {
-              final byte[] key = rocksIterator.key();
-              final byte[] value = rocksIterator.value();
-              assertThat(key).isEqualTo(
-                  (MessageFormat.format("key{0}", sortedElements.get(i))).getBytes());
-              assertThat(value).isEqualTo(
-                  (MessageFormat.format("value{0}", sortedElements.get(i))).getBytes());
-              rocksIterator.next();
-              rocksIterator.status();
-              i++;
-            }
-          }
-          assertThat(i).isEqualTo(KV_COUNT);
-          final long afterCount =
+          final long beforeOperationsCount =
+              statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT);
+          System.out.println(MessageFormat.format("re-opened {0}", beforeOperationsCount));
+          operations.performOperations(db, verifyFlag);
+          final long afterOperationsCount =
               statistics.getTickerCount(TickerType.BLOCK_CHECKSUM_COMPUTE_COUNT);
           if (verifyFlag) {
             // We don't need to be exact - we are checking that the checksums happen
             // exactly how many depends on block size etc etc, so may not be entirely stable
-            System.out.println(MessageFormat.format("verify=true {0}", afterCount));
-            // assertThat(afterCount).isGreaterThan(beforeChecksumComputeCount + 20);
+            System.out.println(MessageFormat.format("verify=true {0}", afterOperationsCount));
+            assertThat(afterOperationsCount).isGreaterThan(beforeOperationsCount + 20);
           } else {
-            System.out.println(MessageFormat.format("verify=false {0}", afterCount));
-            // assertThat(afterCount).isEqualTo(beforeChecksumComputeCount);
+            System.out.println(MessageFormat.format("verify=false {0}", afterOperationsCount));
+            assertThat(afterOperationsCount).isEqualTo(beforeOperationsCount);
           }
         }
       }
     }
+  }
+
+  @Test
+  public void verifyChecksumsInIteration() throws RocksDBException {
+    // noinspection AnonymousInnerClassMayBeStatic
+    verifyChecksums(new Operations(KV_COUNT) {
+      @Override
+      void performOperations(final RocksDB db, final boolean verifyFlag) throws RocksDBException {
+        iterate(db, verifyFlag);
+      }
+    });
+  }
+
+  @Test
+  public void verifyChecksumsGet() throws RocksDBException {
+    // noinspection AnonymousInnerClassMayBeStatic
+    verifyChecksums(new Operations(KV_COUNT) {
+      @Override
+      void performOperations(final RocksDB db, final boolean verifyFlag) throws RocksDBException {
+        get(db, verifyFlag);
+      }
+    });
+  }
+
+  @Test
+  public void verifyChecksumsMultiGet() throws RocksDBException {
+    // noinspection AnonymousInnerClassMayBeStatic
+    verifyChecksums(new Operations(KV_COUNT) {
+      @Override
+      void performOperations(final RocksDB db, final boolean verifyFlag) throws RocksDBException {
+        multiGet(db, verifyFlag);
+      }
+    });
   }
 }
