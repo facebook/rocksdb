25e31d1a942b6937d683f9360a58d1448659b478
Duncan Bellamy <dunk@denkimushi.com>
2022-04-08 13:29:19 -0700

    tools/db_bench_tool.cc use uint64_t instead of size_t (#9800)
    Summary:
    to fix compilation for 32bit
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/9800
    
    Reviewed By: riversand963
    
    Differential Revision: D35404447
    
    fbshipit-source-id: 6a1185bb38f3a718357aa120e3b26a1ea77f023d

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -6397,9 +6397,9 @@ class Benchmark {
       }
 
       // Pick a Iterator to use
-      size_t db_idx_to_use =
+      uint64_t db_idx_to_use =
           (db_.db == nullptr)
-              ? (size_t{thread->rand.Next()} % multi_dbs_.size())
+              ? (uint64_t{thread->rand.Next()} % multi_dbs_.size())
               : 0;
       std::unique_ptr<Iterator> single_iter;
       Iterator* iter_to_use;
