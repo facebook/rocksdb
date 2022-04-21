1ec8a4c6cbbc31a879f37475d28c323adb2ca197
Andrew Kryczka <andrewkr@fb.com>
2022-04-21 12:09:10 -0700

    remove loop

--- tools/run_with_pstack.sh
+++ tools/run_with_pstack.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # While wrapped command runs, a background subshell prints its stack traces
-# after every minute of waiting.
+# after two minutes of waiting.
 
 # Enable job control so background subshell gets its own process group ID.
 set -m
-# For cleaning up background loop on exit (adapted from
+# For cleaning up background jobs on exit (adapted from
 # https://stackoverflow.com/a/2173421).
 trap 'trap - SIGTERM && jobs -p | xargs printf -- "-%s " | xargs kill --' SIGINT SIGTERM EXIT
 
 "$@" &
 test_pid=$!
-while true; do sleep 60; pstack $test_pid; done &
+(sleep 120; pstack $test_pid) &
 wait $test_pid
