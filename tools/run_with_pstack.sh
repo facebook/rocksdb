8a437531d4cb901c542e7cdf03c87eb6ce31660d
Andrew Kryczka <andrewkr@fb.com>
2022-04-21 12:28:28 -0700

    more commentary

--- tools/run_with_pstack.sh
+++ tools/run_with_pstack.sh
@@ -7,6 +7,7 @@
 set -m
 # For cleaning up background jobs on exit (adapted from
 # https://stackoverflow.com/a/2173421).
+# Negating the PIDs passed to kill causes the whole process group to be killed.
 trap 'trap - SIGTERM && jobs -p | xargs printf -- "-%s " | xargs kill --' INT TERM EXIT
 
 "$@" &
