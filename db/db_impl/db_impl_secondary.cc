c5d367f47226cac748148882e156f9c73cca38af
Andrew Kryczka <andrewkr@fb.com>
2022-04-26 14:46:53 -0700

    Revert open logic changes in #9634 (#9906)
    Summary:
    Left HISTORY.md and unit tests.
    Added a new unit test to repro the corruption scenario that this PR fixes, and HISTORY.md line for that.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/9906
    
    Reviewed By: riversand963
    
    Differential Revision: D35940093
    
    Pulled By: ajkr
    
    fbshipit-source-id: 9816f99e1ce405ba36f316beb4f6378c37c8c86b

--- db/db_impl/db_impl_secondary.cc
+++ db/db_impl/db_impl_secondary.cc
@@ -33,8 +33,7 @@ DBImplSecondary::~DBImplSecondary() {}
 Status DBImplSecondary::Recover(
     const std::vector<ColumnFamilyDescriptor>& column_families,
     bool /*readonly*/, bool /*error_if_wal_file_exists*/,
-    bool /*error_if_data_exists_in_wals*/, uint64_t*,
-    RecoveryContext* /*recovery_ctx*/) {
+    bool /*error_if_data_exists_in_wals*/, uint64_t*) {
   mutex_.AssertHeld();
 
   JobContext job_context(0);
