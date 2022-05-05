526b6797c6c24a0bcad59661ee7de74f5a4926cb
sdong <siying.d@fb.com>
2022-05-05 12:56:45 -0700

    a bug

--- java/src/test/java/org/rocksdb/StatisticsTest.java
+++ java/src/test/java/org/rocksdb/StatisticsTest.java
@@ -155,7 +155,7 @@ public class StatisticsTest {
   }
 
   @Test
-  public void std::to_string() throws RocksDBException {
+  public void toString() throws RocksDBException {
     try (final Statistics statistics = new Statistics();
          final Options opt = new Options()
              .setStatistics(statistics)
