4d0bfdc91466c21e8d85d3825bdc766aa2b3a1af
Andrew Kryczka <andrewkr@fb.com>
2022-05-23 12:20:54 -0700

    Transaction batches

--- utilities/transactions/write_prepared_txn_db.cc
+++ utilities/transactions/write_prepared_txn_db.cc
@@ -166,6 +166,16 @@ Status WritePreparedTxnDB::WriteInternal(const WriteOptions& write_options_orig,
     // increased for this batch.
     return Status::OK();
   }
+
+  if (batch->GetProtectionBytesPerKey() == 0 &&
+      write_options_orig.protection_bytes_per_key != 0) {
+    auto s = WriteBatchInternal::SetProtectionBytesPerKey(
+        batch, write_options_orig.protection_bytes_per_key);
+    if (!s.ok()) {
+      return s;
+    }
+  }
+
   if (batch_cnt == 0) {  // not provided, then compute it
     // TODO(myabandeh): add an option to allow user skipping this cost
     SubBatchCounter counter(*GetCFComparatorMap());
