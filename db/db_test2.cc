23f34c7ae5dfa40cf3e7031bfc3b5c6fb957fbdc
Jay Zhuang <zjay@fb.com>
2022-05-24 15:44:49 -0700

    Skip ZSTD dict tests if the version doesn't support it (#10046)
    Summary:
    For example, the default ZSTD version for ubuntu20 is 1.4.4, which will
    fail the test `PresetCompressionDict`:
    
    ```
    db/db_test_util.cc:607: Failure
    Invalid argument: zstd finalizeDictionary cannot be used because ZSTD 1.4.5+ is not linked with the binary.
    terminate called after throwing an instance of 'testing::internal::GoogleTestFailureException'
    ```
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/10046
    
    Test Plan: test pass with old zstd
    
    Reviewed By: cbi42
    
    Differential Revision: D36640067
    
    Pulled By: jay-zhuang
    
    fbshipit-source-id: b1c49fb7295f57f4515ce4eb3a52ae7d7e45da86

--- db/db_test2.cc
+++ db/db_test2.cc
@@ -1326,7 +1326,8 @@ TEST_F(DBTest2, PresetCompressionDict) {
           options.compression_opts.zstd_max_train_bytes = 0;
           break;
         case kWithZSTDfinalizeDict:
-          if (compression_type != kZSTD) {
+          if (compression_type != kZSTD ||
+              !ZSTD_FinalizeDictionarySupported()) {
             continue;
           }
           options.compression_opts.max_dict_bytes = kBlockSizeBytes;
@@ -1334,7 +1335,7 @@ TEST_F(DBTest2, PresetCompressionDict) {
           options.compression_opts.use_zstd_dict_trainer = false;
           break;
         case kWithZSTDTrainedDict:
-          if (compression_type != kZSTD) {
+          if (compression_type != kZSTD || !ZSTD_TrainDictionarySupported()) {
             continue;
           }
           options.compression_opts.max_dict_bytes = kBlockSizeBytes;
