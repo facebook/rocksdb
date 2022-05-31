63c9aaffa445dadb266c6133d969712049f59e81
Guido Tagliavini Ponce <tagliavini@fb.com>
2022-05-31 12:05:59 -0700

    Add support for FastLRUCache.

--- cache/cache_bench_tool.cc
+++ cache/cache_bench_tool.cc
@@ -76,7 +76,12 @@ DEFINE_string(secondary_cache_uri, "",
 static class std::shared_ptr<ROCKSDB_NAMESPACE::SecondaryCache> secondary_cache;
 #endif  // ROCKSDB_LITE
 
-DEFINE_bool(use_clock_cache, false, "");
+DEFINE_bool(use_clock_cache, false,
+            "Replace default LRU block cache with clock cache.");
+
+DEFINE_bool(
+    use_fast_lru_cache, false,
+    "Replace default LRU block cache with experimental LRU block cache.");
 
 // ## BEGIN stress_cache_key sub-tool options ##
 // See class StressCacheKey below.
@@ -279,12 +284,19 @@ class CacheBench {
       if (max_key > (static_cast<uint64_t>(1) << max_log_)) max_log_++;
     }
 
+    if (FLAGS_use_clock_cache && FLAGS_use_fast_lru_cache) {
+      fprintf(stderr, "Only one type of block cache can be chosen.");
+      exit(1);
+    }
+
     if (FLAGS_use_clock_cache) {
       cache_ = NewClockCache(FLAGS_cache_size, FLAGS_num_shard_bits);
       if (!cache_) {
         fprintf(stderr, "Clock cache not supported.\n");
         exit(1);
       }
+    } else if (FLAGS_use_fast_lru_cache) {
+      cache_ = NewFastLRUCache((size_t)capacity, num_shard_bits);
     } else {
       LRUCacheOptions opts(FLAGS_cache_size, FLAGS_num_shard_bits, false, 0.5);
 #ifndef ROCKSDB_LITE
