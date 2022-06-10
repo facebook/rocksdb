05954b057e0aba4903fe9ef57bc26069bd51f4ac
Guido Tagliavini Ponce <tagliavini@fb.com>
2022-06-10 13:47:06 -0700

    Copyright.

--- cache/cache_test.cc
+++ cache/cache_test.cc
@@ -2,6 +2,10 @@
 //  This source code is licensed under both the GPLv2 (found in the
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
+//
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
 
 #include "rocksdb/cache.h"
 
