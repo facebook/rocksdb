d75056a25e2217e40719b60752fdc73eb47404ee
Levi Tamasi <ltamasi@fb.com>
2022-06-22 15:00:54 -0700

    Extend and clean up unit test

--- db/wide/db_wide_basic_test.cc
+++ db/wide/db_wide_basic_test.cc
@@ -4,13 +4,9 @@
 //  (found in the LICENSE.Apache file in the root directory).
 
 #include <array>
-#include <sstream>
-#include <string>
 
 #include "db/db_test_util.h"
 #include "port/stack_trace.h"
-#include "test_util/sync_point.h"
-#include "utilities/fault_injection_env.h"
 
 namespace ROCKSDB_NAMESPACE {
 
@@ -23,64 +19,81 @@ class DBWideBasicTest : public DBTestBase {
 TEST_F(DBWideBasicTest, Entity) {
   Options options = GetDefaultOptions();
 
-  constexpr char key[] = "key";
-  WideColumns columns{{"foo", "bar"}, {"hello", "world"}};
+  constexpr size_t num_keys = 2;
 
+  // Use the DB::PutEntity API
+  constexpr char first_key[] = "first";
+  WideColumns first_columns{{"foo", "bar"}, {"hello", "world"}};
+
+  ASSERT_OK(db_->PutEntity(WriteOptions(), db_->DefaultColumnFamily(),
+                           first_key, first_columns));
+
+  // Use WriteBatch
+  constexpr char second_key[] = "second";
+  WideColumns second_columns{{"one", "two"}, {"three", "four"}};
+
+  WriteBatch batch;
   ASSERT_OK(
-      db_->PutEntity(WriteOptions(), db_->DefaultColumnFamily(), key, columns));
+      batch.PutEntity(db_->DefaultColumnFamily(), second_key, second_columns));
+  ASSERT_OK(db_->Write(WriteOptions(), &batch));
+
+  // Note: currently, read APIs are supposed to return NotSupported
+  auto verify = [&]() {
+    {
+      PinnableSlice result;
+      ASSERT_TRUE(db_->Get(ReadOptions(), db_->DefaultColumnFamily(), first_key,
+                           &result)
+                      .IsNotSupported());
+    }
+
+    {
+      PinnableSlice result;
+      ASSERT_TRUE(db_->Get(ReadOptions(), db_->DefaultColumnFamily(),
+                           second_key, &result)
+                      .IsNotSupported());
+    }
+
+    {
+      std::array<Slice, num_keys> keys{{first_key, second_key}};
+      std::array<PinnableSlice, num_keys> values;
+      std::array<Status, num_keys> statuses;
+
+      db_->MultiGet(ReadOptions(), db_->DefaultColumnFamily(), num_keys,
+                    &keys[0], &values[0], &statuses[0]);
+
+      ASSERT_TRUE(values[0].empty());
+      ASSERT_TRUE(statuses[0].IsNotSupported());
+
+      ASSERT_TRUE(values[1].empty());
+      ASSERT_TRUE(statuses[1].IsNotSupported());
+    }
+
+    {
+      std::unique_ptr<Iterator> iter(db_->NewIterator(ReadOptions()));
+
+      iter->SeekToFirst();
+      ASSERT_FALSE(iter->Valid());
+      ASSERT_TRUE(iter->status().IsNotSupported());
+
+      iter->SeekToLast();
+      ASSERT_FALSE(iter->Valid());
+      ASSERT_TRUE(iter->status().IsNotSupported());
+    }
+  };
 
   // Try reading from memtable
-  {
-    PinnableSlice result;
-    ASSERT_TRUE(
-        db_->Get(ReadOptions(), db_->DefaultColumnFamily(), key, &result)
-            .IsNotSupported());
-  }
+  verify();
 
-  {
-    std::unique_ptr<Iterator> iter(db_->NewIterator(ReadOptions()));
-
-    iter->SeekToFirst();
-    ASSERT_FALSE(iter->Valid());
-    ASSERT_TRUE(iter->status().IsNotSupported());
-  }
-
+  // Try reading after recovery
   options.avoid_flush_during_recovery = true;
   Reopen(options);
 
-  // Try reading after recovery
-  {
-    PinnableSlice result;
-    ASSERT_TRUE(
-        db_->Get(ReadOptions(), db_->DefaultColumnFamily(), key, &result)
-            .IsNotSupported());
-  }
-
-  {
-    std::unique_ptr<Iterator> iter(db_->NewIterator(ReadOptions()));
-
-    iter->SeekToFirst();
-    ASSERT_FALSE(iter->Valid());
-    ASSERT_TRUE(iter->status().IsNotSupported());
-  }
-
-  ASSERT_OK(Flush());
+  verify();
 
   // Try reading from storage
-  {
-    PinnableSlice result;
-    ASSERT_TRUE(
-        db_->Get(ReadOptions(), db_->DefaultColumnFamily(), key, &result)
-            .IsNotSupported());
-  }
+  ASSERT_OK(Flush());
 
-  {
-    std::unique_ptr<Iterator> iter(db_->NewIterator(ReadOptions()));
-
-    iter->SeekToFirst();
-    ASSERT_FALSE(iter->Valid());
-    ASSERT_TRUE(iter->status().IsNotSupported());
-  }
+  verify();
 }
 
 }  // namespace ROCKSDB_NAMESPACE
