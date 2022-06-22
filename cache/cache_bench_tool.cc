ec3427ffaa58a60e558eac521425a8ef0067f75a
Guido Tagliavini Ponce <tagliavini@fb.com>
2022-06-22 14:36:48 -0700

    Format.

--- cache/cache_bench_tool.cc
+++ cache/cache_bench_tool.cc
@@ -322,9 +322,9 @@ class CacheBench {
     Random64 rnd(1);
     KeyGen keygen;
     for (uint64_t i = 0; i < 2 * FLAGS_cache_size; i += FLAGS_value_bytes) {
-      char *value = createValue(rnd);
+      char* value = createValue(rnd);
       Status s = cache_->Insert(keygen.GetRand(rnd, max_key_, max_log_), value,
-                        &helper1, FLAGS_value_bytes);
+                                &helper1, FLAGS_value_bytes);
       assert(s == Status::OK());
     }
   }
@@ -545,9 +545,9 @@ class CacheBench {
                              FLAGS_value_bytes);
         } else {
           // do insert
-          char *value = createValue(thread->rnd);
-          Status s = cache_->Insert(key, value, &helper2,
-                          FLAGS_value_bytes, &handle);
+          char* value = createValue(thread->rnd);
+          Status s =
+              cache_->Insert(key, value, &helper2, FLAGS_value_bytes, &handle);
           assert(s == Status::OK());
         }
       } else if (random_op < insert_threshold_) {
@@ -556,9 +556,9 @@ class CacheBench {
           handle = nullptr;
         }
         // do insert
-        char *value = createValue(thread->rnd);
-        Status s = cache_->Insert(key, value, &helper3,
-                        FLAGS_value_bytes, &handle);
+        char* value = createValue(thread->rnd);
+        Status s =
+            cache_->Insert(key, value, &helper3, FLAGS_value_bytes, &handle);
         assert(s == Status::OK());
       } else if (random_op < lookup_threshold_) {
         if (handle) {
