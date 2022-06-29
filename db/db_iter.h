cc85a880ba6b5e1432acf7bf17d5b4021eff0476
Yanqin Jin <yanqin@fb.com>
2022-06-28 17:28:27 -0700

    Add a comment

--- db/db_iter.h
+++ db/db_iter.h
@@ -379,6 +379,8 @@ class DBIter final : public Iterator {
   const Slice* const timestamp_lb_;
   const size_t timestamp_size_;
   std::string saved_timestamp_;
+
+  // Used only if timestamp_lb_ is not nullptr.
   std::string saved_ikey_;
 };
 
