7679f22a89ace8ecb6502448612daa39ef1ee952
Yanqin Jin <yanqin@fb.com>
2022-07-12 13:17:21 -0700

    Add coverage for the combination of write-prepared and WAL recycling (#10350)
    Summary:
    as title.
    Test plan
    - make check
    - CI on PR
    - TEST_TMPDIR=/dev/shm make crash_test_with_multiops_wp_txn (tested with successful run)
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/10350
    
    Reviewed By: ajkr
    
    Differential Revision: D37792872
    
    Pulled By: riversand963
    
    fbshipit-source-id: ff064093b7f715d0acf387af2e3ae87b1278b52b

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -424,7 +424,6 @@ multiops_wp_txn_params = {
     "checkpoint_one_in": 0,
     # Required to be 1 in order to use commit-time-batch
     "use_only_the_last_commit_time_batch_for_recovery": 1,
-    "recycle_log_file_num": 0,
     "clear_wp_commit_cache_one_in": 10,
     "create_timestamped_snapshot_one_in": 0,
 }
