e7e24358dafe7ccb40d53bb083bc7896b81d379e
Guido Tagliavini Ponce <tagliavini@fb.com>
2022-07-12 17:33:58 -0700

    Format.

--- cache/clock_cache.h
+++ cache/clock_cache.h
@@ -472,7 +472,6 @@ class ClockCache
 
 }  // namespace clock_cache
 
-
 // Only for internal testing, temporarily replacing NewClockCache.
 // TODO(Guido) Remove once NewClockCache constructs a ClockCache again.
 extern std::shared_ptr<Cache> ExperimentalNewClockCache(
