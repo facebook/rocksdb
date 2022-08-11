73184779818c1f140a2655bca206f002ce51be62
sdong <siying.d@fb.com>
2022-08-10 21:27:33 -0700

    Fix an unintended line removed.

--- util/async_file_reader.h
+++ util/async_file_reader.h
@@ -3,6 +3,7 @@
 //  This source code is licensed under both the GPLv2 (found in the
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).#pragma once
+#pragma once
 
 #if USE_COROUTINES
 #include "file/random_access_file_reader.h"
