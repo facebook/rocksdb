26230d222ad8c15732fbd220b8562f1ebd727018
Edvard Davtyan <davtyaneg@gmail.com>
2022-08-18 16:19:29 +0200

    .

--- table/meta_blocks.cc
+++ table/meta_blocks.cc
@@ -425,9 +425,7 @@ Status ReadTableProperties(RandomAccessFileReader* file, uint64_t file_size,
   }
 
   if (!block_handle.IsNull()) {
-    ReadOptions ro;
-    ro.verify_checksums = false;
-    s = ReadTablePropertiesHelper(ro, block_handle, file, prefetch_buffer,
+    s = ReadTablePropertiesHelper(ReadOptions(), block_handle, file, prefetch_buffer,
                                   footer, ioptions, properties,
                                   memory_allocator);
   } else {
@@ -491,9 +489,7 @@ Status ReadMetaIndexBlockInFile(RandomAccessFileReader* file,
   }
 
   auto metaindex_handle = footer.metaindex_handle();
-  ReadOptions ro;
-  ro.verify_checksums = false;
-  return BlockFetcher(file, prefetch_buffer, footer, ro, metaindex_handle,
+  return BlockFetcher(file, prefetch_buffer, footer, ReadOptions(), metaindex_handle,
                       metaindex_contents, ioptions,
                       false /* do decompression */, false /*maybe_compressed*/,
                       BlockType::kMetaIndex, UncompressionDict::GetEmptyDict(),
@@ -546,9 +542,7 @@ Status ReadMetaBlock(RandomAccessFileReader* file,
     return status;
   }
 
-  ReadOptions ro;
-  ro.verify_checksums = false;
-  return BlockFetcher(file, prefetch_buffer, footer, ro, block_handle, contents,
+  return BlockFetcher(file, prefetch_buffer, footer, ReadOptions(), block_handle, contents,
                       ioptions, false /* decompress */,
                       false /*maybe_compressed*/, block_type,
                       UncompressionDict::GetEmptyDict(),
