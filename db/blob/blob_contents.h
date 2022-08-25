9c82d8e83575f8db6cec882af426773d0a8c3a72
Levi Tamasi <ltamasi@fb.com>
2022-08-24 17:52:34 -0700

    Move method implementations to a .cc file

--- db/blob/blob_contents.h
+++ db/blob/blob_contents.h
@@ -5,10 +5,8 @@
 
 #pragma once
 
-#include <cassert>
 #include <memory>
 
-#include "cache/cache_helpers.h"
 #include "memory/memory_allocator.h"
 #include "rocksdb/cache.h"
 #include "rocksdb/rocksdb_namespace.h"
@@ -25,6 +23,11 @@ class BlobContents {
 
   BlobContents(const BlobContents&) = delete;
   BlobContents& operator=(const BlobContents&) = delete;
+
+  BlobContents(BlobContents&&) = default;
+  BlobContents& operator=(BlobContents&&) = default;
+
+  ~BlobContents() = default;
 
   const Slice& data() const { return data_; }
   size_t size() const { return data_.size(); }
@@ -48,55 +51,6 @@ class BlobContents {
 
   CacheAllocationPtr allocation_;
   Slice data_;
-};
-
-inline std::unique_ptr<BlobContents> BlobContents::Create(
-    CacheAllocationPtr&& allocation, size_t size) {
-  return std::unique_ptr<BlobContents>(
-      new BlobContents(std::move(allocation), size));
-}
-
-inline size_t BlobContents::SizeCallback(void* obj) {
-  assert(obj);
-
-  return static_cast<const BlobContents*>(obj)->size();
-}
-
-inline Status BlobContents::SaveToCallback(void* from_obj, size_t from_offset,
-                                           size_t length, void* out) {
-  assert(from_obj);
-
-  const BlobContents* buf = static_cast<const BlobContents*>(from_obj);
-  assert(buf->size() >= from_offset + length);
-
-  memcpy(out, buf->data().data() + from_offset, length);
-
-  return Status::OK();
-}
-
-inline void BlobContents::DeleteCallback(const Slice& key, void* value) {
-  DeleteCacheEntry<BlobContents>(key, value);
-}
-
-inline Cache::CacheItemHelper* BlobContents::GetCacheItemHelper() {
-  static Cache::CacheItemHelper cache_helper(&SizeCallback, &SaveToCallback,
-                                             &DeleteCallback);
-
-  return &cache_helper;
-}
-
-inline Status BlobContents::CreateCallback(const void* buf, size_t size,
-                                           void** out_obj, size_t* charge) {
-  CacheAllocationPtr allocation(new char[size]);
-  memcpy(allocation.get(), buf, size);
-
-  std::unique_ptr<BlobContents> obj = Create(std::move(allocation), size);
-  BlobContents* const contents = obj.release();
-
-  *out_obj = contents;
-  *charge = contents->size();
-
-  return Status::OK();
 };
 
 }  // namespace ROCKSDB_NAMESPACE
