7df41462eaf2a8abd192b4db30e1926a62bc58d2
Andrew Kryczka <andrewkr@fb.com>
2022-09-04 16:12:40 -0700

    Disable RateLimiterTest.Rate in CircleCI with valgrind

--- util/rate_limiter_test.cc
+++ util/rate_limiter_test.cc
@@ -346,7 +346,7 @@ TEST_F(RateLimiterTest, Rate) {
 
   // This can fail in heavily loaded CI environments
   bool skip_minimum_rate_check =
-#if defined(CIRCLECI) && defined(OS_MACOSX)
+#if defined(CIRCLECI) && (defined(OS_MACOSX) || defined(ROCKSDB_VALGRIND_RUN))
       true;
 #else
       getenv("SANDCASTLE");
