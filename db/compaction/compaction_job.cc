248d527ae3292a5234ebc317fc926da55a59f5bf
Yanqin Jin <yanqin@fb.com>
2022-09-09 10:55:48 -0700

    Fix a bug by setting up subcompaction bounds properly
    When user-defined timestamp is enabled, subcompaction bounds should be
    set up properly. When creating InputIterator for the compaction, the
    `start` and `end` should have their timestamp portions set to
    kMaxTimestamp, which is the highest possible timestamp. This is similar
    to what we do with setting up their sequence numbers to
    `kMaxSequenceNumber`.
    
    (Will add new unit tests)
    
    Test plan:
    ```bash
    make check
    rm -rf /dev/shm/rocksdb/* && mkdir
    /dev/shm/rocksdb/rocksdb_crashtest_expected && ./db_stress
    --allow_data_in_errors=True --clear_column_family_one_in=0
    --continuous_verification_interval=0 --data_block_index_type=1
    --db=/dev/shm/rocksdb//rocksdb_crashtest_blackbox --delpercent=5
    --delrangepercent=0
    --expected_values_dir=/dev/shm/rocksdb//rocksdb_crashtest_expected
    --iterpercent=0 --max_background_compactions=20
    --max_bytes_for_level_base=10485760 --max_key=25000000
    --max_write_batch_group_size_bytes=1048576 --nooverwritepercent=1
    --ops_per_thread=300000 --paranoid_file_checks=1 --partition_filters=0
    --prefix_size=8 --prefixpercent=5 --readpercent=30 --reopen=0
    --snapshot_hold_ops=100000 --subcompactions=4
    --target_file_size_base=65536 --target_file_size_multiplier=2
    --test_batches_snapshots=0 --test_cf_consistency=0 --use_multiget=1
    --user_timestamp_size=8 --value_size_mult=32 --verify_checksum=1
    --write_buffer_size=65536 --writepercent=60 -disable_wal=1
    -column_families=1
    ```

--- db/compaction/compaction_job.cc
+++ db/compaction/compaction_job.cc
@@ -254,7 +254,7 @@ void CompactionJob::Prepare() {
       // timestamp part).
       assert(i == 0 || i == boundaries_.size() ||
              cfd->user_comparator()->CompareWithoutTimestamp(
-                 boundaries_[i - 1], true, boundaries_[i], true) < 0);
+                 boundaries_[i - 1], boundaries_[i]) < 0);
     }
     RecordInHistogram(stats_, NUM_SUBCOMPACTIONS_SCHEDULED,
                       compact_->sub_compact_states.size());
@@ -486,8 +486,8 @@ void CompactionJob::GenSubcompactionBoundaries() {
   std::sort(
       all_anchors.begin(), all_anchors.end(),
       [cfd_comparator](TableReader::Anchor& a, TableReader::Anchor& b) -> bool {
-        return cfd_comparator->CompareWithoutTimestamp(a.user_key, true,
-                                                       b.user_key, true) < 0;
+        return cfd_comparator->CompareWithoutTimestamp(a.user_key, b.user_key) <
+               0;
       });
 
   // Remove duplicated entries from boundaries.
@@ -496,7 +496,7 @@ void CompactionJob::GenSubcompactionBoundaries() {
                   [cfd_comparator](TableReader::Anchor& a,
                                    TableReader::Anchor& b) -> bool {
                     return cfd_comparator->CompareWithoutTimestamp(
-                               a.user_key, true, b.user_key, true) == 0;
+                               a.user_key, b.user_key) == 0;
                   }),
       all_anchors.end());
 
@@ -1085,13 +1085,34 @@ void CompactionJob::ProcessKeyValueCompaction(SubcompactionState* sub_compact) {
   Slice start_slice;
   Slice end_slice;
 
+  static constexpr char kMaxTs[] =
+      "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
+  Slice ts_slice;
+  std::string max_ts;
+  if (ts_sz > 0) {
+    if (ts_sz <= strlen(kMaxTs)) {
+      ts_slice = Slice(kMaxTs, ts_sz);
+    } else {
+      max_ts = std::string(ts_sz, '\xff');
+      ts_slice = Slice(max_ts);
+    }
+  }
+
   if (start.has_value()) {
     start_ikey.SetInternalKey(start.value(), kMaxSequenceNumber,
                               kValueTypeForSeek);
+    if (ts_sz > 0) {
+      start_ikey.UpdateInternalKey(kMaxSequenceNumber, kValueTypeForSeek,
+                                   &ts_slice);
+    }
     start_slice = start_ikey.GetInternalKey();
   }
   if (end.has_value()) {
     end_ikey.SetInternalKey(end.value(), kMaxSequenceNumber, kValueTypeForSeek);
+    if (ts_sz > 0) {
+      end_ikey.UpdateInternalKey(kMaxSequenceNumber, kValueTypeForSeek,
+                                 &ts_slice);
+    }
     end_slice = end_ikey.GetInternalKey();
   }
 
