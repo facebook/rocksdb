0b376a436607910e2f2cae8ff149844e2e393990
sdong <siying.d@fb.com>
2022-10-24 14:30:09 -0700

    Revert port/win/port_win.h header order for mystery build failure.

--- port/win/port_win.h
+++ port/win/port_win.h
@@ -16,20 +16,21 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
-#include <intrin.h>
-#include <malloc.h>
-#include <process.h>
-#include <stdint.h>
-#include <string.h>
 #include <windows.h>
-
-#include <condition_variable>
-#include <limits>
-#include <mutex>
 #include <string>
 #include <thread>
+#include <string.h>
+#include <mutex>
+#include <limits>
+#include <condition_variable>
+#include <malloc.h>
+#include <intrin.h>
+#include <process.h>
+
+#include <stdint.h>
 
 #include "port/win/win_thread.h"
+
 #include "rocksdb/options.h"
 
 #undef min
