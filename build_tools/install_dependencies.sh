bd62c99c6b17b4fdf810b100d0f2100b63f4a1ff
vrdhn <vrdhn0@gmail.com>
2022-11-14 13:49:59 +0530

    Trying newer version of liburing, and misc

--- build_tools/install_dependencies.sh
+++ build_tools/install_dependencies.sh
@@ -7,8 +7,8 @@ J=-j8
 
 ## Install static dependencies for compiling static rocksdbjava with cmake.
 ## Note that .a are made with -fPIC, as they end up in a .so
-## Also note that this should be enhanced to arc specific defines.
-## generate cmake flags
+## Also note that this should be enhanced to arch specific defines.
+
 
 CMAKEFLAGS="-DWITH_JNI=OFF -DWITH_LIBURING=OFF -DWITH_LZ4=OFF -DWITH_SNAPPY=OFF -DWITH_ZLIB=OFF -DWITH_ZSTD=OFF -DWITH_BZ2=OFF -DWITH_GFLAGS=OFF -DWITH_JEMALLOC=OFF"
 
@@ -21,7 +21,7 @@ esac
 
 usage ()
 {
-    echo ' installs dependencies, and generate cmake flags for rocksdb compilation.
+    echo ' installs dependencies, and generates cmake flags for rocksdb compilation.
     ./build_tools/install_deps.sh <dirname> packages ...
     cmake $(cat <dirname>/cmake-flags) ...
     packages:'
@@ -50,52 +50,73 @@ download ()
 
 with_iouring()  # makes liburing.a with -fPIC
 {
-    echo Compiling iouring to $DIR/iouring/ >&2
+    ORG=axboe
+    REPO=liburing
+    TAG=liburing-2.3
+    ROOT=$REPO-$TAG
+    DEST=iouring
+
+    echo Compiling $ORG/$REPO[$TAG] to $DIR/$DEST/ >&2
     cd $DIR/
-    download liburing-0.7.tar.gz https://github.com/axboe/liburing/archive/refs/tags/liburing-0.7.tar.gz
-    rm -fr liburing-liburing-0.7 $DIR/iouring/
-    tar xf liburing-0.7.tar.gz
-    cd liburing-liburing-0.7/
-    ./configure --prefix=$DIR/iouring/
+    download $REPO-$TAG.tar.gz https://github.com/$ORG/$REPO/archive/refs/tags/$TAG.tar.gz
+    rm -fr $REPO-$TAG $DIR/$DEST/
+    tar xf $REPO-$TAG.tar.gz
+    cd $ROOT/
+
+    ./configure --prefix=$DIR/$DEST/
     make $J V=1 -C src ENABLE_SHARED=0 CFLAGS=" -g -fomit-frame-pointer -O2 -fPIC" install
-    cmakeflags LIBURING -During_INCLUDE_DIR=$DIR/iouring/include -During_LIBRARIES=$DIR/iouring/lib/liburing.a
+    cmakeflags LIBURING -During_INCLUDE_DIR=$DIR/$DEST/include -During_LIBRARIES=$DIR/$DEST/lib/liburing.a
 }
 
 with_lz4() # makes liblz4.a with -fPIC
 {
-    echo Compiling lz4 to $DIR/lz4/ >&2
+    ORG=lz4
+    REPO=lz4
+    TAG=v1.9.4
+    DEST=lz4
+    ROOT=$REPO-${TAG:1}
+
+    echo Compiling $ORG/$REPO[$TAG] to $DIR/$DEST/ >&2
     cd $DIR/
-    download lz4-v1.9.4.tar.gz https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.tar.gz
-    rm -fr lz4-1.9.4 $DIR/lz4/
-    tar xf lz4-v1.9.4.tar.gz
-    cd lz4-1.9.4
+    download $REPO-$TAG.tar.gz https://github.com/$ORG/$REPO/archive/refs/tags/$TAG.tar.gz
+    rm -fr $REPO-$TAG $DIR/$DEST/
+    tar xf $REPO-$TAG.tar.gz
+    cd $ROOT/
+
     case $OS in
         *Win* )
             cd build/cmake
-            cmake -B build -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=1  -DCMAKE_INSTALL_PREFIX=$DIR/lz4/ -DCMAKE_BUILD_TYPE=Release
+            cmake -B build -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=1  -DCMAKE_INSTALL_PREFIX=$DIR/$DEST/ -DCMAKE_BUILD_TYPE=Release
             cmake --build build/ -t install -v --config Release
-            cmakeflags LZ4 -DLZ4_INCLUDE=$DIR/lz4/include -DLZ4_LIB_RELEASE=$DIR/lz4/lib/lz4.lib
+            cmakeflags LZ4 -DLZ4_INCLUDE=$DIR/$DEST/include -DLZ4_LIB_RELEASE=$DIR/$DEST/lib/lz4.lib
             ;;
         * )
-            make $J V=1 PREFIX=$DIR/lz4/ CFLAGS="-O3 -fPIC $ARCH_CFLAGS"  BUILD_SHARED=no BUILD_STATIC=yes install
-            cmakeflags LZ4 -Dlz4_INCLUDE_DIRS=$DIR/lz4/include -Dlz4_LIBRARIES=$DIR/lz4/lib/liblz4.a
+            make $J V=1 PREFIX=$DIR/$DEST/ CFLAGS="-O3 -fPIC $ARCH_CFLAGS"  BUILD_SHARED=no BUILD_STATIC=yes install
+            cmakeflags LZ4 -Dlz4_INCLUDE_DIRS=$DIR/$DEST/include -Dlz4_LIBRARIES=$DIR/$DEST/lib/liblz4.a
             ;;
     esac
 }
 
 with_jemalloc()  # makes libjemalloc_pic.a
 {
-    echo Compiling jemalloc to $DIR/jemalloc >&2
-    cd $DIR
-    download jemalloc-5.3.0.tar.gz https://github.com/jemalloc/jemalloc/archive/refs/tags/5.3.0.tar.gz
-    rm -fr jemalloc-5.3.0 $DIR/jemalloc
-    tar xf jemalloc-5.3.0.tar.gz
-    cd jemalloc-5.3.0
+    ORG=jemalloc
+    REPO=jemalloc
+    TAG=5.3.0
+    DEST=jemalloc
+    ROOT=$REPO-$TAG
+
+    echo Compiling $ORG/$REPO[$TAG] to $DIR/$DEST/ >&2
+    cd $DIR/
+    download $REPO-$TAG.tar.gz https://github.com/$ORG/$REPO/archive/refs/tags/$TAG.tar.gz
+    rm -fr $REPO-$TAG $DIR/$DEST/
+    tar xf $REPO-$TAG.tar.gz
+    cd $ROOT
+
     ## Sadly ... it needs autoconf.
     autoconf
-    ./autogen.sh   --prefix=$DIR/jemalloc --disable-initial-exec-tls
+    ./autogen.sh   --prefix=$DIR/$DEST --disable-initial-exec-tls
     make $J install_lib_static install_include
-    cmakeflags JEMALLOC -DJeMalloc_INCLUDE_DIRS=$DIR/jemalloc/include -DJeMalloc_LIBRARIES=$DIR/jemalloc/lib/libjemalloc_pic.a
+    cmakeflags JEMALLOC -DJeMalloc_INCLUDE_DIRS=$DIR/$DEST/include -DJeMalloc_LIBRARIES=$DIR/$DEST/lib/libjemalloc_pic.a
 }
 
 with_jni() # ensure JAVA_HOME is set.
