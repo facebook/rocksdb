f1574a20ff9ac2b84ef151c95034c612492dbd81
Hui Xiao <huixiao@fb.com>
2022-11-29 10:56:42 -0800

    Revert PR 10777 "Fix FIFO causing overlapping seqnos in L0 files due to overla…" (#10999)
    Summary:
    **Context/Summary:**
    
    This reverts commit fc74abb436c5807e967aecc892bb8fa127f2fa85 and related HISTORY record.
    
    The issue with PR 10777 or general approach using earliest_mem_seqno like https://github.com/facebook/rocksdb/pull/5958#issue-511150930 is that the earliest seqno of memtable of each CFs does not get persisted and will always start with 0 upon Recover(). Later when creating a new memtable in certain CF, we use the last seqno of the whole DB (but not of that CF from previous DB session) for this CF.  This will lead to false positive overlapping seqno and PR 10777 will throw something like https://github.com/facebook/rocksdb/blob/main/db/compaction/compaction_picker.cc#L1002-L1004
    
    Luckily a more elegant and complete solution to the overlapping seqno problem these PR aim to solve does not have above problem, see https://github.com/facebook/rocksdb/pull/10922. It is already being pursued and in the process of review. So we can just revert this PR and focus on getting PR10922 to land.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/10999
    
    Test Plan: make check
    
    Reviewed By: anand1976
    
    Differential Revision: D41572604
    
    Pulled By: hx235
    
    fbshipit-source-id: 9d9bdf594abd235e2137045cef513ca0b14e0a3a

--- db_stress_tool/db_stress_test_base.cc
+++ db_stress_tool/db_stress_test_base.cc
@@ -3121,11 +3121,6 @@ void InitializeOptionsFromFlags(
   options.max_background_flushes = FLAGS_max_background_flushes;
   options.compaction_style =
       static_cast<ROCKSDB_NAMESPACE::CompactionStyle>(FLAGS_compaction_style);
-  if (options.compaction_style ==
-      ROCKSDB_NAMESPACE::CompactionStyle::kCompactionStyleFIFO) {
-    options.compaction_options_fifo.allow_compaction =
-        FLAGS_fifo_allow_compaction;
-  }
   options.compaction_pri =
       static_cast<ROCKSDB_NAMESPACE::CompactionPri>(FLAGS_compaction_pri);
   options.num_levels = FLAGS_num_levels;
