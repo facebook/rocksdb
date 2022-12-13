ab592b39547517ebbb235435384d9828842a100f
Paul <41552663+molecula451@users.noreply.github.com>
2022-12-13 03:46:17 -0600

    update script to latest rocksdb version
    the defaulted zstd version works, unless they want to be updated to current one

--- build_tools/setup_centos7.sh
+++ build_tools/setup_centos7.sh
@@ -2,7 +2,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 set -ex
 
-ROCKSDB_VERSION="6.7.3"
+ROCKSDB_VERSION="7.7.3"
 ZSTD_VERSION="1.4.4"
 
 echo "This script configures CentOS with everything needed to build and run RocksDB"
