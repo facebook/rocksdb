4590e43c39bd5e79766bf8704bc46507c9ad4488
Peter Dillinger <peterd@fb.com>
2023-01-27 09:08:06 -0800

    Fix uninit bug

--- db/db_impl/db_impl.cc
+++ db/db_impl/db_impl.cc
@@ -247,7 +247,8 @@ DBImpl::DBImpl(const DBOptions& options, const std::string& dbname,
       atomic_flush_install_cv_(&mutex_),
       blob_callback_(immutable_db_options_.sst_file_manager.get(), &mutex_,
                      &error_handler_, &event_logger_,
-                     immutable_db_options_.listeners, dbname_) {
+                     immutable_db_options_.listeners, dbname_),
+      lock_wal_extra_outstanding_(0) {
   // !batch_per_trx_ implies seq_per_batch_ because it is only unset for
   // WriteUnprepared, which should use seq_per_batch_.
   assert(batch_per_txn_ || seq_per_batch_);
