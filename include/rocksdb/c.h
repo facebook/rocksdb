19cdb44dd772de4c5bb3c9105fb83415c5a0c731
Alex Erofeev <a.p.erofeev@yandex.ru>
2023-01-29 00:00:09 +0000

    add c-api for allowing FIFO compaction

--- include/rocksdb/c.h
+++ include/rocksdb/c.h
@@ -2198,6 +2198,12 @@ extern ROCKSDB_LIBRARY_API void rocksdb_universal_compaction_options_destroy(
 extern ROCKSDB_LIBRARY_API rocksdb_fifo_compaction_options_t*
 rocksdb_fifo_compaction_options_create(void);
 extern ROCKSDB_LIBRARY_API void
+rocksdb_fifo_compaction_options_set_allow_compaction(
+    rocksdb_fifo_compaction_options_t* fifo_opts, bool allow_compaction);
+extern ROCKSDB_LIBRARY_API bool
+rocksdb_fifo_compaction_options_get_allow_compaction(
+    rocksdb_fifo_compaction_options_t* fifo_opts);
+extern ROCKSDB_LIBRARY_API void
 rocksdb_fifo_compaction_options_set_max_table_files_size(
     rocksdb_fifo_compaction_options_t* fifo_opts, uint64_t size);
 extern ROCKSDB_LIBRARY_API uint64_t
