ba3d0c92365b30e01d317864db9381d18654cb7c
Alan Paxton <alan.paxton@gmail.com>
2023-02-08 13:19:38 +0000

    Java test/support for Int64Add merge operator
    Implement encoding and zigzag in Java so that we can test it more thoroughly.

--- java/src/main/java/org/rocksdb/Int64AddOperator.java
+++ java/src/main/java/org/rocksdb/Int64AddOperator.java
@@ -1,13 +1,11 @@
 package org.rocksdb;
 
 public class Int64AddOperator extends MergeOperator {
-
   protected Int64AddOperator() {
     super(newSharedInt64AddOperator());
   }
 
-  @Override
-  protected native void disposeInternal(final long handle);
+  @Override protected native void disposeInternal(final long handle);
 
   private static native long newSharedInt64AddOperator();
 }
