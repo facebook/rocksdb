7e293cb7e1eaba767c4a13740281b0ae5e6a2b0a
Alan Paxton <alan.paxton@gmail.com>
2023-02-08 16:32:15 +0000

    Extract merge encodings as a public API
    Exporting the int64 merge operator to Java requires support for encoding to make it useful.
    
    It also requires that support in order to test it from Java.

--- java/src/test/java/org/rocksdb/MergeTest.java
+++ java/src/test/java/org/rocksdb/MergeTest.java
@@ -6,10 +6,8 @@
 package org.rocksdb;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.rocksdb.util.MergeEncodings.*;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -327,109 +325,6 @@ public class MergeTest {
         }
       }
     }
-  }
-
-  private long fromZigzag(final long z) {
-    final long half = z / 2;
-    return (half + half == z) ? half : -half - 1;
-  }
-
-  private static long MAX_ZIGZAG = Long.MAX_VALUE / 2;
-  private static long MIN_ZIGZAG = Long.MIN_VALUE / 2;
-
-  private long toZigzag(final long i) {
-    if (i < MIN_ZIGZAG || i > MAX_ZIGZAG) {
-      throw new IllegalArgumentException(
-          "Zigzag can only be applied to Long.MIN_VALUE/2..Long.MAX_VALUE/2, parameter is " + i);
-    }
-    final long twice = i + i;
-    return (i >= 0) ? twice : -twice - 1;
-  }
-
-  @Test
-  public void testZigzagEncodingForTesting() {
-    assertThat(toZigzag(0)).isEqualTo(0);
-    assertThat(toZigzag(-1)).isEqualTo(1);
-    assertThat(toZigzag(1)).isEqualTo(2);
-    assertThat(toZigzag(-2)).isEqualTo(3);
-    assertThat(toZigzag(2)).isEqualTo(4);
-    assertThat(toZigzag(-3)).isEqualTo(5);
-    assertThat(toZigzag(3)).isEqualTo(6);
-    assertThat(toZigzag(-4)).isEqualTo(7);
-    assertThat(toZigzag(100)).isEqualTo(200);
-    assertThat(fromZigzag(202)).isEqualTo(101);
-  }
-
-  final byte topBit = (byte) (1 << 7 & ~127);
-
-  private byte[] encodeVarint(long v) {
-    final byte[] bytes = new byte[Long.BYTES + 2];
-    int i = 0;
-    while (v >= 128) {
-      bytes[i++] = (byte) (topBit | (v & 127));
-      v = v >> 7;
-    }
-    bytes[i++] = (byte) v;
-
-    return bytes;
-  }
-
-  private long decodeVarint(final byte[] bytes) {
-    int i = 0;
-    int shift = 0;
-    long acc = 0;
-    while (bytes[i] < 0) { // continuation bit reads as -ve 2s complement
-      acc |= (long) (bytes[i++] + 128) << shift;
-      shift += 7;
-    }
-    acc |= (long) (bytes[i++]) << shift;
-
-    return acc;
-  }
-
-  @Test
-  public void testVarintEncodingForTesting() {
-    byte[] bytes = encodeVarint(127);
-    assertThat(bytes[0]).isEqualTo((byte) 127);
-    assertThat(bytes[1]).isEqualTo((byte) 0);
-    bytes = encodeVarint(128);
-    assertThat(bytes[0]).isEqualTo((byte) -128);
-    assertThat(bytes[1]).isEqualTo((byte) 1);
-    bytes = encodeVarint(129);
-    assertThat(bytes[0]).isEqualTo((byte) -127);
-    assertThat(bytes[1]).isEqualTo((byte) 1);
-
-    assertThat(decodeVarint(encodeVarint(127))).isEqualTo(127);
-    assertThat(decodeVarint(encodeVarint(128))).isEqualTo(128);
-    assertThat(decodeVarint(encodeVarint(129))).isEqualTo(129);
-    assertThat(decodeVarint(encodeVarint(65536))).isEqualTo(65536);
-    assertThat(decodeVarint(encodeVarint(65537))).isEqualTo(65537);
-    assertThat(decodeVarint(encodeVarint(0))).isEqualTo(0);
-    assertThat(decodeVarint(encodeVarint(1))).isEqualTo(1);
-    assertThat(decodeVarint(encodeVarint(Long.MAX_VALUE))).isEqualTo(Long.MAX_VALUE);
-  }
-
-  private byte[] encodeSigned(final long v) {
-    return encodeVarint(toZigzag(v));
-  }
-
-  private long decodeSigned(final byte[] bytes) {
-    return fromZigzag(decodeVarint(bytes));
-  }
-
-  @Test
-  public void testSignedEncodingForTesting() {
-    assertThat(decodeSigned(encodeSigned(127))).isEqualTo(127);
-    assertThat(decodeSigned(encodeSigned(-1))).isEqualTo(-1);
-    assertThat(decodeSigned(encodeSigned(0))).isEqualTo(0);
-    assertThat(decodeSigned(encodeSigned(Integer.MAX_VALUE))).isEqualTo(Integer.MAX_VALUE);
-    assertThat(decodeSigned(encodeSigned(Integer.MIN_VALUE))).isEqualTo(Integer.MIN_VALUE);
-    assertThatThrownBy(() -> { decodeSigned(encodeSigned(Long.MAX_VALUE)); })
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining("Zigzag");
-    assertThatThrownBy(() -> { decodeSigned(encodeSigned(Long.MIN_VALUE)); })
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining("Zigzag");
   }
 
   @Test
