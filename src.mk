50efa1ceb7dfe2e6391d5b84bccd8221cade35a1
Hui Xiao <huixiao@fb.com>
2023-05-17 14:32:44 -0700

    Improve comment

--- src.mk
+++ src.mk
@@ -377,6 +377,7 @@ STRESS_LIB_SOURCES =                                            \
   db_stress_tool/db_stress_test_base.cc                        \
   db_stress_tool/db_stress_tool.cc                             \
   db_stress_tool/expected_state.cc                             \
+  db_stress_tool/expected_value.cc                             \
   db_stress_tool/no_batched_ops_stress.cc                      \
   db_stress_tool/multi_ops_txns_stress.cc                      \
 
