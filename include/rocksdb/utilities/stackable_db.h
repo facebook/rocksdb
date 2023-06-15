fa878a01074fe039135e37720f669391d1663525
mayue.fight <mayue.fight@bytedance.com>
2023-06-15 12:25:04 -0700

    Support to create a CF by importing multiple non-overlapping CFs (#11378)
    Summary:
    The original Feature Request is from [https://github.com/facebook/rocksdb/issues/11317](https://github.com/facebook/rocksdb/issues/11317).
    Flink uses rocksdb as the state backend,  all DB options are the same, and the keys of each DB instance are adjacent and there is no key overlap between two db instances.
    In the Flink rescaling scenario, it is necessary to quickly split the DB according to a certain key range or quickly merge multiple DBs into one.
    
    This PR is mainly used to quickly merge multiple DBs into one.
    
    We hope to extend the function of `CreateColumnFamilyWithImports` to support creating ColumnFamily by importing multiple ColumnFamily with no overlapping keys.
    
    The import logic is almost the same as `CreateColumnFamilyWithImport`, but it will check whether there is key overlap between CF when importing. The import will fail if there are key overlaps.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/11378
    
    Reviewed By: ajkr
    
    Differential Revision: D46413709
    
    Pulled By: cbi42
    
    fbshipit-source-id: 846d0049fad11c59cf460fa846c345b26c658dfb

--- include/rocksdb/utilities/stackable_db.h
+++ include/rocksdb/utilities/stackable_db.h
@@ -172,10 +172,10 @@ class StackableDB : public DB {
   virtual Status CreateColumnFamilyWithImport(
       const ColumnFamilyOptions& options, const std::string& column_family_name,
       const ImportColumnFamilyOptions& import_options,
-      const ExportImportFilesMetaData& metadata,
+      const std::vector<const ExportImportFilesMetaData*>& metadatas,
       ColumnFamilyHandle** handle) override {
     return db_->CreateColumnFamilyWithImport(options, column_family_name,
-                                             import_options, metadata, handle);
+                                             import_options, metadatas, handle);
   }
 
   using DB::ClipColumnFamily;
