338573607ab470d1822dce34fa5302b5ea62055b
Andrew Kryczka <ajkr@users.noreply.github.com>
2023-08-21 11:45:50 -0700

    Update db_log_iter_test.cc
    ASSERT_OK()

--- db/db_log_iter_test.cc
+++ db/db_log_iter_test.cc
@@ -155,18 +155,18 @@ TEST_F(DBTestXactLogIterator, TransactionLogIteratorCheckWhenArchive) {
     auto s = dbfull()->CreateColumnFamily(ColumnFamilyOptions(), "CF", &cf);
     ASSERT_TRUE(s.ok());
 
-    dbfull()->Put(WriteOptions(), cf, "key1", DummyString(1024));
+    ASSERT_OK(dbfull()->Put(WriteOptions(), cf, "key1", DummyString(1024)));
 
-    dbfull()->Put(WriteOptions(), "key2", DummyString(1024));
+    ASSERT_OK(dbfull()->Put(WriteOptions(), "key2", DummyString(1024)));
 
-    dbfull()->Flush(FlushOptions());
+    ASSERT_OK(dbfull()->Flush(FlushOptions()));
 
-    dbfull()->Put(WriteOptions(), "key3", DummyString(1024));
+    ASSERT_OK(dbfull()->Put(WriteOptions(), "key3", DummyString(1024)));
 
-    dbfull()->Flush(FlushOptions());
+    ASSERT_OK(dbfull()->Flush(FlushOptions()));
 
-    dbfull()->Put(WriteOptions(), "key4", DummyString(1024));
-    dbfull()->Flush(FlushOptions());
+    ASSERT_OK(dbfull()->Put(WriteOptions(), "key4", DummyString(1024)));
+    ASSERT_OK(dbfull()->Flush(FlushOptions()));
 
     ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
         "WalManager::PurgeObsoleteFiles:1", [&](void*) {
@@ -175,7 +175,7 @@ TEST_F(DBTestXactLogIterator, TransactionLogIteratorCheckWhenArchive) {
         });
 
     ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->EnableProcessing();
-    dbfull()->Flush(FlushOptions(), cf);
+    ASSERT_OK(dbfull()->Flush(FlushOptions(), cf));
 
     delete cf;
   } while (ChangeCompactOptions());
