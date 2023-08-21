000b6e6990662a53ce7787858d0e47520bb047d3
chuhao zeng <chuhao.zeng@mail.utoronto.ca>
2023-08-20 23:13:04 -0400

    Reverse sort order in dedup to enable iter checking in callback

--- db/db_log_iter_test.cc
+++ db/db_log_iter_test.cc
@@ -145,6 +145,41 @@ TEST_F(DBTestXactLogIterator, TransactionLogIteratorRace) {
     } while (ChangeCompactOptions());
   }
 }
+
+TEST_F(DBTestXactLogIterator, TransactionLogIteratorCheckWhenArchive) {
+  do {
+    ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->ClearTrace();
+    Options options = OptionsForLogIterTest();
+    DestroyAndReopen(options);
+    ColumnFamilyHandle* cf;
+    auto s = dbfull()->CreateColumnFamily(ColumnFamilyOptions(), "CF", &cf);
+    ASSERT_TRUE(s.ok());
+
+    dbfull()->Put(WriteOptions(), cf, "key1", DummyString(1024));
+
+    dbfull()->Put(WriteOptions(), "key2", DummyString(1024));
+
+    dbfull()->Flush(FlushOptions());
+
+    dbfull()->Put(WriteOptions(), "key3", DummyString(1024));
+
+    dbfull()->Flush(FlushOptions());
+
+    dbfull()->Put(WriteOptions(), "key4", DummyString(1024));
+    dbfull()->Flush(FlushOptions());
+
+    ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
+        "WalManager::PurgeObsoleteFiles:1", [&](void*) {
+          auto iter = OpenTransactionLogIter(0);
+          ExpectRecords(4, iter);
+        });
+
+    ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->EnableProcessing();
+    dbfull()->Flush(FlushOptions(), cf);
+
+    delete cf;
+  } while (ChangeCompactOptions());
+}
 #endif
 
 TEST_F(DBTestXactLogIterator, TransactionLogIteratorStallAtLastRecord) {
