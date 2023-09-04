7e5b64f4165d96a187253ab94d1519cb2c6a5ccc
Radek Hubner <rhubner@users.noreply.github.com>
2023-09-04 22:04:47 +0100

    Expose perf context API in java.

--- java/rocksjni/portal.h
+++ java/rocksjni/portal.h
@@ -27,6 +27,7 @@
 #include "rocksdb/convenience.h"
 #include "rocksdb/db.h"
 #include "rocksdb/filter_policy.h"
+#include "rocksdb/perf_level.h"
 #include "rocksdb/rate_limiter.h"
 #include "rocksdb/status.h"
 #include "rocksdb/table.h"
@@ -5907,6 +5908,52 @@ class MemoryUsageTypeJni {
       default:
         // undefined/default: use kNumUsageTypes
         return ROCKSDB_NAMESPACE::MemoryUtil::UsageType::kNumUsageTypes;
+    }
+  }
+};
+
+class PerfLevelTypeJni {
+ public:
+  static jbyte toJavaPerfLevelType(const ROCKSDB_NAMESPACE::PerfLevel level) {
+    switch (level) {
+      case ROCKSDB_NAMESPACE::PerfLevel::kUninitialized:
+        return 0x0;
+      case ROCKSDB_NAMESPACE::PerfLevel::kDisable:
+        return 0x1;
+      case ROCKSDB_NAMESPACE::PerfLevel::kEnableCount:
+        return 0x2;
+      case ROCKSDB_NAMESPACE::PerfLevel::kEnableTimeExceptForMutex:
+        return 0x3;
+      case ROCKSDB_NAMESPACE::PerfLevel::kEnableTimeAndCPUTimeExceptForMutex:
+        return 0x4;
+      case ROCKSDB_NAMESPACE::PerfLevel::kEnableTime:
+        return 0x5;
+      case ROCKSDB_NAMESPACE::PerfLevel::kOutOfBounds:
+        return 0x6;
+      default:
+        return 0x6;
+    }
+  }
+
+  static ROCKSDB_NAMESPACE::PerfLevel toCppPerfLevelType(const jbyte level) {
+    switch (level) {
+      case 0x0:
+        return ROCKSDB_NAMESPACE::PerfLevel::kUninitialized;
+      case 0x1:
+        return ROCKSDB_NAMESPACE::PerfLevel::kDisable;
+      case 0x2:
+        return ROCKSDB_NAMESPACE::PerfLevel::kEnableCount;
+      case 0x3:
+        return ROCKSDB_NAMESPACE::PerfLevel::kEnableTimeExceptForMutex;
+      case 0x4:
+        return ROCKSDB_NAMESPACE::PerfLevel::
+            kEnableTimeAndCPUTimeExceptForMutex;
+      case 0x5:
+        return ROCKSDB_NAMESPACE::PerfLevel::kEnableTime;
+      case 0x6:
+        return ROCKSDB_NAMESPACE::PerfLevel::kOutOfBounds;
+      default:
+        return ROCKSDB_NAMESPACE::PerfLevel::kOutOfBounds;
     }
   }
 };
