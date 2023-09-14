84d335b61953b885b113764ab096b26a135f5dbb
Jonah Gao <jonahgaox@gmail.com>
2023-09-14 11:25:33 -0700

    Remove an unused variable: `last_stats_dump_time_microsec_` (#11824)
    Summary:
    `last_stats_dump_time_microsec_`  is not used after initialization.
    
    I guess that it was previously used to implement periodically dumping stats,
    but this functionality has now been delegated to the `PeriodicTaskScheduler`.
    https://github.com/facebook/rocksdb/blob/4b79e8c0039389fc731f8bc9bd6109257c21d9d1/db/db_impl/db_impl.cc#L770-L778
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/11824
    
    Reviewed By: cbi42
    
    Differential Revision: D49278311
    
    Pulled By: jowlyzhang
    
    fbshipit-source-id: 5856245580afc026e6b490755a45c5436a2375c9

--- db/db_impl/db_impl.h
+++ db/db_impl/db_impl.h
@@ -2633,9 +2633,6 @@ class DBImpl : public DB {
   // initialized with startup time.
   uint64_t delete_obsolete_files_last_run_;
 
-  // last time stats were dumped to LOG
-  std::atomic<uint64_t> last_stats_dump_time_microsec_;
-
   // The thread that wants to switch memtable, can wait on this cv until the
   // pending writes to memtable finishes.
   std::condition_variable switch_cv_;
