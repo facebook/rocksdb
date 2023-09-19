f66b96a366060d0ccd09e6ed73286b41b3d6f6a8
Changyu Bi <changyubi@meta.com>
2023-09-19 00:13:41 -0700

    disable write fault injection

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -179,7 +179,7 @@ default_params = {
     "max_key_len": 3,
     "key_len_percent_dist": "1,30,69",
     "read_fault_one_in": lambda: random.choice([0, 32, 1000]),
-    "write_fault_one_in": lambda: random.choice([0, 1000]),
+    "write_fault_one_in": 0,
     "open_metadata_write_fault_one_in": lambda: random.choice([0, 0, 8]),
     "open_write_fault_one_in": lambda: random.choice([0, 0, 16]),
     "open_read_fault_one_in": lambda: random.choice([0, 0, 32]),
