ae52f152aef4073b67871bf67f22e17d45be04cb
Jay Huh <jewoongh@meta.com>
2023-09-27 16:45:11 -0700

    Remove regex library import

--- db/db_impl/db_impl_open.cc
+++ db/db_impl/db_impl_open.cc
@@ -7,7 +7,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 #include <cinttypes>
-#include <regex>
 
 #include "db/builder.h"
 #include "db/db_impl/db_impl.h"
