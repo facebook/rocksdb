e4d7b59293268166805ef2dd22475b44ee0362ff
Andrew Kryczka <andrew.kryczka2@gmail.com>
2023-10-10 06:31:45 -0700

    Consolidate `ErrorHandler`'s recovery status variables (#11937)
    Summary:
    cbi42 pointed out a race condition in which `recovery_io_error_` and `recovery_error_` could be updated inconsistently due to releasing the DB mutex in `EventHelpers::NotifyOnBackgroundError()`. There doesn't seem to be a point to having two status objects, so this PR consolidates them.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/11937
    
    Reviewed By: cbi42
    
    Differential Revision: D50105793
    
    Pulled By: ajkr
    
    fbshipit-source-id: 3de95baccfa44351a49a5c2aa0986c9bc81baa8f

--- db/error_handler.h
+++ db/error_handler.h
@@ -46,7 +46,6 @@ class ErrorHandler {
     // Clear the checked flag for uninitialized errors
     bg_error_.PermitUncheckedError();
     recovery_error_.PermitUncheckedError();
-    recovery_io_error_.PermitUncheckedError();
   }
 
   void EnableAutoRecovery() { auto_recovery_ = true; }
@@ -87,10 +86,7 @@ class ErrorHandler {
   Status bg_error_;
   // A separate Status variable used to record any errors during the
   // recovery process from hard errors
-  Status recovery_error_;
-  // A separate IO Status variable used to record any IO errors during
-  // the recovery process. At the same time, recovery_error_ is also set.
-  IOStatus recovery_io_error_;
+  IOStatus recovery_error_;
   // The condition variable used with db_mutex during auto resume for time
   // wait.
   InstrumentedCondVar cv_;
