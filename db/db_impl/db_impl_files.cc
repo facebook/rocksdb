75fab5932a1a16b70851441776ca8cb7fb38d965
Yu Zhang <yuzhangyu@fb.com>
2023-10-28 09:50:52 -0700

    Rate limiting stale sst files' deletion during recovery (#12016)
    Summary:
    As titled. If SstFileManager is available, deleting stale sst files will be delegated to it so it can be rate limited.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/12016
    
    Reviewed By: hx235
    
    Differential Revision: D50670482
    
    Pulled By: jowlyzhang
    
    fbshipit-source-id: bde5b76ea1d98e67f6b4f08bfba3db48e46aab4e

--- db/db_impl/db_impl_files.cc
+++ db/db_impl/db_impl_files.cc
@@ -1003,7 +1003,7 @@ Status DBImpl::DeleteUnreferencedSstFiles(RecoveryContext* recovery_ctx) {
       if (type == kTableFile && number >= next_file_number &&
           recovery_ctx->files_to_delete_.find(normalized_fpath) ==
               recovery_ctx->files_to_delete_.end()) {
-        recovery_ctx->files_to_delete_.emplace(normalized_fpath);
+        recovery_ctx->files_to_delete_.emplace(normalized_fpath, path);
       }
     }
   }
