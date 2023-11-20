39d33475da7b6db8372ebd3c4db74993aacc18a2
Timo Riski <github@timoriski.fi>
2023-11-20 10:11:16 -0800

    Fix build on FreeBSD (#11218) (#12078)
    Summary:
    Fixes https://github.com/facebook/rocksdb/issues/11218
    
    Changes from https://github.com/facebook/rocksdb/issues/10881 broke FreeBSD builds with:
    
        env/io_posix.h:39:9: error: 'POSIX_MADV_NORMAL' macro redefined [-Werror,-Wmacro-redefined]
    
    This commit fixes FreeBSD builds by ignoring MADV defines.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/12078
    
    Reviewed By: cbi42
    
    Differential Revision: D51452802
    
    Pulled By: ajkr
    
    fbshipit-source-id: 0a1f5a90954e7d257a95794277a843ac77f3a709

--- env/io_posix.h
+++ env/io_posix.h
@@ -29,7 +29,8 @@
 
 // For non linux platform, the following macros are used only as place
 // holder.
-#if !(defined OS_LINUX) && !(defined CYGWIN) && !(defined OS_AIX)
+#if !(defined OS_LINUX) && !(defined OS_FREEBSD) && !(defined CYGWIN) && \
+    !(defined OS_AIX)
 #define POSIX_FADV_NORMAL 0     /* [MC1] no further special treatment */
 #define POSIX_FADV_RANDOM 1     /* [MC1] expect random page refs */
 #define POSIX_FADV_SEQUENTIAL 2 /* [MC1] expect sequential page refs */
