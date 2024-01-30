1d8c54aeaa5f9f6e622100b9f08b041176e0e54c
Radek Hubner <rhubner@users.noreply.github.com>
2024-01-29 16:19:59 -0800

    Fix build on OpenBSD i386 (#12142)
    Summary: Pull Request resolved: https://github.com/facebook/rocksdb/pull/12142
    
    Reviewed By: pdillinger
    
    Differential Revision: D53150218
    
    Pulled By: ajkr
    
    fbshipit-source-id: a4c4d9d22d99e8a82d93d1a7ef37ec5326855cb5

--- java/src/test/java/org/rocksdb/PerfContextTest.java
+++ java/src/test/java/org/rocksdb/PerfContextTest.java
@@ -17,6 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
+import org.rocksdb.util.Environment;
 
 public class PerfContextTest {
   @ClassRule
@@ -80,6 +81,8 @@ public class PerfContextTest {
 
   @Test
   public void testGetBlockReadCpuTime() throws RocksDBException {
+    Assume.assumeFalse("We are not running this test on OpenBSD or Windows",
+        Environment.isOpenBSD() || Environment.isWindows());
     db.setPerfLevel(PerfLevel.ENABLE_TIME_AND_CPU_TIME_EXCEPT_FOR_MUTEX);
     db.put("key".getBytes(), "value".getBytes());
     db.compactRange();
--- utilities/transactions/pessimistic_transaction.cc
+++ utilities/transactions/pessimistic_transaction.cc
@@ -688,7 +688,7 @@ Status WriteCommittedTxn::CommitWithoutPrepareInternal() {
           const Comparator* ucmp =
               WriteBatchWithIndexInternal::GetUserComparator(*wbwi, cf);
           return ucmp ? ucmp->timestamp_size()
-                      : std::numeric_limits<uint64_t>::max();
+                      : std::numeric_limits<size_t>::max();
         });
     if (!s.ok()) {
       return s;
@@ -763,7 +763,7 @@ Status WriteCommittedTxn::CommitInternal() {
         const Comparator* ucmp =
             WriteBatchWithIndexInternal::GetUserComparator(*wbwi, cf);
         return ucmp ? ucmp->timestamp_size()
-                    : std::numeric_limits<uint64_t>::max();
+                    : std::numeric_limits<size_t>::max();
       });
     }
   }
