e1d45d1b2f2028bb9e21ce175522117d925f3a0e
Radek Hubner <rhubner@users.noreply.github.com>
2024-03-25 20:20:05 +0400

    Address PR comments, delete local references in loop.

--- java/rocksjni/portal.h
+++ java/rocksjni/portal.h
@@ -9228,6 +9228,7 @@ class WideColumnJni {
         return;
       }
       auto _name = env->GetDirectBufferAddress(jname);
+      env->DeleteLocalRef(jname);
       if (_name == nullptr) {
         ROCKSDB_NAMESPACE::RocksDBExceptionJni::ThrowNew(
             env,
@@ -9243,6 +9244,7 @@ class WideColumnJni {
         return;
       }
       auto _value = env->GetDirectBufferAddress(j_value);
+      env->DeleteLocalRef(j_value);
       if (_value == nullptr) {
         ROCKSDB_NAMESPACE::RocksDBExceptionJni::ThrowNew(
             env,
@@ -9294,6 +9296,7 @@ class WideColumnJni {
       auto jname_len = env->GetArrayLength(jname);
       auto name = std::make_unique<jbyte[]>(jname_len);
       env->GetByteArrayRegion(jname, 0, jname_len, name.get());
+      env->DeleteLocalRef(jname);
       if (env->ExceptionCheck()) {
         return;
       }
@@ -9309,6 +9312,7 @@ class WideColumnJni {
       auto jvalue_len = env->GetArrayLength(j_value);
       auto value = std::make_unique<jbyte[]>(jvalue_len);
       env->GetByteArrayRegion(j_value, 0, jvalue_len, value.get());
+      env->DeleteLocalRef(j_value);
       if (env->ExceptionCheck()) {
         return;
       }
