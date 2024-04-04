c95c486f18ca77401ad6a0e126c957321aa144f7
Radek Hubner <rhubner@users.noreply.github.com>
2024-04-05 01:08:25 +0400

    Fix buid with make.

--- java/src/main/java/org/rocksdb/RocksDB.java
+++ java/src/main/java/org/rocksdb/RocksDB.java
@@ -617,7 +617,7 @@ public class RocksDB extends RocksObject {
     final long[] cfDescriptorHandles = new long[columnFamilyDescriptors.size()];
 
     for (int i = 0; i < columnFamilyDescriptors.size(); i++) {
-      final ColumnFamilyDescriptor cfDescriptor =  // NOPMD - CloseResource
+      final ColumnFamilyDescriptor cfDescriptor = // NOPMD - CloseResource
           columnFamilyDescriptors.get(i);
       cfDescriptorHandles[i] = cfDescriptor.nativeHandle_;
     }
