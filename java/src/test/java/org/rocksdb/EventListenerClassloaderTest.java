fa068ca252221f71d2483aeb0801360d3f0bc86d
Radek Hubner <rhubner@users.noreply.github.com>
2024-07-08 11:14:59 +0400

    Refactor class-loader test.

--- java/src/test/java/org/rocksdb/EventListenerClassloaderTest.java
+++ java/src/test/java/org/rocksdb/EventListenerClassloaderTest.java
@@ -1,18 +1,14 @@
 package org.rocksdb;
 
-
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
-
-
+import org.junit.Test;
 
 /**
  * Only this class can be on default classpath.
@@ -20,36 +16,31 @@ import static org.assertj.core.api.Assertions.fail;
  * event data for event listener can be instantiated.
  */
 public class EventListenerClassloaderTest {
-
-
-    @Test
-    public void testCallback() throws Exception {
-
-        try {
-            this.getClass().getClassLoader().loadClass("org.rocksdb.RocksDB");
-            fail("It looks like RocksDB is on classpath. This test must load RocksDB via custom classLoader" +
-                    " to verify callback cache all class instances.");
-        }catch (ClassNotFoundException e) {
-            ;
-        }
-
-        String jarPath = System.getProperty("rocks-jar");
-        assertThat(jarPath).isNotNull().as("Java property 'rocks-jar' was not setup properly");
-
-        Path classesDir = Paths.get(jarPath);
-        ClassLoader cl = new URLClassLoader(new URL[]{
-                classesDir.toAbsolutePath().toUri().toURL()
-        });
-
-        Class rocksDBclazz = cl.loadClass("org.rocksdb.RocksDB");
-        Method loadLibrary = rocksDBclazz.getMethod("loadLibrary");
-        loadLibrary.invoke(null);
-
-        Class testableEventListenerClazz = cl.loadClass("org.rocksdb.test.TestableEventListener");
-        Method invokeAllCallbacksInThread = testableEventListenerClazz.getMethod("invokeAllCallbacksInThread");
-        Object instance = testableEventListenerClazz.getDeclaredConstructor().newInstance();
-        invokeAllCallbacksInThread.invoke(instance);
-
-
+  @Test
+  public void testCallback() throws Exception {
+    try {
+      this.getClass().getClassLoader().loadClass("org.rocksdb.RocksDB");
+      fail("It looks like RocksDB is on classpath. This test must load RocksDB via custom "
+          + "classLoader"
+          + " to verify that callback cache all class instances.");
+    } catch (ClassNotFoundException e) {
+      ;
     }
+
+    String jarPath = System.getProperty("rocks-jar");
+    assertThat(jarPath).isNotNull().as("Java property 'rocks-jar' was not setup properly");
+
+    Path classesDir = Paths.get(jarPath);
+    ClassLoader cl = new URLClassLoader(new URL[] {classesDir.toAbsolutePath().toUri().toURL()});
+
+    Class rocksDBclazz = cl.loadClass("org.rocksdb.RocksDB");
+    Method loadLibrary = rocksDBclazz.getMethod("loadLibrary");
+    loadLibrary.invoke(null);
+
+    Class testableEventListenerClazz = cl.loadClass("org.rocksdb.test.TestableEventListener");
+    Method invokeAllCallbacksInThread =
+        testableEventListenerClazz.getMethod("invokeAllCallbacksInThread");
+    Object instance = testableEventListenerClazz.getDeclaredConstructor().newInstance();
+    invokeAllCallbacksInThread.invoke(instance);
+  }
 }
