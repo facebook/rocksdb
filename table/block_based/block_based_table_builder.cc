c388a72830e8b89b88955522857e08352e76e882
Andrew Ryan Chang <andrewrchang@fb.com>
2024-10-21 10:53:43 -0700

    Remove old set to oldest_ancestor_time for newest_key_time

--- table/block_based/block_based_table_builder.cc
+++ table/block_based/block_based_table_builder.cc
@@ -2183,7 +2183,7 @@ void BlockBasedTableBuilder::SetSeqnoTimeTableProperties(
   relevant_mapping.EncodeTo(rep_->props.seqno_to_time_mapping);
   rep_->props.creation_time = oldest_ancestor_time;
   // For now, just replicate behavior of creation_time with newest_key_time
-  rep_->props.newest_key_time = oldest_ancestor_time;
+  // rep_->props.newest_key_time = oldest_ancestor_time;
 }
 
 const std::string BlockBasedTable::kObsoleteFilterBlockPrefix = "filter.";
