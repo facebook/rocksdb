8c02aa53f6768018c849c3441f47f5d1cd94053f
Andrew Chang <andrewrchang@meta.com>
2024-12-03 09:46:14 -0800

    Temporarily disable fs buffer reuse optimization

--- file/file_prefetch_buffer.h
+++ file/file_prefetch_buffer.h
@@ -486,14 +486,8 @@ class FilePrefetchBuffer {
   }
 
   // Whether we reuse the file system provided buffer
-  // Until we also handle the async read case, only enable this optimization
-  // for the synchronous case when num_buffers_ = 1.
-  bool UseFSBuffer(RandomAccessFileReader* reader) {
-    return reader->file() != nullptr && !reader->use_direct_io() &&
-           fs_ != nullptr &&
-           CheckFSFeatureSupport(fs_, FSSupportedOps::kFSBuffer) &&
-           num_buffers_ == 1;
-  }
+  // TODO: renable when use-after-free issue is resolved
+  bool UseFSBuffer(RandomAccessFileReader*) { return false; }
 
   // When we are reusing the file system provided buffer, we are not concerned
   // with alignment. However, quite a bit of prefetch code incorporates
