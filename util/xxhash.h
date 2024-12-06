d0aceb61fc966a6de3edcec5a34e31622877165b
Giacomo Bergami <bergamigiacomo@gmail.com>
2024-12-06 18:49:04 +0000

    Some of this code did not compile at all on G++

--- util/xxhash.h
+++ util/xxhash.h
@@ -2066,6 +2066,19 @@ static int XXH_isLittleEndian(void)
 #  define XXH_HAS_BUILTIN(x) 0
 #endif
 
+namespace std{
+[[noreturn]] inline void unreachable()
+{
+    // Uses compiler specific extensions if possible.
+    // Even if no extension is used, undefined behavior is still raised by
+    // an empty function body and the noreturn attribute.
+#if defined(_MSC_VER) && !defined(__clang__) // MSVC
+    __assume(false);
+#else // GCC, Clang
+    __builtin_unreachable();
+#endif
+}
+}
 
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ > 201710L)
 /* C23 and future versions have standard "unreachable()" */
