2ce180a449374fa417af67c890238e43c4a9643c
Alan Paxton <alan.paxton@gmail.com>
2025-01-13 16:24:36 +0000

    Share large temporary library files used by Java
    The rocksdbjni jar unpacks its library file to temporay space if it is not available on the library load path. In pathological cases, large numbers of these files build up;
    In particular when processes are killed, rather than being allowed to deleteOnExit the libraries.
    
    Our solution is a mechanism to let a single instance of the file be shared by all the processes that want to use it. It’s not perfect, but the worst case should only result in a small fixed number of instances of the file being left in temporary space.

---
+++ java/src/test/java/org/rocksdb/LeakedSharedObjectTest.java
@@ -0,0 +1,144 @@
+package org.rocksdb;
+
+import org.junit.Test;
+import org.rocksdb.util.SharedTempFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LeakedSharedObjectTest {
+
+    final static Random random = new Random();
+
+    static InputStream mockContent() throws IOException {
+
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("shared-temp-file.txt");
+        return new BufferedInputStream(is);
+    }
+
+    static BufferedReader mockContentReader() {
+
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("shared-temp-file.txt");
+        return new BufferedReader(new InputStreamReader(is));
+    }
+
+    static void compare(BufferedReader expected, BufferedReader actual) throws IOException {
+        int lines = 0;
+        String expectedLine = expected.readLine();
+        String actualLine = actual.readLine();
+        while (expectedLine != null) {
+            lines++;
+            assertThat(actualLine).isEqualTo(expectedLine);
+            expectedLine = expected.readLine();
+            actualLine = actual.readLine();
+        }
+        assertThat(actualLine).isNull();
+    }
+
+    @Test
+    public void sharedTempFresh() throws IOException {
+        String prefix = "rocksdbjnitest__" + random.nextLong() + "__";
+        SharedTempFile.Instance instance = new SharedTempFile.Instance(prefix, "jnilib");
+        assertThat(instance.search()).isEmpty();
+        SharedTempFile sharedTemp = instance.create();
+        System.err.println(sharedTemp);
+        Path content;
+        try (SharedTempFile.Lock ignored = sharedTemp.lock(LeakedSharedObjectTest::mockContent)) {
+            content = sharedTemp.getContent();
+            assertThat(Files.exists(content)).isTrue();
+            try (BufferedReader shared = new BufferedReader(new InputStreamReader(Files.newInputStream(content))); BufferedReader resource = mockContentReader()) {
+                compare(resource, shared);
+            }
+        }
+        assertThat(Files.exists(content)).isFalse();
+    }
+
+    @Test
+    public void sharedTempExists() throws IOException {
+
+        String prefix = "rocksdbjnitest__" + random.nextLong() + "__";
+        SharedTempFile.Instance instance = new SharedTempFile.Instance(prefix, "jnilib");
+        assertThat(instance.search()).isEmpty();
+        SharedTempFile sharedTemp = instance.create();
+        System.err.println("Created: " + sharedTemp);
+        List<SharedTempFile> existing = instance.search();
+        assertThat(existing).isNotEmpty();
+        sharedTemp = existing.get(0);
+        Path content;
+        try (SharedTempFile.Lock ignored = sharedTemp.lock(LeakedSharedObjectTest::mockContent)) {
+            content = sharedTemp.getContent();
+            assertThat(Files.exists(content)).isTrue();
+            try (BufferedReader shared = new BufferedReader(new InputStreamReader(Files.newInputStream(content))); BufferedReader resource = mockContentReader()) {
+                compare(resource, shared);
+            }
+        }
+        assertThat(Files.exists(content)).isFalse();
+    }
+
+
+    @Test
+    public void openJar() {
+        RocksDB.loadLibrary();
+    }
+
+    @Test
+    public void openManySharedTemp() throws IOException {
+        openMany("org.rocksdb.SharedTempFileMockMain");
+    }
+
+    @Test
+    public void openManyRocksDB() throws IOException {
+        openMany("org.rocksdb.SharedTempFileRocksDBMain");
+    }
+
+    public void openMany(final String mainClass) throws IOException {
+
+        final int DB_COUNT = 50;
+        List<Process> processes = new ArrayList<>();
+        List<BufferedReader> readers = new ArrayList<>();
+
+        for (int i = 0; i < DB_COUNT; i++) {
+
+            Process process = Runtime.getRuntime()
+                .exec("java -cp target/classes:target/test-classes:target/* " + mainClass);
+            processes.add(process);
+            BufferedReader err = new BufferedReader( new InputStreamReader(process.getErrorStream()));
+            readers.add(err);
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        int i = 0;
+        for (BufferedReader reader : readers) {
+            i++;
+            String line = reader.readLine();
+            System.err.println(i + ":" + line);
+        }
+
+        for (Process process : processes) {
+            process.destroyForcibly();
+        }
+
+        for (Process process : processes) {
+            try {
+                process.waitFor();
+                System.err.println("Exit value " + process.exitValue());
+            } catch (InterruptedException ie) {
+                throw new RuntimeException("Process interrupted");
+            }
+        }
+    }
+
+}
