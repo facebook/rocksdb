2cb0c9ac44fe8ef3a8ff0b6ba31d188351b8c54f
Alan Paxton <alan.paxton@gmail.com>
2025-01-15 10:57:34 +0000

    Better clean up files after test
    Put our temporaries inside JUnit temporary when we can
    Rationalise passing of parameters to test processes (add ArgUtil)

--- java/src/test/java/org/rocksdb/SharedTempFileRocksDBMain.java
+++ java/src/test/java/org/rocksdb/SharedTempFileRocksDBMain.java
@@ -1,33 +1,41 @@
 package org.rocksdb;
 
+import org.rocksdb.util.ArgUtil;
+
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Duration;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Map;
 
 public class SharedTempFileRocksDBMain {
     public static void main(final String[] args) throws InterruptedException {
 
         boolean waitKill = false;
-        for (String arg : args) {
-            if ("--waitkill".equals(arg)) waitKill = true;
+        Map<String, String> argMap = ArgUtil.parseArgs(args);
+        if (argMap.containsKey("waitkill")) {
+            waitKill = Boolean.parseBoolean(argMap.get("waitkill"));
+        }
+
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        if (argMap.containsKey("tmpdir")) {
+            tmpDir = argMap.get("tmpdir");
         }
 
         try {
             RocksDB.loadLibrary();
 
-            Path tmpDir = Files.createTempDirectory("tmpRocksDBTestDir");
-            try (final RocksDB db = RocksDB.open(tmpDir.toFile().getAbsolutePath())) {
+            Path dbDir = Files.createTempDirectory(Paths.get(tmpDir), "rocksdbdir");
+            try (final RocksDB db = RocksDB.open(dbDir.toFile().getAbsolutePath())) {
                 db.put("key".getBytes(), "value".getBytes());
                 if (!Arrays.equals("value".getBytes(), db.get("key".getBytes()))) {
                     System.err.println("Get: key != value");
                     System.exit(2);
                 }
             } finally {
-                purgeDirectory(tmpDir.toFile());
-                Files.delete(tmpDir);
+                purgeDirectory(dbDir.toFile());
+                Files.delete(dbDir);
             }
         } catch (Exception e) {
             System.err.println("Exit with exception: " + e);
---
+++ java/src/test/java/org/rocksdb/util/ArgUtil.java
@@ -0,0 +1,21 @@
+package org.rocksdb.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgUtil {
+
+    public static Map<String, String> parseArgs(String[] args) {
+        final Map<String, String> map = new HashMap<>();
+        for (String arg : args) {
+            String[] split = arg.trim().split("=");
+            if (split.length == 2 && split[0].startsWith("-")) {
+                String key = split[0].replaceFirst("-[-]","").toLowerCase();
+                if (!key.isEmpty()) {
+                    map.put(key, split[1]);
+                }
+            }
+        }
+        return map;
+    }
+}
