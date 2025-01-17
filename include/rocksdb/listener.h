b98c21b2812643c689ece6749f8c4b1caac144e1
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2025-01-17 10:04:10 -0800

    Support flush reasons above 12 in Java integration (#13246)
    Summary:
    FlushReason enum in C++ has members up to 15, but in Java, the mirroring FlushReason only supports reason codes up to 12. This causes exceptions when adding a flush listener.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/13246
    
    Reviewed By: pdillinger
    
    Differential Revision: D68241620
    
    Pulled By: jaykorean
    
    fbshipit-source-id: 1e2856dad28dff0cbb1772f5a8ea03cc1e224088

--- include/rocksdb/listener.h
+++ include/rocksdb/listener.h
@@ -183,6 +183,8 @@ enum class FlushReason : int {
   kWalFull = 0xd,
   // SwitchMemtable will not be called for this flush reason.
   kCatchUpAfterErrorRecovery = 0xe,
+
+  // When adding flush reason, make sure to also add it to FlushReason in Java.
 };
 
 const char* GetFlushReasonString(FlushReason flush_reason);
