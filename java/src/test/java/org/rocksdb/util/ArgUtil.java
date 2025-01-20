40c2b2bc25192b31fa8a60c3fdf7ff520f28346e
Alan Paxton <alan.paxton@gmail.com>
2025-01-20 11:25:55 +0000

    fix format

--- java/src/test/java/org/rocksdb/util/ArgUtil.java
+++ java/src/test/java/org/rocksdb/util/ArgUtil.java
@@ -4,18 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ArgUtil {
-
-    public static Map<String, String> parseArgs(String[] args) {
-        final Map<String, String> map = new HashMap<>();
-        for (String arg : args) {
-            String[] split = arg.trim().split("=");
-            if (split.length == 2 && split[0].startsWith("-")) {
-                String key = split[0].replaceFirst("-[-]","").toLowerCase();
-                if (!key.isEmpty()) {
-                    map.put(key, split[1]);
-                }
-            }
+  public static Map<String, String> parseArgs(String[] args) {
+    final Map<String, String> map = new HashMap<>();
+    for (String arg : args) {
+      String[] split = arg.trim().split("=");
+      if (split.length == 2 && split[0].startsWith("-")) {
+        String key = split[0].replaceFirst("-[-]", "").toLowerCase();
+        if (!key.isEmpty()) {
+          map.put(key, split[1]);
         }
-        return map;
+      }
     }
+    return map;
+  }
 }
