14c949df8ba25bb8518b4283f9dd7e002bdf8e4a
anand76 <anand1976@users.noreply.github.com>
2025-03-05 16:30:46 -0800

    Initial implementation of ExternalTableBuilder (#13434)
    Summary:
    This PR adds the ability to use an ExternalTableBuilder through the SstFileWriter to create external tables. This is a counterpart to https://github.com/facebook/rocksdb/issues/13401 , which adds the ExternalTableReader. The support for external tables is confined to ingestion only DBs, with external table files ingested into the bottommost level only. https://github.com/facebook/rocksdb/issues/13431 enforces ingestion only DBs by adding a disallow_memtable_writes column family option.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/13434
    
    Test Plan: New unit tests in table_test.cc
    
    Reviewed By: pdillinger
    
    Differential Revision: D70532054
    
    Pulled By: anand1976
    
    fbshipit-source-id: a837487eadfabed9627a0eceb403bfc5fc2c427c

--- table/external_table.cc
+++ table/external_table.cc
@@ -3,7 +3,7 @@
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
 
-#include "rocksdb/external_table_reader.h"
+#include "rocksdb/external_table.h"
 
 #include "rocksdb/table.h"
 #include "table/internal_iterator.h"
@@ -16,7 +16,8 @@ namespace {
 
 class ExternalTableIterator : public InternalIterator {
  public:
-  explicit ExternalTableIterator(Iterator* iterator) : iterator_(iterator) {}
+  explicit ExternalTableIterator(Iterator* iterator)
+      : iterator_(iterator), valid_(false) {}
 
   // No copying allowed
   ExternalTableIterator(const ExternalTableIterator&) = delete;
@@ -24,7 +25,7 @@ class ExternalTableIterator : public InternalIterator {
 
   ~ExternalTableIterator() override {}
 
-  bool Valid() const override { return iterator_ && iterator_->Valid(); }
+  bool Valid() const override { return valid_; }
 
   void SeekToFirst() override {
     status_ = Status::OK();
@@ -94,23 +95,29 @@ class ExternalTableIterator : public InternalIterator {
     return Slice();
   }
 
-  Status status() const override {
-    return !status_.ok() ? status_
-                         : (iterator_ ? iterator_->status() : Status::OK());
-  }
+  Status status() const override { return status_; }
 
  private:
   std::unique_ptr<Iterator> iterator_;
   InternalKey key_;
+  bool valid_;
   Status status_;
 
-  void UpdateKey() { key_.Set(iterator_->key(), 0, ValueType::kTypeValue); }
+  void UpdateKey() {
+    if (iterator_) {
+      valid_ = iterator_->Valid();
+      status_ = iterator_->status();
+      if (valid_ && status_.ok()) {
+        key_.Set(iterator_->key(), 0, ValueType::kTypeValue);
+      }
+    }
+  }
 };
 
 class ExternalTableReaderAdapter : public TableReader {
  public:
   explicit ExternalTableReaderAdapter(
-      std::unique_ptr<ExternalTableReader> reader)
+      std::unique_ptr<ExternalTableReader>&& reader)
       : reader_(std::move(reader)) {}
 
   ~ExternalTableReaderAdapter() override {}
@@ -170,6 +177,62 @@ class ExternalTableReaderAdapter : public TableReader {
   std::unique_ptr<ExternalTableReader> reader_;
 };
 
+class ExternalTableBuilderAdapter : public TableBuilder {
+ public:
+  explicit ExternalTableBuilderAdapter(
+      std::unique_ptr<ExternalTableBuilder>&& builder)
+      : builder_(std::move(builder)), num_entries_(0) {}
+
+  void Add(const Slice& key, const Slice& value) override {
+    ParsedInternalKey pkey;
+    status_ = ParseInternalKey(key, &pkey, /*log_err_key=*/false);
+    if (status_.ok()) {
+      if (pkey.type != ValueType::kTypeValue) {
+        status_ = Status::NotSupported(
+            "Value type " + std::to_string(pkey.type) + "not supported");
+      } else {
+        builder_->Add(pkey.user_key, value);
+        num_entries_++;
+      }
+    }
+  }
+
+  Status status() const override {
+    if (status_.ok()) {
+      return builder_->status();
+    } else {
+      return status_;
+    }
+  }
+
+  IOStatus io_status() const override { return status_to_io_status(status()); }
+
+  Status Finish() override { return builder_->Finish(); }
+
+  void Abandon() override { builder_->Abandon(); }
+
+  uint64_t FileSize() const override { return builder_->FileSize(); }
+
+  uint64_t NumEntries() const override { return num_entries_; }
+
+  TableProperties GetTableProperties() const override {
+    return builder_->GetTableProperties();
+  }
+
+  std::string GetFileChecksum() const override {
+    return builder_->GetFileChecksum();
+  }
+
+  const char* GetFileChecksumFuncName() const override {
+    return builder_->GetFileChecksumFuncName();
+  }
+
+ private:
+  Status status_;
+  std::unique_ptr<ExternalTableBuilder> builder_;
+  uint64_t num_entries_;
+};
+
 class ExternalTableFactoryAdapter : public TableFactory {
  public:
   explicit ExternalTableFactoryAdapter(
@@ -197,8 +260,18 @@ class ExternalTableFactoryAdapter : public TableFactory {
     return Status::OK();
   }
 
-  TableBuilder* NewTableBuilder(const TableBuilderOptions&,
-                                WritableFileWriter*) const override {
+  using TableFactory::NewTableBuilder;
+  TableBuilder* NewTableBuilder(const TableBuilderOptions& topts,
+                                WritableFileWriter* file) const override {
+    std::unique_ptr<ExternalTableBuilder> builder;
+    ExternalTableBuilderOptions ext_topts(
+        topts.read_options, topts.write_options,
+        topts.moptions.prefix_extractor, topts.ioptions.user_comparator,
+        topts.column_family_name, topts.reason);
+    builder.reset(inner_->NewTableBuilder(ext_topts, file->file_name()));
+    if (builder) {
+      return new ExternalTableBuilderAdapter(std::move(builder));
+    }
     return nullptr;
   }
 
