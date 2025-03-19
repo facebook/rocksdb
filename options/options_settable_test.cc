fb64822807826fe630b67095025e2b2c0938b5ba
Maciej Szeszko <mszeszko@meta.com>
2025-03-18 17:52:00 -0700

    Add missing references

--- options/options_settable_test.cc
+++ options/options_settable_test.cc
@@ -471,7 +471,8 @@ TEST_F(OptionsSettableTest, DBOptionsAllFieldsSettable) {
                              "background_close_inactive_wals=true;"
                              "write_dbid_to_manifest=true;"
                              "write_identity_file=true;"
-                             "prefix_seek_opt_in_only=true;",
+                             "prefix_seek_opt_in_only=true;"
+                             "calculate_sst_write_lifetime_hint_bitmap=1;",
                              new_options));
 
   ASSERT_EQ(unset_bytes_base, NumUnsetBytes(new_options_ptr, sizeof(DBOptions),
