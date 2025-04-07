72571d09ad61e6ef6721499d629082944d9fda62
Hui Xiao <huixiao@fb.com>
2025-04-07 12:50:56 -0700

    Clean up in repair, file ingestion and cf import (#13524)
    Summary:
    **Context/Summary:**
    Rebased on https://github.com/facebook/rocksdb/pull/13522/files, this is to use the refactored function to calculate tail size from table property "tail_start_offset"
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/13524
    
    Test Plan: CI
    
    Reviewed By: anand1976
    
    Differential Revision: D72576262
    
    Pulled By: hx235
    
    fbshipit-source-id: 78c126bc64024c2341d183d6871e06d55fd27501

--- db/import_column_family_job.cc
+++ db/import_column_family_job.cc
@@ -201,15 +201,8 @@ Status ImportColumnFamilyJob::Run() {
       const auto& f = files_to_import_[i][j];
       const auto& file_metadata = *metadatas_[i][j];
 
-      uint64_t tail_size = 0;
-      bool contain_no_data_blocks = f.table_properties.num_entries > 0 &&
-                                    (f.table_properties.num_entries ==
-                                     f.table_properties.num_range_deletions);
-      if (f.table_properties.tail_start_offset > 0 || contain_no_data_blocks) {
-        uint64_t file_size = f.fd.GetFileSize();
-        assert(f.table_properties.tail_start_offset <= file_size);
-        tail_size = file_size - f.table_properties.tail_start_offset;
-      }
+      uint64_t tail_size = FileMetaData::CalculateTailSize(f.fd.GetFileSize(),
+                                                           f.table_properties);
 
       VersionEdit dummy_version_edit;
       dummy_version_edit.AddFile(
