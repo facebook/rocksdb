233add25cf048991ff7f64f5eef730ea38a27efa
rockeet <rockeet@gmail.com>
2025-04-29 23:18:07 +0800

    MemTableList::Add: `MarkImmutable()` before `current_->Add()`

--- db/memtable_list.cc
+++ db/memtable_list.cc
@@ -672,8 +672,8 @@ void MemTableList::Add(ReadOnlyMemTable* m,
   // and when moving to the immutable list we don't unref it,
   // we don't have to ref the memtable here. we just take over the
   // reference from the DBImpl.
-  current_->Add(m, to_delete);
   m->MarkImmutable();
+  current_->Add(m, to_delete);
   num_flush_not_started_++;
   if (num_flush_not_started_ == 1) {
     imm_flush_needed.store(true, std::memory_order_release);
