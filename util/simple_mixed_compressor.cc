adb750fdf4078d1088d157a94993fecf6a85bb19
Sujit Maharjan <sujitmaharjan@meta.com>
2025-06-06 09:32:12 -0700

    Separating into cc and header file for simple_mixed_compressor.h (#13665)
    Summary:
    **Summary**
    This pull request fixes the issue of having a single file simple_mixed_compressor.h containing both implementation and declaration. To improve code organization and follow best practices, I have separated the implementation into a new file simple_mixed_compressor.cc and updated the original file to only contain the necessary declarations.
    
    **Testing**
    Testing was performed by verifying the stdout output from both RoundRobinCompressor and BuiltInCompressorV2.
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/13665
    
    Reviewed By: pdillinger
    
    Differential Revision: D76060831
    
    Pulled By: shubhajeet
    
    fbshipit-source-id: c034868be51ea7b89c1a8dd12082b0159f49f588

---
+++ util/simple_mixed_compressor.cc
@@ -0,0 +1,110 @@
+//  Copyright (c) Meta Platforms, Inc. and affiliates.
+//  This source code is licensed under both the GPLv2 (found in the
+//  COPYING file in the root directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in the root directory).
+//
+// Creates mixed compressor wrapper which uses multiple compression algorithm
+// within same SST file.
+
+#include "simple_mixed_compressor.h"
+
+#include <options/options_helper.h>
+
+#include "random.h"
+#include "rocksdb/advanced_compression.h"
+namespace ROCKSDB_NAMESPACE {
+
+// MultiCompressorWrapper implementation
+MultiCompressorWrapper::MultiCompressorWrapper(const CompressionOptions& opts,
+                                               CompressionType type,
+                                               CompressionDict&& dict) {
+  assert(type != kNoCompression);
+  assert(type == kZSTD);
+  auto builtInManager = GetDefaultBuiltinCompressionManager();
+  const auto& compressions = GetSupportedCompressions();
+  for (auto type_ : compressions) {
+    if (type_ == kNoCompression) {
+      continue;
+    }
+    compressors_.push_back(builtInManager->GetCompressor(opts, type_));
+  }
+  (void)dict;
+  (void)type;
+}
+
+size_t MultiCompressorWrapper::GetMaxSampleSizeIfWantDict(
+    CacheEntryRole block_type) const {
+  return compressors_.back()->GetMaxSampleSizeIfWantDict(block_type);
+}
+
+Slice MultiCompressorWrapper::GetSerializedDict() const {
+  return compressors_.back()->GetSerializedDict();
+}
+
+CompressionType MultiCompressorWrapper::GetPreferredCompressionType() const {
+  return kZSTD;
+}
+
+Compressor::ManagedWorkingArea MultiCompressorWrapper::ObtainWorkingArea() {
+  return compressors_.back()->ObtainWorkingArea();
+}
+
+std::unique_ptr<Compressor> MultiCompressorWrapper::MaybeCloneSpecialized(
+    CacheEntryRole block_type, DictSampleArgs&& dict_samples) {
+  return compressors_.back()->MaybeCloneSpecialized(block_type,
+                                                    std::move(dict_samples));
+}
+
+// SimpleMixedCompressor implementation
+Status SimpleMixedCompressor::CompressBlock(
+    Slice uncompressed_data, std::string* compressed_output,
+    CompressionType* out_compression_type, ManagedWorkingArea* wa) {
+  auto selected =
+      Random::GetTLSInstance()->Uniform(static_cast<int>(compressors_.size()));
+  auto& compressor = compressors_[selected];
+  return compressor->CompressBlock(uncompressed_data, compressed_output,
+                                   out_compression_type, wa);
+}
+
+// SimpleMixedCompressionManager implementation
+const char* SimpleMixedCompressionManager::Name() const {
+  return wrapped_->Name();
+  // return "SimpleMixedCompressionManager";
+}
+
+std::unique_ptr<Compressor> SimpleMixedCompressionManager::GetCompressorForSST(
+    const FilterBuildingContext& context, const CompressionOptions& opts,
+    CompressionType preferred) {
+  assert(preferred == kZSTD);
+  (void)context;
+  return std::make_unique<SimpleMixedCompressor>(opts, preferred);
+}
+
+// RoundRobinCompressor implementation
+Status RoundRobinCompressor::CompressBlock(
+    Slice uncompressed_data, std::string* compressed_output,
+    CompressionType* out_compression_type, ManagedWorkingArea* wa) {
+  auto counter = block_counter.FetchAddRelaxed(1);
+  auto sel_idx = counter % (compressors_.size());
+  auto& compressor = compressors_[sel_idx];
+  return compressor->CompressBlock(uncompressed_data, compressed_output,
+                                   out_compression_type, wa);
+}
+
+RelaxedAtomic<uint64_t> RoundRobinCompressor::block_counter{0};
+
+// RoundRobinManager implementation
+const char* RoundRobinManager::Name() const {
+  // return "RoundRobinManager";
+  return wrapped_->Name();
+}
+
+std::unique_ptr<Compressor> RoundRobinManager::GetCompressorForSST(
+    const FilterBuildingContext& context, const CompressionOptions& opts,
+    CompressionType preferred) {
+  assert(preferred == kZSTD);
+  (void)context;
+  return std::make_unique<RoundRobinCompressor>(opts, preferred);
+}
+
+}  // namespace ROCKSDB_NAMESPACE
