4e2d27fd6957c7bca395adaa0e2dbd67e833e125
Sujit Maharjan <sujitmaharjan@meta.com>
2025-07-25 13:16:20 -0700

    Adding stress and crash test support

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -351,6 +351,7 @@ default_params = {
     "universal_reduce_file_locking": lambda: random.randint(0, 1),
     "compression_manager": lambda: random.choice(
         ["mixed"] * 1
+        + ["autotunecompressor"] * 1
         + ["none"] * 2
         + ["autoskip"] * 2
         + ["randommixed"] * 2
@@ -1030,6 +1031,19 @@ def finalize_and_sanitize(src_params):
                 ["snappy", "zlib", "lz4", "lz4hc", "xpress", "zstd"]
             )
         dest_params["block_align"] = 0
+    elif dest_params.get("compression_manager") == "autotunecompressor":
+        # ensuring the compression is being used
+        if dest_params.get("compression_type") == "none":
+            dest_params["compression_type"] = random.choice(
+                ["snappy", "lz4", "lz4hc", "xpress", "zstd"]
+            )
+        if dest_params.get("bottommost_compression_type") == "none":
+            dest_params["bottommost_compression_type"] = random.choice(
+                ["snappy", "lz4", "lz4hc", "xpress", "zstd"]
+            )
+        dest_params["block_align"] = 0
+        # make sure rate_limiter is enabled
+        dest_params["rate_limiter_bytes_per_sec"] = 1000000000
     else:
         # Enabling block_align with compression is not supported
         if dest_params.get("block_align") == 1:
