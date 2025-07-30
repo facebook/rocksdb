87af5f61ad8ef3db8f4288952eccce6ccb88aacf
Ryan Hancock <krhancock@meta.com>
2025-07-30 14:57:36 -0700

    All Tests Run

--- table/table_test.cc
+++ table/table_test.cc
@@ -7227,7 +7227,7 @@ TEST_F(ExternalTableTest, DBMultiScanTest) {
 
   // Test the overlapping scan case
   key_ranges[1] = "k30";
-  scan_options[0] = ScanOptions(key_ranges[0], key_ranges[1]);
+  scan_options.GetScanOptions()[0] = ScanOptions(key_ranges[0], key_ranges[1]);
   iter = db->NewMultiScan(ro, cfh, scan_options);
   try {
     int idx = 0;
@@ -7253,8 +7253,9 @@ TEST_F(ExternalTableTest, DBMultiScanTest) {
   iter.reset();
 
   // Test the no limit scan case
-  scan_options[0] = ScanOptions(key_ranges[0]);
-  scan_options[1] = ScanOptions(key_ranges[2]);
+  auto& opts = scan_options.GetScanOptions();
+  opts[0] = ScanOptions(key_ranges[0]);
+  opts[1] = ScanOptions(key_ranges[2]);
   iter = db->NewMultiScan(ro, cfh, scan_options);
   try {
     int idx = 0;
