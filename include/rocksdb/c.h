e98c2016958f8f5772adb91ee1b806ab3c7b6ece
zaidoon <zaidoon@cloudflare.com>
2025-08-01 12:55:07 -0400

    allow setting sst file manager via c api

--- include/rocksdb/c.h
+++ include/rocksdb/c.h
@@ -1154,6 +1154,8 @@ rocksdb_block_based_options_set_unpartitioned_pinning_tier(
     rocksdb_block_based_table_options_t*, int);
 extern ROCKSDB_LIBRARY_API void rocksdb_options_set_write_buffer_manager(
     rocksdb_options_t* opt, rocksdb_write_buffer_manager_t* wbm);
+extern ROCKSDB_LIBRARY_API void rocksdb_options_set_sst_file_manager(
+    rocksdb_options_t* opt, rocksdb_sst_file_manager_t* sfm);
 
 /* Flush job info */
 
