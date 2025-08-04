8946df8e02e0566369be1a3a4aa5d423749684c6
Peter Dillinger <peterd@meta.com>
2025-08-04 10:27:55 -0700

    Fix unity build

--- table/block_based/flush_block_policy_impl.h
+++ table/block_based/flush_block_policy_impl.h
@@ -3,6 +3,7 @@
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
 
+#pragma once
 #include "rocksdb/flush_block_policy.h"
 
 namespace ROCKSDB_NAMESPACE {
