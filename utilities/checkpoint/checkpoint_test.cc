3f078f6498f89bf80e0ca43b3c80a0140371b94b
Viraj Thakur <virajthakur@fb.com>
2025-10-20 19:47:27 -0700

    reserve vector space early - resolve linter issue

--- utilities/checkpoint/checkpoint_test.cc
+++ utilities/checkpoint/checkpoint_test.cc
@@ -143,6 +143,7 @@ class CheckpointTest : public testing::Test {
     Close();
     EXPECT_EQ(cfs.size(), options.size());
     std::vector<ColumnFamilyDescriptor> column_families;
+    column_families.reserve(cfs.size());
     for (size_t i = 0; i < cfs.size(); ++i) {
       column_families.emplace_back(cfs[i], options[i]);
     }
@@ -516,6 +517,7 @@ TEST_F(CheckpointTest, CheckpointCF) {
   std::vector<std::string> cfs;
   cfs = {kDefaultColumnFamilyName, "one", "two", "three", "four", "five"};
   std::vector<ColumnFamilyDescriptor> column_families;
+  column_families.reserve(cfs.size());
   for (size_t i = 0; i < cfs.size(); ++i) {
     column_families.emplace_back(cfs[i], options);
   }
@@ -574,6 +576,7 @@ TEST_F(CheckpointTest, CheckpointCFNoFlush) {
   std::vector<std::string> cfs;
   cfs = {kDefaultColumnFamilyName, "one", "two", "three", "four", "five"};
   std::vector<ColumnFamilyDescriptor> column_families;
+  column_families.reserve(cfs.size());
   for (size_t i = 0; i < cfs.size(); ++i) {
     column_families.emplace_back(cfs[i], options);
   }
