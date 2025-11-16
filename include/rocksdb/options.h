4266d9068f285f4a576a7755f56588e4becd2df2
Viraj Thakur <virajthakur@fb.com>
2025-11-16 15:12:21 -0800

    update comment with backwards compatibility update

--- include/rocksdb/options.h
+++ include/rocksdb/options.h
@@ -415,6 +415,10 @@ struct ColumnFamilyOptions : public AdvancedColumnFamilyOptions {
   // open. This is an immutable option - once a CF is declared transient, it
   // cannot be changed (and vice versa). Transient CFs are currently included in
   // the manifest/WAL, but are excluded from backups/snapshots.
+  // If a rocksdb instance is created with a transient CF, it can be reopened by
+  // older versions of RocksDB, as long as ignore_unknown_options is set to true
+  // during db open. This will ignore the is_transient field in the options
+  // file, and allow the user to reopen all transient CFs as regular CFs.
   bool is_transient = false;
 
   void Dump(Logger* log) const;
