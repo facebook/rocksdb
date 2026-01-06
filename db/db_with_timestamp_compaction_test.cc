aef1c9a76eaf7132df3c2052e0944e8fac6398cc
Xingbo Wang <xbw@meta.com>
2026-01-05 16:59:22 -0800

    wait until all bg compaction work done

--- db/db_with_timestamp_compaction_test.cc
+++ db/db_with_timestamp_compaction_test.cc
@@ -541,10 +541,8 @@ TEST_F(TimestampCompatibleCompactionTest, UdtTombstoneCollapsingTest) {
   ASSERT_TRUE(
       db_->GetIntProperty(cfh, DB::Properties::kTotalSstFilesSize, &cf_size));
 
-  // use TEST_WaitForCompact to wait for compaction to run for 5 seconds
-  WaitForCompactOptions wait_for_compact_options;
-  wait_for_compact_options.timeout = std::chrono::seconds(5);
-  ASSERT_OK(dbfull()->TEST_WaitForCompact(wait_for_compact_options));
+  // use TEST_WaitForCompact to wait for compaction to complete
+  ASSERT_OK(dbfull()->TEST_WaitForCompact());
 
   // As the compaction scheduling is not deterministic, use a loose bound check
   // to reduce the flakiness of the test.
