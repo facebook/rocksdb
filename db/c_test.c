ed8f2a67c92beaf53bc482c4ac41cdb0d2fced5a
CJP10 <cjpet10@gmail.com>
2026-01-25 23:52:50 -0500

    Revert "testing"
    This reverts commit 31e905a43019dcc5a257d4c45a7c51c62bb597e2.

--- db/c_test.c
+++ db/c_test.c
@@ -83,9 +83,8 @@ static void CheckEqual(const char* expected, const char* v, size_t n) {
     // ok
     return;
   } else {
-    fprintf(stderr, "%s: expected '%s' (len=%zu), got '%s' (len=%zu)\n", phase,
-            (expected ? expected : "(null)"), expected ? strlen(expected) : 0,
-            (v ? v : "(null)"), n);
+    fprintf(stderr, "%s: expected '%s', got '%s'\n", phase,
+            (expected ? expected : "(null)"), (v ? v : "(null)"));
     abort();
   }
 }
