453ebd6f3756880ccc44b9074c6311eb57fe19e8
Andrew Chang <andrewrchang@meta.com>
2026-03-26 09:24:04 -0700

    Fix MultiScan not respecting SupportedOps for async IO (#14510)
    Summary:
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/14510
    
    When a filesystem does not return kAsyncIO in SupportedOps(), MultiScan
    still sends ReadAsync/Poll/AbortIO requests. Regular iterators check via
    CheckFSFeatureSupport in ArenaWrappedDBIter::Init and ForwardIterator,
    but MultiScan blindly trusted the caller-provided
    MultiScanArgs::use_async_io flag.
    
    Fix: In the MultiScan constructor, after scan_opts_ is initialized,
    check CheckFSFeatureSupport and disable use_async_io if the FS doesn't
    support it. Also pass the (potentially modified) scan_opts_ to Prepare()
    instead of the original scan_opts parameter.
    
    Reviewed By: mszeszko-meta
    
    Differential Revision: D97995735
    
    fbshipit-source-id: 331639d950fd3cb9f491feed996d5820294beb4e

--- db/multi_scan.cc
+++ db/multi_scan.cc
@@ -3,7 +3,9 @@
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
 
+#include "file/file_util.h"
 #include "rocksdb/db.h"
+#include "rocksdb/file_system.h"
 
 namespace ROCKSDB_NAMESPACE {
 
@@ -12,30 +14,43 @@ using MultiScanIterator = MultiScan::MultiScanIterator;
 MultiScan::MultiScan(const ReadOptions& read_options,
                      const MultiScanArgs& scan_opts, DB* db,
                      ColumnFamilyHandle* cfh)
-    : read_options_(read_options), scan_opts_(scan_opts), db_(db), cfh_(cfh) {
+    : read_options_(read_options),
+      scan_opts_([&] {
+        // Disable async IO if the filesystem does not support it, consistent
+        // with how ArenaWrappedDBIter::Init() handles ReadOptions::async_io.
+        auto opts = scan_opts;
+        if (opts.use_async_io &&
+            !CheckFSFeatureSupport(db->GetFileSystem(),
+                                   FSSupportedOps::kAsyncIO)) {
+          opts.use_async_io = false;
+        }
+        return opts;
+      }()),
+      db_(db),
+      cfh_(cfh) {
   bool slow_path = false;
   // Setup read_options with iterate_uuper_bound based on the first scan.
   // Subsequent scans will update and allocate a new DB iterator as necessary
-  if (scan_opts.GetScanRanges()[0].range.limit) {
-    upper_bound_ = *scan_opts.GetScanRanges()[0].range.limit;
+  if (scan_opts_.GetScanRanges()[0].range.limit) {
+    upper_bound_ = *scan_opts_.GetScanRanges()[0].range.limit;
     read_options_.iterate_upper_bound = &upper_bound_;
   } else {
     read_options_.iterate_upper_bound = nullptr;
   }
-  for (const auto& opts : scan_opts.GetScanRanges()) {
+  for (const auto& opts : scan_opts_.GetScanRanges()) {
     // Check that all the ScanOptions either specify an upper bound or not. If
     // its mixed we take the slow path which avoids calling Prepare: we have to
     // reallocate the Iterator with updated read_options everytime we switch
     // between upper bound or no upper bound, which complicates Prepare.
     if (opts.range.limit.has_value() !=
-        scan_opts.GetScanRanges()[0].range.limit.has_value()) {
+        scan_opts_.GetScanRanges()[0].range.limit.has_value()) {
       slow_path = true;
       break;
     }
   }
   db_iter_.reset(db->NewIterator(read_options_, cfh));
   if (!slow_path) {
-    db_iter_->Prepare(scan_opts);
+    db_iter_->Prepare(scan_opts_);
   }
 }
 
