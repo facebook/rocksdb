ce8daf3c2bf3a02ad7a1d0f525dc857c2ec7e765
Xingbo Wang <xbw@meta.com>
2026-04-01 11:05:20 -0700

    add db_bench support

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -1098,6 +1098,22 @@ DEFINE_bool(
     ROCKSDB_NAMESPACE::AdvancedColumnFamilyOptions().enable_blob_files,
     "[Integrated BlobDB] Enable writing large values to separate blob files.");
 
+DEFINE_bool(enable_blob_direct_write,
+            ROCKSDB_NAMESPACE::AdvancedColumnFamilyOptions()
+                .enable_blob_direct_write,
+            "[Integrated BlobDB] Enable direct-write blob file creation on "
+            "the write path.");
+
+DEFINE_uint64(blob_direct_write_partitions,
+              ROCKSDB_NAMESPACE::AdvancedColumnFamilyOptions()
+                  .blob_direct_write_partitions,
+              "[Integrated BlobDB] Number of partitions for direct-write blob "
+              "files.");
+static const bool FLAGS_blob_direct_write_partitions_dummy
+    __attribute__((__unused__)) =
+        RegisterFlagValidator(&FLAGS_blob_direct_write_partitions,
+                              &ValidateUint32Range);
+
 DEFINE_uint64(min_blob_size,
               ROCKSDB_NAMESPACE::AdvancedColumnFamilyOptions().min_blob_size,
               "[Integrated BlobDB] The size of the smallest value to be stored "
@@ -4996,6 +5012,9 @@ class Benchmark {
 
     // Integrated BlobDB
     options.enable_blob_files = FLAGS_enable_blob_files;
+    options.enable_blob_direct_write = FLAGS_enable_blob_direct_write;
+    options.blob_direct_write_partitions =
+        static_cast<uint32_t>(FLAGS_blob_direct_write_partitions);
     options.min_blob_size = FLAGS_min_blob_size;
     options.blob_file_size = FLAGS_blob_file_size;
     options.blob_compression_type =
