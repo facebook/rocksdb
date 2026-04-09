c9a64d84edcecf1eacd2c098620603a65f1fffbc
Xingbo Wang <xbw@meta.com>
2026-04-09 11:21:19 -0700

    fix license duplication

--- include/rocksdb/c_base.h
+++ include/rocksdb/c_base.h
@@ -8,15 +8,7 @@
 //   python3 tools/c_api_gen/regen_all.py
 // The public header for consumers is include/rocksdb/c.h
 //
-//  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
-//  This source code is licensed under both the GPLv2 (found in the
-//  COPYING file in the root directory) and Apache 2.0 License
-//  (found in the LICENSE.Apache file in the root directory).
-
-/* Copyright (c) 2011 The LevelDB Authors. All rights reserved.
-  Use of this source code is governed by a BSD-style license that can be
-  found in the LICENSE file. See the AUTHORS file for names of contributors.
-
+/*
   Does not support:
   . getters for the option types
   . custom comparators that implement key shortening
--- tools/db_crashtest_test.py
+++ tools/db_crashtest_test.py
@@ -1,9 +1,7 @@
+#!/usr/bin/env python3
 #  Copyright (c) Meta Platforms, Inc. and affiliates.
 #  This source code is licensed under both the GPLv2 (found in the COPYING file in the root directory)
 #  and the Apache 2.0 License (found in the LICENSE.Apache file in the root directory).
-
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 import importlib.util
 import os
