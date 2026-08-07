21b5e0ec8b5ebd2fcf41d7cbcf46cc5d3f48ddd2
Peter (Stig) Edwards <thatsafunnyname@gmail.com>
2026-08-07 17:48:42 +0100

    Fix format

--- include/rocksdb/slice.h
+++ include/rocksdb/slice.h
@@ -100,7 +100,7 @@ class Slice {
   // Decodes the current slice interpreted as an hexadecimal string into result,
   // if successful returns true, if this isn't a valid hex string
   // (e.g not coming from Slice::ToString(true)) DecodeHex returns false.
-  // On failure, *result is cleared.  This slice is expected to have an even 
+  // On failure, *result is cleared.  This slice is expected to have an even
   // number of 0-9A-F characters, also accepts lowercase (a-f).
   bool DecodeHex(std::string* result) const;
 
